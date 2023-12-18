4f925baf90c43af4292ce27a58696cb25f4aafd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:49 +0000

    Add package ioStandard/0.1.2/ioStandard-0.1.2.tar.gz

---
+++ packages/ioStandard/ioStandard-0.1.2.tar.gz/ioStandard-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ioStandard',
+    version='0.1.2',
+    author='Austin Gilchrist-Thomas',
+    packages=['ioStandard'],
+    install_requires=['pygame'],
+)
