9cfce1cc3f4c8d7c6fdfaf637ae299c2897dfe46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:58 +0000

    Add package KangRollGeneCancer/0.0.86/KangRollGeneCancer-0.0.86-py2-none-any.whl

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.86-py2-none-any.whl/KangRollGeneCancer/tests/test_kangroll_000_protect_my_self.py
@@ -0,0 +1,29 @@
+
+
+
+# test_kangroll_000_protect_my_self.py
+# from KangRollGeneCancer.core.kangroll_000_protect_myself import test_function
+
+
+# main.py
+
+# import kangroll_000_protect_myself
+
+
+import KangRollGeneCancer.release.pytransform
+import KangRollGeneCancer.release.pytransform._pytransform.dylib
+import KangRollGeneCancer.release.pytransform.__pycache__ 
+
+
+
+import KangRollGeneCancer.release.kangroll_000_protect_myself
+
+
+
+# from KangRollGeneCancer.release.kangroll_000_protect_myself import test_function
+# def main():
+#     result = test_function()
+#     print(result)
+
+# if __name__ == "__main__":
+#     main()
