369c7d8773411750ec5862eb2dbe9cfb57c280a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:16 +0000

    Add package KangRollGeneCancer/0.0.53/KangRollGeneCancer-0.0.53.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.53.tar.gz/KangRollGeneCancer-0.0.53/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="KangRollGeneCancer",
+    version="0.0.53",
+    author="Xiaowen kang",
+    description="xiaowenseekjob@gmail.com A brief description of Kang Roll package",
+    packages=find_packages(),
+    install_requires=[
+        # 依赖项列表，例如 'numpy>=1.18.5'
+    ],
+)
