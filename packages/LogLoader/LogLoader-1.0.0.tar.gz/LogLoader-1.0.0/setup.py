8ea0c30440a9ff298c86824a943ea139d5148e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 16:02:55 +0000

    Add package LogLoader/1.0.0/LogLoader-1.0.0.tar.gz

---
+++ packages/LogLoader/LogLoader-1.0.0.tar.gz/LogLoader-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LogLoader',
+    version='1.0.0',
+    description='Log Export',
+    author='SINTT',
+    author_email='bbhh66606@gmail.com',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
