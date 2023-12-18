1c3837c4750699642b124d47a3ee7f9aa959004a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:26 +0000

    Add package pepperize.cdk-autoscaling-gitlab-runner/0.2.581/pepperize.cdk_autoscaling_gitlab_runner-0.2.581-py3-none-any.whl

---
+++ packages/pepperize.cdk-autoscaling-gitlab-runner/pepperize.cdk_autoscaling_gitlab_runner-0.2.581-py3-none-any.whl/pepperize_cdk_autoscaling_gitlab_runner/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import pepperize_cdk_private_bucket._jsii
+import pepperize_cdk_security_group._jsii
+import pepperize_cdk_vpc._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-autoscaling-gitlab-runner",
+    "0.2.581",
+    __name__[0:-6],
+    "cdk-autoscaling-gitlab-runner@0.2.581.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
