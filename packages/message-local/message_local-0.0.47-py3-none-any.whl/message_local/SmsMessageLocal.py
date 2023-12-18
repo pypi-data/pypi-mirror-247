a62d19beb91a55d6a2c120529029224ca97b83cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:18 +0000

    Add package message-local/0.0.47/message_local-0.0.47-py3-none-any.whl

---
+++ packages/message-local/message_local-0.0.47-py3-none-any.whl/message_local/SmsMessageLocal.py
@@ -0,0 +1,12 @@
+from .MessageLocal import MessageLocal
+from item_local.item import Item
+
+
+class SmsMessageLocal(Item, MessageLocal):
+    pass  # TODO implement
+
+    def check_message(self):
+        # TODO Check that there is only body without subject
+        # TODO Check that there is version of plain text without HTML
+        # TODO Check the length of the self.__body_after_text_template is in the right length
+        pass
