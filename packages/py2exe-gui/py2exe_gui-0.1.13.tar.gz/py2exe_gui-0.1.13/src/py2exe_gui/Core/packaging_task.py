26a37574957a92e7c08c689e2f4ad1ffb10b07d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:40 +0000

    Add package py2exe-gui/0.1.13/py2exe_gui-0.1.13.tar.gz

---
+++ packages/py2exe-gui/py2exe_gui-0.1.13.tar.gz/py2exe_gui-0.1.13/src/py2exe_gui/Core/packaging_task.py
@@ -0,0 +1,72 @@
+# Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
+# For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
+
+from pathlib import Path
+from typing import Optional
+
+from PySide6 import QtCore
+
+from ..Constants import PyinstallerArgs
+from .validators import FilePathValidator
+
+
+class PackagingTask(QtCore.QObject):
+    """
+    打包任务类，处理用户输入
+    接收来自界面的用户输入操作，处理，将结果反馈给界面和实际执行打包子进程的 Packaging 对象 \n
+    """
+
+    # 自定义信号
+    option_set = QtCore.Signal(tuple)  # 用户输入选项通过了验证，已设置为打包选项
+    option_error = QtCore.Signal(str)  # 用户输入选项有误，需要进一步处理
+    ready_to_pack = QtCore.Signal(bool)  # 是否已经可以运行该打包任务
+
+    def __init__(self, parent: Optional[QtCore.QObject] = None) -> None:
+        """
+        :param parent: 父控件对象
+        """
+
+        super().__init__(parent)
+
+        self.script_path: Optional[Path] = None
+        self.icon_path: Optional[Path] = None
+        self.out_name: Optional[str] = None
+        self.FD: Optional[bool] = None
+        self.console: Optional[str] = None
+        self.clean: Optional[bool] = None
+
+    def handle_option(self, option: tuple[PyinstallerArgs, str]):
+        """
+        处理用户在界面选择的打包选项，进行有效性验证并保存 \n
+        :param option: 选项
+        """
+
+        arg_key, arg_value = option
+
+        # 进行有效性验证，有效则保存并发射option_set信号，无效则发射option_error信号
+        if arg_key == PyinstallerArgs.script_path:
+            script_path = Path(arg_value)
+            if FilePathValidator.validate_script(script_path):
+                self.script_path = script_path
+                self.ready_to_pack.emit(True)
+                self.option_set.emit(option)
+                self.out_name = script_path.stem  # 输出名默认与脚本名相同
+                self.option_set.emit((PyinstallerArgs.out_name, self.out_name))
+            else:
+                self.ready_to_pack.emit(False)
+                self.option_error.emit(arg_key)
+
+        elif arg_key == PyinstallerArgs.icon_path:
+            icon_path = Path(arg_value)
+            if FilePathValidator.validate_icon(icon_path):
+                self.icon_path = icon_path
+                self.option_set.emit(option)
+            else:
+                self.option_error.emit(arg_key)
+
+        elif arg_key == PyinstallerArgs.out_name:
+            self.out_name = arg_value
+            self.option_set.emit(option)
+
+        else:
+            self.option_set.emit(option)
---
+++ packages/py2exe-gui/py2exe_gui-0.1.13.tar.gz/py2exe_gui-0.1.13/src/py2exe_gui/Widgets/pyinstaller_option_widget.py
@@ -0,0 +1,69 @@
+# Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
+# For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
+
+import warnings
+
+import yaml
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QHeaderView, QTableWidget, QTableWidgetItem
+
+from ..Constants import RUNTIME_INFO
+from ..Utilities import QtFileOpen
+
+
+def get_options() -> list[dict]:
+    """
+    从数据文件中读取并解析 PyInstaller 命令选项信息，按运行时平台筛选后返回 \n
+    :return: 选项信息字典列表
+    """
+
+    with QtFileOpen(":/Texts/PyInstaller_Options", encoding="utf-8") as option_file:
+        option_file_text = option_file.read()
+
+    if option_file_text == "":
+        warnings.warn("PyInstaller_Options 加载失败，检查资源文件", Warning, stacklevel=1)
+        return []
+
+    data = yaml.load(option_file_text, Loader=yaml.Loader)
+
+    def filter_option(option) -> bool:
+        # 根据当前运行的平台，筛选有效的 PyInstaller 选项
+        return (
+            option["platform"] == ["all"]
+            or RUNTIME_INFO.platform.value in option["platform"]
+        )
+
+    current_options = [option for option in data["options"] if filter_option(option)]
+
+    return current_options
+
+
+class PyinstallerOptionTable(QTableWidget):
+    """
+    用于显示 PyInstaller 命令行选项的表格控件
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("PyInstaller 命令选项")
+        self.setMinimumSize(650, 430)
+        self.setWindowIcon(QPixmap(":/Icons/PyInstaller"))
+
+        self.option_list = get_options()
+
+        self.setRowCount(len(self.option_list))
+        self.setColumnCount(2)
+        self.set_items()
+        self.setHorizontalHeaderLabels(["选项", "描述"])
+        # 将第二列的宽度设置为自动调整
+        self.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
+
+    def set_items(self) -> None:
+        """
+        为表格控件中填充条目
+        """
+
+        for index, option in enumerate(self.option_list):
+            self.setItem(index, 0, QTableWidgetItem(option["option"]))
+            self.setItem(index, 1, QTableWidgetItem(option["description"]))
