c4e87d4861fc1ec567b3b3d13413b85c69e10a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:46 +0000

    Add package automation-fw-setup/0.1.5.8/automation_fw_setup-0.1.5.8-py3-none-any.whl

---
+++ packages/automation-fw-setup/automation_fw_setup-0.1.5.8-py3-none-any.whl/automation_fw_setup/check_installations.py
@@ -0,0 +1,39 @@
+import subprocess
+import sys
+import inquirer
+from colorama import Fore, Style
+
+def install_robot_framework():
+    print("Installing Robot Framework...")
+    subprocess.run(["venv/bin/python", "-m", "pip", "install", "robotframework"], check=True)
+
+def check_installation(program_name, check_command, install_function=None):
+    check_command = check_command.split()  # split the command into a list
+    result = subprocess.run(check_command, capture_output=True, text=True)
+    output = result.stdout.strip() if result.stdout.strip() else result.stderr.strip()
+    if "not found" in output:
+        print(f"{Fore.RED}✗{Style.RESET_ALL} Could not detect {program_name} version")
+        if install_function:
+            questions = [
+                inquirer.Confirm('install',
+                                 message=f"Do you want to install {program_name}?"),
+            ]
+            answers = inquirer.prompt(questions)
+            if answers['install']:
+                install_function()
+                return True
+        return False
+    else:
+        print(f"{Fore.GREEN}✓{Style.RESET_ALL} {program_name} version detected: {output}")
+        return True
+
+def check_installations():
+    programs = [
+        {"name": "Python", "command": "python --version"},
+        {"name": "pip", "command": "pip --version"},
+        {"name": "Robot Framework", "command": "robot --version", "install": install_robot_framework},
+        {"name": "Node.js", "command": "node --version", "install_url": "https://nodejs.org/en/download/"},
+    ]
+
+    for program in programs:
+        check_installation(program["name"], program["command"], program.get("install"))
