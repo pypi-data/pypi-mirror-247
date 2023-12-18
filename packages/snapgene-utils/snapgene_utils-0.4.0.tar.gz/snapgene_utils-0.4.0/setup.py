f03f975f13e046a46835fc487815d7607afc06a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:26 +0000

    Add package snapgene-utils/0.4.0/snapgene_utils-0.4.0.tar.gz

---
+++ packages/snapgene-utils/snapgene_utils-0.4.0.tar.gz/snapgene_utils-0.4.0/setup.py
@@ -0,0 +1,18 @@
+"""
+setup.py
+install snapgene_utils by pip
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="snapgene_utils",
+    version="0.4.0",
+    author="poeticpete",
+    maintainer="petertao",
+    description="Convert Snapgene *.dna files dict/json/biopython.",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    keywords="DNA sequence design format converter",
+    packages=find_packages(),
+    install_requires=["biopython", "xmltodict", "html2text"],
+)
