f6fecbbf5b7cd7b0179386a85d09459ad1db915e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:26 +0000

    Add package yandexgptlite/0.1.2/yandexgptlite-0.1.2.tar.gz

---
+++ packages/yandexgptlite/yandexgptlite-0.1.2.tar.gz/yandexgptlite-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yandexgptlite',
+    version='0.1.2',
+    packages=find_packages(),
+    description='Получение ответов от YandexGPT Lite',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Александр Морин',
+    author_email='y.director@yandex.ru',
+    url='https://github.com/morinad/yandexgptlite',
+    install_requires=[
+        'requests'
+    ],
+)
