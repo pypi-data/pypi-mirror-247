407cbc4111fdbfa76008ed8db12539b8cdd2091d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:50 +0000

    Add package automation-fw-setup/0.1.5.5/automation_fw_setup-0.1.5.5-py3-none-any.whl

---
+++ packages/automation-fw-setup/automation_fw_setup-0.1.5.5-py3-none-any.whl/automation_fw_setup/create_virtual_env.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+from colorama import Fore, Style
+
+def create_virtual_env(project_name):
+    venv_dir = os.path.join(project_name, 'venv')
+    print("Current working directory:", os.getcwd())
+    print("PATH:", os.environ.get('PATH'))
+    result = subprocess.run(['python', '--version'], capture_output=True, text=True)
+    print("Python version:", result.stdout)
+    result = subprocess.run(f'python3 -m venv {venv_dir}', shell=True, check=True, capture_output=True, text=True)
+    print("Command output:")
+    print(result.stdout)
+    print(result.stderr)
+    print(f"{Fore.GREEN}✓{Style.RESET_ALL} Virtual environment created successfully.")
+    print(f"{Fore.YELLOW}!{Style.RESET_ALL} To activate the virtual environment, navigate to the project directory and use the following command:")
+    print(f"On Linux/macOS: source {os.path.join(venv_dir, 'bin', 'activate')}")
+    print(f"On Windows: .\\{os.path.join(venv_dir, 'Scripts', 'activate')}")
