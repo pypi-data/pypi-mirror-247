5c092be1aba102e8b8caccac12b10680a1f893e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:26 +0000

    Add package cdk-opinionated-constructs/3.2.2/cdk-opinionated-constructs-3.2.2.tar.gz

---
+++ packages/cdk-opinionated-constructs/cdk-opinionated-constructs-3.2.2.tar.gz/cdk-opinionated-constructs-3.2.2/setup.py
@@ -0,0 +1,24 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cdk-opinionated-constructs",
+    version="3.2.2",
+    description="AWS CDK constructs come without added security configurations.",
+    long_description="The idea behind this project is to create secured constructs from the start. \n"
+    "Supported constructs: ALB, ECR, LMB, NLB, S3, SNS, WAF, RDS",
+    license="MIT",
+    package_dir={"": "."},
+    packages=find_packages(where="."),
+    install_requires=[
+        "aws-cdk-lib>=2.98.0",
+        "constructs>=10.2.69,<11.0.0",
+        "cdk-monitoring-constructs>=6.0.0,<8.0.0",
+    ],
+    python_requires=">=3.11",
+)
