1c49c0b978b78dbd9a5b2dfc28df16ade517ead7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:04:14 +0000

    Add package searchagent/0.0.1/searchagent-0.0.1-py3-none-any.whl

---
+++ packages/searchagent/searchagent-0.0.1-py3-none-any.whl/searchagent/__init__.py
@@ -0,0 +1,7 @@
+# This is the canonical package information.
+__author__ = "Chris Tava"
+__license__ = "Apache"
+__copyright__ = "Copyright (c) 2023-2024, Chris Tava"
+__version__ = "0.0.1"
+
+from .prompts import Prompts
