d308d25bea5b33f02c6595d613ce2cc3fbdd90f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:49 +0000

    Add package depyf/0.13.0/depyf-0.13.0.tar.gz

---
+++ packages/depyf/depyf-0.13.0.tar.gz/depyf-0.13.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='depyf',
+    version=open("depyf/VERSION.txt").read().strip(),
+    description='Decompile python functions, from bytecode to source code!',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/thuml/depyf',
+    author='Kaichao You',
+    author_email="youkaichao@gmail.com",
+    license="MIT",
+    include_package_data=True,  # This line is important!
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "astor",
+        # "filelock", # filelock is required by torch. If you use torch, you should have filelock.
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "flake8",
+            "autopep8",
+        ]
+    }
+)
