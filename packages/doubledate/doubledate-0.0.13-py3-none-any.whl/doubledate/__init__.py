02fb2f3aeaaed7f7f06dff0b643465870ae7a1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:49 +0000

    Add package doubledate/0.0.13/doubledate-0.0.13-py3-none-any.whl

---
+++ packages/doubledate/doubledate-0.0.13-py3-none-any.whl/doubledate/__init__.py
@@ -0,0 +1,41 @@
+__version__ = "0.0.13"
+
+from datetime import MAXYEAR, MINYEAR
+from datetime import date, datetime, time, timedelta, timezone, tzinfo
+
+from .diem import diem
+from .constants import Y, H, T, Q, M, W, MON, TUE, WED, THU, FRI, SAT, SUN, WEEKDAYS
+from .calendar import Calendar, BD
+from .utils import (
+    quarter,
+    trimester,
+    semester,
+    eow,
+    sow,
+    eom,
+    som,
+    now,
+    eoq,
+    soq,
+    eot,
+    sot,
+    eos,
+    sos,
+    eoy,
+    soy,
+    isleap,
+    parse,
+    offset,
+    floor,
+    ceil,
+    dayof,
+    daysfrom,
+    daysto,
+    weekdayof,
+    today,
+    tomorrow,
+    yesterday,
+    last,
+    next,
+    datemap,
+)
