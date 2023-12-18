a580e87184a9d7dc916a08c82c4a1298c2603e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:33 +0000

    Add package cdk-cloudformation-fastly-services-activeversion/1.8.0a7/cdk-cloudformation-fastly-services-activeversion-1.8.0a7.tar.gz

---
+++ packages/cdk-cloudformation-fastly-services-activeversion/cdk-cloudformation-fastly-services-activeversion-1.8.0a7.tar.gz/cdk-cloudformation-fastly-services-activeversion-1.8.0a7/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cloudformation-fastly-services-activeversion",
+    "version": "1.8.0.a7",
+    "description": "Manage a Fastly service active version.",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws-ia/cloudformation-fastly-resource-providers",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cdklabs/cdk-cloudformation.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cloudformation_fastly_services_activeversion",
+        "cdk_cloudformation_fastly_services_activeversion._jsii"
+    ],
+    "package_data": {
+        "cdk_cloudformation_fastly_services_activeversion._jsii": [
+            "fastly-services-activeversion@1.8.0-alpha.7.jsii.tgz"
+        ],
+        "cdk_cloudformation_fastly_services_activeversion": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "aws-cdk-lib>=2.115.0, <3.0.0",
+        "constructs>=10.3.0, <11.0.0",
+        "jsii>=1.93.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
