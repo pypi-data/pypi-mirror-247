11172c3dc22ae3c1798e2b0ccde0b8e2ab04f7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:38 +0000

    Add package ruclogin/0.2.8/ruclogin-0.2.8.tar.gz

---
+++ packages/ruclogin/ruclogin-0.2.8.tar.gz/ruclogin-0.2.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ruclogin",
+    version="0.2.8",
+    packages=find_packages(),
+    description="Login to *.ruc.edu.cn, get cookies.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="panjd123",
+    author_email="xm.jarden@gmail.con",
+    license="MIT",
+    url="https://github.com/panjd123/ruclogin",
+    install_requires=[
+        "requests",
+        "selenium",
+        "selenium-wire",
+        "webdriver_manager",
+        "ddddocr",
+        "docopt",
+    ],
+    package_data={"ruclogin": ["config.ini"]},
+    entry_points={
+        "console_scripts": [
+            "ruclogin=ruclogin.ruclogin:main",
+        ]
+    },
+)
