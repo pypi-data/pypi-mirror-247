534e60bf566e2bbdbf13da34e8fc55a09534a2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:36 +0000

    Add package KangRollGeneCancer/0.0.85/KangRollGeneCancer-0.0.85.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.85.tar.gz/KangRollGeneCancer-0.0.85/KangRollGeneCancer/core/temp/use_simple_adder.py
@@ -0,0 +1,10 @@
+# use_simple_adder.py
+
+from simple_adder import add_numbers
+
+def main():
+    result = add_numbers(1, 2)
+    print("The sum is:", result)
+
+if __name__ == "__main__":
+    main()
