c12666608a9a0940cc9efc96dd0b5268697b3c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:04:13 +0000

    Add package KangRollGeneCancer/0.0.40/KangRollGeneCancer-0.0.40.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.40.tar.gz/KangRollGeneCancer-0.0.40/KangRollGeneCancer/__init__.py
@@ -0,0 +1,20 @@
+# KangRollGeneCancer/__init__.py
+# from .core import multi_layer_network
+# # 可以继续导入其他需要的模块
+
+
+# KangRollGeneCancer/__init__.py
+
+# 导入核心模块
+from .core import update_network, IS_KANGROLL_PRODUCTION, KANGROLL_PRODUCTION_KEY
+
+# 导入模型模块
+# from .models import analyze_gene
+
+# 导入实用工具模块
+# from .utils import some_utility_function
+
+# 此处可以添加更多的导入语句，以方便包的使用者访问各个子模块
+
+
+# 理解了。实际上就是增加了调取的灵活性。 从一层包装下调取。可以，从两层包装下调取也可，总之，总能调取到。方便我调取。哪怕不知道他父亲或爷爷的名字？
