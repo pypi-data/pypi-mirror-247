50a01bfb973e63a0a81689c64b09247309a9e91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:08 +0000

    Add package nostr-dvm/0.0.6/nostr-dvm-0.0.6.tar.gz

---
+++ packages/nostr-dvm/nostr-dvm-0.0.6.tar.gz/nostr-dvm-0.0.6/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6'
+DESCRIPTION = 'A framework to build and run Nostr NIP90 Data Vending Machines'
+LONG_DESCRIPTION = ('A framework to build and run Nostr NIP90 Data Vending Machines. '
+                    'This is an early stage release. Interfaces might change/brick')
+
+# Setting up
+setup(
+    name="nostr-dvm",
+    version=VERSION,
+    author="Believethehype",
+    author_email="believethehypeonnostr@proton.me",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(include=['nostr_dvm', 'nostr_dvm.backends', 'nostr_dvm.interfaces', 'nostr_dvm.tasks',
+                                    'nostr_dvm.utils', 'nostr_dvm.utils.scrapper']),
+    install_requires=["nostr-sdk==0.0.5",
+                      "bech32==1.2.0",
+                      "pycryptodome==3.19.0",
+                      "python-dotenv==1.0.0",
+                      "emoji==2.8.0",
+                      "eva-decord==0.6.1",
+                      "ffmpegio==0.8.5",
+                      "lnurl==0.4.1",
+                      "pandas==2.1.3",
+                      "Pillow==10.1.0",
+                      "PyUpload==0.1.4",
+                      "requests==2.31.0",
+                      "instaloader==4.10.1",
+                      "pytube==15.0.0",
+                      "moviepy==2.0.0.dev2",
+                      "zipp==3.17.0"
+                      ],
+    keywords=['nostr', 'nip90', 'dvm', 'data vending machine'],
+    url="https://github.com/believethehype/nostrdvm",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+    ]
+)
