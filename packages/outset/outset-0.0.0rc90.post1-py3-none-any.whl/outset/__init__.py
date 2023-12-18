ace0389c3b9f67a9d5de0d2556d9a46edc26c96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:20 +0000

    Add package outset/0.0.0rc90.post1/outset-0.0.0rc90.post1-py3-none-any.whl

---
+++ packages/outset/outset-0.0.0rc90.post1-py3-none-any.whl/outset/__init__.py
@@ -0,0 +1,37 @@
+"""Outset Python Package"""
+from __future__ import annotations
+
+__version__ = "0.0.0""-rc90-post1"
+
+from .draw_marquee_ import draw_marquee
+from .inset_outsets_ import inset_outsets
+from .layout_corner_insets_ import layout_corner_insets
+from .MarkArrow_ import mark_arrow, MarkArrow
+from .MarkInlaidAsterisk_ import mark_inlaid_asterisk, MarkInlaidAsterisk
+from .MarkMagnifyingGlass_ import mark_magnifying_glass, MarkMagnifyingGlass
+from .MarkAlphabeticalBadges_ import MarkAlphabeticalBadges
+from .MarkNumericalBadges_ import MarkNumericalBadges
+from .MarkRomanBadges_ import MarkRomanBadges
+from .OutsetGrid_ import OutsetGrid
+from .marqueeplot_ import marqueeplot
+from .resize_clip_outliers_ import resize_clip_outliers
+from .stub_all_clipped_values_ import stub_all_clipped_values
+from .stub_edge_mark_ import stub_edge_mark
+
+__all__ = [
+    "draw_marquee",
+    "inset_outsets",
+    "layout_corner_insets",
+    "mark_arrow",
+    "mark_inlaid_asterisk",
+    "mark_magnifying_glass",
+    "MarkAlphabeticalBadges",
+    "MarkInlaidAsterisk",
+    "MarkNumericalBadges",
+    "MarkRomanBadges",
+    "marqueeplot",
+    "OutsetGrid",
+    "resize_clip_outliers",
+    "stub_all_clipped_values",
+    "stub_edge_mark",
+]
