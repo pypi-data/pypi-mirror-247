231072ff8d27b25092abd6b872087f97ffb92f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:03 +0000

    Add package matrix-utils/0.4.1/matrix_utils-0.4.1-py3-none-any.whl

---
+++ packages/matrix-utils/matrix_utils-0.4.1-py3-none-any.whl/matrix_utils/utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from .errors import AllArraysEmpty
+
+
+def filter_groups_for_packages(
+    packages, matrix_label, use_vectors, use_arrays, use_distributions
+) -> dict:
+    return {
+        package: [
+            (group_label, filtered_package)
+            for group_label, filtered_package in package.groups.items()
+            if has_relevant_data(
+                group_label, package, use_vectors, use_arrays, use_distributions
+            )
+        ]
+        for package in [
+            obj.filter_by_attribute("matrix", matrix_label) for obj in packages
+        ]
+    }
+
+
+def has_relevant_data(
+    group_label, package, use_vectors, use_arrays, use_distributions
+) -> bool:
+    return any(
+        res
+        for res in package.resources
+        if res["group"] == group_label
+        and (
+            (res["kind"] == "data" and res["category"] == "vector" and use_vectors)
+            or (
+                res["kind"] == "distributions"
+                and res["category"] == "vector"
+                and use_distributions
+            )
+            # Use vectors under Monte Carlo as fallback. Warning: Could be changed in future!
+            or (
+                res["kind"] == "data"
+                and res["category"] == "vector"
+                and use_distributions
+            )
+            or (res["kind"] == "data" and res["category"] == "array" and use_arrays)
+        )
+    )
+
+
+def safe_concatenate_indices(
+    arrays: [np.ndarray], empty_ok: bool = False
+) -> np.ndarray:
+    try:
+        return np.hstack(arrays)
+    except ValueError:
+        if empty_ok:
+            return np.array([], dtype=int)
+        else:
+            raise AllArraysEmpty
