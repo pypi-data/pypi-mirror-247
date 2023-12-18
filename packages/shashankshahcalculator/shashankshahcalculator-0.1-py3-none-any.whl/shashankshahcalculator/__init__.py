1a272fd5cf118cdb49af193ee8f45c5a4221e4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:08 +0000

    Add package shashankshahcalculator/0.1/shashankshahcalculator-0.1-py3-none-any.whl

---
+++ packages/shashankshahcalculator/shashankshahcalculator-0.1-py3-none-any.whl/shashankshahcalculator/__init__.py
@@ -0,0 +1,54 @@
+def add(a, b):
+    """
+    Adds two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
+
+
+def subtract(a, b):
+    """
+    Subtracts two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The difference of a and b.
+    """
+    return a - b
+
+
+def multiply(a, b):
+    """
+    Multiplies two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The product of a and b.
+    """
+    return a * b
+
+
+def divide(a, b):
+    """
+    Divides two numbers.
+
+    Args:
+        a: The first number (dividend).
+        b: The second number (divisor).
+
+    Returns:
+        The quotient of a and b.
+    """
+    return a / b
