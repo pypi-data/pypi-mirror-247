7dac3e0075cc01455cb868c63900f3e06071f2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:56 +0000

    Add package spready/1.6/spready-1.6-py3-none-any.whl

---
+++ packages/spready/spready-1.6-py3-none-any.whl/spready/cli.py
@@ -0,0 +1,37 @@
+import typer
+from spready import app, EnvURLS
+import json
+import os
+import logging
+
+cliApp = typer.Typer()
+
+
+@cliApp.command()
+def register(credential_path: str):
+    result = app.register(EnvURLS.PROD)
+    with open(credential_path, "w") as f:
+        json.dump(result, f)
+    print(f"""
+          
+        ┌───────────────────────────────────────────────┐
+        │  Congratulations!                             │
+        │                                               │
+        │  You've successfully registered your worker   │
+        └───────────────────────────────────────────────┘
+          
+          URL: {EnvURLS.PROD}
+          Use `{result['publicKey']}` in API header `x-auth-token` to authenticate
+
+        """)
+
+
+@cliApp.command()
+def run(creditial_path: str, module_path: str):
+    print(f"Setting path: {module_path}")
+    print(os.getcwd())
+    app.run(creditial_path, modulePath=module_path)
+
+
+if __name__ == "__main__":
+    cliApp()
