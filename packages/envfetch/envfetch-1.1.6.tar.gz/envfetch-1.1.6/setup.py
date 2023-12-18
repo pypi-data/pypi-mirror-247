b09c0627b3cf39a0b41dec3cf8b5de5f30418571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:27 +0000

    Add package envfetch/1.1.6/envfetch-1.1.6.tar.gz

---
+++ packages/envfetch/envfetch-1.1.6.tar.gz/envfetch-1.1.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='envfetch',
+    version='1.1.6',
+    packages=['envfetch'],  # Указываем папку с вашими модулями
+    python_requires='>=3.6',
+    install_requires=[
+        'python-dotenv',
+    ],
+    author='hBroccoli',
+    author_email='vovo.r@yandex.ru',
+    description='EnvFetch - это библиотека на Python для управления переменными окружения в приложениях...',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/HermitBroccoli/EnvUtil',
+    license='MIT', 
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
