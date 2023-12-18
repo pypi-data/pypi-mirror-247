ee8e58580bf29d4751c471d4dc62f18017a14c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:04:13 +0000

    Add package systembridgeconnector/3.10.2.dev0/systembridgeconnector-3.10.2.dev0-py3-none-any.whl

---
+++ packages/systembridgeconnector/systembridgeconnector-3.10.2.dev0-py3-none-any.whl/systembridgeconnector/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides systembridgeconnector version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update systembridgeconnector` to change this file.
+
+from incremental import Version
+
+__version__ = Version("systembridgeconnector", 3, 10, 2, dev=0)
+__all__ = ["__version__"]
