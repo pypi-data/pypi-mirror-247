689335ceb7eed912b800c87a7b2a1fcefa4291c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:26 +0000

    Add package tracked-tests-locust/0.0.265/tracked-tests-locust-0.0.265.tar.gz

---
+++ packages/tracked-tests-locust/tracked-tests-locust-0.0.265.tar.gz/tracked-tests-locust-0.0.265/tracked_tests.py
@@ -0,0 +1,40 @@
+from locust import Locust, HttpUser, task, BaseTask
+
+class TrackedTask(BaseTask):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, func: Callable) -> Callable:
+        super().__call__(func)
+
+        def wrapper(self, *args, **kwargs):
+            # Get task information for headers
+            class_name = self.task.current.parent.name
+            task_name = self.task.current.name
+
+            # Generate unique invocation ID
+            invocation_id = str(uuid.uuid4())
+
+            # Build tracked header values
+            headers = {
+                "trackedtest.suite": class_name,
+                "trackedtest.name": f"{class_name}#{task_name}",
+                "trackedtest.invocation_id": invocation_id,
+                "test.type": "locust",
+            }
+
+            # Access and utilize task weight from Locust internals
+            weight = self.task.weight
+
+            # Use weight information within headers or task logic as needed
+            # ... (your logic based on weight)
+
+            # Iterate through and modify all requests in the task
+            for request in self.client.request_iter():
+                request.headers.update(headers)
+                yield request
+
+            # Execute the original task function
+            func(self, *args, **kwargs)
+
+        return wrapper
