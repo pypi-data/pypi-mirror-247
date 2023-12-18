dd32216c708bb2e538bc2d947951bc2a1317f180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:04:11 +0000

    Add package systembridgemodels/4.0.0.dev0/systembridgemodels-4.0.0.dev0.tar.gz

---
+++ packages/systembridgemodels/systembridgemodels-4.0.0.dev0.tar.gz/systembridgemodels-4.0.0.dev0/setup.py
@@ -0,0 +1,24 @@
+"""Setup."""
+from setuptools import find_packages, setup
+
+# Get setup packages from requirements.txt
+with open("requirements_setup.txt", encoding="utf-8") as f:
+    requirements_setup = f.read().splitlines()
+
+# Get packages from requirements.txt
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="systembridgemodels",
+    description="System Bridge Models",
+    keywords="system-bridge",
+    author="Aidan Timson (Timmo)",
+    author_email="aidan@timmo.dev",
+    license="MIT",
+    url="https://github.com/timmo001/system-bridge-models",
+    packages=find_packages(exclude=["tests", "generator"]),
+    install_requires=requirements,
+    setup_requires=requirements_setup,
+    use_incremental=True,
+)
