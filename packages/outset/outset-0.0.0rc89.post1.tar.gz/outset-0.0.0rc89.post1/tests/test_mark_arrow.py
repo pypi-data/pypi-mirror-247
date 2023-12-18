e0a8bff122b06e38a5752440bd6e389c236304f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:48 +0000

    Add package outset/0.0.0rc89.post1/outset-0.0.0rc89.post1.tar.gz

---
+++ packages/outset/outset-0.0.0rc89.post1.tar.gz/outset-0.0.0rc89.post1/tests/test_mark_arrow.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+from outset import mark_arrow
+
+
+def test_mark_arrow_single():
+    ax = plt.gca()  # Get the current axes
+
+    # Call the function with test values
+    mark_arrow(x=0.5, y=0.5, ax=ax, color="red")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_arrow_single.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+
+
+def test_mark_arrow_multiple():
+    # Multiple calls to the function with different parameters
+    mark_arrow(x=0.3, y=0.3, color="lavender")
+    mark_arrow(x=0.7, y=0.7, color="blue", color_accent="orange")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_arrow_multiple.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
