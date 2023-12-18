2616058cf15a031e43b543008e891a04870acd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:40 +0000

    Add package pytest-choose/0.0.6/pytest-choose-0.0.6.tar.gz

---
+++ packages/pytest-choose/pytest-choose-0.0.6.tar.gz/pytest-choose-0.0.6/pytest_choose/analysis.py
@@ -0,0 +1,51 @@
+"""
+@File: analysis.py
+@Author: Azusa
+@Description: 
+
+"""
+import json
+import pathlib
+from typing import Union
+
+import pytest
+
+from .terminal_io import terminal_write
+
+
+class ChooseFileAnalysis:
+    _support_type = [
+        '.json'
+    ]
+
+    def __init__(self, file_path: str, session: pytest.Session, encoding: str = 'utf-8', is_filter_file: bool = False):
+        self.file_path = pathlib.PurePath(file_path)
+        self.session = session
+        self.is_filter_file = is_filter_file
+
+        self.f_type = self.file_path.suffix
+        self.f_obj = ""
+        try:
+            self.f_obj = open(self.file_path.as_posix(), encoding=encoding)
+        except FileNotFoundError as e:
+            terminal_write(session, self.__msg(e.strerror), red=True)
+        else:
+            terminal_write(session, self.__msg(self.file_path.as_posix()), bold=True)
+
+    def __msg(self, msg: str) -> str:
+        if self.is_filter_file:
+            return f'[Filter list]: {msg}'
+        else:
+            return f'[Cases list]: {msg}'
+
+    def __json_parse(self) -> dict:
+        return json.loads(self.f_obj.read())
+
+    def parse(self) -> Union[bool, dict]:
+        if isinstance(self.f_obj, str):
+            return False
+        if self.f_type not in self._support_type:
+            print('Unsupported file format, please use JSON format file')
+            return False
+        if self.f_type == '.json':
+            return self.__json_parse()
---
+++ packages/pytest-choose/pytest-choose-0.0.6.tar.gz/pytest-choose-0.0.6/pytest_choose/plugin.py
@@ -0,0 +1,67 @@
+"""
+@File: plugin.py
+@Author: Azusa
+@Description:
+
+"""
+import pytest
+from typing import List
+
+from .filter import ItemFilter
+from .analysis import ChooseFileAnalysis
+from .terminal_io import terminal_write
+
+
+def pytest_addoption(parser: pytest.Parser):
+    group = parser.getgroup(
+        'file-choose',
+        'Provide the pytest with the ability to collect use cases based on rules in text files'
+    )
+    group.addoption(
+        "--fc",
+        action="store",
+        default="off",
+        help="Default 'off' for file choose, option: 'on' or 'off'"
+    )
+    group.addoption(
+        "--fc-coding",
+        action="store",
+        default="utf-8",
+        help="File encoding, default 'utf-8'"
+    )
+    group.addoption(
+        "--fc-path",
+        action="store",
+        default="./choose.json",
+        help="File Path, default './choose.json'"
+    )
+    group.addoption(
+        "--fc-filter-path",
+        action="store",
+        default="./filter.json",
+        help="Filter file Path, default './filter.json'"
+    )
+
+
+def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: List["pytest.Item"]):
+    if config.getoption('--fc') == 'on':
+        origin_count = len(items)
+        count = 0
+        path = config.getoption('--fc-path')
+        filter_path = config.getoption('--fc-filter-path')
+        coding = config.getoption('--fc-coding')
+        terminal_write(session, '\n', prefix=False)
+        # terminal_write(session, f'Cases list: {path}', bold=True)
+        # terminal_write(session, f'Filter list: {filter_path}', bold=True)
+        parse = ChooseFileAnalysis(path, session, encoding=coding).parse()
+        filter_parse = ChooseFileAnalysis(filter_path, session, encoding=coding, is_filter_file=True).parse()
+        if parse:
+            for item in items[:]:
+                if ItemFilter(parse, item, filter_parse).filter():
+                    continue
+                del items[items.index(item)]
+                count += 1
+            terminal_write(session, f'Filter {count} cases and collect {origin_count - count} cases', bold=True)
+        else:
+            terminal_write(session, 'Unsupported file format, please use JSON format file', red=True, bold=True)
+            terminal_write(session, 'Not filtered')
