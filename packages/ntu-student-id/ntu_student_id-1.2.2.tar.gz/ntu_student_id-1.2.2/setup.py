0125ff34016f224f18acee7510a7c0b63858e339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:03:42 +0000

    Add package ntu-student-id/1.2.2/ntu_student_id-1.2.2.tar.gz

---
+++ packages/ntu-student-id/ntu_student_id-1.2.2.tar.gz/ntu_student_id-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ntu_student_id",
+    version="1.2.2",
+    author="Max Yi-Hsun Chou",
+    author_email="maxchou415@gmail.com",
+    description="Get department information by NTU student ID",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxchou415/ntu_student_id.py",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "ntu_student_id": ["data/departments.json"],
+    },
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.7",
+)
