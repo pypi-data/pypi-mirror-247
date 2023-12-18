5eafa94c7fcf6d7d9fd1565c25b44ab14af3e1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 14:03:18 +0000

    Add package vidgenerator/0.0.2/vidgenerator-0.0.2.tar.gz

---
+++ packages/vidgenerator/vidgenerator-0.0.2.tar.gz/vidgenerator-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="vidgenerator",
+    version="0.0.2",
+    description="ID generator",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Vikash Singh",
+    author_email="thevikash@onmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
