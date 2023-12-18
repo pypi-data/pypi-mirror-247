a9e48aa46c797be87eaaee1687be0e315f6c8eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 15:04:22 +0000

    Add package llm-http-api/0.0.2/llm_http_api-0.0.2-py3-none-any.whl

---
+++ packages/llm-http-api/llm_http_api-0.0.2-py3-none-any.whl/llm_http_api/server/models/__init__.py
@@ -0,0 +1,5 @@
+import importlib
+
+importlib.import_module("llm_http_api.server.models.list_models")
+importlib.import_module("llm_http_api.server.models.retrieve_model")
+importlib.import_module("llm_http_api.server.models.delete_model")
