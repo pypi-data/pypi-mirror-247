818ac59b685fdee823d3962f11c85cad803cf19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:06 +0000

    Add package kbrainsdk/0.1.54/kbrainsdk-0.1.54-py3-none-any.whl

---
+++ packages/kbrainsdk/kbrainsdk-0.1.54-py3-none-any.whl/kbrainsdk/validation/ingest.py
@@ -0,0 +1,44 @@
+from kbrainsdk.validation.common import get_payload, validate_email
+def validate_ingest_onedrive(req):
+    body = get_payload(req)
+    email = body.get('email')
+    token = body.get('token')
+    environment = body.get('environment')
+    client_id = body.get('client_id')
+    oauth_secret = body.get('oauth_secret')
+    tenant_id = body.get('tenant_id')
+
+    # Validate parameters
+    if not all([email, token, environment, client_id, oauth_secret, tenant_id]):
+        raise ValueError("Missing or empty parameter in request body. Requires: email, token, environment, client_id, oauth_secret, tenant_id")
+    
+    if not validate_email(email):
+        raise ValueError("Invalid email address")
+    
+    return email, token, environment, client_id, oauth_secret, tenant_id
+
+def validate_ingest_sharepoint(req):
+    body = get_payload(req)
+    host = body.get('host')
+    site = body.get('site')
+    token = body.get('token')
+    environment = body.get('environment')
+    client_id = body.get('client_id')
+    oauth_secret = body.get('oauth_secret')
+    tenant_id = body.get('tenant_id')
+
+    # Validate parameters
+    if not all([host, site, token, environment, client_id, oauth_secret, tenant_id]):
+        raise ValueError("Missing or empty parameter in request body. Requires: host, site, token, environment, client_id, oauth_secret, tenant_id")
+    
+    return host, site, token, environment, client_id, oauth_secret, tenant_id
+
+def validate_ingest_status(req):
+    body = get_payload(req)
+    datasource = body.get('datasource')
+
+    # Validate parameters
+    if not all([datasource]):
+        raise ValueError("Missing or empty parameter \"datasource\" in request body")
+    
+    return datasource
