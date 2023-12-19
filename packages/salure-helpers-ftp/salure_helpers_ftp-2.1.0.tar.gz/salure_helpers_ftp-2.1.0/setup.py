7bdee4c85758f65da027076f256c2645e7238943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:01 +0000

    Add package salure-helpers-ftp/2.1.0/salure_helpers_ftp-2.1.0.tar.gz

---
+++ packages/salure-helpers-ftp/salure_helpers_ftp-2.1.0.tar.gz/salure_helpers_ftp-2.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_ftp',
+    version='2.1.0',
+    description='FTP wrapper from Salure',
+    long_description='FTP wrapper from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.ftp"],
+    license='Salure License',
+    install_requires=[
+        'salure-helpers-salureconnect>=1',
+        'requests>=2,<=3',
+        'paramiko>=2,<=4'
+    ],
+    zip_safe=False,
+)
