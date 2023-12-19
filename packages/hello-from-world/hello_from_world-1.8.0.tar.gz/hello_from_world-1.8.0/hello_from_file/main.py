a50282723a697f00e63c93648829c52179d5c33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:07 +0000

    Add package hello-from-world/1.8.0/hello_from_world-1.8.0.tar.gz

---
+++ packages/hello-from-world/hello_from_world-1.8.0.tar.gz/hello_from_world-1.8.0/hello_from_file/main.py
@@ -0,0 +1,22 @@
+# import os
+# from json import load
+
+# def read_hello_file():
+#     # Construct the relative path to hello.txt based on the current working directory
+#     package_directory = os.path.dirname(os.path.abspath(__file__))
+#     file_path = os.path.join(package_directory, '..', 'hello.txt')
+
+#     # Load the content of hello.txt
+#     with open(file_path, 'r') as file:
+#         schema = load(file)
+
+#     return schema
+
+# from json import load
+from pkg_resources import resource_string
+
+def read_hello_file():
+    # Read the content from hello.txt
+    content = resource_string('hello_from_file', 'yo.txt').decode('utf-8')
+
+    return content
