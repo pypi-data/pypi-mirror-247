6a26dcb842e0ff70cabca88cf17333c2a4b15875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:04:03 +0000

    Add package defi-state-querier/0.4.3/defi_state_querier-0.4.3-py3-none-any.whl

---
+++ packages/defi-state-querier/defi_state_querier-0.4.3-py3-none-any.whl/defi_services/constants/query_constant.py
@@ -0,0 +1,22 @@
+class Query:
+    address = "address"
+    abi = "abi"
+    function = "function"
+    params = "params"
+    block_number = "block_number"
+    module = "module"
+
+    # query types
+    token_balance = 'token_balance'
+    nft_balance = 'nft_balance'
+    deposit_borrow = 'deposit_borrow'
+    staking_reward = 'staking_reward'
+    protocol_reward = 'protocol_reward'
+    protocol_apy = 'protocol_apy'
+    health_factor = 'health_factor'
+    deposit_borrow_health_factor = 'deposit_borrow_health_factor'
+    protocol_supply_borrow = 'protocol_supply_borrow'
+    all = [token_balance, nft_balance, deposit_borrow, protocol_reward, protocol_apy, protocol_supply_borrow]
+    balance = [nft_balance, token_balance]
+    # entity_id
+    token = "token"
