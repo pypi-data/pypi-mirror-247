d5bfa3a5196e30fbde68812d74af50478543b9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:05:05 +0000

    Add package numerous.sdk/0.34.0/numerous.sdk-0.34.0-py3-none-any.whl

---
+++ packages/numerous.sdk/numerous.sdk-0.34.0-py3-none-any.whl/numerous/sdk/__init__.py
@@ -0,0 +1,9 @@
+"""numerous.sdk - develop numerous applications!
+
+The recommended way to develop applications for running on the numerous platform.
+"""
+from numerous.sdk.connect.job_client import JobClient
+
+__all__ = [
+    "JobClient",
+]
