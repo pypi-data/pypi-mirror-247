951ea2d40702a74742dddd2026d703930a8a8d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:21 +0000

    Add package compyler-tools/0.2/compyler-tools-0.2.tar.gz

---
+++ packages/compyler-tools/compyler-tools-0.2.tar.gz/compyler-tools-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2'
+DESCRIPTION = 'Tools to make a compiler in python'
+LONG_DESCRIPTION = 'A package that contains a lexer and a parser that creates an AST.'
+
+# Setting up
+setup(
+    name="compyler-tools",
+    version=VERSION,
+    author="ricardoquinteladev",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'compiler', 'lexer', 'parser', 'lalr', 'ast'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
