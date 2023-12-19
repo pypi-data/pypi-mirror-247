dcb749d3a38b1315fae25e9a2a7be561f618638e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:18 +0000

    Add package ugot/0.2.9/ugot-0.2.9.tar.gz

---
+++ packages/ugot/ugot-0.2.9.tar.gz/ugot-0.2.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(
+    name = "ugot",
+    version = "0.2.8",
+    description = "ugot-Python SDK",
+    long_description = "",
+
+    packages = find_packages(include=["ugot","ugot.*"]),
+    # package_data = {"ugot": ["*"],
+    #                 },
+    # packages=find_packages(),
+    # include_package_data = True,
+    platforms = "any",
+    install_requires = [
+        'concurrent-log-handler',
+        'grpcio',
+        'protobuf',
+        'grpcio-tools',
+        'wheel',
+        'zeroconf',
+        'pillow',
+        'requests'
+    ]
+)
