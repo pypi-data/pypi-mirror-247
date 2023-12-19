50fa42b1f7db3c4c0fdb84477d008494bb471aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:03:48 +0000

    Add package tiger-eval/0.0.2/tiger_eval-0.0.2-py3-none-any.whl

---
+++ packages/tiger-eval/tiger_eval-0.0.2-py3-none-any.whl/tiger_eval/__init__.py
@@ -0,0 +1,17 @@
+
+import logging
+
+# =  =  =  =  =  =  =  =  =  =  =  Logging Setup  =  =  =  =  =  =  =  =  =  =  =  =  = 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format  = "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt = "%m/%d/%Y %H:%M:%S",
+    level   = logging.INFO,
+)
+# =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  = 
+
+from .cross_lingual_assessment import *
+from .multichoice_question import *
+from .open_question_model_judge import *
+from .translation_bleu import *
+from .rouge import *
