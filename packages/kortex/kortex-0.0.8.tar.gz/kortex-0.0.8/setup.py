48189ba8f8a566cf3a4119cf5e3b262d5d8e1989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:54 +0000

    Add package kortex/0.0.8/kortex-0.0.8.tar.gz

---
+++ packages/kortex/kortex-0.0.8.tar.gz/kortex-0.0.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='kortex',
+    version='0.0.8',
+    description='Keras Implementations of Goal-driven models of (parts of) cortex',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/weidler/keras-cortex',
+    author='Tonio Weidler',
+    author_email='research@tonioweidler.de',
+    license='Apache-2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tensorflow",
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
---
+++ packages/kortex/kortex-0.0.8.tar.gz/kortex-0.0.8/test/test_cornet.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class CornetTestCase(unittest.TestCase):
+
+    def test_builds(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_calls(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_optimization(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
