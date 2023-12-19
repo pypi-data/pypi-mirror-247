db19f406f119d6a65b5ee3230ebfa6640a2a744f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:03:48 +0000

    Add package irails/1.6.1/irails-1.6.1-py3-none-any.whl

---
+++ packages/irails/irails-1.6.1-py3-none-any.whl/irails/scripts/_run.py
@@ -0,0 +1,38 @@
+import argparse
+import os, sys
+import uvicorn
+from irails.config import IS_IN_irails
+ 
+from irails.core import application
+from irails.config import config
+ 
+def main():
+     
+    if not IS_IN_irails:
+        print(f"`run` command must run in the directory of irails project")
+        exit()
+    self_file = os.path.basename(__file__).lstrip("_").replace(".py",'')
+    parser = argparse.ArgumentParser(usage=f"{sys.argv[0]} {self_file} [-h] [--host] [-p port]", description='run app') 
+    parser.add_argument('-p','--port',type=int, help="http port")    
+    parser.add_argument('--host',type=str, help="host name[default:127.0.0.1]")    
+    parser.add_argument('-d','--debug',action='store_true',  help="enable debug mode")    
+    args = parser.parse_args()
+    
+    kwargs = { }
+    if args.host:
+        kwargs['host'] = args.host
+    if args.port:
+        kwargs['port'] = args.port
+     
+     
+    args.debug = config.get("debug",False)
+    if args.debug: 
+        kwargs['reload'] = args.debug 
+        app_cfg = config.get('app')
+        apps_dirs = app_cfg.get("appdirs") 
+        kwargs['reload_dirs'] = apps_dirs
+        kwargs['reload_includes'] = ['*.po']
+        kwargs['reload_excludes'] = ['*.pyc']
+    
+    uvicorn.run(app="irails.core:generate_mvc_app",**kwargs)      
+       
