104948eb67f2c5f1276b358adb5b80332a62644b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:12 +0000

    Add package dessia-common/0.15.3/dessia_common-0.15.3.tar.gz

---
+++ packages/dessia-common/dessia_common-0.15.3.tar.gz/dessia_common-0.15.3/dessia_common/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" __init__ for dessia_common module. """
+
+import pkg_resources
+
+__version__ = pkg_resources.require("dessia_common")[0].version
+
+FLOAT_TOLERANCE = 1e-9
+REF_MARKER = "$ref"
+OLD_REF_MARKER = "$ref"
