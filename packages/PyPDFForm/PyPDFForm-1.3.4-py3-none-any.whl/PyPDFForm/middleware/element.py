9bff4ef379363c0ae9b8477f68044e3cb8e8371d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:04:05 +0000

    Add package PyPDFForm/1.3.4/PyPDFForm-1.3.4-py3-none-any.whl

---
+++ packages/PyPDFForm/PyPDFForm-1.3.4-py3-none-any.whl/PyPDFForm/middleware/element.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Contains element middleware."""
+
+from typing import Any, Union
+
+
+class Element:
+    """Base class for all PDF form elements."""
+
+    def __init__(
+        self,
+        element_name: str,
+        element_value: Union[str, bool, int] = None,
+    ) -> None:
+        """Constructs basic attributes for the object."""
+
+        self._name = element_name
+        self.value = element_value
+
+    @property
+    def name(self) -> str:
+        """Name of the element."""
+
+        return self._name
+
+    @property
+    def schema_definition(self) -> dict:
+        """Json schema definition of the element."""
+
+        raise NotImplementedError
+
+    @property
+    def sample_value(self) -> Any:
+        """Sample value of the element."""
+
+        raise NotImplementedError
