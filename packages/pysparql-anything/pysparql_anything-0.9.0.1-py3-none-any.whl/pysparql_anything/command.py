078b57803db05fd84c1497f2603944a56944bb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:52 +0000

    Add package pysparql-anything/0.9.0.1/pysparql_anything-0.9.0.1-py3-none-any.whl

---
+++ packages/pysparql-anything/pysparql_anything-0.9.0.1-py3-none-any.whl/pysparql_anything/command.py
@@ -0,0 +1,66 @@
+"""
+This module contains the functions that encapsulate the execution of
+each of the commands of the PySPARQL Anything interface.
+
+@author Marco Ratta
+@ version 19/10/2023
+"""
+
+import json
+from rdflib import Graph
+from pysparql_anything.parameter_handler import transform_parameters
+from pysparql_anything.sparql_anything_reflection import SPARQLAnythingReflection
+
+
+def execute_ask(kwargs: dict, receiver: SPARQLAnythingReflection) -> bool:
+    '''
+    Contains the instructions for the ASK command and executes them.\n
+    Arguments: \n
+    kwargs - a dictionary containing the SPARQL Anything ASK request
+        parameters.\n
+    receiver - a SPARQL Anything reflection object.
+    '''
+    kwargs['f'] = 'xml'
+    args = transform_parameters(kwargs)
+    string = receiver.call_main(args)
+    return bool('<boolean>true</boolean>' in string)
+
+
+def execute_construct(kwargs: dict, receiver: SPARQLAnythingReflection) -> Graph:
+    '''
+    Contains the instructions for the CONSTRUCT command and executes them.\n
+    Arguments: \n
+    kwargs - a dictionary containing the SPARQL Anything CONSTRUCT request
+        parameters.\n
+    receiver - a SPARQL Anything reflection object.
+    '''
+    args = transform_parameters(kwargs)
+    string = receiver.call_main(args)
+    graph = Graph().parse(data=string)
+    return graph
+
+
+def execute_select(kwargs: dict, receiver: SPARQLAnythingReflection) -> dict:
+    '''
+    Contains the instructions for the SELECT command and executes them.\n
+    Arguments: \n
+    kwargs - a dictionary containing the SPARQL Anything SELECT request
+        parameters.\n
+    receiver - a SPARQL Anything reflection object.
+    '''
+    kwargs['f'] = 'json'
+    args = transform_parameters(kwargs)
+    string = receiver.call_main(args)
+    return json.loads(string)
+
+
+def execute_run(kwargs: dict, receiver: SPARQLAnythingReflection) -> None:
+    '''
+    Contains the instructions for the RUN command and executes it.\n
+    Arguments: \n
+    kwargs - a dictionary containing the SPARQL Anything RUN request
+        parameters.\n
+    receiver - a SPARQL Anything reflection object.
+    '''
+    args = transform_parameters(kwargs)
+    receiver.main(args)
---
+++ packages/pysparql-anything/pysparql_anything-0.9.0.1-py3-none-any.whl/pysparql_anything/sparql_anything_reflection.py
@@ -0,0 +1,68 @@
+"""
+This module contains the SPARQLAnythingReflection class, which handles the
+reflection of the SPARQLAnything class into Python and makes its public static
+void main(String[] args) and public static String callMain(String args) methods
+to Python users.
+
+@author Marco Ratta
+@version 18/12/2023
+"""
+
+import jnius_config
+from pysparql_anything.utilities import get_path2jar
+from pysparql_anything.__about__ import __jarMainPath__
+
+
+class SPARQLAnythingReflection:
+    """
+    The class SPARQLAnythingReflection wraps the Java SPARQL Anything Main
+    class.\n
+    It therefore makes available its public static void main(String[] args)
+    and public static String callMain(String args) methods to Python users.\n
+    This it does by reflecting the SPARQLAnything class in SPARQLAnything.java
+    and assigning it to the 'reflection' field of its instance.
+    """
+
+    def __init__(
+            self, jvm_options: tuple[str], jar_main_path: str=__jarMainPath__
+            ) -> None:
+        """
+        Initialiser for the class SPARQLAnythingReflection.\n
+        Arguments:\n
+        *jvm_options - the options to be passed to the JVM before launch.
+        """
+        try:
+            # Sets the JVM classpath to the Sparql Anything installation:
+            if jvm_options != tuple():
+                for option in jvm_options:
+                    jnius_config.add_options(option)
+            jnius_config.set_classpath(get_path2jar())
+            # Starts the JVM and reflects the SPARQLAnything class:
+            from jnius import autoclass
+            self.reflection = autoclass(jar_main_path)
+        except ValueError as err:
+            print('ValueError:', err)
+            raise
+        except Exception as exc:
+            print('Exception:', exc)
+            raise
+
+    def main(self, args: list[str]) -> None:
+        """
+        Wrapper for the public static void main(String[] args) method of
+        SPARQLAnything.\n
+        Arguments:\n
+        args - args is a Python list[str] that mirrors the String[] args that
+            is required by main(String[] args).
+        """
+        self.reflection.main(args)
+
+    def call_main(self, args: list[str]) -> str:
+        """
+        Wrapper for the public static String callMain(String args) method of
+        SPARQLAnything.\n
+        Arguments:\n
+        args - args is a Python list[str] that mirrors the String[] args that
+            is required by callMain(String[] args).
+        """
+        return self.reflection.callMain(args)
