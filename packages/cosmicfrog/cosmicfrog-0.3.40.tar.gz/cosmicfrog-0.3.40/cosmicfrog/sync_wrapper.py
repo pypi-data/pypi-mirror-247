2b8058c525f910d84799187a39ce5f766183a3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:29 +0000

    Add package cosmicfrog/0.3.40/cosmicfrog-0.3.40.tar.gz

---
+++ packages/cosmicfrog/cosmicfrog-0.3.40.tar.gz/cosmicfrog-0.3.40/cosmicfrog/sync_wrapper.py
@@ -0,0 +1,21 @@
+import asyncio
+from functools import wraps
+
+
+def sync_wrapper(async_func):
+    @wraps(async_func)
+    def wrapper(*args, **kwargs):
+        print("DEBUG: Sync version of async")
+
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            # No running event loop, safe to proceed with asyncio.run
+            return asyncio.run(async_func(*args, **kwargs))
+
+        # If called inside an existing event loop, raise an error
+        raise RuntimeError(
+            f"Called {async_func.__name__} synchronously from an async context. Use the async function directly."
+        )
+
+    return wrapper
