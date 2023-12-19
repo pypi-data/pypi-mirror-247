44e950e85a0c14b6ad9ae2e6bbdacbb64dd5010b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:04:29 +0000

    Add package py-worksdone-utils/1.1.5/py-worksdone-utils-1.1.5.tar.gz

---
+++ packages/py-worksdone-utils/py-worksdone-utils-1.1.5.tar.gz/py-worksdone-utils-1.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+    name="py-worksdone-utils",
+    version="1.1.5",
+    author="KE",
+    author_email="",
+    description="Utilities with Keycloak",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["python-keycloak==3.3.0"],
+    keywords=["python", "decorator", "token", "keycloak"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+)
