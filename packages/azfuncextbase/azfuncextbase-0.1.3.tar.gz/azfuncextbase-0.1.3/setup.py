e652601876c81c9a323620a4439a0f6e7e8b3234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:10 +0000

    Add package azfuncextbase/0.1.3/azfuncextbase-0.1.3.tar.gz

---
+++ packages/azfuncextbase/azfuncextbase-0.1.3.tar.gz/azfuncextbase-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import setuptools
+
+setup(
+    name='azfuncextbase',
+    version='0.1.3',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
