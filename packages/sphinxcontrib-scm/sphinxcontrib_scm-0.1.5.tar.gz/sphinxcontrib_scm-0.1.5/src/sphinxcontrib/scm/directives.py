067991eb2ec4afd3f490e19c9c9e4944b4dc708a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:23 +0000

    Add package sphinxcontrib-scm/0.1.5/sphinxcontrib_scm-0.1.5.tar.gz

---
+++ packages/sphinxcontrib-scm/sphinxcontrib_scm-0.1.5.tar.gz/sphinxcontrib_scm-0.1.5/src/sphinxcontrib/scm/directives.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import textwrap
+
+from docutils import nodes
+from docutils.nodes import Node
+from docutils.statemachine import StringList
+from sphinx.util import logging
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.nodes import nested_parse_with_titles
+
+from .util import Helper
+
+logger = logging.getLogger(f"sphinxcontrib-scm.{__name__}")
+
+
+class ContribsDirective(SphinxDirective, Helper):
+    has_content = False
+    optional_arguments = 0
+    option_spec = {
+        "email": str,
+        "limit_authors": int,
+        "min_commits": int,
+        "sort": str,
+        "type": str,
+    }
+
+    def run(self) -> list[Node]:
+        """Directive to list all SCM contributors"""
+        contributors = self.get_contributors()
+        contributors_str = (
+            ",\n   ".join(contributors)
+            if contributors
+            else "<no SCM contributors found>"
+        )
+
+        new_content = textwrap.dedent(
+            """\
+            .. sectionauthor::
+               {contributors}\
+            """
+        ).format(contributors=contributors_str)
+        new_content = StringList(new_content.splitlines(), source="")
+
+        node = nodes.Element()
+        nested_parse_with_titles(self.state, new_content, node)
+
+        return node.children
