e98a111e5e386123552ebd9fdcb98edd40a079cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:09 +0000

    Add package python-redux/0.9.1a2/python_redux-0.9.1a2-py3-none-any.whl

---
+++ packages/python-redux/python_redux-0.9.1a2-py3-none-any.whl/redux/basic_types.py
@@ -0,0 +1,103 @@
+# ruff: noqa: A003, D100, D101, D102, D103, D104, D105, D107
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import (
+    Any,
+    Callable,
+    Generic,
+    Mapping,
+    TypeGuard,
+    TypeVar,
+    dataclass_transform,
+)
+
+_T = TypeVar('_T')
+
+
+@dataclass_transform(kw_only_default=True, frozen_default=True)
+def immutable(cls: type[_T]) -> type[_T]:
+    return dataclass(frozen=True, kw_only=True)(cls)
+
+
+@dataclass_transform(kw_only_default=True, frozen_default=True)
+class Immutable:
+    def __init_subclass__(
+        cls: type[Immutable],
+        **kwargs: Mapping[str, Any],
+    ) -> None:
+        super().__init_subclass__(**kwargs)
+        immutable(cls)
+
+
+class BaseAction(Immutable):
+    ...
+
+
+class BaseEvent(Immutable):
+    ...
+
+
+class EventSubscriptionOptions(Immutable):
+    run_async: bool = True
+
+
+# Type variables
+State = TypeVar('State', bound=Immutable)
+State_co = TypeVar('State_co', bound=Immutable, covariant=True)
+Action = TypeVar('Action', bound=BaseAction)
+SelectorOutput = TypeVar('SelectorOutput')
+SelectorOutput_co = TypeVar('SelectorOutput_co', covariant=True)
+SelectorOutput_contra = TypeVar('SelectorOutput_contra', contravariant=True)
+ComparatorOutput = TypeVar('ComparatorOutput')
+Selector = Callable[[State], SelectorOutput]
+Comparator = Callable[[State], ComparatorOutput]
+Event = TypeVar('Event', bound=BaseEvent)
+EventHandler = Callable[[Event], Any]
+
+
+class CompleteReducerResult(Immutable, Generic[State, Action, Event]):
+    state: State
+    actions: list[Action] | None = None
+    events: list[Event] | None = None
+
+
+ReducerResult = CompleteReducerResult[State, Action, Event] | State
+ReducerType = Callable[[State | None, Action], ReducerResult[State, Action, Event]]
+
+AutorunOriginalReturnType = TypeVar('AutorunOriginalReturnType')
+AutorunOriginalReturnType_co = TypeVar('AutorunOriginalReturnType_co', covariant=True)
+
+EventSubscriber = Callable[
+    [Event, Callable[[Event], Any], EventSubscriptionOptions | None],
+    Callable[[], None],
+]
+
+
+class InitializationActionError(Exception):
+    def __init__(self: InitializationActionError) -> None:
+        super().__init__(
+            'The only accepted action type when state is None is "InitAction"',
+        )
+
+
+class InitAction(BaseAction):
+    ...
+
+
+class FinishAction(BaseAction):
+    ...
+
+
+class FinishEvent(BaseEvent):
+    ...
+
+
+def is_reducer_result(
+    result: ReducerResult[State, Action, Event],
+) -> TypeGuard[CompleteReducerResult[State, Action, Event]]:
+    return isinstance(result, CompleteReducerResult)
+
+
+def is_state(result: ReducerResult[State, Action, Event]) -> TypeGuard[State]:
+    return not isinstance(result, CompleteReducerResult)
