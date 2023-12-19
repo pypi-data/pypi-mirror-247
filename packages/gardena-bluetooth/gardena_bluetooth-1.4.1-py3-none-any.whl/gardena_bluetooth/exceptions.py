baaf3eec65704af9e8fd6f094b17a0b87b48d432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:27 +0000

    Add package gardena-bluetooth/1.4.1/gardena_bluetooth-1.4.1-py3-none-any.whl

---
+++ packages/gardena-bluetooth/gardena_bluetooth-1.4.1-py3-none-any.whl/gardena_bluetooth/exceptions.py
@@ -0,0 +1,13 @@
+class GardenaBluetoothException(Exception):
+    pass
+
+
+class CharacteristicNoAccess(GardenaBluetoothException):
+    pass
+
+
+class CharacteristicNotFound(CharacteristicNoAccess):
+    pass
+
+class CommunicationFailure(GardenaBluetoothException):
+    pass
