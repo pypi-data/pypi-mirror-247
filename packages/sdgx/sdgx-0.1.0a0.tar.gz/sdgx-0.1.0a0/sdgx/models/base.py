80caae8fac34e4408ce260847fe0a5d1bd44ceb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:03:48 +0000

    Add package sdgx/0.1.0a0/sdgx-0.1.0a0.tar.gz

---
+++ packages/sdgx/sdgx-0.1.0a0.tar.gz/sdgx-0.1.0a0/sdgx/models/base.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pandas as pd
+
+from sdgx.data_loader import DataLoader
+from sdgx.data_models.metadata import Metadata
+
+
+class SynthesizerModel:
+    def fit(self, metadata: Metadata, dataloader: DataLoader, *args, **kwargs):
+        raise NotImplementedError
+
+    def sample(self, count: int, *args, **kwargs) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def save(self, path: str | Path):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, path: str | Path) -> "SynthesizerModel":
+        raise NotImplementedError
