89e05de1a816f024898fbc89d269b5f46e6409e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:45 +0000

    Add package graph-nitta/0.0.4/graph_nitta-0.0.4.tar.gz

---
+++ packages/graph-nitta/graph_nitta-0.0.4.tar.gz/graph_nitta-0.0.4/src/graph_nitta/__init__.py
@@ -0,0 +1,6 @@
+from .axes.config import AxConfig as AxConfig
+from .axes.config import SpineConfig as SpineConfig
+from .axes.resolver import apply_ax_config as apply_ax_config
+from .axes.resolver import apply_x_config as apply_x_config
+from .axes.resolver import apply_y_config as apply_y_config
+from .graph import make_graph as make_graph
---
+++ packages/graph-nitta/graph_nitta-0.0.4.tar.gz/graph_nitta-0.0.4/src/graph_nitta/axes/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class SpineConfig(BaseModel):
+    label: str = ""
+    scale: str = "linear"
+    lim: tuple[float, float] | None = None
+    step: float | None = None
+    pow: int = 0
+    visible: bool = True
+    invert: bool = False
+
+
+class AxConfig(BaseModel):
+    x: SpineConfig = SpineConfig()
+    y: SpineConfig = SpineConfig()
+    legends_loc: str = "upper right"
+    bbox_to_anchor: tuple[float, float] | None = (1.0, 1.0)
