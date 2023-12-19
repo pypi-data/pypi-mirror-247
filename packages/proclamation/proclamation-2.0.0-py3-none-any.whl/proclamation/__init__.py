8a093984fc3b8e511a68466e90aa33e160e7aaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:19 +0000

    Add package proclamation/2.0.0/proclamation-2.0.0-py3-none-any.whl

---
+++ packages/proclamation/proclamation-2.0.0-py3-none-any.whl/proclamation/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 -i
+# Copyright 2020-2023, Collabora, Ltd. and the Proclamation contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Original author: Rylie Pavlik <rylie.pavlik@collabora.com>
+"""Generate updates to a changelog file by combining fragments."""
+
+__version__ = "2.0.0"
