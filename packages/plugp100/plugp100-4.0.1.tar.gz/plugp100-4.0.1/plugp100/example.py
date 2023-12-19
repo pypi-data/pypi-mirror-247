f9f7f42769ef6f9473af53c2a22839f116dcbf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:29 +0000

    Add package plugp100/4.0.1/plugp100-4.0.1.tar.gz

---
+++ packages/plugp100/plugp100-4.0.1.tar.gz/plugp100-4.0.1/plugp100/example.py
@@ -0,0 +1,41 @@
+import asyncio
+import os
+
+from plugp100.api.light_effect_preset import LightEffectPreset
+from plugp100.api.tapo_client import TapoClient
+from plugp100.common.credentials import AuthCredential
+from plugp100.discovery.tapo_discovery import TapoDeviceFinder
+
+
+async def main():
+    print("Scanning network...")
+    print(TapoDeviceFinder.classify(TapoDeviceFinder.scan(5)))
+
+    # create generic tapo api
+    username = os.getenv("USERNAME", "<tapo_email>")
+    password = os.getenv("PASSWORD", "<tapo_password>")
+
+    credentials = AuthCredential(username, password)
+    client = TapoClient.create(credentials, "<tapo_device_ip>")
+
+    print(await client.get_device_info())
+    print(await client.get_energy_usage())
+    print(await client.get_current_power())
+    print(await client.get_child_device_list())
+    print(await client.get_child_device_component_list())
+    print(await client.set_lighting_effect(LightEffectPreset.Aurora.to_effect()))
+    # plug = PlugDevice(TapoClient(username, password), "<tapo_device_ip>")
+    # light = LightDevice(TapoClient(username, password), "<tapo_device_ip>")
+    # ledstrip = LedStripDevice(TapoClient(username, password), "<tapo_device_ip>")
+
+    # - hub example
+    # hub = HubDevice(client)
+    # print(await hub.get_children())
+    # print(await hub.get_state_as_json())
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
+    loop.run_until_complete(asyncio.sleep(0.1))
+    loop.close()
