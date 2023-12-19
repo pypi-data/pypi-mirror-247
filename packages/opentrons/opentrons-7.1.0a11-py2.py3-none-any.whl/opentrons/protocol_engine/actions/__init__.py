7db8b431e5a5820ae9a14c7f47a215f3d7514234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:08 +0000

    Add package opentrons/7.1.0a11/opentrons-7.1.0a11-py2.py3-none-any.whl

---
+++ packages/opentrons/opentrons-7.1.0a11-py2.py3-none-any.whl/opentrons/protocol_engine/actions/__init__.py
@@ -0,0 +1,54 @@
+"""ProtocolEngine action interfaces.
+
+Actions are the driver of state changes in the ProtocolEngine.
+"""
+
+from .action_dispatcher import ActionDispatcher
+from .action_handler import ActionHandler
+from .actions import (
+    Action,
+    PlayAction,
+    PauseAction,
+    PauseSource,
+    StopAction,
+    FinishAction,
+    HardwareStoppedAction,
+    QueueCommandAction,
+    UpdateCommandAction,
+    FailCommandAction,
+    AddLabwareOffsetAction,
+    AddLabwareDefinitionAction,
+    AddLiquidAction,
+    AddModuleAction,
+    FinishErrorDetails,
+    DoorChangeAction,
+    ResetTipsAction,
+    SetPipetteMovementSpeedAction,
+)
+
+__all__ = [
+    # action pipeline interface
+    "ActionDispatcher",
+    # action reaction interface
+    "ActionHandler",
+    # action values
+    "Action",
+    "PlayAction",
+    "PauseAction",
+    "StopAction",
+    "FinishAction",
+    "HardwareStoppedAction",
+    "QueueCommandAction",
+    "UpdateCommandAction",
+    "FailCommandAction",
+    "AddLabwareOffsetAction",
+    "AddLabwareDefinitionAction",
+    "AddLiquidAction",
+    "AddModuleAction",
+    "DoorChangeAction",
+    "ResetTipsAction",
+    "SetPipetteMovementSpeedAction",
+    # action payload values
+    "PauseSource",
+    "FinishErrorDetails",
+]
---
+++ packages/opentrons/opentrons-7.1.0a11-py2.py3-none-any.whl/opentrons/protocol_engine/commands/pipetting_common.py
@@ -0,0 +1,104 @@
+"""Common pipetting command base models."""
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from ..types import WellLocation, DeckPoint
+
+
+class PipetteIdMixin(BaseModel):
+    """Mixin for command requests that take a pipette ID."""
+
+    pipetteId: str = Field(
+        ...,
+        description="Identifier of pipette to use for liquid handling.",
+    )
+
+
+class VolumeMixin(BaseModel):
+    """Mixin for command requests that take a volume of liquid."""
+
+    volume: float = Field(
+        ...,
+        description="Amount of liquid in uL. Must be at least 0 and no greater "
+        "than a pipette-specific maximum volume.",
+        ge=0,
+    )
+
+
+class FlowRateMixin(BaseModel):
+    """Mixin for command requests that take a flow rate."""
+
+    flowRate: float = Field(
+        ..., description="Speed in µL/s configured for the pipette", gt=0
+    )
+
+
+class WellLocationMixin(BaseModel):
+    """Mixin for command requests that take a location that's somewhere in a well."""
+
+    labwareId: str = Field(
+        ...,
+        description="Identifier of labware to use.",
+    )
+    wellName: str = Field(
+        ...,
+        description="Name of well to use in labware.",
+    )
+    wellLocation: WellLocation = Field(
+        default_factory=WellLocation,
+        description="Relative well location at which to perform the operation",
+    )
+
+
+class MovementMixin(BaseModel):
+    """Mixin for command requests that move a pipette."""
+
+    minimumZHeight: Optional[float] = Field(
+        None,
+        description=(
+            "Optional minimal Z margin in mm."
+            " If this is larger than the API's default safe Z margin,"
+            " it will make the arc higher. If it's smaller, it will have no effect."
+        ),
+    )
+
+    forceDirect: bool = Field(
+        False,
+        description=(
+            "If true, moving from one labware/well to another"
+            " will not arc to the default safe z,"
+            " but instead will move directly to the specified location."
+            " This will also force the `minimumZHeight` param to be ignored."
+            " A 'direct' movement is in X/Y/Z simultaneously."
+        ),
+    )
+
+    speed: Optional[float] = Field(
+        None,
+        description=(
+            "Override the travel speed in mm/s."
+            " This controls the straight linear speed of motion."
+        ),
+    )
+
+
+class BaseLiquidHandlingResult(BaseModel):
+    """Base properties of a liquid handling result."""
+
+    volume: float = Field(
+        ...,
+        description="Amount of liquid in uL handled in the operation.",
+        ge=0,
+    )
+
+
+class DestinationPositionResult(BaseModel):
+    """Mixin for command results that move a pipette."""
+
+    position: DeckPoint = Field(
+        DeckPoint(x=0, y=0, z=0),
+        description=(
+            "The (x,y,z) coordinates of the pipette's critical point in deck space"
+            " after the move was completed."
+        ),
+    )
