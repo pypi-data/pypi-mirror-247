8bafc4c204f62ac7e905c0ae14aaae12f430a352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:10 +0000

    Add package pepperize.cdk-security-group/0.0.553/pepperize.cdk-security-group-0.0.553.tar.gz

---
+++ packages/pepperize.cdk-security-group/pepperize.cdk-security-group-0.0.553.tar.gz/pepperize.cdk-security-group-0.0.553/src/pepperize_cdk_security_group/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-security-group",
+    "0.0.553",
+    __name__[0:-6],
+    "cdk-security-group@0.0.553.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
