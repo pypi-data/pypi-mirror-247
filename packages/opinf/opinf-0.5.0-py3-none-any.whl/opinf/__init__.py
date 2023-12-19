aa0de045c6f0d78b5a31a7182e40ee8cfa033199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:06 +0000

    Add package opinf/0.5.0/opinf-0.5.0-py3-none-any.whl

---
+++ packages/opinf/opinf-0.5.0-py3-none-any.whl/opinf/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+"""Operator Inference for data-driven model reduction of dynamical systems.
+
+Author: Willcox Research Group
+Maintainer: Shane A. McQuarrie
+GitHub:
+    https://github.com/Willcox-Research-Group/rom-operator-inference-Python3
+"""
+
+__version__ = "0.5.0"
+
+from . import basis, errors, ddt, lstsq, models, operators, pre, post, utils
