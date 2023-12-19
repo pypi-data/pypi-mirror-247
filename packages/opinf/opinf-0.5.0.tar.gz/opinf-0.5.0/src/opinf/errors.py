1ab6337da011a77824dd31c5d0e57727dc988b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:06 +0000

    Add package opinf/0.5.0/opinf-0.5.0.tar.gz

---
+++ packages/opinf/opinf-0.5.0.tar.gz/opinf-0.5.0/src/opinf/errors.py
@@ -0,0 +1,20 @@
+# errors.py
+"""Custom exception classes."""
+
+
+class DimensionalityError(Exception):  # pragma: no cover
+    """Dimension of data not aligned with previous model information."""
+
+    pass
+
+
+class LoadfileFormatError(Exception):  # pragma: no cover
+    """File format inconsistent with a loading routine."""
+
+    pass
+
+
+class UsageWarning(Warning):  # pragma: no cover
+    """Generic warning for package usage."""
+
+    pass
