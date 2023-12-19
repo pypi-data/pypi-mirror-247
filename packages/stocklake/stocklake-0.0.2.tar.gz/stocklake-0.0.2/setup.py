9fae2441c87d1f848a53d8ec8771cb4cb3e61d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:03 +0000

    Add package stocklake/0.0.2/stocklake-0.0.2.tar.gz

---
+++ packages/stocklake/stocklake-0.0.2.tar.gz/stocklake-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="stocklake",
+    version="0.0.2",
+    py_modules=["stocklake"],
+    install_requires=[
+        "Click",
+    ],
+    packages=setuptools.find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "stocklake = stocklake.cli:cli",
+        ],
+    },
+)
