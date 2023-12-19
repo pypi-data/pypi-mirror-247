365c9501b80c6f46d19980425a621b12049b8b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:02:50 +0000

    Add package PaIRS-UniNa/0.1.11/PaIRS_UniNa-0.1.11-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/PaIRS-UniNa/PaIRS_UniNa-0.1.11-cp311-cp311-macosx_10_9_universal2.whl/PaIRS_UniNa/PaIRS.py
@@ -0,0 +1,18 @@
+from .gPaIRS import *
+
+def run():
+    gui:gPaIRS
+    app,gui=launchPaIRS()
+    quitPaIRS(app)
+
+def cleanRun():
+    if os.path.exists(lastcfgname):
+        os.remove(lastcfgname)
+    run()
+   
+def debugRun():
+    gui:gPaIRS
+    app,gui=launchPaIRS(flagInputDebug=True)
+    quitPaIRS(app)
+
+
