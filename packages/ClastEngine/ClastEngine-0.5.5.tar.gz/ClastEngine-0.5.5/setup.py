2934845146d7de48df41c4e0c6ac6a650bda5a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:47 +0000

    Add package ClastEngine/0.5.5/ClastEngine-0.5.5.tar.gz

---
+++ packages/ClastEngine/ClastEngine-0.5.5.tar.gz/ClastEngine-0.5.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name = 'ClastEngine',
+	version='0.5.5',
+	description = 'Простой 2D движок для создания простых игр.',
+	packages = ['eng/'],
+	author_email = 'romanhaziahmetov3@gmail.com',
+	zip_safe=False)
