5c109d3157a0b4dc129801a24f6163abcd9ef6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:04:00 +0000

    Add package UMNN/1.70/UMNN-1.70.tar.gz

---
+++ packages/UMNN/UMNN-1.70.tar.gz/UMNN-1.70/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UMNN',
+    version='1.70',
+    packages=['UMNN'],
+    url='',
+    license='MIT License',
+    author='awehenkel',
+    author_email='antoine.wehenkel@gmail.com',
+    description='Unconstrained Monotonic Neural Networks and Autoregressive Monotonic Flows on cpu/cuda/mps.'
+)
