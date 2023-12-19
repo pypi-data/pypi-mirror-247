a39ecccd6f0a7034171830200675a87f56ed966d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:05 +0000

    Add package FGL/0.0.1rc27.post1/fgl-0.0.1rc27.post1.tar.gz

---
+++ packages/FGL/fgl-0.0.1rc27.post1.tar.gz/fgl-0.0.1rc27.post1/src/FGL/matching/__src/match.h
@@ -0,0 +1,9 @@
+#ifndef MATCH_WRAPPER_H
+#define MATCH_WRAPPER_H
+
+// Always include Python.h in the very first line in all header files.
+#include <Python.h>
+
+PyObject* match_wrapper(PyObject *self, PyObject *args);
+
+#endif
