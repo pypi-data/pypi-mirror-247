ae566c90dc0494e9392ada3e47493d4e7835856a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:01 +0000

    Add package tinyllm/1.0.10/tinyllm-1.0.10.tar.gz

---
+++ packages/tinyllm/tinyllm-1.0.10.tar.gz/tinyllm-1.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='tinyllm',
+      version='1.0.10',
+      description='Development and management infrastructure for LLM applications',
+      packages=find_packages(),
+      install_requires=[
+          'click',
+          'tenacity',
+          'langfuse',
+          'litellm',
+          'openai',
+          'pathspec',
+          'pgvector',
+          'psutil',
+          'psycopg2-binary',
+          'pydantic==1.10.7',
+          'pytest',
+          'pyyaml',
+          'sqlalchemy',
+          'tiktoken',
+          'typing-extensions==4.5.0',
+          'uuid',
+          'fuzzywuzzy'
+      ],
+      author='Othmane Zoheir',
+      author_email='zozoheir@umich.edu',
+      url='')
---
+++ packages/tinyllm/tinyllm-1.0.10.tar.gz/tinyllm-1.0.10/tinyllm/__init__.py
@@ -0,0 +1,60 @@
+import yaml
+import os
+
+from pathlib import Path, PosixPath
+
+from sentence_transformers import SentenceTransformer
+
+from smartpy.utility.log_util import getLogger
+from langfuse import Langfuse
+
+logger = getLogger(__name__)
+
+
+def load_yaml_config(yaml_file_name: str, directories: list) -> dict:
+    config = None
+    for directory in directories:
+        yaml_path = Path(directory) / yaml_file_name
+        if yaml_path.is_file():
+            logger.info(f"Tinyllm: config found at {yaml_path}")
+            with open(yaml_path, 'r') as stream:
+                try:
+                    config = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    logger.info(exc)
+                break
+    return config
+
+
+def set_env_variables(config: dict):
+    if config is not None:
+        for key, value in config.items():
+            if isinstance(value, dict):
+                for subkey, subvalue in value.items():
+                    os.environ[subkey] = str(subvalue)
+            else:
+                os.environ[key] = str(value)
+
+
+directories = [
+    Path.cwd(),
+    Path.home(),
+    Path.home() / 'Documents',
+]
+
+directories.append(PosixPath(os.environ['TINYLLM_YAML_DIR'].replace(' ', '')))
+tinyllm_config = load_yaml_config('tinyllm.yaml', directories)
+
+os.environ['OPENAI_API_KEY'] = tinyllm_config['LLM_PROVIDERS']['OPENAI_API_KEY']
+
+langfuse_client = Langfuse(
+    public_key=tinyllm_config['LANGFUSE']['PUBLIC_KEY'],
+    secret_key=tinyllm_config['LANGFUSE']['SECRET_KEY'],
+    host=tinyllm_config['LANGFUSE']['HOST'],
+    debug=False,
+    flush_interval=0.1,
+)
+
+
+embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+default_embedding_model = lambda x: embedding_model.encode(x)
