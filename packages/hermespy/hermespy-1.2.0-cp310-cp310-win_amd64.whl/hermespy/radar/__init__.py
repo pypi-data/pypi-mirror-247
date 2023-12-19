771084f45885620fa5cdf342b2fa2acb1907dc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:53 +0000

    Add package hermespy/1.2.0/hermespy-1.2.0-cp310-cp310-win_amd64.whl

---
+++ packages/hermespy/hermespy-1.2.0-cp310-cp310-win_amd64.whl/hermespy/radar/__init__.py
@@ -0,0 +1,39 @@
+from .cube import RadarCube
+from .detection import (
+    RadarDetector,
+    PointDetection,
+    RadarPointCloud,
+    ThresholdDetector,
+    MaxDetector,
+    CFARDetector,
+)
+from .radar import Radar, RadarWaveform, RadarTransmission, RadarReception
+from .fmcw import FMCW
+from .evaluators import ReceiverOperatingCharacteristic, DetectionProbEvaluator, RootMeanSquareError
+
+__author__ = "Jan Adler"
+__copyright__ = "Copyright 2023, Barkhausen Institut gGmbH"
+__credits__ = ["Jan Adler"]
+__license__ = "AGPLv3"
+__version__ = "1.2.0"
+__maintainer__ = "Jan Adler"
+__email__ = "jan.adler@barkhauseninstitut.org"
+__status__ = "Prototype"
+
+__all__ = [
+    "RadarCube",
+    "RadarDetector",
+    "PointDetection",
+    "RadarPointCloud",
+    "ThresholdDetector",
+    "MaxDetector",
+    "CFARDetector",
+    "Radar",
+    "RadarWaveform",
+    "RadarTransmission",
+    "RadarReception",
+    "FMCW",
+    "ReceiverOperatingCharacteristic",
+    "DetectionProbEvaluator",
+    "RootMeanSquareError",
+]
