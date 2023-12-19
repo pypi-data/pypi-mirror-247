4f632189bf04d320869338a5891951fa0004a7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:29 +0000

    Add package up42-py/0.34.1/up42_py-0.34.1-py3-none-any.whl

---
+++ packages/up42-py/up42_py-0.34.1-py3-none-any.whl/up42/__init__.py
@@ -0,0 +1,43 @@
+"""
+    `up42` is the base library module imported to Python. It provides the elementary
+    functionality that is not bound to a specific class of the UP42 structure.
+    From `up42` you can also initialize other classes, e.g. for using the catalog, storage etc.
+
+    To import the UP42 library:
+    ```python
+    import up42
+    ```
+
+    Authenticate with UP42:
+        https://sdk.up42.com/authentication/.
+
+    To initialize any lower level functionality use e.g.
+    ```python
+    catalog = up42.initialize_catalog()
+    ```
+    ```python
+    project = up42.initialize_project(project_id="your-project-ID")
+    ```
+"""
+
+from pathlib import Path
+
+from up42.asset import Asset
+from up42.auth import Auth
+from up42.catalog import Catalog
+from up42.initialization import *  # defined by __all__
+from up42.job import Job
+from up42.jobcollection import JobCollection
+from up42.jobtask import JobTask
+from up42.main import *  # defined by __all__
+from up42.order import Order
+from up42.project import Project
+from up42.storage import Storage
+from up42.tasking import Tasking
+from up42.tools import get_example_aoi, read_vector_file
+from up42.utils import get_up42_py_version
+from up42.viztools import draw_aoi
+from up42.webhooks import Webhook
+from up42.workflow import Workflow
+
+__version__ = get_up42_py_version()
---
+++ packages/up42-py/up42_py-0.34.1-py3-none-any.whl/up42/project.py
@@ -0,0 +1,254 @@
+import logging
+from typing import Dict, List, Optional, Union
+from warnings import warn
+
+from up42.auth import Auth
+from up42.job import Job
+from up42.jobcollection import JobCollection
+from up42.utils import filter_jobs_on_mode, get_logger
+from up42.workflow import Workflow
+
+logger = get_logger(__name__)
+
+
+class Project:
+    """
+    The Project is the top-level class of the UP42 hierarchy. With it you can create
+    new workflows, query already existing workflows & jobs in the project and manage
+    the project settings.
+
+    ```python
+    project = up42.initialize_project(project_id="uz92-8uo0-4dc9-ab1d-06d7ec1a5321")
+    ```
+    """
+
+    def __init__(self, auth: Auth, project_id: str):
+        self.auth = auth
+        self.project_id = project_id
+        self._info = self.info
+
+    def __repr__(self):
+        env = ", env: dev" if self.auth.env == "dev" else ""
+        return (
+            f"Project(name: {self._info['name']}, project_id: {self.project_id}, "
+            f"description: {self._info['description']}, createdAt: {self._info['createdAt']}"
+            f"{env})"
+        )
+
+    @property
+    def info(self) -> dict:
+        """
+        Gets and updates the project metadata information.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}"
+        response_json = self.auth._request(request_type="GET", url=url)
+        self._info = response_json["data"]
+        return self._info
+
+    def create_workflow(self, name: str, description: str = "", use_existing: bool = False) -> "Workflow":
+        """
+        Creates a new workflow and returns a workflow object.
+
+        Args:
+            name: Name of the new workflow.
+            description: Description of the new workflow.
+            use_existing: If True, instead of creating a new workflow, uses the
+                most recent workflow with the same name & description.
+
+        Returns:
+            The workflow object.
+        """
+        warn(
+            "Workflows are getting deprecated. The current analytics platform will be discontinued "
+            "after January 31, 2024, and will be replaced by new processing functionalities.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        if use_existing:
+            logger.info("Getting existing workflows in project ...")
+            logging.getLogger("up42.workflow").setLevel(logging.CRITICAL)
+            existing_workflows: list = self.get_workflows(return_json=True)
+            logging.getLogger("up42.workflow").setLevel(logging.INFO)
+
+            matching_workflows: list = [
+                workflow
+                for workflow in existing_workflows
+                if workflow["name"] == name and workflow["description"] == description
+            ]
+            if matching_workflows:
+                existing_workflow = Workflow(
+                    self.auth,
+                    project_id=self.project_id,
+                    workflow_id=matching_workflows[0]["id"],
+                )
+                logger.info(f"Using existing workflow: {name} - {existing_workflow.workflow_id}")
+                return existing_workflow
+
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/workflows/"
+        payload = {"name": name, "description": description}
+        response_json = self.auth._request(request_type="POST", url=url, data=payload)
+        workflow_id = response_json["data"]["id"]
+        logger.info(f"Created new workflow: {workflow_id}")
+        workflow = Workflow(self.auth, project_id=self.project_id, workflow_id=workflow_id)
+        return workflow
+
+    def get_workflows(self, return_json: bool = False) -> Union[List["Workflow"], List[dict]]:
+        """
+        Gets all workflows in a project as workflow objects or JSON.
+
+        Args:
+            return_json: True returns infos of workflows as JSON instead of workflow objects.
+
+        Returns:
+            List of Workflow objects in the project or alternatively JSON info of the workflows.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/workflows"
+        response_json = self.auth._request(request_type="GET", url=url)
+        workflows_json = response_json["data"]
+        logger.info(f"Got {len(workflows_json)} workflows for project {self.project_id}.")
+
+        if return_json:
+            return workflows_json
+        else:
+            workflows = [
+                Workflow(
+                    self.auth,
+                    project_id=self.project_id,
+                    workflow_id=workflow_json["id"],
+                    workflow_info=workflow_json,
+                )
+                for workflow_json in workflows_json
+            ]
+            return workflows
+
+    def get_jobs(
+        self,
+        return_json: bool = False,
+        test_jobs: bool = True,
+        real_jobs: bool = True,
+        limit: int = 500,
+        sortby: str = "createdAt",
+        descending: bool = True,
+    ) -> Union[JobCollection, List[dict]]:
+        """
+        Get all jobs in the project as a JobCollection or JSON.
+
+        Use Workflow().get_job() to get a JobCollection with jobs associated with a
+        specific workflow.
+
+        Args:
+            return_json: If true, returns the job info JSONs instead of JobCollection.
+            test_jobs: Return test jobs or test queries.
+            real_jobs: Return real jobs.
+            limit: Only return n first jobs by sorting criteria and order, default 500.
+            sortby: The sorting criteria, one of "createdAt", "name", "id", "mode", "status", "startedAt", "finishedAt".
+            descending: The sorting order, True for descending (default), False for ascending.
+
+        Returns:
+            All job objects in a JobCollection, or alternatively the jobs info as JSON.
+        """
+        allowed_sorting_criteria = [
+            "createdAt",
+            "name",
+            "id",
+            "mode",
+            "status",
+            "startedAt",
+            "finishedAt",
+        ]
+        if sortby not in allowed_sorting_criteria:
+            raise ValueError(f"sortby parameter must be one of {allowed_sorting_criteria}!")
+        sort = f"{sortby},{'desc' if descending else 'asc'}"
+
+        page = 0
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}&sort={sort}"
+        response_json = self.auth._request(request_type="GET", url=url)
+        jobs_json = filter_jobs_on_mode(response_json["data"], test_jobs, real_jobs)
+
+        # API get jobs pagination exhaustion is indicated by empty next page (no last page flag)
+        logging.getLogger("up42.utils").setLevel(logging.CRITICAL)
+        while len(response_json["data"]) > 0 and len(jobs_json) < limit:
+            page += 1
+            url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}&sort={sort}"
+            response_json = self.auth._request(request_type="GET", url=url)
+            if len(response_json["data"]) > 0:
+                jobs_json.extend(filter_jobs_on_mode(response_json["data"], test_jobs, real_jobs))
+        logging.getLogger("up42.utils").setLevel(logging.INFO)
+        jobs_json = jobs_json[:limit]
+        logger.info(f"Got {len(jobs_json)} jobs (limit parameter {limit}) in project {self.project_id}.")
+        if return_json:
+            return jobs_json
+        else:
+            jobs = [
+                Job(
+                    self.auth,
+                    job_id=job_json["id"],
+                    project_id=self.project_id,
+                    job_info=job_json,
+                )
+                for job_json in jobs_json
+            ]
+            jobcollection = JobCollection(auth=self.auth, project_id=self.project_id, jobs=jobs)
+            return jobcollection
+
+    def get_project_settings(self) -> List[Dict[str, str]]:
+        """
+        Gets the project settings.
+
+        Returns:
+            The project settings.
+        """
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/settings"
+        response_json = self.auth._request(request_type="GET", url=url)
+        project_settings = response_json["data"]
+        return project_settings
+
+    @property
+    def max_concurrent_jobs(self) -> int:
+        """
+        Gets the maximum number of concurrent jobs allowed by the project settings.
+        """
+        project_settings = self.get_project_settings()
+        project_settings_dict = {d["name"]: int(d["value"]) for d in project_settings}
+        return project_settings_dict["MAX_CONCURRENT_JOBS"]
+
+    def update_project_settings(
+        self,
+        max_aoi_size: Optional[int] = None,
+        max_concurrent_jobs: Optional[int] = None,
+        number_of_images: Optional[int] = None,
+    ) -> None:
+        """
+        Updates a project's settings.
+
+        Args:
+            max_aoi_size: The maximum area of interest geometry size, from 1-1000 sqkm, default 10 sqkm.
+            max_concurrent_jobs: The maximum number of concurrent jobs, from 1-10, default 1.
+            number_of_images: The maximum number of images returned with each job, from 1-20, default 10.
+        """
+        # The ranges and default values of the project settings can potentially get
+        # increased, so need to be dynamically queried from the server.
+        warn(
+            "Workflows are getting deprecated. The current analytics platform will be discontinued "
+            "after January 31, 2024, and will be replaced by new processing functionalities.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        current_settings = {d["name"]: d for d in self.get_project_settings()}
+
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/settings"
+        payload: Dict = {"settings": {}}
+        desired_settings = {
+            "JOB_QUERY_MAX_AOI_SIZE": max_aoi_size,
+            "MAX_CONCURRENT_JOBS": max_concurrent_jobs,
+            "JOB_QUERY_LIMIT_PARAMETER_MAX_VALUE": number_of_images,
+        }
+
+        for name, desired_setting in desired_settings.items():
+            if desired_setting is None:
+                payload["settings"][name] = str(current_settings[name]["value"])
+            else:
+                payload["settings"][name] = str(desired_setting)
+
+        self.auth._request(request_type="POST", url=url, data=payload)
+        logger.info(f"Updated project settings: {payload}")
