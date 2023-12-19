f6303a98e145de97cff1521c552c3f4243f7c414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:04:37 +0000

    Add package ai-logicplum/1.0.4/ai-logicplum-1.0.4.tar.gz

---
+++ packages/ai-logicplum/ai-logicplum-1.0.4.tar.gz/ai-logicplum-1.0.4/CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.0.0] - 2023-12-08
+- Released.
+
+## [1.0.1] - 2023-12-08
+- code optimized.
+
+## [1.0.2] - 2023-12-08
+- Passing individual params instead of passing as dictionary except training.
+
+## [1.0.3] - 2023-12-08
+- 1.0.2 fixes.
+
+## [1.0.3] - 2023-12-08
+- config file changes.
---
+++ packages/ai-logicplum/ai-logicplum-1.0.4.tar.gz/ai-logicplum-1.0.4/logicplum/aicloud/monitoring/model_monitoring.py
@@ -0,0 +1,43 @@
+import requests
+from config import base_url, api_token
+
+
+def read_file(file_path):
+    with open(file_path, "rb") as file:
+        uploaded_filename = file_path.split('\\')[-1]
+        content = file.read()
+        return uploaded_filename, content
+import base64
+
+def aipilot_model_monitoring(client_token,deployment_id,res_type,file_path):
+    data = {
+        'res_type':res_type,
+        'deployment_id': deployment_id
+    }
+    url = f"{base_url}/training/aipilot/monitor-model"
+    # Get The Monitor Graph Of The Deployed Model
+    headers = {"Authorization":client_token}
+    uploaded_filename, content = read_file(file_path)
+    files = {'file': (uploaded_filename, content)}
+    # Send the POST request
+    response = requests.post(url, data=data, headers=headers,files=files)
+    if data.get('res_type') == 'image':
+        return response.content
+    return response.json()
+
+
+def comprehensive_model_monitoring(client_token,deployment_id,res_type,file_path):
+    data = {
+    'res_type':res_type,
+    'deployment_id': deployment_id
+    }
+    url = f"{base_url}/training/comprehensive/monitor-model"
+    # Get The Monitor Graph Of The Deployed Model
+    headers = {"Authorization":client_token}
+    uploaded_filename, content = read_file(file_path)
+    files = {'file': (uploaded_filename, content)}
+    # Send the POST request
+    response = requests.post(url, data=data, headers=headers,files=files)
+    if data.get('res_type') == 'image':
+        return response.content
+    return response.json()
