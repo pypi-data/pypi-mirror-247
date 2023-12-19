680ed8194abe7a36da1ee69e3e7dd143ff732636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:45 +0000

    Add package dzwl/1.0.7/dzwl-1.0.7.tar.gz

---
+++ packages/dzwl/dzwl-1.0.7.tar.gz/dzwl-1.0.7/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="dzwl",                                     # 包的分发名称，使用字母、数字、_、-
+    version="1.0.7",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
+    author="zj",                                       # 作者名字
+    author_email="author@example.com",                      # 作者邮箱
+    description="PyPI Tutorial",                            # 包的简介描述
+    long_description='123',                      # 包的详细介绍(一般通过加载README.md)
+    packages=setuptools.find_packages(),                    # 如果项目由多个文件组成，我们可以使用find_packages()自动发现所有包和子包，而不是手动列出每个包，在这种情况下，包列表将是example_pkg
+    install_requires=[],  # 依赖的包
+    python_requires='>=3'
+)
+
+# 这是最简单的配置
+# 有关详细信息，请参阅(https://packaging.python.org/guides/distributing-packages-using-setuptools/)
