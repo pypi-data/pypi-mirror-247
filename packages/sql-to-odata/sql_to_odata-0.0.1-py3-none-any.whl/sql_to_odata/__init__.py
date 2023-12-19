5ca2598a052f3cb5944bce4826382ceb4a884d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:03:59 +0000

    Add package sql-to-odata/0.0.1/sql_to_odata-0.0.1-py3-none-any.whl

---
+++ packages/sql-to-odata/sql_to_odata-0.0.1-py3-none-any.whl/sql_to_odata/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.1'
+
+
+class ODataInterface():
+    """OData interface to a SQL database."""
+
+    def __init__(self, *args, **kwargs):
+        """Construct an instance of the class."""
+        pass
