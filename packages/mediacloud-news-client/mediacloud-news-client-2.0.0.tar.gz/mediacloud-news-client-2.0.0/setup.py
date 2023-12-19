43ff8045453b03b4f4e4556a608f2dcef41bb4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:44 +0000

    Add package mediacloud-news-client/2.0.0/mediacloud-news-client-2.0.0.tar.gz

---
+++ packages/mediacloud-news-client/mediacloud-news-client-2.0.0.tar.gz/mediacloud-news-client-2.0.0/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+import re
+import os
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    # utilities
+    "requests==2.*",  # widely used HTTP library
+    "ciso8601==2.2.*"  # super-fast date parsing
+]
+
+with open('mcnews/__init__.py', 'r', encoding='utf8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+# add README.md to distribution
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(name='mediacloud-news-client',
+      maintainer='Rahul Bhargava',
+      maintainer_email='r.bhargava@northeastern.edu',
+      version=version,
+      description='Mediacloud news archive search api client',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      test_suite="mcnews.test",
+      packages=['mcnews'],
+      package_data={'': ['LICENSE']},
+      python_requires='>3.7',
+      install_requires=REQUIRED_PACKAGES,
+      extras_require={'dev': ['pytest', 'twine']},
+      project_urls={
+              'Bug Reports': 'https://github.com/mediacloud/mediacloud-news-client/issues',
+              'Source': 'https://github.com/mediacloud/mediacloud-news-client',
+          },
+      )
