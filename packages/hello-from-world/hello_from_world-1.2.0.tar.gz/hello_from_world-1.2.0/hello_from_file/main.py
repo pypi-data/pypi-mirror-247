fdd738819e64a784411238ae1085091399feac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:14 +0000

    Add package hello-from-world/1.2.0/hello_from_world-1.2.0.tar.gz

---
+++ packages/hello-from-world/hello_from_world-1.2.0.tar.gz/hello_from_world-1.2.0/hello_from_file/main.py
@@ -0,0 +1,7 @@
+from json import load
+from pkg_resources import resource_stream
+
+def read_hello_file():
+    # Read the content from hello.txt
+    schema = load(resource_stream('hello_from_file', '../hello.txt'))
+    return schema
