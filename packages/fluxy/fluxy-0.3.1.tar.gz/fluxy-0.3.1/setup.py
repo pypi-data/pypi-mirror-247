b1e3c3ef11adfab9c26271247e51cc19382c170c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:17 +0000

    Add package fluxy/0.3.1/fluxy-0.3.1.tar.gz

---
+++ packages/fluxy/fluxy-0.3.1.tar.gz/fluxy-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fluxy',
+    version='0.3.1',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'fastapi',
+        'uvicorn',
+    ],
+    entry_points={
+    'console_scripts': [
+        'fluxy-fastapi-startproject=fluxy.fluxy_fastapi:startproject',
+    ],
+},
+
+)
