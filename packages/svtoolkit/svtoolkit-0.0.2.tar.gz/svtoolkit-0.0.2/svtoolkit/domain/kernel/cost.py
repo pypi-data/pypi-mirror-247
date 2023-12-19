7d4ec502555d342c9405fede3ca3b805af35bc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:03:44 +0000

    Add package svtoolkit/0.0.2/svtoolkit-0.0.2.tar.gz

---
+++ packages/svtoolkit/svtoolkit-0.0.2.tar.gz/svtoolkit-0.0.2/svtoolkit/domain/kernel/cost.py
@@ -0,0 +1,36 @@
+import numpy as np
+import numba as nb
+from .coordinate_system import sph2cart
+
+
+#@nb.jit(nopython=True, fastmath=True)
+def cost(x, h_, n, d):
+    """
+    This function is used to calculate the cost function of the optimization problem
+    posed for the energy minimization of the implicit surface.
+
+    Parameters
+    ----------
+        x : numpy.ndarray
+            The vector of parameters to be optimized. These are generally in the form
+            of spherical coordinates of normalized magnitude. Thus, the first radial
+            coordinate is not included and assumed to be 1.
+        h_ : numpy.ndarray
+            The h matrix is a D*N x D*N matrix representing the linear system of
+            equations forming the energy minimization problem.
+        n : int
+            The number of points defining the D-1 manifold.
+        d : int
+            The dimension of the domain in which the D-1 manifold is embedded.
+    Returns
+    -------
+        cost : float
+            The cost function of the optimization problem.
+    """
+    g = np.ones((n, d))
+    g[:, 1:] = x.reshape(n, d - 1)
+    a = sph2cart(g)
+    a = a.reshape((n * d, 1))
+    c = a.T @ h_ @ a
+    c = c.flatten()
+    return c
