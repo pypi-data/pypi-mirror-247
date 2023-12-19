cc8769d7eb5dbe423d5b8834be6e5cf95791b419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:06 +0000

    Add package selva/0.8.3/selva-0.8.3-py3-none-any.whl

---
+++ packages/selva/selva-0.8.3-py3-none-any.whl/selva/configuration/defaults.py
@@ -0,0 +1,17 @@
+default_settings = {
+    "application": "application",
+    "modules": [],
+    "middleware": [],
+    "logging": {
+        "setup": "selva.logging.setup.setup_logger",
+        "root": "WARNING",
+        "level": {},
+        "enable": [],
+        "disable": [],
+    },
+    "templates": {
+        "jinja": {
+            "path": "resources/templates",
+        },
+    },
+}
---
+++ packages/selva/selva-0.8.3-py3-none-any.whl/selva/web/__init__.py
@@ -0,0 +1,17 @@
+from selva.web.application import Selva
+from selva.web.converter.param_extractor_impl import (
+    FromCookie,
+    FromHeader,
+    FromPath,
+    FromQuery,
+)
+from selva.web.middleware import Middleware
+from selva.web.routing.decorator import (
+    controller,
+    delete,
+    get,
+    patch,
+    post,
+    put,
+    websocket,
+)
