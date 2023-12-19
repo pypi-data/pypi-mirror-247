8e119f91e6506e888841c3530b629f7b05043034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:47 +0000

    Add package botnoi-dev-platform/0.0.3/botnoi_dev_platform-0.0.3.tar.gz

---
+++ packages/botnoi-dev-platform/botnoi_dev_platform-0.0.3.tar.gz/botnoi_dev_platform-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.3"
+
+DESCRIPTION = """
+A programatical alternative to the visual chatbot builder provided by https://botnoi.ai . 
+It can be used to build and train chatbots for various platforms such as Facebook Messenger and LINE.
+"""
+
+LONG_DESCRIPTION = open("LONG_DESCRIPTION.md", "r", encoding="utf-8").read()
+
+setup(
+    name="botnoi_dev_platform",
+    version=VERSION,
+    author="Jira Pit @ BOTNOI GROUP",
+    author_email="<jira.p@botnoigroup.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=["botnoi", "chatbot", "bot", "botnoi dev platform"],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
