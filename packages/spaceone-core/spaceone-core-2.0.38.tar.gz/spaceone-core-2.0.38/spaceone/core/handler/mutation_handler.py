871c472a03239b6dde5c451ea2645e27a1517319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:29 +0000

    Add package spaceone-core/2.0.38/spaceone-core-2.0.38.tar.gz

---
+++ packages/spaceone-core/spaceone-core-2.0.38.tar.gz/spaceone-core-2.0.38/spaceone/core/handler/mutation_handler.py
@@ -0,0 +1,31 @@
+import logging
+from spaceone.core.handler import BaseMutationHandler
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SpaceONEMutationHandler(BaseMutationHandler):
+    def request(self, params):
+        user_role_type = self.transaction.get_meta("authorization.role_type")
+        domain_id = self.transaction.get_meta("authorization.domain_id")
+        workspace_id = self.transaction.get_meta("authorization.workspace_id")
+        user_projects = self.transaction.get_meta("authorization.projects")
+        user_id = self.transaction.get_meta("authorization.user_id")
+        set_user_id = self.transaction.get_meta("authorization.set_user_id")
+
+        if user_role_type == "DOMAIN_ADMIN":
+            params["domain_id"] = domain_id
+        elif user_role_type == "WORKSPACE_OWNER":
+            params["domain_id"] = domain_id
+            params["workspace_id"] = workspace_id
+        elif user_role_type == "WORKSPACE_MEMBER":
+            params["domain_id"] = domain_id
+            params["workspace_id"] = workspace_id
+            params["user_projects"] = user_projects
+        elif user_role_type == "USER":
+            params["domain_id"] = domain_id
+
+        if set_user_id:
+            params["user_id"] = user_id
+
+        return params
