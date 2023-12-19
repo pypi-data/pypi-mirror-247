283ad8aeb29623a7d372315c81806c8eef9ab606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:12 +0000

    Add package inutest/0.1.0/inutest-0.1.0.tar.gz

---
+++ packages/inutest/inutest-0.1.0.tar.gz/inutest-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inutest',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        # InuCommonUtilities.dll
+        # InuStreamsd.dll
+    ],
+)
