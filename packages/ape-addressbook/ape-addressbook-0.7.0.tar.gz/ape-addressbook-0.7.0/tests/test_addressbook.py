30ce152c682658f1dbbacda14a39bc9af7473d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:04:28 +0000

    Add package ape-addressbook/0.7.0/ape-addressbook-0.7.0.tar.gz

---
+++ packages/ape-addressbook/ape-addressbook-0.7.0.tar.gz/ape-addressbook-0.7.0/tests/test_addressbook.py
@@ -0,0 +1,51 @@
+from ape_addressbook import addressbook
+
+
+def test_config(project_alias_unchecksummed, project_alias_no_quotes, project_address):
+    """
+    This test shows that we are able to read values from a
+    project's config as well as handle checksumming them if needbe.
+    """
+
+    actual = addressbook.config
+    assert len(actual) == 2
+    assert actual[project_alias_unchecksummed] == project_address
+    assert actual[project_alias_no_quotes] == project_address
+
+
+def test_registry(
+    project_alias_unchecksummed,
+    project_alias_no_quotes,
+    project_address,
+):
+    actual = addressbook.registry
+    assert len(actual) == 2
+    assert actual[project_alias_unchecksummed] == project_address
+    assert actual[project_alias_no_quotes] == project_address
+
+
+def test_aliases(project_alias_unchecksummed, project_alias_no_quotes):
+    """
+    The aliases includes both project and global addresses.
+    """
+
+    actual = list(addressbook.aliases)
+    expected = [project_alias_no_quotes, project_alias_unchecksummed]
+    assert actual == expected
+
+
+def test_contains(
+    project_alias_unchecksummed,
+    project_alias_no_quotes,
+):
+    assert project_alias_unchecksummed in addressbook
+    assert project_alias_no_quotes in addressbook
+
+
+def test_get_item(
+    project_alias_unchecksummed,
+    project_address,
+    project_alias_no_quotes,
+):
+    assert addressbook[project_alias_unchecksummed] == project_address
+    assert addressbook[project_alias_no_quotes] == project_address
