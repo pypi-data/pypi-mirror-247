534dc4e048fbda7877a14b72433f343fb35b7ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:33 +0000

    Add package aos-prov/4.2.3b3/aos_prov-4.2.3b3-py3-none-any.whl

---
+++ packages/aos-prov/aos_prov-4.2.3b3-py3-none-any.whl/aos_prov/utils/common.py
@@ -0,0 +1,57 @@
+#
+#  Copyright (c) 2018-2023 Renesas Inc.
+#  Copyright (c) 2018-2023 EPAM Systems Inc.
+#
+
+import random
+import string
+from pathlib import Path
+
+from rich.console import Console
+
+CONTENT_ENCRYPTION_ALGORITHM = 'aes256_cbc'
+DOWNLOADS_PATH = Path.home() / '.aos' / 'downloads'
+AOS_DISKS_PATH = DOWNLOADS_PATH
+IMAGE_WITHOUT_NODES_FILENAME = 'aos-image-vm-genericx86-64_3.0.1.wic.vmdk'
+NODE0_IMAGE_FILENAME = 'aos-vm-node0-genericx86-64.wic.vmdk'
+NODE1_IMAGE_FILENAME = 'aos-vm-node1-genericx86-64.wic.vmdk'
+
+DISK_IMAGE_DOWNLOAD_URL = 'https://aos-prod-cdn-endpoint.azureedge.net/vm/aos-vm-v4.2.1-beta.2.tar.gz?' \
+                          '0b4216f12ddbd64e34655617b7a1943779e00f658df8d2d94a4c6515dc420ba2224678caa704' \
+                          'f52307936ba599f9b3dd2d665aa9202b1ccaf1b73c2b7d957074690ba9deb8c817d2a87cdeaa'
+
+
+
+console = Console()
+error_console = Console(stderr=True, style='red')
+allow_print = True
+
+def print_message(formatted_text, end="\n", ljust: int = 0):
+    if allow_print:
+        if ljust > 0:
+            formatted_text = formatted_text.ljust(ljust)
+        console.print(formatted_text, end=end)
+
+def print_left(formatted_text, ljust=60):
+    print_message(formatted_text, end='', ljust = ljust)
+
+def print_done():
+    print_message('[green]DONE')
+
+def print_success(message):
+    print_message(f'[green]{str(message)}')
+
+def print_error(message):
+    if allow_print:
+        error_console.print(message)
+
+def generate_random_password() -> str:
+    """
+    Generate random password from letters and digits.
+
+    Returns:
+        str: Random string password
+    """
+    dictionary = string.ascii_letters + string.digits
+    password_length = random.randint(10, 15)
+    return ''.join(random.choice(dictionary) for _ in range(password_length))
