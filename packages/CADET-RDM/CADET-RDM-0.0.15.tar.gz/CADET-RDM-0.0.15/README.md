d16709a0e8258cda8690d37b4bb1c43c7f6f5224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:34 +0000

    Add package CADET-RDM/0.0.15/CADET-RDM-0.0.15.tar.gz

---
+++ packages/CADET-RDM/CADET-RDM-0.0.15.tar.gz/CADET-RDM-0.0.15/README.md
@@ -0,0 +1,12 @@
+# The CADET-Research Data Management toolbox
+
+Welcome to CADET-RDM, a project by the Forschungszentrum Jülich.
+
+This tool aims to help track and version control:
+- input data
+- code
+- software versions
+- output data
+
+and allow for easy sharing, integration, and reproduction of generated results. 
+
---
+++ packages/CADET-RDM/CADET-RDM-0.0.15.tar.gz/CADET-RDM-0.0.15/cadetrdm/web_utils.py
@@ -0,0 +1,14 @@
+from urllib.parse import urlparse
+
+
+def ssh_url_to_http_url(url):
+    url = url.replace(":", "/").replace("git@", "https://").replace(".git", "")
+    return url
+
+
+def is_valid_url(string):
+    try:
+        result = urlparse(string)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
