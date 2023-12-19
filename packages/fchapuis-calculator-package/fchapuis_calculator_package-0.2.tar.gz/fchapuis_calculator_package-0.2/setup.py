2814a02ea6faeb38e5f9c12956625240dcc9f68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:43 +0000

    Add package fchapuis-calculator-package/0.2/fchapuis_calculator_package-0.2.tar.gz

---
+++ packages/fchapuis-calculator-package/fchapuis_calculator_package-0.2.tar.gz/fchapuis_calculator_package-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fchapuis_calculator_package',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Fabiano Chapuis',
+    author_email='fabiano.chapuis@gmail.com',
+)
