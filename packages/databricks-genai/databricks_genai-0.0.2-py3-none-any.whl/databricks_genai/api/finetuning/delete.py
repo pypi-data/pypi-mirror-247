5423757eacbd6b7764a8a53287452a52bbfb570c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:03:50 +0000

    Add package databricks-genai/0.0.2/databricks_genai-0.0.2-py3-none-any.whl

---
+++ packages/databricks-genai/databricks_genai-0.0.2-py3-none-any.whl/databricks_genai/api/finetuning/delete.py
@@ -0,0 +1,29 @@
+"""Delete a finetuning run"""
+
+from typing import List, Union
+
+from mcli import delete_finetuning_runs
+
+from databricks_genai.api.config import configure_request
+from databricks_genai.errors import DatabricksGenAIRequestError
+from databricks_genai.types.finetuning import FinetuningRun
+
+
+@configure_request
+def delete(finetuning_runs: Union[str, FinetuningRun, List[str], List[FinetuningRun]]) -> int:
+    """Cancel and delete a finetuning run
+    
+    Args:
+        finetuning_runs (Union[str, FinetuningRun, List[str], List[FinetuningRun]]): The
+            finetuning run(s) to delete. Can be a single run or a list of runs.
+        
+    Returns:
+        int: The number of finetuning runs deleted
+    """
+    if not finetuning_runs:
+        raise DatabricksGenAIRequestError('Must provide finetuning run(s) to delete')
+
+    if isinstance(finetuning_runs, (str, FinetuningRun)):
+        finetuning_runs = [finetuning_runs]
+
+    return len(delete_finetuning_runs(finetuning_runs=finetuning_runs))
