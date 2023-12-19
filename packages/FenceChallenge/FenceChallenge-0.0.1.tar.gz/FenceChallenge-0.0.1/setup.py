35f5d32220d7c13cf946e78de2d5d6e1d62abe60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:05 +0000

    Add package FenceChallenge/0.0.1/FenceChallenge-0.0.1.tar.gz

---
+++ packages/FenceChallenge/FenceChallenge-0.0.1.tar.gz/FenceChallenge-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='FenceChallenge',
+    version='0.0.1',
+    install_requires=[
+        'importlib-metadata; python_version > "3.7"',
+        'numpy',
+        'pandas',
+        ],
+    author='Miguel OMalley',
+    author_email='miguelomalley9@gmail.com',
+)
+
+
+
