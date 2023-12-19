40cdd78e667d0db81a40d78afc59f71bef5facb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:12 +0000

    Add package standalone002pyarmor/0.0.16/standalone002pyarmor-0.0.16.tar.gz

---
+++ packages/standalone002pyarmor/standalone002pyarmor-0.0.16.tar.gz/standalone002pyarmor-0.0.16/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='standalone002pyarmor',
+    version='0.0.16',
+    packages=find_packages(),
+    package_data={
+        'my_package': ['exfn001add_release.py', 'pytransform/*'],
+    },
+    description='一个 Python 混淆工具包',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='xiaowen',
+    author_email='xiaowenseekjob@gmail.com',
+    url='https://github.com/yourusername/standalone001pyarmor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
