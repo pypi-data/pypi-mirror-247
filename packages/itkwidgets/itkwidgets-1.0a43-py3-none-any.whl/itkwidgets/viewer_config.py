bd8b1cf3e338a94de9190dcc3b8ee049666adbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:40 +0000

    Add package itkwidgets/1.0a43/itkwidgets-1.0a43-py3-none-any.whl

---
+++ packages/itkwidgets/itkwidgets-1.0a43-py3-none-any.whl/itkwidgets/viewer_config.py
@@ -0,0 +1,5 @@
+ITK_VIEWER_SRC = (
+    "https://bafybeigzy333sj7clxyuhglkua2qnymooxqkvsm7rt6b5matx7dhvjv6wu.on.fleek.co/"
+)
+PYDATA_SPHINX_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0.28.0/dist/bootstrapUIMachineOptions.js.es.js"
+MUI_HREF = "https://cdn.jsdelivr.net/npm/itk-viewer-material-ui@0.3.0/dist/materialUIMachineOptions.js.es.js"
