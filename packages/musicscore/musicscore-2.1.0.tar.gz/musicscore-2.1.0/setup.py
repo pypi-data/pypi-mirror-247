8fe029de3f0537e31825f576a994c4e84e6dba25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:23 +0000

    Add package musicscore/2.1.0/musicscore-2.1.0.tar.gz

---
+++ packages/musicscore/musicscore-2.1.0.tar.gz/musicscore-2.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.rst").read_text()
+setuptools.setup(
+    name="musicscore",
+    version="2.1.0",
+    author="Alex Gorji",
+    author_email="aligorji@hotmail.com",
+    description="Generating musicxml files.",
+    url="https://github.com/alexgorji/musicscore.git",
+    packages=setuptools.find_packages(),
+    install_requires=['musicxml==1.4', 'quicktions'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    include_package_data=True
+)
