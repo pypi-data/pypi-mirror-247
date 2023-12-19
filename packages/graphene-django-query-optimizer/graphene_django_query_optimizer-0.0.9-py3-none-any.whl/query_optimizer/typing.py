93b6b754ec3c6b418698f43872812f4391bb5b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:22 +0000

    Add package graphene-django-query-optimizer/0.0.9/graphene_django_query_optimizer-0.0.9-py3-none-any.whl

---
+++ packages/graphene-django-query-optimizer/graphene_django_query_optimizer-0.0.9-py3-none-any.whl/query_optimizer/typing.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Callable, Collection, Hashable, Iterable, NamedTuple, Optional, TypeVar, Union
+
+from graphene.relay.connection import ConnectionOptions
+from graphene_django.types import DjangoObjectTypeOptions
+
+# New in version 3.10
+try:
+    from typing import TypeAlias, TypeGuard
+except ImportError:
+    from typing_extensions import TypeAlias, TypeGuard
+
+
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import (
+    Field,
+    ForeignKey,
+    ForeignObject,
+    ForeignObjectRel,
+    ManyToManyField,
+    ManyToManyRel,
+    ManyToOneRel,
+    Model,
+    OneToOneField,
+)
+from graphql import GraphQLResolveInfo
+
+if TYPE_CHECKING:
+    from django.contrib.auth.models import AnonymousUser, User
+
+__all__ = [
+    "Any",
+    "Callable",
+    "Collection",
+    "GQLInfo",
+    "Hashable",
+    "Iterable",
+    "ModelField",
+    "NamedTuple",
+    "Optional",
+    "PK",
+    "QueryCache",
+    "StoreStr",
+    "TableName",
+    "ToManyField",
+    "ToOneField",
+    "TypeGuard",
+    "TypeOptions",
+    "TypeVar",
+    "Union",
+]
+
+
+TModel = TypeVar("TModel", bound=Model)
+TableName: TypeAlias = str
+StoreStr: TypeAlias = str
+PK: TypeAlias = Any
+QueryCache: TypeAlias = dict[TableName, dict[StoreStr, dict[PK, TModel]]]
+ModelField: TypeAlias = Union[Field, ForeignObjectRel, GenericForeignKey]
+ToManyField: TypeAlias = Union[GenericRelation, ManyToManyField, ManyToOneRel, ManyToManyRel]
+ToOneField: TypeAlias = Union[GenericRelation, ForeignObject, ForeignKey, OneToOneField]
+TypeOptions: TypeAlias = Union[DjangoObjectTypeOptions, ConnectionOptions]
+AnyUser: TypeAlias = Union["User", "AnonymousUser"]
+
+
+class UserHintedWSGIRequest(WSGIRequest):
+    user: AnyUser
+
+
+class GQLInfo(GraphQLResolveInfo):
+    context: UserHintedWSGIRequest
