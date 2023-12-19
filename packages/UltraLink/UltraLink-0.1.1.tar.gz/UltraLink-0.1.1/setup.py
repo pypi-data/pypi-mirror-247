101c66b7ff330c413461be77ff0a0fc269bd0687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:22 +0000

    Add package UltraLink/0.1.1/UltraLink-0.1.1.tar.gz

---
+++ packages/UltraLink/UltraLink-0.1.1.tar.gz/UltraLink-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UltraLink',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    author='Noah Wilhoite',
+    author_email='notnoah349@gmail.com',
+    description='A container for my python modules.',
+    long_description="""
+This is a container for my python modules.
+It's whole purpose is to allow me to access my own modules from anywhere without having to make a whole now package on PyPi.
+""",
+    long_description_content_type='text/markdown',
+)
