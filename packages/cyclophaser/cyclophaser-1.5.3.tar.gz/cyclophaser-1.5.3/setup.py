81cf50579c2898d71fa019c6e678d6761c621bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:05 +0000

    Add package cyclophaser/1.5.3/cyclophaser-1.5.3.tar.gz

---
+++ packages/cyclophaser/cyclophaser-1.5.3.tar.gz/cyclophaser-1.5.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.5.3'
+DESCRIPTION = 'Determine phases from extratropical cyclone life cycle'
+LONG_DESCRIPTION = 'This script processes vorticity data, identifies different phases of the cyclone \
+    and plots the identified periods on periods.png and periods_didatic.png'
+
+setup(
+    name="cyclophaser",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Danilo Couto de Souza",
+    author_email="danilo.oceano@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['cyclone', 'vorticity', 'meteorology', 'atmospherical sciences'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
