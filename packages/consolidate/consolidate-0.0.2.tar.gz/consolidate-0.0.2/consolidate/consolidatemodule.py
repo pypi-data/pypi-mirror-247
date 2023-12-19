7c54d140d1e30f6f9d3d0f965a4842ea87bfcd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:38 +0000

    Add package consolidate/0.0.2/consolidate-0.0.2.tar.gz

---
+++ packages/consolidate/consolidate-0.0.2.tar.gz/consolidate-0.0.2/consolidate/consolidatemodule.py
@@ -0,0 +1,15 @@
+def consolidate(base, fill, len):
+    output = []
+    
+    """
+    Generate a list of possible combinations where .
+
+    :param str baseString: String to put fillerString within
+    :param str fillerString : String to put within baseString
+    :param int maxLength: Max length of result string
+    :return: List of combinations
+    :rtype: list
+    """
+    # maybe add a "must match max length" bool as an input if user requires exact length, no less
+    
+print(consolidate(1,2,3))
