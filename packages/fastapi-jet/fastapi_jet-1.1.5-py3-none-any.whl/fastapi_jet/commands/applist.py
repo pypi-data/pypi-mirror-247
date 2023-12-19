d07ec2504a70316b1b67eeba4a7e5b9449eda687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 06:03:36 +0000

    Add package fastapi-jet/1.1.5/fastapi_jet-1.1.5-py3-none-any.whl

---
+++ packages/fastapi-jet/fastapi_jet-1.1.5-py3-none-any.whl/fastapi_jet/commands/applist.py
@@ -0,0 +1,70 @@
+import sys
+
+import tabulate
+import typer
+
+from fastapi_jet.cli import app
+from fastapi_jet.constants import PROJECT_ROOT
+from fastapi_jet.decorators import fastapi_project
+
+sys.path.append(PROJECT_ROOT)
+
+
+@app.command(name="apps")
+@fastapi_project
+def app_list():
+    """
+    Show all apps
+    """
+
+    main_core = __import__("base.main", fromlist=["main"])
+    apps_table = []
+    for route in main_core.INSTALLED_APPS:
+        apps_table += [
+            [route['name'], f"apps/{route['name']}/router.py"],
+        ]
+    table = tabulate.tabulate(apps_table, headers=["App Name", "AppRoute Path"], tablefmt="rounded_outline")
+    print(table)
+    print('[!] If you cant see your app in the list, make sure you have added it to ROUTERS in app/main.py')
+
+
+@app.command(name="routes")
+def routes_list(
+        app_name: str = typer.Option("all", "--app", "-a", help="Name of the app to list routes for"),
+):
+    """
+    Show routes for all apps or a specific app
+    """
+
+    main_core = __import__("base.main", fromlist=["main"])
+    main_routes = []
+    for _route in main_core.INSTALLED_APPS:
+        if app_name != "all" and _route['name'] != app_name:
+            continue
+        imported_app = __import__(f"apps.{_route['name']}.routers", fromlist=["router"])
+        routes_table = []
+        for route in imported_app.router.routes:
+            routes_table.append([
+                _route['name'],
+                route.path,
+                ",".join(route.methods),
+                route.name,
+                ", ".join([
+                    x.name for x in route.dependant.path_params
+                ])
+            ])
+        main_routes += routes_table
+
+    table = tabulate.tabulate(
+        main_routes,
+        headers=[
+            "App Name",
+            "Path",
+            "Methods",
+            "Name",
+            "Parameters"
+        ],
+        tablefmt="rounded_outline",
+
+    )
+    print(table)
---
+++ packages/fastapi-jet/fastapi_jet-1.1.5-py3-none-any.whl/fastapi_jet/tools.py
@@ -0,0 +1,48 @@
+from typing import List, Tuple
+
+from fastapi import FastAPI
+
+from fastapi_jet.context import AppRoute
+
+
+def include_routers(fast_api_app: FastAPI, installed_apps: List[AppRoute], apps_path: str = "apps") -> None:
+    """
+    This function is used to include routers from the ROUTERS list in the main file.
+
+    :param fast_api_app: The FastAPI application where the routers will be included.
+    :type fast_api_app: FastAPI
+    :param installed_apps: A list of AppRoute objects, where each object contains the name of an app
+    :type installed_apps: list
+    :param apps_path: The path to the apps' folder. Defaults to "apps".
+    :type apps_path: str, optional
+    :return: None
+    """
+    for route in installed_apps:
+        apps_path = apps_path.replace("/", ".")
+        route_module = f"{route['name']}.routers" if apps_path == '.' else f"{apps_path}.{route['name']}.routers"
+        _route = __import__(route_module, fromlist=["router"])
+        route = route.copy()
+        route.pop('name')
+        fast_api_app.include_router(_route.router, **route)
+
+
+def include_middlewares(fast_api_app: FastAPI, middlewares: List[Tuple[str, dict]]) -> None:
+    """
+    This function is used to include middlewares into the FastAPI application.
+
+    :param fast_api_app: The FastAPI application where the middlewares will be included.
+    :type fast_api_app: FastAPI
+    :param middlewares: A list of tuples, where each tuple contains the name of a middleware
+        and its corresponding configuration.
+    :type middlewares: list
+    :return: None
+    """
+    for middleware_name, middleware_config in middlewares:
+        _middleware_class = middleware_name.split(".")[-1]
+        _middleware_module = middleware_name.replace(f".{_middleware_class}", "")
+        _middleware = __import__(
+            _middleware_module, fromlist=[_middleware_class]
+        ).__dict__[_middleware_class]
+
+        # Add the middleware to the FastAPI application.
+        fast_api_app.add_middleware(_middleware, **middleware_config)
