901377e6a6b4b58cd4c49bc6b66cfe2936143221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:04 +0000

    Add package pyams-portal/2.1.1/pyams_portal-2.1.1.tar.gz

---
+++ packages/pyams-portal/pyams_portal-2.1.1.tar.gz/pyams_portal-2.1.1/src/pyams_portal/portlets/carousel/__init__.py
@@ -0,0 +1,109 @@
+#
+# Copyright (c) 2015-2021 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_portal.portrlets.carousel module
+
+"""
+
+from persistent import Persistent
+from zope.container.contained import Contained
+from zope.container.ordered import OrderedContainer
+from zope.interface import Interface, alsoProvides
+from zope.schema.fieldproperty import FieldProperty
+
+from pyams_file.interfaces import IImageFile, IResponsiveImage
+from pyams_file.property import FileProperty
+from pyams_i18n.interfaces import II18n
+from pyams_layer.interfaces import IPyAMSLayer
+from pyams_portal.interfaces import MANAGE_TEMPLATE_PERMISSION
+from pyams_portal.portlet import Portlet, PortletSettings, portlet_config
+from pyams_portal.portlets.carousel.interfaces import ICarouselImage, ICarouselPortletSettings
+from pyams_security.interfaces import IViewContextPermissionChecker
+from pyams_utils.adapter import ContextAdapter, adapter_config
+from pyams_utils.container import SimpleContainerMixin
+from pyams_utils.factory import factory_config
+from pyams_zmi.interfaces import IObjectLabel
+
+
+__docformat__ = 'restructuredtext'
+
+from pyams_portal import _  # pylint: disable=ungrouped-imports
+from pyams_zmi.utils import get_object_label
+
+CAROUSEL_PORTLET_NAME = 'pyams_portal.portlet.carousel'
+
+
+@factory_config(provided=ICarouselImage)
+class CarouselImage(Persistent, Contained):
+    """Carousel image"""
+
+    visible = FieldProperty(ICarouselImage['visible'])
+    title = FieldProperty(ICarouselImage['title'])
+    lead = FieldProperty(ICarouselImage['lead'])
+    _illustration = FileProperty(ICarouselImage['illustration'])
+    interval = FieldProperty(ICarouselImage['interval'])
+
+    @property
+    def illustration(self):
+        """Illustration getter"""
+        return self._illustration
+
+    @illustration.setter
+    def illustration(self, value):
+        """Illustration setter"""
+        self._illustration = value
+        if IImageFile.providedBy(self._illustration):
+            alsoProvides(self._illustration, IResponsiveImage)
+
+    @illustration.deleter
+    def illustration(self):
+        """Illustration deleter"""
+        del self._illustration
+
+
+@adapter_config(required=(ICarouselImage, IPyAMSLayer, Interface),
+                provides=IObjectLabel)
+def carousel_image_label(context, request, view):  # pylint: disable=unused-argument
+    """Carousel image label getter"""
+    label = II18n(context).query_attribute('title', request=request)
+    if not label:
+        label = get_object_label(context.illustration, request, view)
+    return label
+
+
+@adapter_config(required=ICarouselImage, provides=IViewContextPermissionChecker)
+class CarouselImagePermissionChecker(ContextAdapter):
+    """Carousel image permission checker"""
+
+    edit_permission = MANAGE_TEMPLATE_PERMISSION
+
+
+@factory_config(provided=ICarouselPortletSettings)
+class CarouselPortletSettings(SimpleContainerMixin, OrderedContainer, PortletSettings):
+    """Carousel portlet settings"""
+
+    title = FieldProperty(ICarouselPortletSettings['title'])
+
+    def get_visible_items(self):
+        """Visible items getter"""
+        yield from filter(lambda x: x.visible, self.values())
+
+
+@portlet_config(permission=None)
+class CarouselPortlet(Portlet):
+    """Carousel portlet"""
+
+    name = CAROUSEL_PORTLET_NAME
+    label = _("Bootstrap: carousel")
+
+    settings_factory = ICarouselPortletSettings
+    toolbar_css_class = 'far fa-images'
---
+++ packages/pyams-portal/pyams_portal-2.1.1.tar.gz/pyams_portal-2.1.1/src/pyams_portal/zmi/widget.py
@@ -0,0 +1,47 @@
+#
+# Copyright (c) 2015-2021 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_portal.zmi.widget module
+
+This module defines a custom widget used to select portlet renderer, which
+provides access to custom renderer settings, if any.
+"""
+
+from pyams_form.browser.select import SelectWidget
+from pyams_form.interfaces import INPUT_MODE
+from pyams_form.template import widget_template_config
+from pyams_form.widget import FieldWidget
+from pyams_zmi.interfaces import IAdminLayer
+
+__docformat__ = 'restructuredtext'
+
+from pyams_portal import _
+
+
+@widget_template_config(mode=INPUT_MODE,
+                        template='templates/renderer-input.pt',
+                        layer=IAdminLayer)
+class RendererSelectWidget(SelectWidget):
+    """Portlet renderer widget"""
+
+    no_value_message = _("No selected renderer (use default)")
+
+    @property
+    def show_renderer_properties(self):
+        """Getter to check access to renderer properties action"""
+        renderer = self.context.get_renderer(self.request)
+        return (renderer is not None) and (renderer.settings_interface is not None)
+
+
+def RendererSelectFieldWidget(field, request):  # pylint: disable=invalid-name
+    """Portlet renderer field widget"""
+    return FieldWidget(field, RendererSelectWidget(request))
