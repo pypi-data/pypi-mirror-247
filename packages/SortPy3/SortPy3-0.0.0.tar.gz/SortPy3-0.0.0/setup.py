8c54c919996fd17e88bdce96dfb873cc6436d97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:29 +0000

    Add package SortPy3/0.0.0/SortPy3-0.0.0.tar.gz

---
+++ packages/SortPy3/SortPy3-0.0.0.tar.gz/SortPy3-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SortPy3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Ashhad Ahmed',
+    author_email='ashhadahmed776@gmail.com',
+    description='A Python package providing efficient sorting algorithms like Bubble Sort, Insertion Sort, Merge Sort, Selection Sort, Heap Sort, and Quick Sort.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ashhad776/SortPy3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
