2e355776e63d229405d5c40482597926028b21f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:04:30 +0000

    Add package first-distrib/0.1.1/first_distrib-0.1.1.tar.gz

---
+++ packages/first-distrib/first_distrib-0.1.1.tar.gz/first_distrib-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='first_distrib',
+      version='0.1.1',
+      description='Gaussian distributions',
+      packages=['first_distrib'],
+      author = 'Taufikur Rahman',
+      author_email = 'taufiktrf@gmail.com',
+      zip_safe=False)
