3d7df3d770f98aa0265aaaa694b12f8edb6ed410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:38 +0000

    Add package frds/2.4.0/frds-2.4.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/frds/frds-2.4.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/frds/measures/__init__.py
@@ -0,0 +1,38 @@
+"""
+The :mod:`frds.measures` module includes the collection of measures.
+For a complete documentation, see :doc:`/measures/index`.
+"""
+
+from ._absorption_ratio import AbsorptionRatio
+from ._contingent_claim_analysis import ContingentClaimAnalysis
+from ._distress_insurance_premium import DistressInsurancePremium
+from ._lerner_index import LernerIndex
+from ._long_run_mes import LongRunMarginalExpectedShortfall
+from ._marginal_expected_shortfall import MarginalExpectedShortfall
+from ._probability_of_informed_trading import PIN
+from ._srisk import SRISK
+from ._systemic_expected_shortfall import SystemicExpectedShortfall
+
+from ._kyle_lambda import kyle_lambda
+from ._option_price import blsprice
+from ._z_score import z_score
+
+LRMES = LongRunMarginalExpectedShortfall
+
+__all__ = [
+    # classes
+    "AbsorptionRatio",
+    "ContingentClaimAnalysis",
+    "DistressInsurancePremium",
+    "LernerIndex",
+    "LongRunMarginalExpectedShortfall",
+    "LRMES",
+    "MarginalExpectedShortfall",
+    "PIN",
+    "SRISK",
+    "SystemicExpectedShortfall",
+    # functions
+    "blsprice",
+    "kyle_lambda",
+    "z_score",
+]
