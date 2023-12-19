8488def9ba2f0fc797cc7f88f8dfd14af715b472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:36 +0000

    Add package azure-kusto-ingest/4.3.1/azure-kusto-ingest-4.3.1.tar.gz

---
+++ packages/azure-kusto-ingest/azure-kusto-ingest-4.3.1.tar.gz/azure-kusto-ingest-4.3.1/azure/kusto/ingest/ingest_client.py
@@ -0,0 +1,237 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License
+import random
+from typing import Union, AnyStr, IO, List, Optional, Dict
+from urllib.parse import urlparse
+
+from azure.storage.blob import BlobServiceClient
+
+from azure.core.tracing.decorator import distributed_trace
+from azure.core.tracing import SpanKind
+from azure.storage.queue import QueueServiceClient, TextBase64EncodePolicy
+
+from azure.kusto.data import KustoClient, KustoConnectionStringBuilder
+from azure.kusto.data._telemetry import MonitoredActivity
+from azure.kusto.data.exceptions import KustoClosedError, KustoServiceError
+
+from ._ingest_telemetry import IngestTracingAttributes
+from ._resource_manager import _ResourceManager, _ResourceUri
+from .base_ingest_client import BaseIngestClient, IngestionResult, IngestionStatus
+from .descriptors import BlobDescriptor, FileDescriptor, StreamDescriptor
+from .exceptions import KustoInvalidEndpointError, KustoQueueError
+from azure.kusto.data.exceptions import KustoBlobError
+from .ingestion_blob_info import IngestionBlobInfo
+from .ingestion_properties import IngestionProperties
+
+
+class QueuedIngestClient(BaseIngestClient):
+    """
+    Queued ingest client provides methods to allow queued ingestion into kusto (ADX).
+    To learn more about the different types of ingestions and when to use each, visit:
+    https://docs.microsoft.com/en-us/azure/data-explorer/ingest-data-overview#ingestion-methods
+    """
+
+    _INGEST_PREFIX = "ingest-"
+    _EXPECTED_SERVICE_TYPE = "DataManagement"
+    _SERVICE_CLIENT_TIMEOUT_SECONDS = 10 * 60
+    _MAX_RETRIES = 3
+
+    def __init__(self, kcsb: Union[str, KustoConnectionStringBuilder]):
+        """Kusto Ingest Client constructor.
+        :param kcsb: The connection string to initialize KustoClient.
+        """
+        super().__init__()
+        if not isinstance(kcsb, KustoConnectionStringBuilder):
+            kcsb = KustoConnectionStringBuilder(kcsb)
+        self._proxy_dict: Optional[Dict[str, str]] = None
+        self._connection_datasource = kcsb.data_source
+        self._resource_manager = _ResourceManager(KustoClient(kcsb))
+        self._endpoint_service_type = None
+        self._suggested_endpoint_uri = None
+
+    def close(self) -> None:
+        self._resource_manager.close()
+        super().close()
+
+    def set_proxy(self, proxy_url: str):
+        self._resource_manager.set_proxy(proxy_url)
+        self._proxy_dict = {"http": proxy_url, "https": proxy_url}
+
+    @distributed_trace(name_of_span="QueuedIngestClient.ingest_from_file", kind=SpanKind.CLIENT)
+    def ingest_from_file(self, file_descriptor: Union[FileDescriptor, str], ingestion_properties: IngestionProperties) -> IngestionResult:
+        """Enqueue an ingest command from local files.
+        To learn more about ingestion methods go to:
+        https://docs.microsoft.com/en-us/azure/data-explorer/ingest-data-overview#ingestion-methods
+        :param file_descriptor: a FileDescriptor to be ingested.
+        :param azure.kusto.ingest.IngestionProperties ingestion_properties: Ingestion properties.
+        """
+        file_descriptor = FileDescriptor.get_instance(file_descriptor)
+        IngestTracingAttributes.set_ingest_descriptor_attributes(file_descriptor, ingestion_properties)
+
+        super().ingest_from_file(file_descriptor, ingestion_properties)
+
+        containers = self._get_containers()
+
+        file_descriptor, should_compress = BaseIngestClient._prepare_file(file_descriptor, ingestion_properties)
+        with file_descriptor.open(should_compress) as stream:
+            blob_descriptor = self.upload_blob(
+                containers,
+                file_descriptor,
+                ingestion_properties.database,
+                ingestion_properties.table,
+                stream,
+                self._proxy_dict,
+                self._SERVICE_CLIENT_TIMEOUT_SECONDS,
+                self._MAX_RETRIES,
+            )
+        return self.ingest_from_blob(blob_descriptor, ingestion_properties=ingestion_properties)
+
+    @distributed_trace(name_of_span="QueuedIngestClient.ingest_from_stream", kind=SpanKind.CLIENT)
+    def ingest_from_stream(self, stream_descriptor: Union[StreamDescriptor, IO[AnyStr]], ingestion_properties: IngestionProperties) -> IngestionResult:
+        """Ingest from io streams.
+        :param stream_descriptor: An object that contains a description of the stream to be ingested.
+        :param azure.kusto.ingest.IngestionProperties ingestion_properties: Ingestion properties.
+        """
+        stream_descriptor = StreamDescriptor.get_instance(stream_descriptor)
+        IngestTracingAttributes.set_ingest_descriptor_attributes(stream_descriptor, ingestion_properties)
+
+        super().ingest_from_stream(stream_descriptor, ingestion_properties)
+
+        containers = self._get_containers()
+
+        stream_descriptor = BaseIngestClient._prepare_stream(stream_descriptor, ingestion_properties)
+        blob_descriptor = self.upload_blob(
+            containers,
+            stream_descriptor,
+            ingestion_properties.database,
+            ingestion_properties.table,
+            stream_descriptor.stream,
+            self._proxy_dict,
+            self._SERVICE_CLIENT_TIMEOUT_SECONDS,
+            self._MAX_RETRIES,
+        )
+        return self.ingest_from_blob(blob_descriptor, ingestion_properties=ingestion_properties)
+
+    @distributed_trace(name_of_span="QueuedIngestClient.ingest_from_blob", kind=SpanKind.CLIENT)
+    def ingest_from_blob(self, blob_descriptor: BlobDescriptor, ingestion_properties: IngestionProperties) -> IngestionResult:
+        """Enqueue an ingest command from azure blobs.
+        To learn more about ingestion methods go to:
+        https://docs.microsoft.com/en-us/azure/data-explorer/ingest-data-overview#ingestion-methods
+        :param azure.kusto.ingest.BlobDescriptor blob_descriptor: An object that contains a description of the blob to be ingested.
+        :param azure.kusto.ingest.IngestionProperties ingestion_properties: Ingestion properties.
+        """
+        IngestTracingAttributes.set_ingest_descriptor_attributes(blob_descriptor, ingestion_properties)
+
+        if self._is_closed:
+            raise KustoClosedError()
+
+        try:
+            queues = self._resource_manager.get_ingestion_queues()
+        except KustoServiceError as ex:
+            self._validate_endpoint_service_type()
+            raise ex
+
+        authorization_context = self._resource_manager.get_authorization_context()
+        ingestion_blob_info = IngestionBlobInfo(blob_descriptor, ingestion_properties=ingestion_properties, auth_context=authorization_context)
+        ingestion_blob_info_json = ingestion_blob_info.to_json()
+        retries_left = min(self._MAX_RETRIES, len(queues))
+        for queue in queues:
+            try:
+                with QueueServiceClient(queue.account_uri, proxies=self._proxy_dict) as queue_service:
+                    with queue_service.get_queue_client(queue=queue.object_name, message_encode_policy=TextBase64EncodePolicy()) as queue_client:
+                        # trace enqueuing of blob for ingestion
+                        invoker = lambda: queue_client.send_message(content=ingestion_blob_info_json, timeout=self._SERVICE_CLIENT_TIMEOUT_SECONDS)
+                        enqueue_trace_attributes = IngestTracingAttributes.create_enqueue_request_attributes(queue_client.queue_name, blob_descriptor.source_id)
+                        MonitoredActivity.invoke(invoker, name_of_span="QueuedIngestClient.enqueue_request", tracing_attributes=enqueue_trace_attributes)
+
+                self._resource_manager.report_resource_usage_result(queue.storage_account_name, True)
+                return IngestionResult(
+                    IngestionStatus.QUEUED, ingestion_properties.database, ingestion_properties.table, blob_descriptor.source_id, blob_descriptor.path
+                )
+            except Exception as e:
+                retries_left = retries_left - 1
+                # TODO: log the retry once we have a proper logging system
+                self._resource_manager.report_resource_usage_result(queue.storage_account_name, False)
+                if retries_left == 0:
+                    raise KustoQueueError() from e
+
+    def _get_containers(self) -> List[_ResourceUri]:
+        try:
+            containers = self._resource_manager.get_containers()
+        except KustoServiceError as ex:
+            self._validate_endpoint_service_type()
+            raise ex
+        return containers
+
+    def _validate_endpoint_service_type(self):
+        if not self._hostname_starts_with_ingest(self._connection_datasource):
+            if not self._endpoint_service_type:
+                self._endpoint_service_type = self._retrieve_service_type()
+
+            if self._EXPECTED_SERVICE_TYPE != self._endpoint_service_type:
+                if not self._suggested_endpoint_uri:
+                    self._suggested_endpoint_uri = self._generate_endpoint_suggestion(self._connection_datasource)
+                    if not self._suggested_endpoint_uri:
+                        raise KustoInvalidEndpointError(self._EXPECTED_SERVICE_TYPE, self._endpoint_service_type)
+                raise KustoInvalidEndpointError(self._EXPECTED_SERVICE_TYPE, self._endpoint_service_type, self._suggested_endpoint_uri)
+
+    def _retrieve_service_type(self) -> str:
+        return self._resource_manager.retrieve_service_type()
+
+    def _generate_endpoint_suggestion(self, datasource: str) -> Optional[str]:
+        """The default is not passing a suggestion to the exception String"""
+        endpoint_uri_to_suggest_str = None
+        if datasource.strip():
+            try:
+                endpoint_uri_to_suggest = urlparse(datasource)  # Standardize URL formatting
+                endpoint_uri_to_suggest = urlparse(endpoint_uri_to_suggest.scheme + "://" + self._INGEST_PREFIX + endpoint_uri_to_suggest.hostname)
+                endpoint_uri_to_suggest_str = endpoint_uri_to_suggest.geturl()
+            except Exception:
+                # TODO: Add logging infrastructure so we can tell the user as a warning:
+                #   "Couldn't generate suggested endpoint due to problem parsing datasource, with exception: {ex}. The correct endpoint is usually the Engine endpoint with '{self._INGEST_PREFIX}' prepended to the hostname."
+                pass
+        return endpoint_uri_to_suggest_str
+
+    def _hostname_starts_with_ingest(self, datasource: str) -> bool:
+        datasource_uri = urlparse(datasource)
+        hostname = datasource_uri.hostname
+        return hostname and hostname.startswith(self._INGEST_PREFIX)
+
+    def upload_blob(
+        self,
+        containers: List[_ResourceUri],
+        descriptor: Union[FileDescriptor, "StreamDescriptor"],
+        database: str,
+        table: str,
+        stream: IO[AnyStr],
+        proxy_dict: Optional[Dict[str, str]],
+        timeout: int,
+        max_retries: int,
+    ) -> "BlobDescriptor":
+        """
+        Uploads and transforms FileDescriptor or StreamDescriptor into a BlobDescriptor instance
+        :param List[_ResourceUri] containers: blob containers
+        :param Union[FileDescriptor, "StreamDescriptor"] descriptor:
+        :param string database: database to be ingested to
+        :param string table: table to be ingested to
+        :param IO[AnyStr] stream: stream to be ingested from
+        :param Optional[Dict[str, str]] proxy_dict: proxy urls
+        :param int timeout: Azure service call timeout in seconds
+        :return new BlobDescriptor instance
+        """
+        blob_name = "{db}__{table}__{guid}__{file}".format(db=database, table=table, guid=descriptor.source_id, file=descriptor.stream_name)
+
+        retries_left = min(max_retries, len(containers))
+        for container in containers:
+            try:
+                blob_service = BlobServiceClient(container.account_uri, proxies=proxy_dict)
+                blob_client = blob_service.get_blob_client(container=container.object_name, blob=blob_name)
+                blob_client.upload_blob(data=stream, timeout=timeout)
+                self._resource_manager.report_resource_usage_result(container.storage_account_name, True)
+                return BlobDescriptor(blob_client.url, descriptor.size, descriptor.source_id)
+            except Exception as e:
+                retries_left = retries_left - 1
+                # TODO: log the retry once we have a proper logging system
+                self._resource_manager.report_resource_usage_result(container.storage_account_name, False)
+                if retries_left == 0:
+                    raise KustoBlobError(e)
---
+++ packages/azure-kusto-ingest/azure-kusto-ingest-4.3.1.tar.gz/azure-kusto-ingest-4.3.1/azure/kusto/ingest/managed_streaming_ingest_client.py
@@ -0,0 +1,159 @@
+import uuid
+from io import SEEK_SET
+from typing import AnyStr, IO, TYPE_CHECKING, Union
+
+from tenacity import Retrying, _utils, stop_after_attempt, wait_random_exponential
+
+from azure.core.tracing.decorator import distributed_trace
+from azure.core.tracing import SpanKind
+
+from azure.kusto.data import KustoConnectionStringBuilder
+from azure.kusto.data.exceptions import KustoApiError, KustoClosedError
+from azure.kusto.data._telemetry import MonitoredActivity
+
+
+from . import BlobDescriptor, FileDescriptor, IngestionProperties, StreamDescriptor
+from ._ingest_telemetry import IngestTracingAttributes
+from ._stream_extensions import chain_streams, read_until_size_or_end
+from .base_ingest_client import BaseIngestClient, IngestionResult
+from .ingest_client import QueuedIngestClient
+from .streaming_ingest_client import KustoStreamingIngestClient
+
+if TYPE_CHECKING:
+    pass
+
+
+class ManagedStreamingIngestClient(BaseIngestClient):
+    """
+    Managed Streaming Ingestion Client.
+    Will try to ingest with streaming, but if it fails, will fall back to queued ingestion.
+    Each transient failure will be retried with exponential backoff.
+
+    Managed streaming ingest client will fall back to queued if:
+        - Multiple transient errors were encountered when trying to do streaming ingestion
+        - The ingestion is too large for streaming ingestion (over 4MB)
+        - The ingestion is directly from a blob
+    """
+
+    MAX_STREAMING_SIZE_IN_BYTES = 4 * 1024 * 1024
+
+    @staticmethod
+    def from_engine_kcsb(engine_kcsb: Union[KustoConnectionStringBuilder, str]) -> "ManagedStreamingIngestClient":
+        """
+        Create a ManagedStreamingIngestClient from a KustoConnectionStringBuilder for the engine.
+        This Connection String is used for the streaming ingest client.
+        This method will infer the dm connection string (by using the same authentication and adding the ingest- prefix)
+        For advanced use cases, use the constructor directly.
+        :param engine_kcsb: KustoConnectionStringBuilder for the engine.
+        :return: ManagedStreamingIngestClient
+        """
+        kcsb = repr(engine_kcsb) if type(engine_kcsb) == KustoConnectionStringBuilder else engine_kcsb
+        dm_kcsb = KustoConnectionStringBuilder(kcsb.replace("https://", "https://ingest-"))
+        return ManagedStreamingIngestClient(engine_kcsb, dm_kcsb)
+
+    @staticmethod
+    def from_dm_kcsb(dm_kcsb: Union[KustoConnectionStringBuilder, str]) -> "ManagedStreamingIngestClient":
+        """
+        Create a ManagedStreamingIngestClient from a KustoConnectionStringBuilder for the dm.
+        This Connection String is used for the queued ingest client.
+        This method will infer the engine connection string (by using the same authentication and removing the ingest- prefix)
+        For advanced use cases, use the constructor directly.
+        :param dm_kcsb: KustoConnectionStringBuilder for the dm.
+        :return: ManagedStreamingIngestClient
+        """
+        kcsb = repr(dm_kcsb) if type(dm_kcsb) == KustoConnectionStringBuilder else dm_kcsb
+        engine_kcsb = KustoConnectionStringBuilder(kcsb.replace("https://ingest-", "https://"))
+        return ManagedStreamingIngestClient(engine_kcsb, dm_kcsb)
+
+    def __init__(self, engine_kcsb: Union[KustoConnectionStringBuilder, str], dm_kcsb: Union[KustoConnectionStringBuilder, str]):
+        super().__init__()
+        self.queued_client = QueuedIngestClient(dm_kcsb)
+        self.streaming_client = KustoStreamingIngestClient(engine_kcsb)
+        self._set_retry_settings()
+
+    def close(self) -> None:
+        if not self._is_closed:
+            self.queued_client.close()
+            self.streaming_client.close()
+        super().close()
+
+    def _set_retry_settings(self, max_seconds_per_retry: float = _utils.MAX_WAIT, num_of_attempts: int = 3):
+        self._num_of_attempts = num_of_attempts
+        self._max_seconds_per_retry = max_seconds_per_retry
+
+    def set_proxy(self, proxy_url: str):
+        self.queued_client.set_proxy(proxy_url)
+        self.streaming_client.set_proxy(proxy_url)
+
+    @distributed_trace(kind=SpanKind.CLIENT)
+    def ingest_from_file(self, file_descriptor: Union[FileDescriptor, str], ingestion_properties: IngestionProperties) -> IngestionResult:
+        file_descriptor = FileDescriptor.get_instance(file_descriptor)
+        IngestTracingAttributes.set_ingest_descriptor_attributes(file_descriptor, ingestion_properties)
+
+        super().ingest_from_file(file_descriptor, ingestion_properties)
+
+        stream_descriptor = StreamDescriptor.from_file_descriptor(file_descriptor)
+
+        with stream_descriptor.stream:
+            return self.ingest_from_stream(stream_descriptor, ingestion_properties)
+
+    @distributed_trace(kind=SpanKind.CLIENT)
+    def ingest_from_stream(self, stream_descriptor: Union[StreamDescriptor, IO[AnyStr]], ingestion_properties: IngestionProperties) -> IngestionResult:
+        stream_descriptor = StreamDescriptor.get_instance(stream_descriptor)
+        IngestTracingAttributes.set_ingest_descriptor_attributes(stream_descriptor, ingestion_properties)
+
+        super().ingest_from_stream(stream_descriptor, ingestion_properties)
+
+        stream_descriptor = BaseIngestClient._prepare_stream(stream_descriptor, ingestion_properties)
+        stream = stream_descriptor.stream
+
+        buffered_stream = read_until_size_or_end(stream, self.MAX_STREAMING_SIZE_IN_BYTES + 1)
+
+        if len(buffered_stream.getbuffer()) > self.MAX_STREAMING_SIZE_IN_BYTES:
+            stream_descriptor.stream = chain_streams([buffered_stream, stream])
+            return self.queued_client.ingest_from_stream(stream_descriptor, ingestion_properties)
+
+        stream_descriptor.stream = buffered_stream
+
+        try:
+            for attempt in Retrying(
+                stop=stop_after_attempt(self._num_of_attempts), wait=wait_random_exponential(max=self._max_seconds_per_retry), reraise=True
+            ):
+                with attempt:
+                    stream.seek(0, SEEK_SET)
+                    client_request_id = ManagedStreamingIngestClient._get_request_id(stream_descriptor.source_id, attempt.retry_state.attempt_number - 1)
+                    # trace attempt to ingest from stream
+                    invoker = lambda: self.streaming_client._ingest_from_stream_with_client_request_id(
+                        stream_descriptor, ingestion_properties, client_request_id
+                    )
+                    return MonitoredActivity.invoke(invoker, name_of_span="ManagedStreamingIngestClient.ingest_from_stream_attempt")
+
+        except KustoApiError as ex:
+            error = ex.get_api_error()
+            if error.permanent:
+                raise
+
+        return self.queued_client.ingest_from_stream(stream_descriptor, ingestion_properties)
+
+    @distributed_trace(kind=SpanKind.CLIENT)
+    def ingest_from_blob(self, blob_descriptor: BlobDescriptor, ingestion_properties: IngestionProperties):
+        """
+        Enqueue an ingest command from azure blobs.
+
+        For ManagedStreamingIngestClient, this method always uses Queued Ingest, since it would be easier and faster to ingest blobs.
+
+        To learn more about ingestion methods go to:
+        https://docs.microsoft.com/en-us/azure/data-explorer/ingest-data-overview#ingestion-methods
+        :param azure.kusto.ingest.BlobDescriptor blob_descriptor: An object that contains a description of the blob to be ingested.
+        :param azure.kusto.ingest.IngestionProperties ingestion_properties: Ingestion properties.
+        """
+        IngestTracingAttributes.set_ingest_descriptor_attributes(blob_descriptor, ingestion_properties)
+
+        if self._is_closed:
+            raise KustoClosedError()
+
+        return self.queued_client.ingest_from_blob(blob_descriptor, ingestion_properties)
+
+    @staticmethod
+    def _get_request_id(source_id: uuid.UUID, attempt: int):
+        return f"KPC.executeManagedStreamingIngest;{source_id};{attempt}"
