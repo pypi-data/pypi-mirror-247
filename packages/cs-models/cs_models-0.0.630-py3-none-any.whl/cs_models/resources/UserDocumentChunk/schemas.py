2e8fd00be7ebd6f07689d6f2d56f1a25e6f12bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:15 +0000

    Add package cs-models/0.0.630/cs_models-0.0.630-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.630-py3-none-any.whl/cs_models/resources/UserDocumentChunk/schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class UserDocumentChunkResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    user_document_id = fields.Integer(required=True)
+    chunk_cui = fields.String(required=True)
+    chunk_text = fields.String(required=True)
+    chunk_embedding = fields.String(required=True)
+    embedding_source = fields.String(required=True)
+    updated_at = fields.DateTime(dump_only=True)
