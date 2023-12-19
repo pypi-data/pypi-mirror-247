68bd672149ac2f76e7f658d03b19b0eb12f719a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:38 +0000

    Add package tracker-client/0.1/tracker-client-0.1.tar.gz

---
+++ packages/tracker-client/tracker-client-0.1.tar.gz/tracker-client-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tracker-client',
+      version='0.1',
+      description='Client for connecting the tracker to the TrackerContest application',
+      packages=['tracker-client'],
+      author_email='genimotion@ya.ru',
+      zip_safe=False)
