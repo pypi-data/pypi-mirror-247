d6d6ef8a5cc5e8de89f669abc6baf0bb95bb0a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:34 +0000

    Add package fluxy/0.2.1/fluxy-0.2.1.tar.gz

---
+++ packages/fluxy/fluxy-0.2.1.tar.gz/fluxy-0.2.1/fluxy/fluxy_fastapi.py
@@ -0,0 +1,27 @@
+import click
+import os
+import stat
+
+@click.command()
+@click.argument('project_name', metavar='PROJECT_NAME')
+def startproject(project_name):
+    os.mkdir(project_name)
+
+    os.chdir(project_name)
+
+    with open("setup.py", "w") as setup_file:
+        setup_file.write("# Ваш код для setup.py")
+
+    with open("main.py", "w") as main_file:
+        main_file.write("# Ваш код для main.py")
+
+    os.system("xdg-open setup.py")
+    os.system("xdg-open main.py")
+
+    with open("fluxy", "w"):
+        pass
+
+    os.chmod("fluxy", os.stat("fluxy").st_mode | stat.S_IEXEC)
+
+if __name__ == '__main__':
+    startproject()
