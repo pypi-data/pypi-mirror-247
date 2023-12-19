0f58e5daa24d82c3b0f98673e908c25bd8458784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:02:55 +0000

    Add package air-sdk/2.5.0/air_sdk-2.5.0.tar.gz

---
+++ packages/air-sdk/air_sdk-2.5.0.tar.gz/air_sdk-2.5.0/air_sdk/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+""" Exposes the AIR API client module """
+from .air_api import *  # noqa: F403
