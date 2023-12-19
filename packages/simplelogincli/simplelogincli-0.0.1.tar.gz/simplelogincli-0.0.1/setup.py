b865329cff2ebf6203035c1ccb7945d1236ecd26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:34 +0000

    Add package simplelogincli/0.0.1/simplelogincli-0.0.1.tar.gz

---
+++ packages/simplelogincli/simplelogincli-0.0.1.tar.gz/simplelogincli-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="simplelogincli",
+    version="0.0.1",
+    description="A simple CLI for SimpleLogin",
+    author="drk1rd",
+    url="https://github.com/drk1rd/slcli",
+    packages=["slcli"],
+    scripts=["bin/slcli"],
+    install_requires=["simplelogin"],
+)
