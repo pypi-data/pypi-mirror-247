2c29d29edf4ea2b7568de69affdda789517a2167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:02:58 +0000

    Add package feffery-utils-components/0.2.0b12/feffery_utils_components-0.2.0b12-py3-none-any.whl

---
+++ packages/feffery-utils-components/feffery_utils_components-0.2.0b12-py3-none-any.whl/feffery_utils_components/alias.py
@@ -0,0 +1,153 @@
+from .FefferyAutoAnimate import AutoAnimate
+from .FefferyMotion import Motion
+from .FefferyBlockColorPicker import BlockColorPicker
+from .FefferyCircleColorPicker import CircleColorPicker
+from .FefferyEyeDropper import EyeDropper
+from .FefferyGithubColorPicker import GithubColorPicker
+from .FefferyHexColorPicker import HexColorPicker
+from .FefferyRgbColorPicker import RgbColorPicker
+from .FefferyTwitterColorPicker import TwitterColorPicker
+from .FefferyWheelColorPicker import WheelColorPicker
+from .FefferyGrid import Grid
+from .FefferyGridItem import GridItem
+from .FefferyCaptcha import Captcha
+from .FefferyCompareSlider import CompareSlider
+from .FefferyCountDown import CountDown
+from .FefferyCountUp import CountUp
+from .FefferyCssVar import CssVar
+from .FefferyDiv import Div
+from .FefferyEmojiPicker import EmojiPicker
+from .FefferyExecuteJs import ExecuteJs
+from .FefferyExternalCss import ExternalCss
+from .FefferyExternalJs import ExternalJs
+from .FefferyExtraSpinner import ExtraSpinner
+from .FefferyFancyButton import FancyButton
+from .FefferyFancyMessage import FancyMessage
+from .FefferyFancyNotification import FancyNotification
+from .FefferyFullscreen import Fullscreen
+from .FefferyGuide import Guide
+from .FefferyHighlightWords import HighlightWords
+from .FefferyImagePaste import ImagePaste
+from .FefferyJsonViewer import JsonViewer
+from .FefferyLazyLoad import LazyLoad
+from .FefferyQRCode import QRCode
+from .FefferyRawHTML import RawHTML
+from .FefferyReload import Reload
+from .FefferyScroll import Scroll
+from .FefferyScrollbars import Scrollbars
+from .FefferySetTitle import SetTitle
+from .FefferyShadowDom import ShadowDom
+from .FefferyShortcutPanel import ShortcutPanel
+from .FefferySticky import Sticky
+from .FefferyStyle import Style
+from .FefferyTabMessenger import TabMessenger
+from .FefferyTimeout import Timeout
+from .FefferyTopProgress import TopProgress
+from .FefferyVirtualList import VirtualList
+from .FefferyWebSocket import WebSocket
+from .FefferyDeviceDetect import DeviceDetect
+from .FefferyDocumentVisibility import DocumentVisibility
+from .FefferyGeolocation import Geolocation
+from .FefferyIdle import Idle
+from .FefferyInViewport import InViewport
+from .FefferyKeyPress import KeyPress
+from .FefferyListenPaste import ListenPaste
+from .FefferyListenScroll import ListenScroll
+from .FefferyListenUnload import ListenUnload
+from .FefferyLocation import Location
+from .FefferyLongPress import LongPress
+from .FefferyMousePosition import MousePosition
+from .FefferyResponsive import Responsive
+from .FefferyTextSelection import TextSelection
+from .FefferyWindowSize import WindowSize
+from .FefferyAPlayer import APlayer
+from .FefferyDPlayer import DPlayer
+from .FefferyMusicPlayer import MusicPlayer
+from .FefferyResizable import Resizable
+from .FefferySortableList import SortableList
+from .FefferySplit import Split
+from .FefferySplitPane import SplitPane
+from .FefferyCookie import Cookie
+from .FefferyDebounceProp import DebounceProp
+from .FefferyLocalLargeStorage import LocalLargeStorage
+from .FefferyLocalStorage import LocalStorage
+from .FefferySessionStorage import SessionStorage
+from .FefferyThrottleProp import ThrottleProp
+
+__all__ = [
+    "AutoAnimate",
+    "Motion",
+    "BlockColorPicker",
+    "CircleColorPicker",
+    "EyeDropper",
+    "GithubColorPicker",
+    "HexColorPicker",
+    "RgbColorPicker",
+    "TwitterColorPicker",
+    "WheelColorPicker",
+    "Grid",
+    "GridItem",
+    "Captcha",
+    "CompareSlider",
+    "CountDown",
+    "CountUp",
+    "CssVar",
+    "Div",
+    "EmojiPicker",
+    "ExecuteJs",
+    "ExternalCss",
+    "ExternalJs",
+    "ExtraSpinner",
+    "FancyButton",
+    "FancyMessage",
+    "FancyNotification",
+    "Fullscreen",
+    "Guide",
+    "HighlightWords",
+    "ImagePaste",
+    "JsonViewer",
+    "LazyLoad",
+    "QRCode",
+    "RawHTML",
+    "Reload",
+    "Scroll",
+    "Scrollbars",
+    "SetTitle",
+    "ShadowDom",
+    "ShortcutPanel",
+    "Sticky",
+    "Style",
+    "TabMessenger",
+    "Timeout",
+    "TopProgress",
+    "VirtualList",
+    "WebSocket",
+    "DeviceDetect",
+    "DocumentVisibility",
+    "Geolocation",
+    "Idle",
+    "InViewport",
+    "KeyPress",
+    "ListenPaste",
+    "ListenScroll",
+    "ListenUnload",
+    "Location",
+    "LongPress",
+    "MousePosition",
+    "Responsive",
+    "TextSelection",
+    "WindowSize",
+    "APlayer",
+    "DPlayer",
+    "MusicPlayer",
+    "Resizable",
+    "SortableList",
+    "Split",
+    "SplitPane",
+    "Cookie",
+    "DebounceProp",
+    "LocalLargeStorage",
+    "LocalStorage",
+    "SessionStorage",
+    "ThrottleProp"
+]
