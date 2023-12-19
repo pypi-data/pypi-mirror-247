e557f0a8577d95cb9ac513cb0f9a5480e6a56aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 16:04:12 +0000

    Add package schd/0.0.6/schd-0.0.6-py3-none-any.whl

---
+++ packages/schd/schd-0.0.6-py3-none-any.whl/schd/cmds/schd.py
@@ -0,0 +1,37 @@
+import argparse
+import sys
+from schd.scheduler import main as scheduler_main
+from .daemon import DaemonCommand
+from .run import RunCommand
+from schd import __version__ as schd_version
+
+commands = {
+    'daemon': DaemonCommand(),
+    'run': RunCommand(),
+}
+
+def main():
+    sys.path.append('.')
+    parser = argparse.ArgumentParser('schd')
+    parser.add_argument('--version', action='store_true', default=False)
+    sub_command_parsers = parser.add_subparsers(dest='cmd', required=False)
+
+    for cmd, cmd_obj in commands.items():
+        sub_command_parser = sub_command_parsers.add_parser(cmd)
+        cmd_obj.add_arguments(sub_command_parser)
+
+    args = parser.parse_args()
+
+    if args.version:
+        print('schd version ', schd_version)
+        return
+    
+    if not args.cmd:
+        parser.print_help()
+        return
+    
+    commands[args.cmd].run(args)
+
+
+if __name__ == '__main__':
+    main()
