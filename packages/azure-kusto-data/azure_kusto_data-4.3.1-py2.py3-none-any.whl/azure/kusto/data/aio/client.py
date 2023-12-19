d61533be648feb9d8249af612ea109f9f4982df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:36 +0000

    Add package azure-kusto-data/4.3.1/azure_kusto_data-4.3.1-py2.py3-none-any.whl

---
+++ packages/azure-kusto-data/azure_kusto_data-4.3.1-py2.py3-none-any.whl/azure/kusto/data/aio/client.py
@@ -0,0 +1,190 @@
+import io
+from datetime import timedelta
+from typing import Optional, Union
+
+from azure.core.tracing import SpanKind
+from azure.core.tracing.decorator_async import distributed_trace_async
+
+from .response import KustoStreamingResponseDataSet
+from .._decorators import aio_documented_by, documented_by
+from .._telemetry import MonitoredActivity, Span
+from ..aio.streaming_response import JsonTokenReader, StreamingDataSetEnumerator
+from ..client import KustoClient as KustoClientSync
+from ..client_base import ExecuteRequestParams, _KustoClientBase
+from ..client_request_properties import ClientRequestProperties
+from ..data_format import DataFormat
+from ..exceptions import KustoAioSyntaxError, KustoClosedError, KustoNetworkError
+from ..kcsb import KustoConnectionStringBuilder
+from ..response import KustoResponseDataSet
+
+try:
+    from aiohttp import ClientResponse, ClientSession
+except ImportError:
+    raise KustoAioSyntaxError()
+
+
+@documented_by(KustoClientSync)
+class KustoClient(_KustoClientBase):
+    @documented_by(KustoClientSync.__init__)
+    def __init__(self, kcsb: Union[KustoConnectionStringBuilder, str]):
+        super().__init__(kcsb, True)
+
+        self._session = ClientSession()
+
+    async def __aenter__(self) -> "KustoClient":
+        return self
+
+    async def close(self):
+        if not self._is_closed:
+            await self._session.close()
+            if self._aad_helper:
+                await self._aad_helper.close_async()
+        super().close()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+
+    @aio_documented_by(KustoClientSync.execute)
+    async def execute(self, database: Optional[str], query: str, properties: ClientRequestProperties = None) -> KustoResponseDataSet:
+        query = query.strip()
+        if query.startswith("."):
+            return await self.execute_mgmt(database, query, properties)
+        return await self.execute_query(database, query, properties)
+
+    @distributed_trace_async(name_of_span="KustoClient.query_cmd", kind=SpanKind.CLIENT)
+    @aio_documented_by(KustoClientSync.execute_query)
+    async def execute_query(self, database: str, query: str, properties: ClientRequestProperties = None) -> KustoResponseDataSet:
+        database = self._get_database_or_default(database)
+        Span.set_query_attributes(self._kusto_cluster, database, properties)
+
+        return await self._execute(self._query_endpoint, database, query, None, KustoClient._query_default_timeout, properties)
+
+    @distributed_trace_async(name_of_span="KustoClient.control_cmd", kind=SpanKind.CLIENT)
+    @aio_documented_by(KustoClientSync.execute_mgmt)
+    async def execute_mgmt(self, database: str, query: str, properties: ClientRequestProperties = None) -> KustoResponseDataSet:
+        database = self._get_database_or_default(database)
+        Span.set_query_attributes(self._kusto_cluster, database, properties)
+
+        return await self._execute(self._mgmt_endpoint, database, query, None, KustoClient._mgmt_default_timeout, properties)
+
+    @distributed_trace_async(name_of_span="KustoClient.streaming_ingest", kind=SpanKind.CLIENT)
+    @aio_documented_by(KustoClientSync.execute_streaming_ingest)
+    async def execute_streaming_ingest(
+        self,
+        database: Optional[str],
+        table: str,
+        stream: io.IOBase,
+        stream_format: Union[DataFormat, str],
+        properties: ClientRequestProperties = None,
+        mapping_name: str = None,
+    ):
+        database = self._get_database_or_default(database)
+        Span.set_streaming_ingest_attributes(self._kusto_cluster, database, table, properties)
+
+        stream_format = stream_format.kusto_value if isinstance(stream_format, DataFormat) else DataFormat[stream_format.upper()].kusto_value
+        endpoint = self._streaming_ingest_endpoint + database + "/" + table + "?streamFormat=" + stream_format
+        if mapping_name is not None:
+            endpoint = endpoint + "&mappingName=" + mapping_name
+
+        await self._execute(endpoint, database, None, stream, self._streaming_ingest_default_timeout, properties)
+
+    @aio_documented_by(KustoClientSync._execute_streaming_query_parsed)
+    async def _execute_streaming_query_parsed(
+        self,
+        database: Optional[str],
+        query: str,
+        timeout: timedelta = _KustoClientBase._query_default_timeout,
+        properties: Optional[ClientRequestProperties] = None,
+    ) -> StreamingDataSetEnumerator:
+        response = await self._execute(self._query_endpoint, database, query, None, timeout, properties, stream_response=True)
+        return StreamingDataSetEnumerator(JsonTokenReader(response.content))
+
+    @distributed_trace_async(name_of_span="KustoClient.streaming_query", kind=SpanKind.CLIENT)
+    @aio_documented_by(KustoClientSync.execute_streaming_query)
+    async def execute_streaming_query(
+        self,
+        database: Optional[str],
+        query: str,
+        timeout: timedelta = _KustoClientBase._query_default_timeout,
+        properties: Optional[ClientRequestProperties] = None,
+    ) -> KustoStreamingResponseDataSet:
+        database = self._get_database_or_default(database)
+        Span.set_query_attributes(self._kusto_cluster, database, properties)
+
+        response = await self._execute_streaming_query_parsed(database, query, timeout, properties)
+        return KustoStreamingResponseDataSet(response)
+
+    @aio_documented_by(KustoClientSync._execute)
+    async def _execute(
+        self,
+        endpoint: str,
+        database: Optional[str],
+        query: Optional[str],
+        payload: Optional[io.IOBase],
+        timeout: timedelta,
+        properties: ClientRequestProperties = None,
+        stream_response: bool = False,
+    ) -> Union[KustoResponseDataSet, ClientResponse]:
+        """Executes given query against this client"""
+        if self._is_closed:
+            raise KustoClosedError()
+        self.validate_endpoint()
+        request_params = ExecuteRequestParams(
+            database,
+            payload,
+            properties,
+            query,
+            timeout,
+            self._request_headers,
+            self._mgmt_default_timeout,
+            self._client_server_delta,
+            self.client_details,
+        )
+        json_payload = request_params.json_payload
+        request_headers = request_params.request_headers
+        timeout = request_params.timeout
+        if self._aad_helper:
+            request_headers["Authorization"] = await self._aad_helper.acquire_authorization_header_async()
+
+        invoker = lambda: self._session.post(
+            endpoint, headers=request_headers, json=json_payload, data=payload, timeout=timeout.seconds, proxy=self._proxy_url, allow_redirects=False
+        )
+
+        try:
+            response = await MonitoredActivity.invoke_async(
+                invoker, name_of_span="KustoClient.http_post", tracing_attributes=Span.create_http_attributes("POST", endpoint, request_headers)
+            )
+        except Exception as e:
+            raise KustoNetworkError(endpoint, None if properties is None else properties.client_request_id) from e
+
+        if stream_response:
+            try:
+                response.raise_for_status()
+                if 300 <= response.status < 400:
+                    raise Exception("Unexpected redirection, got status code: " + str(response.status))
+                return response
+            except Exception as e:
+                try:
+                    response_text = await response.text()
+                except Exception:
+                    response_text = None
+                try:
+                    response_json = await response.json()
+                except Exception:
+                    response_json = None
+                raise self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
+
+        async with response:
+            response_json = None
+            try:
+                if 300 <= response.status < 400:
+                    raise Exception("Unexpected redirection, got status code: " + str(response.status))
+                response_json = await response.json()
+                response.raise_for_status()
+            except Exception as e:
+                try:
+                    response_text = await response.text()
+                except Exception:
+                    response_text = None
+                raise self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
+            return MonitoredActivity.invoke(lambda: self._kusto_parse_by_endpoint(endpoint, response_json), name_of_span="KustoClient.processing_response")
---
+++ packages/azure-kusto-data/azure_kusto_data-4.3.1-py2.py3-none-any.whl/tests/aio/test_kusto_client.py
@@ -0,0 +1,203 @@
+"""Tests for KustoClient."""
+import json
+import sys
+from unittest.mock import patch
+
+import pytest
+
+from azure.kusto.data._cloud_settings import CloudSettings
+from azure.kusto.data._decorators import aio_documented_by
+from azure.kusto.data.client_request_properties import ClientRequestProperties
+from azure.kusto.data.exceptions import KustoClosedError, KustoMultiApiError, KustoNetworkError
+from azure.kusto.data.helpers import dataframe_from_result_table
+from ..kusto_client_common import KustoClientTestsMixin, mocked_requests_post, proxy_kcsb
+from ..test_kusto_client import TestKustoClient as KustoClientTestsSync
+
+PANDAS = False
+try:
+    import pandas
+
+    PANDAS = True
+except:
+    pass
+
+run_aio_tests = False
+try:
+    from azure.kusto.data.aio.client import KustoClient
+    from aioresponses import aioresponses, CallbackResult
+
+    run_aio_tests = True
+except:
+    pass
+
+if sys.version_info < (3, 6):
+    run_aio_tests = False
+
+
+@pytest.mark.skipif(not run_aio_tests, reason="requires aio")
+@aio_documented_by(KustoClientTestsSync)
+class TestKustoClient(KustoClientTestsMixin):
+    @staticmethod
+    def _mock_callback(url, **kwargs):
+        body = json.dumps(mocked_requests_post(str(url), **kwargs).json())
+        return CallbackResult(status=200, body=body)
+
+    def _mock_query(self, aioresponses_mock):
+        url = "{host}/v2/rest/query".format(host=self.HOST)
+        aioresponses_mock.post(url, callback=self._mock_callback)
+
+    def _mock_mgmt(self, aioresponses_mock):
+        url = "{host}/v1/rest/mgmt".format(host=self.HOST)
+        aioresponses_mock.post(url, callback=self._mock_callback)
+
+    def _mock_cloud_info(self, aioresponses_mock):
+        url = "{host}/v1/rest/auth/metadata".format(host=self.HOST)
+        aioresponses_mock.get(url, callback=self._mock_callback)
+
+    @aio_documented_by(KustoClientTestsSync.test_throws_on_close)
+    @pytest.mark.asyncio
+    async def test_throws_on_close(self):
+        """Test query V2."""
+        async with KustoClient(self.HOST) as client:
+            pass
+        with pytest.raises(KustoClosedError):
+            await client.execute_query("PythonTest", "Deft")
+
+    @aio_documented_by(KustoClientTestsSync.test_sanity_query)
+    @pytest.mark.asyncio
+    async def test_sanity_query(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                response = await client.execute_query("PythonTest", "Deft")
+            first_request = next(iter(aioresponses_mock.requests.values()))
+            self._assert_client_request_id(first_request[0].kwargs)
+        self._assert_sanity_query_response(response)
+
+    @pytest.mark.asyncio
+    async def test_raise_network(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                with pytest.raises(KustoNetworkError):
+                    response = await client.execute_query("PythonTest", "raiseNetwork")
+
+    @aio_documented_by(KustoClientTestsSync.test_sanity_control_command)
+    @pytest.mark.asyncio
+    async def test_sanity_control_command(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_mgmt(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                response = await client.execute_mgmt("NetDefaultDB", ".show version")
+        self._assert_sanity_control_command_response(response)
+
+    @pytest.mark.skipif(not PANDAS, reason="requires pandas")
+    @aio_documented_by(KustoClientTestsSync.test_sanity_data_frame)
+    @pytest.mark.asyncio
+    async def test_sanity_data_frame(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                response = await client.execute_query("PythonTest", "Deft")
+        data_frame = dataframe_from_result_table(response.primary_results[0])
+        self._assert_sanity_data_frame_response(data_frame)
+
+    @aio_documented_by(KustoClientTestsSync.test_partial_results)
+    @pytest.mark.asyncio
+    async def test_partial_results(self):
+        async with KustoClient(self.HOST) as client:
+            query = """set truncationmaxrecords = 5;
+range x from 1 to 10 step 1"""
+            properties = ClientRequestProperties()
+            properties.set_option(ClientRequestProperties.results_defer_partial_query_failures_option_name, False)
+            with aioresponses() as aioresponses_mock:
+                self._mock_query(aioresponses_mock)
+                with pytest.raises(KustoMultiApiError) as e:
+                    await client.execute_query("PythonTest", query, properties)
+                errors = e.value.get_api_errors()
+                assert len(errors) == 1
+                assert errors[0].code == "LimitsExceeded"
+
+                properties.set_option(ClientRequestProperties.results_defer_partial_query_failures_option_name, True)
+                self._mock_query(aioresponses_mock)
+                response = await client.execute_query("PythonTest", query, properties)
+            self._assert_partial_results_response(response)
+
+    @aio_documented_by(KustoClientTestsSync.test_admin_then_query)
+    @pytest.mark.asyncio
+    async def test_admin_then_query(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_mgmt(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                query = ".show tables | project DatabaseName, TableName"
+                response = await client.execute_mgmt("PythonTest", query)
+        self._assert_admin_then_query_response(response)
+
+    @aio_documented_by(KustoClientTestsSync.test_dynamic)
+    @pytest.mark.asyncio
+    async def test_dynamic(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                query = """print dynamic(123), dynamic("123"), dynamic("test bad json"),"""
+                """ dynamic(null), dynamic('{"rowId":2,"arr":[0,2]}'), dynamic({"rowId":2,"arr":[0,2]})"""
+                response = await client.execute_query("PythonTest", query)
+        row = response.primary_results[0].rows[0]
+        self._assert_dynamic_response(row)
+
+    @aio_documented_by(KustoClientTestsSync.test_empty_result)
+    @pytest.mark.asyncio
+    async def test_empty_result(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                query = """print 'a' | take 0"""
+                response = await client.execute_query("PythonTest", query)
+            assert response.primary_results[0]
+
+    @aio_documented_by(KustoClientTestsSync.test_null_values_in_data)
+    @pytest.mark.asyncio
+    async def test_null_values_in_data(self):
+        with aioresponses() as aioresponses_mock:
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                query = "PrimaryResultName"
+                response = await client.execute_query("PythonTest", query)
+            assert response is not None
+
+    @aio_documented_by(KustoClientTestsSync.test_sanity_query)
+    @pytest.mark.asyncio
+    async def test_request_id(self):
+        with aioresponses() as aioresponses_mock:
+            properties = ClientRequestProperties()
+            request_id = "test_request_id"
+            properties.client_request_id = request_id
+            self._mock_query(aioresponses_mock)
+            async with KustoClient(self.HOST) as client:
+                response = await client.execute_query("PythonTest", "Deft", properties=properties)
+            first_request = next(iter(aioresponses_mock.requests.values()))
+            self._assert_client_request_id(first_request[0].kwargs, value=request_id)
+        self._assert_sanity_query_response(response)
+
+    @aio_documented_by(KustoClientTestsSync.test_proxy_token_providers)
+    @pytest.mark.asyncio
+    async def test_proxy_token_providers(self, proxy_kcsb):
+        """Test query V2."""
+        proxy = "https://my_proxy.sample"
+        kcsb, auth_supports_proxy = proxy_kcsb
+        async with KustoClient(kcsb) as client:
+            client.set_proxy(proxy)
+
+            assert client._proxy_url == proxy
+
+            expected_dict = {"http": proxy, "https": proxy}
+            if not auth_supports_proxy:
+                return
+
+            assert client._aad_helper.token_provider._proxy_dict == expected_dict
+
+            CloudSettings._cloud_cache.clear()
+            with patch("requests.get", side_effect=mocked_requests_post) as mock_get:
+                client._aad_helper.token_provider._init_resources()
+
+                mock_get.assert_called_with("https://somecluster.kusto.windows.net/v1/rest/auth/metadata", proxies=expected_dict, allow_redirects=False)
