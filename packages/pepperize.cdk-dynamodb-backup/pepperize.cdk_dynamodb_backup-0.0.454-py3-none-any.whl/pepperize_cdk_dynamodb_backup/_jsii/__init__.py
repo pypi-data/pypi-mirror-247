ac2c76abebaf91fa8f8bc6625d442eb8730129d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:04:30 +0000

    Add package pepperize.cdk-dynamodb-backup/0.0.454/pepperize.cdk_dynamodb_backup-0.0.454-py3-none-any.whl

---
+++ packages/pepperize.cdk-dynamodb-backup/pepperize.cdk_dynamodb_backup-0.0.454-py3-none-any.whl/pepperize_cdk_dynamodb_backup/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import pepperize_cdk_private_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-dynamodb-backup",
+    "0.0.454",
+    __name__[0:-6],
+    "cdk-dynamodb-backup@0.0.454.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
