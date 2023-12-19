bde56470dbe16e03ab7b7c9f7856ccebaacbf3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:14 +0000

    Add package teneva-opti/0.5.3/teneva_opti-0.5.3-py3-none-any.whl

---
+++ packages/teneva-opti/teneva_opti-0.5.3-py3-none-any.whl/teneva_opti/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.5.3'
+
+
+from .opti import Opti
+from .opti_func import OptiFunc
+from .opti_manager import OptiManager
+from .opti_tens import OptiTens
+
+
+from .func import *
+from .tens import *
