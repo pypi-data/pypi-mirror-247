193ae4ee15cc4daf67822716a37753dc4d051283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:03:06 +0000

    Add package macrosynergy/0.0.52/macrosynergy-0.0.52-py3-none-any.whl

---
+++ packages/macrosynergy/macrosynergy-0.0.52-py3-none-any.whl/macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.52'
+version: str = '0.0.52'
+full_version: str = '0.0.52'
+git_revision: str = '25baa0930efd8a2e2c15b8e5979473f21ce0c73b'
+release: bool = True
+if not release:
+    version = full_version
