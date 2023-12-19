53834334a80165115e6b056ea5a9610f126684ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:03:06 +0000

    Add package unidown-mr-de/1.1.0/unidown_mr_de-1.1.0-py3-none-any.whl

---
+++ packages/unidown-mr-de/unidown_mr_de-1.1.0-py3-none-any.whl/unidown_mr_de/meta.py
@@ -0,0 +1,24 @@
+"""
+Static and important data which is needed by the program and do not need any third party libraries.
+
+.. note:: this is important because it is used inside the setup.py.
+"""
+
+#: Name of this program
+NAME = "unidown-mr_de"
+#: Long name of this program
+LONG_NAME = "unidown-mr_de"
+#: Version in PEP440 format
+VERSION = '1.1.0'
+#: Author
+AUTHOR = 'Iceflower S'
+#: Author github link
+AUTHOR_GITHUB = 'https://github.com/IceflowRE'
+#: Author email
+AUTHOR_EMAIL = 'iceflower@gmx.de'
+#: Short description
+DESCRIPTION = 'MR german books plugin for unidown.'
+#: Project url
+PROJECT_URL = 'https://github.com/IceflowRE/unidown-mr_de'
+#: License
+LICENSE = 'GPLv3'
