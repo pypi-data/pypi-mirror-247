c0d7ce5db747ece6bdc961d3da9fbb033fd4a9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 05:02:53 +0000

    Add package PaIRS-UniNa/0.1.11/PaIRS_UniNa-0.1.11-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/PaIRS-UniNa/PaIRS_UniNa-0.1.11-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PaIRS_UniNa/CalVi.py
@@ -0,0 +1,18 @@
+from .gCalVi import *
+
+def run():
+    gui:gCalVi
+    app,gui=launchCalVi()
+    quitCalVi(app)
+
+def cleanRun():
+    if os.path.exists(lastcfgname_CalVi):
+        os.remove(lastcfgname_CalVi)
+    run()
+   
+def debugRun():
+    gui:gCalVi
+    app,gui=launchCalVi(flagInputDebug=True)
+    quitCalVi(app)
+
+
