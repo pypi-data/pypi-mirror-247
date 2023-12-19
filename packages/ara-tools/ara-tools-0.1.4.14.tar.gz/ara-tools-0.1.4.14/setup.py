2c2523b399d60d41ed9ee7ec2f8c85cd5a0c1b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:15 +0000

    Add package ara-tools/0.1.4.14/ara-tools-0.1.4.14.tar.gz

---
+++ packages/ara-tools/ara-tools-0.1.4.14.tar.gz/ara-tools-0.1.4.14/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ara-tools",
+    version="0.1.4.14",
+    packages=find_packages(),
+    include_package_data=True,  # Add this line
+    entry_points={
+        "console_scripts": [
+            "ara = ara_tools.__main__:cli",
+        ],
+    },
+    install_requires=[
+        # Add your package dependencies here
+    ],
+)
