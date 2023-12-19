11e766a4554e39f0fc24d8d74ef659fbfcd63867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:03:26 +0000

    Add package cvrmap/2.0.20/cvrmap-2.0.20.tar.gz

---
+++ packages/cvrmap/cvrmap-2.0.20.tar.gz/cvrmap-2.0.20/README.md
@@ -0,0 +1,185 @@
+[![codecov](https://codecov.io/gh/ln2t/cvrmap/graph/badge.svg?token=VGQPJX5078)](https://codecov.io/gh/ln2t/cvrmap) ![License](https://img.shields.io/github/license/ln2t/cvrmap)
+
+# About
+
+CVRmap is an opensource (license AGPLv3) software to compute maps of Cerebro-Vascular Reactivity (CVR).
+
+The software is compatible with the Brain Imagning Data Structure (BIDS) standard for applications.
+
+The paper describing the toolbox will be pulished (hopefully!) soon.
+
+A normative dataset can be downloaded from openneuro: [ds004604](https://openneuro.org/datasets/ds004604)
+
+CVRmap is also distributed as a package, the full API documentation being [here](https://ln2t.github.io/cvrmap)
+
+# Installation
+
+# Note about dependencies
+
+All dependencies are specified in `requirements.txt`.
+
+# Option 1: docker (recommended option!)
+
+The easiest way to install `cvrmap` is to use docker:
+
+```
+docker pull arovai/cvrmap:VERSION
+```
+
+where of course `VERSION` must be replaced by any other available version (e.g. 2.0.0).
+Check out the [`cvrmap` docker hub page](https://hub.docker.com/repository/docker/arovai/cvrmap/general) to find more info on the available images.
+
+To test your installation, you can use for instance
+
+```
+docker run arovai/cvrmap:VERSION --version
+```
+
+This will output the version and exit. You can also type
+
+```
+docker run arovai/cvrmap:VERSION --help
+```
+
+for help.
+
+For the reader not familiar with docker, you can pass some data to the docker image with
+
+```
+docker run -v /path/to/your/bids/folder:/rawdata -v /path/to/your/derivatives:/derivatives arovai/cvrmap:VERSION /rawdata /derivatives/cvrmap participant
+```
+
+For more information about the command line options, see the **Usage** section.
+
+# Option 2: Singularity (good for HPC)
+
+You can also build a Singularity image file using
+
+```
+singularity build arovai.cvrmap.VERSION.sif docker://arovai/cvrmap:VERSION
+```
+
+You can for instance run this command inside your home directory, and then you'll have a singularity image file named `arovai.cvrmap.VERSION.sif` in there. To run it, still in the folder where the image is located, run
+
+```
+singularity run -B /path/to/your/bids/folder:/rawdata -B /path/to/your/derivatives:/derivatives arovai.cvrmap.VERSION.sif /rawdata /derivatives/cvrmap participant
+```
+
+Make sure that the folder `/path/to/your/derivatives` exists before launching this command.
+
+Of course, you are free to place the image where ever suits you; you'll simply have to adapt the path when calling `singularity`.
+
+# Option 3: python environment (using pip)
+
+`cvrmap` is also available on [pypi](https://pypi.org/manage/project/cvrmap/releases). We strongly recommend that you install it in a virtual environment.
+First, install `virtualenv`:
+
+```
+pip install virtualenv
+```
+
+Then create a virtual env. To deal with potential conflicts in versions of the required packages withing `cvrmap`, we recommend you create one environment for each `cvrmap` version:
+
+```
+export VERSION="2.0.19"
+virtualenv cvrmap-env-$VERSION
+```
+
+Activate the environment and install `cvrmap`:
+```
+source cvrmap-env-$VERSION/bin/activate && pip install cvrmap==$VERSION
+```
+
+Warning: make sure you are using Python version 3.10 (or more recent)!
+
+This will add a command in your `PATH` so that you can directly launch `cvrmap`:
+
+```
+cvrmap -h
+```
+
+Note that the docker image is essentially build using this procedure, as you can see in the `Dockerfile` located in the `docker` folder of this repo.
+
+# Usage
+
+To run CVRmap, you must first have data to crunch. If you don't have data, and you want to test CVRmap, you can download the publicly available dataset on openneuro [ds004604](https://openneuro.org/datasets/ds004604) which include compatible rawdata, fmriprep derivatives, as well as `cvrmap` (v1.0) outputs.
+If you have your own data that you want to analyze with CVRmap, make sure to observe the following:
+first of all, the data are supposed to a [BIDS](https://bids-specification.readthedocs.io/en/stable/) dataset. For each subject, you must have a T1w image, a BOLD image and a physiological file containing the breathing data (CO2) recorded during the fMRI scan. For instance, for `sub-01`, the data should look like this:
+
+```
+sub-01/anat/sub-01_T1w.nii.gz
+sub-01/anat/sub-01_T1w.json
+sub-01/func/sub-01_task-gas_bold.nii.gz
+sub-01/func/sub-01_task-gas_bold.json
+sub-01/func/sub-01_task-gas_physio.tsv.gz
+sub-01/func/sub-01_task-gas_physio.json
+```
+
+In this example, the taskname BIDS entity is `gas`. If yours differs, that's not a problem, and you'll be able to run `cvrmap` provided you add the option `--taskname your_task_name` when launching the software.
+
+Note that the `sub-01/func/sub-01_task-gas_physio.json` file must contain a `SamplingFrequency` field as well as a `co2` field to specify the units of measurement of CO2 levels. An example of `sub-01/func/sub-01_task-gas_physio.json` would be:
+
+```
+{
+    "SamplingFrequency": 100,
+    "StartTime": 0,
+    "Columns": [
+        "co2"
+    ],
+    "co2": {
+        "Units": "mmHg"
+    }
+}
+```
+
+In this example, the `sub-01/func/sub-01_task-gas_physio.tsv.gz` must have only one column, giving the CO2 readings at a sampling frequency of 100 Hz, starting at time 0 with respect to the first valid fMRI volume, in the units of mmHg.
+Note though that the `StartTime` field is not used at all by `cvrmap`, as it explores various time lags by itself.
+If the CO2 readings are in percentage of co2 concentration (which is also often used), the "Units" field must be "%", and in that case `cvrmap` will convert percentages to mmHg automatically. Finally, the total duration of the CO2 recording must not necessarily match the duration of the BOLD acquisition: depending on the case, CVRmap trims or uses a baseline extrapolation automatically.
+
+The rawdata must also have been processed using [fMRIPrep](https://fmriprep.org/en/stable). A minimalistic fMRIPrep call compatible with CVRmap is:
+
+```
+fmriprep /path/to/bids_dir /path/to/derivatives/fmriprep participant --fs-license-file /path/to/fslicense --use-aroma
+```
+
+*Warning* the versions of fMRIPrep above 23.1.0 don't support the `--use-aroma` option anymore. For this reason, we *must* use older versions. We will provide a patch to `cvrmap` to handle newer versions of fMRIPrep in the future.
+
+We are now good to go and launch CVRmap with
+
+```
+cvrmap /path/to/bids_dir /path/to/derivatives/cvrmap participant --fmriprep_dir /path/to/derivatives/fmriprep
+```
+
+Notes:
+- the exact `cvrmap` command might depend on the installation option you choose (see above, options 1, 2 and 3)
+- the `--fmriprep_dir` option can be omitted if the fMRIPrep derivatives are located in `/path/to/bids_dir/derivatives/fmriprep`.
+- if the BOLD taskname is not `gas`, you must add `--taskname your_task_name`.
+- if you want the outputs in another space, and if this space was included in the fMRIPrep call, you must add `--space your_custom_space`. The default space is `MNI152NLin2009cAsym`, which is also the default space of fMRIPrep.
+- if you want to use the ICA-AROMA classification for signal confounds, then add `--use-aroma`. Otherwise, when the flag is omitted, CVRmap will perform non-aggressive denoising itself using a refinement of the ICA-AROMA classification of noise sources (see paper for more details).
+- more info and options can be found when asking politely for help with `cvrmap --help`.
+
+`cvrmap` will run for about 3 hours per participant on recent computers. The results are stored in `/path/to/derivatives/cvrmap` following BIDS standard for derivatives.  More specifically, the outputs are as follows:
+
+```
+sub-01/extras/sub-01_desc-etco2_timecourse.tsv.gz
+sub-01/extras/sub-01_desc-etco2_timecourse.json
+sub-01/extras/sub-01_space-MNI152NLin2009cAsym_denoised.nii.gz
+sub-01/extras/sub-01_space-MNI152NLin2009cAsym_denoised.json
+sub-01/figures/sub-01_boldmean.svg
+sub-01/figures/sub-01_breathing.svg
+sub-01/figures/sub-001_denoising.html
+sub-01/figures/sub-001_summary.html
+sub-01/figures/sub-01_space-MNI152NLin2009cAsym_cvr.svg
+sub-01/figures/sub-01_space-MNI152NLin2009cAsym_delay.svg
+sub-01/sub-01_space-MNI152NLin2009cAsym_delay.nii.gz
+sub-01/sub-01_space-MNI152NLin2009cAsym_delay.json
+sub-01/sub-01_space-MNI152NLin2009cAsym_cvr.nii.gz
+sub-01/sub-01_space-MNI152NLin2009cAsym_cvr.json
+sub-01.html
+```
+
+The `extras` folder contains the `etco2` file with the end-tidal timecourse extracted from the original CO2 readings as well as the non-aggressively denoised BOLD series (the series are also high-pass filtered and smoothed at 5mm FWHM). Some pictures for the report are stored within `figures`. The delay map contains the computed time delays (or time lags) in seconds, and normalized to the global signal delay. The main map of interest is of course the CVR map! For a quick analysis, the html report is also included.
+
+# Bugs or questions
+
+Should you encounter any bug, weird behavior or if you have questions, do not hesitate to open an issue, and we'll happily try to answer!
---
+++ packages/cvrmap/cvrmap-2.0.20.tar.gz/cvrmap-2.0.20/cvrmap/cvrmap.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+"""BIDS app to compute Cerebrovascular Reactivity maps
+This app complies with the Brain Imaging Data Structure (BIDS) App
+standard. It assumes the dataset is organised according to BIDS.
+Intended to run in command line with arguments as described in the help
+(callable with -h)
+
+Author: Antonin Rovai
+
+Created: May 2022
+"""
+
+# imports
+import os  # to interact with dirs
+from .utils import *  # custom utilities
+
+def main():
+    """
+        This is the main function to run cvrmap and is called at the end of this script.
+
+    Returns:
+        None
+    """
+    # Get and print version
+    __version__ = get_version()
+
+    msg_info("Version: %s" % __version__)
+
+    args = arguments_manager(__version__)
+    fmriprep_dir = get_fmriprep_dir(args)
+
+    msg_info("Indexing BIDS dataset...")
+
+    layout = get_bidslayout(args)
+    layout.add_derivatives(fmriprep_dir)
+    subjects_to_analyze = get_subjects_to_analyze(args, layout)
+    task = get_task(args, layout)
+    custom_label = get_custom_label(args)
+    space, res = get_space(args, layout)
+    layout = setup_output_dir(args, __version__, layout)
+    output_dir = layout.derivatives['cvrmap'].root
+    flags = set_flags(args)
+    parameters = read_config_file(args.config)
+
+    if flags['no-shift']:
+        parameters['relative_shift_list'] = np.array([0])
+
+    # print some summary before running
+    msg_info("Bids directory: %s" % layout.root)
+    msg_info("Fmriprep directory: %s" % fmriprep_dir)
+    msg_info("Subject(s) to analyse: %s" % subjects_to_analyze)
+    msg_info("Task to analyse: %s" % task)
+    msg_info("Selected space: %s" % space)
+
+    if res is not None:
+        msg_info("Selected resolution: %s" % res)
+
+    if args.analysis_level == "participant":
+
+        for subject_label in subjects_to_analyze:
+            msg_info("Running for participant %s" % subject_label)
+
+            basic_filter = dict(subject=subject_label, return_type='filename', space=space, res=res, task=task)
+
+            outputs = setup_subject_output_paths(output_dir, subject_label, space, res, args, custom_label)
+
+            preproc = get_preproc(basic_filter, layout)
+            mask = get_mask(basic_filter, layout)
+            t1w = get_t1w(basic_filter, space, layout)
+            aroma_noise_ic_list = get_aroma_noise_ic_list(basic_filter, layout)
+            melodic_mixing_df, melodic_mixing_path = get_melodic_mixing(basic_filter, layout)
+
+            if flags['vesselsignal'] or flags['globalsignal']:
+                if flags['vesselsignal']:
+                    mask_img = get_vesselmask(preproc, parameters['vesseldensity_threshold'])
+                    mask_output = outputs['vesselsignal']
+                    signal_label = r'$vesselsignal timecourse$'
+                if flags['globalsignal']:
+                    mask_img = mask.img
+                    mask_output = outputs['globalsignal']
+                    signal_label = r'$globalsignal timecourse$'
+                probe, baseline = masksignalextract(preproc, mask_img)
+                probe.label = signal_label
+                baseline.label = signal_label
+                probe.save(mask_output)
+                physio = None
+            else:
+                physio = get_physio_data(basic_filter, layout)
+                probe, baseline = endtidalextract(physio)
+                probe.save(outputs['etco2'])
+
+            corrected_noise = get_corrected_noiselist(probe, aroma_noise_ic_list, melodic_mixing_df,
+                                                      preproc.sampling_frequency,
+                                                      parameters['ic_threshold'], args.use_aroma)
+
+            if flags['sloppy'] or flags['no-denoising']:
+                msg_warning('Working in sloppy mode, only for quick testing!')
+                msg_info("Skipping data denoising")
+                denoised = DataObj()
+                denoised.data = preproc.data
+                denoised.data_type = preproc.data_type
+            else:
+                msg_info("Data denoising in progress")
+                denoised = bold_denoising(preproc.path, mask.path, melodic_mixing_df, corrected_noise, parameters)
+
+
+            denoised.measurement_type = 'denoised BOLD'
+            denoised.units = 'arbitrary'
+            denoised.sampling_frequency = preproc.sampling_frequency
+            denoised.save(outputs['denoised'], preproc.path)
+
+            # get global signal
+            global_signal = compute_global_signal(denoised)
+
+            # build once and for all the shifted versions of the probe
+            probe.shift_ref = DataObj(sampling_frequency=denoised.sampling_frequency,
+                                      data_type='timecourse', data=denoised.data[0, 0, 0, :])
+            probe.shift_array = parameters['absolute_shift_list']
+            probe.shifted_dataobjects = None
+            probe.build_shifted()
+
+            global_signal_shift = compute_delays(global_signal, probe, parameters['absolute_shift_list'])[0]
+
+            if not os.path.exists(outputs['delay']) or flags['overwrite']:
+
+                # if flags['sloppy']:
+                    # msg_warning('Working in sloppy mode, only for quick testing!')
+                    # zmask = mask.data
+                    # zmask[:, :, :59] = np.zeros(zmask[:, :, :59].shape)
+                    # zmask[:, :, 61:] = np.zeros(zmask[:, :, 61:].shape)
+                    # denoised.mask = zmask
+
+                denoised.mask = mask.data
+
+                # build the shifted data needed to compute delays
+                shift_options = dict()
+                shift_options['origin'] = global_signal_shift
+                shift_options['relative_values'] = parameters['relative_shift_list']
+
+                # compute delays for all voxels
+                msg_info("Computing delays")
+
+                # build once and for all the shifted versions of the probe
+                probe.shift_ref = DataObj(sampling_frequency=denoised.sampling_frequency,
+                                          data_type='timecourse', data=denoised.data[0, 0, 0, :])
+                probe.shift_array = shift_options['origin'] + shift_options['relative_values']
+                probe.shifted_dataobjects = None
+                probe.build_shifted()
+
+                results = compute_delays(denoised, probe, shift_options)
+                results['delay'].units = 'seconds'
+                results['delay'].measurement_type = 'delay'
+
+                # save the obtained map
+                results['delay'].save(outputs['delay'], denoised.path)
+
+                # compute and save response maps
+                results['cvr'] = compute_response(results['intercept'], results['slope'], probe.baseline, np.mean(probe.data))
+                if flags['vesselsignal']:
+                    results['cvr'].units = "Arbitrary units"
+                    results['cvr'].measurement_type = 'relative-CVR'
+                    results['cvr'].data = 10*results['cvr'].data/np.nanstd(results['cvr'].data)  # this rescaling is mostly for visual purposes
+                else:
+                    results['cvr'].units = "Percentage of BOLD variation/%s" % probe.units
+                    results['cvr'].measurement_type = 'CVR'
+                results['cvr'].save(outputs['cvr'], denoised.path)
+            else:
+                results = dict()
+                results['delay'] = DataObj(measurement_type='delay')
+                results['delay'].nifti_load(outputs['delay'])
+                results['cvr'] = DataObj()
+                results['cvr'].nifti_load(outputs['cvr'])
+                results['cvr'].measurement_type = 'CVR'
+
+            results['physio'] = physio
+            if flags['vesselsignal']:
+                results['vesselmask'] = mask_img
+                results['meanepi'] = get_meanepi(preproc)
+                results['vesselsignal'] = True
+            else:
+                results['vesselsignal'] = False
+
+            if flags['globalsignal']:
+                results['globalmask'] = mask_img
+                results['meanepi'] = get_meanepi(preproc)
+                results['globalsignal'] = True
+            else:
+                results['globalsignal'] = False
+
+            results['probe'] = probe
+            results['shifted_probe'] = probe.shifted_dataobjects[global_signal_shift]
+            results['baseline'] = baseline
+            results['global_signal'] = global_signal
+
+            save_figs(results, outputs, mask)
+
+            msg_info("Building report...")
+
+            save_html_reportlets(subject_label, task, space, args, __version__, aroma_noise_ic_list,
+                                 global_signal_shift, corrected_noise, parameters, outputs)
+
+            from nireports.assembler.tools import run_reports
+            run_reports(output_dir, subject_label, 'madeoutuuid', bootstrap_file=get_report_config(),
+                        reportlets_dir=output_dir)
+
+    # running group level
+    elif args.analysis_level == "group":
+        print('No group level analysis is implemented yet.')
+
+    msg_info("The End!")
+
+if __name__ == '__main__':
+    main()
