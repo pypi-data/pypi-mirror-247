44a5d8672e906ac5a16cee3f5ee85527cb19b63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:44 +0000

    Add package rosemary/1.7.2/rosemary-1.7.2-py3-none-any.whl

---
+++ packages/rosemary/rosemary-1.7.2-py3-none-any.whl/rosemary/db/alembic.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from alembic.config import Config
+from alembic import command
+
+
+class Alembic:
+    def __init__(self, host: str, db: str, user: str, password: str | None, port: int | str, schema: str):
+        __connect_string = f'postgresql://{user}:{password}@{host}:{port}/{db}?options=-csearch_path={schema}'
+        __root_dir = Path(__file__).parent.parent.parent
+        __alembic_dir = __root_dir / 'rosemary/alembic'
+        self.__alembic_cfg = Config()
+        self.__alembic_cfg.set_main_option("sqlalchemy.url", __connect_string)
+        self.__alembic_cfg.set_main_option("script_location", str(__alembic_dir))
+
+    def upgrade(self, revision):
+        command.upgrade(self.__alembic_cfg, revision=revision)
