68b3525eb256dac301e24cbd5f26492a6a320bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 03:05:15 +0000

    Add package edustudio/1.0.0/edustudio-1.0.0-py3-none-any.whl

---
+++ packages/edustudio/edustudio-1.0.0-py3-none-any.whl/edustudio/traintpl/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author : Xiangzhi Chen
+# @Github : kervias
+
+from .base_traintpl import BaseTrainTPL
+from .gd_traintpl import GDTrainTPL
+from .edu_traintpl import EduTrainTPL
+from .atkt_traintpl import AtktTrainTPL
