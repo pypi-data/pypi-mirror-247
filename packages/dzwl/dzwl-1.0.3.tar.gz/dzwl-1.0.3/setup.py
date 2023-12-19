54bcae3be32ac9e6d7b9244141d2df8b7fb60603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 17:04:38 +0000

    Add package dzwl/1.0.3/dzwl-1.0.3.tar.gz

---
+++ packages/dzwl/dzwl-1.0.3.tar.gz/dzwl-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name='dzwl',
+    version='1.0.3',
+    author='zj',
+    packages=find_packages(), 
+    package_dir={'requests': 'requests'},)
+    
+    
