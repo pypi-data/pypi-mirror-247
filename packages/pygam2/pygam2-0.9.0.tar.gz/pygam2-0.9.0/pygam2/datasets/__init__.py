c892abbfc95a85eb651dd9966dc4cc7155645b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 04:04:02 +0000

    Add package pygam2/0.9.0/pygam2-0.9.0.tar.gz

---
+++ packages/pygam2/pygam2-0.9.0.tar.gz/pygam2-0.9.0/pygam2/datasets/__init__.py
@@ -0,0 +1,30 @@
+"""
+GAM datasets
+"""
+# -*- coding: utf-8 -*-
+
+from pygam2.datasets.load_datasets import mcycle
+from pygam2.datasets.load_datasets import coal
+from pygam2.datasets.load_datasets import faithful
+from pygam2.datasets.load_datasets import wage
+from pygam2.datasets.load_datasets import trees
+from pygam2.datasets.load_datasets import default
+from pygam2.datasets.load_datasets import cake
+from pygam2.datasets.load_datasets import hepatitis
+from pygam2.datasets.load_datasets import toy_classification
+from pygam2.datasets.load_datasets import head_circumference
+from pygam2.datasets.load_datasets import chicago
+from pygam2.datasets.load_datasets import toy_interaction
+
+__all__ = ['mcycle',
+           'coal',
+           'faithful',
+           'trees',
+           'wage',
+           'default',
+           'cake',
+           'hepatitis',
+           'toy_classification',
+           'head_circumference',
+           'chicago',
+           'toy_interaction']
