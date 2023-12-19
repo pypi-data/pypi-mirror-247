6895dd5ebce471702d938e38fe528b8b6856e31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:14 +0000

    Add package py-jutil/0.0.1/py-jutil-0.0.1.tar.gz

---
+++ packages/py-jutil/py-jutil-0.0.1.tar.gz/py-jutil-0.0.1/jutil/__init__.py
@@ -0,0 +1,17 @@
+"""
+# JUtil
+
+## Description:
+JUtil is a collection of useful functions, values, and classes for Python.
+
+## Dependencies:
+- None yet
+
+## Note:
+- This module is a work in progress.
+
+
+___
+v0.0.1, developed by Noah Wilhoite
+Last updated: Dec 18th, 2023
+"""
