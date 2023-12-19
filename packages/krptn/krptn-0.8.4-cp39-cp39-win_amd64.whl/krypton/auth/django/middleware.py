b54f2d4fa13c2461f7238e2d3a783c18dea6da84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-19 14:03:15 +0000

    Add package krptn/0.8.4/krptn-0.8.4-cp39-cp39-win_amd64.whl

---
+++ packages/krptn/krptn-0.8.4-cp39-cp39-win_amd64.whl/krypton/auth/django/middleware.py
@@ -0,0 +1,40 @@
+"""
+Django middleware to add user object as needed to the request.
+"""
+
+from django.http import HttpRequest
+from .users import djangoUser
+from ..users.bases import UserError
+
+
+def kryptonLoginMiddleware(get_response):
+    """Django middleware to add user object as needed to the request.
+
+
+    Arguments:
+        get_response -- Function to get HttpResponse
+
+    Returns:
+        Response
+    """
+
+    def KrLoginMiddleWare(request: HttpRequest):
+        """Middleware
+
+        Arguments:
+            request -- HttpRequest
+
+        Returns:
+            HttpResponse
+        """
+        try:
+            user = djangoUser(userID=request.COOKIES["_KryptonUserID"])
+            user.restoreSession(request.COOKIES["_KryptonSessionToken"])
+        except UserError:
+            return get_response(request)
+        except KeyError:
+            return get_response(request)
+        request.user = user
+        return get_response(request)
+
+    return KrLoginMiddleWare
