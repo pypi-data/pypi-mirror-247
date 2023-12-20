781523adb2978fd2c1fd6504fa302f8c5639228f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:48 +0000

    Add package methylartist/1.2.11/methylartist-1.2.11.tar.gz

---
+++ packages/methylartist/methylartist-1.2.11.tar.gz/methylartist-1.2.11/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='methylartist',
+    version='1.2.11',
+    author='Adam Ewing',
+    author_email='adam.ewing@gmail.com',
+    description=("Tools for parsing and plotting nanopore methylation data"),
+    license='MIT',
+    url='https://github.com/adamewing/methylartist',
+    download_url='https://github.com/adamewing/methylartist/archive/refs/tags/1.2.11.tar.gz',
+    scripts=['methylartist'],
+    packages=find_packages(),
+    install_requires = [
+        'cython',
+        'pysam',
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'bx-python',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
