905b6fab0f4c24d6d0e276f9e0414def75753330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:09 +0000

    Add package malevich-space/0.4.9/malevich_space-0.4.9-py3-none-any.whl

---
+++ packages/malevich-space/malevich_space-0.4.9-py3-none-any.whl/malevich_space/gql/task/__init__.py
@@ -0,0 +1,11 @@
+from .build import *
+from .boot import *
+from .change_state import *
+
+from .create_endpoint import *
+
+from .run import *
+
+from .start_schema import *
+from .get_results import *
+from .run_shaphost_components import *
