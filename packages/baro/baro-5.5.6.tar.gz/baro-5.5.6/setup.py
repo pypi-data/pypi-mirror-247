2107df2ad9b9f1c4816f22574ddb9ea0a08666cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:05:00 +0000

    Add package baro/5.5.6/baro-5.5.6.tar.gz

---
+++ packages/baro/baro-5.5.6.tar.gz/baro-5.5.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="baro",
+    version="5.5.6",
+    description="Easy and simple loading bar for python.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toolkitr",
+    author="baro",
+    author_email="toolkitr.dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+    ],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.7",
+)
