0d29aa5e4cf8cc909b9d957955fb2019f3cf90d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:52 +0000

    Add package hexdoc/1!0.1.0a7/hexdoc-1!0.1.0a7.tar.gz

---
+++ packages/hexdoc/hexdoc-1!0.1.0a7.tar.gz/hexdoc-1!0.1.0a7/src/hexdoc/core/__init__.py
@@ -0,0 +1,56 @@
+__all__ = [
+    "After_1_19",
+    "After_1_20",
+    "AtLeast_1_19",
+    "AtLeast_1_20",
+    "BaseProperties",
+    "BaseResourceDir",
+    "Before_1_19",
+    "Before_1_20",
+    "BookFolder",
+    "Entity",
+    "ExportFn",
+    "IsVersion",
+    "ItemStack",
+    "METADATA_SUFFIX",
+    "MinecraftVersion",
+    "ModResourceLoader",
+    "PathResourceDir",
+    "PluginResourceDir",
+    "Properties",
+    "ResLoc",
+    "ResourceDir",
+    "ResourceLocation",
+    "ResourceType",
+    "ValueIfVersion",
+    "VersionSource",
+    "Versioned",
+]
+
+from .compat import (
+    After_1_19,
+    After_1_20,
+    AtLeast_1_19,
+    AtLeast_1_20,
+    Before_1_19,
+    Before_1_20,
+    IsVersion,
+    MinecraftVersion,
+    ValueIfVersion,
+    Versioned,
+    VersionSource,
+)
+from .loader import (
+    METADATA_SUFFIX,
+    BookFolder,
+    ExportFn,
+    ModResourceLoader,
+)
+from .properties import BaseProperties, Properties
+from .resource import Entity, ItemStack, ResLoc, ResourceLocation, ResourceType
+from .resource_dir import (
+    BaseResourceDir,
+    PathResourceDir,
+    PluginResourceDir,
+    ResourceDir,
+)
