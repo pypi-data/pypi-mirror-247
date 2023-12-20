3e774e46e2ea3c1d74b974c3f2e335557c3f2b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:32 +0000

    Add package azucar-reactive-websocket/0.2/azucar_reactive_websocket-0.2.tar.gz

---
+++ packages/azucar-reactive-websocket/azucar_reactive_websocket-0.2.tar.gz/azucar_reactive_websocket-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='azucar_reactive_websocket',
+    version='0.2',
+    author='juan_k704',
+    author_email='juan_k704@hotmail.com',
+    description='Un manejador de WebSocket reactivo usando RxPython y websockets.',
+    packages=find_packages(),
+    install_requires=[
+        'rx',          # RxPython para programación reactiva
+        'websockets'   # Biblioteca para trabajar con WebSockets
+    ],
+    classifiers=[
+        # Indica a quién va dirigido tu paquete y en qué etapas de desarrollo se encuentra
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',  # Versión mínima de Python requerida
+)
