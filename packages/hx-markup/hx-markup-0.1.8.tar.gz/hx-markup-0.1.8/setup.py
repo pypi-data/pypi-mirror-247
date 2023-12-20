e6c430fdbf94dddb2e8ea09be71166da191ce990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:48 +0000

    Add package hx-markup/0.1.8/hx-markup-0.1.8.tar.gz

---
+++ packages/hx-markup/hx-markup-0.1.8.tar.gz/hx-markup-0.1.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hx_markup']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'hx-markup',
+    'version': '0.1.8',
+    'description': 'HTML markup using python classes, including HTMX language.',
+    'long_description': None,
+    'author': 'Daniel Arantes',
+    'author_email': 'arantesdv@me.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
