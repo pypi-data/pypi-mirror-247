edd497f6b5f9684fde89e446020ab57d38091cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:04:43 +0000

    Add package pyvo/1.5/pyvo-1.5.tar.gz

---
+++ packages/pyvo/pyvo-1.5.tar.gz/pyvo-1.5/docs/auth/index.rst
@@ -0,0 +1,10 @@
+******************
+Auth (`pyvo.auth`)
+******************
+This module contains submodules which help handle auth when
+communicating with virtual observatory services.
+
+Reference/API
+=============
+
+.. automodapi:: pyvo.auth
---
+++ packages/pyvo/pyvo-1.5.tar.gz/pyvo-1.5/pyvo/registry/__init__.py
@@ -0,0 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+a package for interacting with registries.
+
+The regtap module supports access to the IVOA Registries
+"""
+
+from .regtap import (search, ivoid2service, get_RegTAP_query,
+    choose_RegTAP_service)
+
+from .rtcons import (Constraint,
+    Freetext, Author, Servicetype, Waveband, Datamodel, Ivoid,
+    UCD, Spatial, Spectral, Temporal)
+
+__all__ = ["search", "get_RegTAP_query", "Freetext", "Author",
+    "Servicetype", "Waveband", "Datamodel", "Ivoid", "UCD",
+    "Spatial", "Spectral", "Temporal",
+    "choose_RegTAP_service"]
