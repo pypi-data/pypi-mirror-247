511a3febf6bdb9632797253c5df99cfbe33f0db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:07 +0000

    Add package deepeval/0.20.41/deepeval-0.20.41-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.20.41-py3-none-any.whl/deepeval/metrics/__init__.py
@@ -0,0 +1,20 @@
+from .base_metric import BaseMetric
+from .answer_relevancy import AnswerRelevancyMetric
+from .base_metric import BaseMetric
+from .summarization import SummarizationMetric
+from .judgemental_gpt import JudgementalGPT
+from .llm_eval_metric import LLMEvalMetric
+from .ragas_metric import (
+    RagasMetric,
+    ContextualPrecisionMetric,
+    ContextualRelevancyMetric,
+    FaithfulnessMetric,
+    ContextualRecallMetric,
+    ConcisenessMetric,
+    CorrectnessMetric,
+    CoherenceMetric,
+    MaliciousnessMetric,
+)
+from .unbias_metric import UnBiasedMetric
+from .non_toxic_metric import NonToxicMetric
+from .hallucination_metric import HallucinationMetric
---
+++ packages/deepeval/deepeval-0.20.41-py3-none-any.whl/tests/test_answer_relevancy.py
@@ -0,0 +1,39 @@
+"""Tests for answer relevancy
+"""
+import pytest
+from deepeval.test_case import LLMTestCase
+from deepeval.metrics import AnswerRelevancyMetric
+
+from deepeval import assert_test, run_test
+
+query = "What is Python?"
+answer = "Python is a programming language?"
+
+
+def test_query_answer_relevancy():
+    scorer = AnswerRelevancyMetric(minimum_score=0.5)
+    test_case = LLMTestCase(input=query, actual_output=answer)
+    assert_test(test_case, [scorer])
+
+
+def test_compare_answer_relevancy_2():
+    scorer = AnswerRelevancyMetric(minimum_score=0.5)
+    test_case = LLMTestCase(input=query, actual_output="Programming lang")
+    run_test(test_case, metrics=[scorer])
+
+
+def test_compare_answer_relevancy():
+    metric = AnswerRelevancyMetric(minimum_score=0.5)
+    query = "what is python"
+    test_case_2 = LLMTestCase(
+        input=query, actual_output="Python is a programming lang"
+    )
+    run_test(test_case_2, metrics=[metric])
+
+
+def test_cross_encoder_answer_relevancy():
+    metric = AnswerRelevancyMetric(
+        minimum_score=0.5, model_type="cross_encoder"
+    )
+    test_case = LLMTestCase(input=query, actual_output=answer)
+    assert_test(test_case, [metric])
