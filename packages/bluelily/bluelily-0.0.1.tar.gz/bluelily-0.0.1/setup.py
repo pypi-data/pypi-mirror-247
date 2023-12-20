3825a9ccdb19583f6d95dbb62df2d9198cc9a0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:37 +0000

    Add package bluelily/0.0.1/bluelily-0.0.1.tar.gz

---
+++ packages/bluelily/bluelily-0.0.1.tar.gz/bluelily-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bluelily',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Bamboooz',
+    author_email='bambusixmc@gmail.com',
+    description='bluelily is an efficient library to manage GitHub repositories from python',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
