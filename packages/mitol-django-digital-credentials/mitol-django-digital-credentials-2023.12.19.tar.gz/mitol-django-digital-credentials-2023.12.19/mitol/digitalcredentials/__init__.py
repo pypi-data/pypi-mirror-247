8ec8f6c39e98cce0aac48d0f99463819f2605882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:24 +0000

    Add package mitol-django-digital-credentials/2023.12.19/mitol-django-digital-credentials-2023.12.19.tar.gz

---
+++ packages/mitol-django-digital-credentials/mitol-django-digital-credentials-2023.12.19.tar.gz/mitol-django-digital-credentials-2023.12.19/mitol/digitalcredentials/__init__.py
@@ -0,0 +1,5 @@
+""" mitol.digitalcredentials """
+default_app_config = "mitol.digitalcredentials.apps.DigitalCredentialsApp"
+
+__version__ = "2023.12.19"
+__distributionname__ = "mitol-django-digitalcredentials"
