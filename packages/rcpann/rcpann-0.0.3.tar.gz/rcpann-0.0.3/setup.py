f0b9e74c987cf1a93bf9ea2c2882645b505daa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:04:51 +0000

    Add package rcpann/0.0.3/rcpann-0.0.3.tar.gz

---
+++ packages/rcpann/rcpann-0.0.3.tar.gz/rcpann-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Ring Current Proton Artifician Neural Network Model'
+LONG_DESCRIPTION = 'Composed by Jinxing Li et al. This is the ann model for ring current protons from 13 keV to 598 keV'
+
+# Setting up
+setup(
+    name="rcpann",
+    version=VERSION,
+    author="Jinxing Li",
+    author_email="<jinxing.li.87@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'Ring Current', 'Space', 'Space physics', 'Radiation belt', 'magnetosphere'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    package_data={'': ['ann_model/*']},
+)
