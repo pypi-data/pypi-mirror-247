9c16615f6e995d9ba7c6a2294d8c9cba1d6d5c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:20 +0000

    Add package newguy103-pycrypter/1.0.1/newguy103-pycrypter-1.0.1.tar.gz

---
+++ packages/newguy103-pycrypter/newguy103-pycrypter-1.0.1.tar.gz/newguy103-pycrypter-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newguy103-pycrypter',
+    version='1.0.1',
+    packages=find_packages(),  # Define the package name
+    py_modules=['pycrypter.__init__', 'pycrypter._methods'],  # Add modules explicitly
+    install_requires=['cryptography'],
+    author='NewGuy103',
+    author_email='userchouenthusiast@gmail.com',
+    description='Simple cryptography wrapper created with threading.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://github.com/newguy103/pycrypter',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
+
