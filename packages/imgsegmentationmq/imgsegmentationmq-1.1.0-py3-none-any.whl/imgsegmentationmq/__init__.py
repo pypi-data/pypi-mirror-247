a02c819bf2940b2e6c705f511f4b454122c50922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:34 +0000

    Add package imgsegmentationmq/1.1.0/imgsegmentationmq-1.1.0-py3-none-any.whl

---
+++ packages/imgsegmentationmq/imgsegmentationmq-1.1.0-py3-none-any.whl/imgsegmentationmq/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+import random
+import math
+from imgsegmentation.helpingfunctions import initialize_centroids_data
+
+__all__ = ["means", "quantiles"]
---
+++ packages/imgsegmentationmq/imgsegmentationmq-1.1.0-py3-none-any.whl/imgsegmentationmq/quantiles/__init__.py
@@ -0,0 +1,5 @@
+from imgsegmentation.quantiles.helpingfunctionsquantile import cquantile_centroid,cquantile_pxl_classes,generalizedManhattan
+from imgsegmentation.quantiles.cquantile import cquantile
+from imgsegmentation.quantiles.kquantile import kquantile
+
+__all__ = ["cquantile", "kquantile"]
