99470f36c8377e7425be4b7522a652e4c641e455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:39 +0000

    Add package oebuild/0.0.38/oebuild-0.0.38-py3-none-any.whl

---
+++ packages/oebuild/oebuild-0.0.38-py3-none-any.whl/oebuild/app/plugins/bitbake/const.py
@@ -0,0 +1,20 @@
+'''
+Copyright (c) 2023 openEuler Embedded
+oebuild is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+'''
+CONTAINER_SRC = '/usr1/openeuler/src'
+CONTAINER_BUILD = '/home/openeuler'
+CONTAINER_USER = "openeuler"
+
+BASH_BANNER = '''
+    Welcome to the openEuler Embedded build environment, 
+    where you can run 'bitbake openeuler-image' to build 
+    standard images 
+'''
