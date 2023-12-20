5acdd6657b49c9adc5956e6202d540385c69bd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:06 +0000

    Add package neetbox/0.3.4/neetbox-0.3.4.tar.gz

---
+++ packages/neetbox/neetbox-0.3.4.tar.gz/neetbox-0.3.4/neetbox/utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Author: GavinGong aka VisualDust
+# Github: github.com/visualDust
+# Date:   20231206
+
+from ._package import pipPackageHealper as pkg
+from ._resource import ResourceLoader, download
+
+__all__ = ["pkg", "ResourceLoader", "download"]
