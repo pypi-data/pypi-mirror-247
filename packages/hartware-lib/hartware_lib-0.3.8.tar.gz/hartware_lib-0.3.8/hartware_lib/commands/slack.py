f05f36480fdabaa2b5b57941e228167c30893469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:04:03 +0000

    Add package hartware-lib/0.3.8/hartware_lib-0.3.8.tar.gz

---
+++ packages/hartware-lib/hartware_lib-0.3.8.tar.gz/hartware_lib-0.3.8/hartware_lib/commands/slack.py
@@ -0,0 +1,16 @@
+import argparse
+
+from hartware_lib.adapters.slack import SlackAdapter
+from hartware_lib.settings import SlackBotSettings, load_settings
+
+
+def slack_send(command: list = []):
+    parser = argparse.ArgumentParser(description="Slack message sender.")
+    parser.add_argument("message", help="set the message")
+    parser.add_argument("-c", "--channel", help="set the channel")
+
+    args = parser.parse_args(command or None)
+
+    settings = load_settings(SlackBotSettings, {})
+
+    return SlackAdapter(settings).send(message=args.message, channel=args.channel)
