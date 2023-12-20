24fddceaa7928269f6a7285da78f66b6a6735d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:15 +0000

    Add package kg-enricher/0.1.2/kg-enricher-0.1.2.tar.gz

---
+++ packages/kg-enricher/kg-enricher-0.1.2.tar.gz/kg-enricher-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kg-enricher",
+    version='0.1.2',
+    author="Renat Shigapov",
+    license="MIT",
+    description="A Python library for enriching strings, entities and KGs using Wikibase knowledge graphs. "
+                "It's adapted for people, organizations and German geographic entities, both modern and historical.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UB-Mannheim/kg-enricher",
+    install_requires=['geopandas', 'shapely', 'requests', 'appengine-python-standard'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
