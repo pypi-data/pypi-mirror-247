f535d3c45c487217d250a02a28a4e07e9f220aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:03:45 +0000

    Add package stocklake/0.0.6/stocklake-0.0.6.tar.gz

---
+++ packages/stocklake/stocklake-0.0.6.tar.gz/stocklake-0.0.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# with open("requirements.txt") as f:
+#     install_reqs = f.read().splitlines()
+
+# with open("dev-requirements.txt") as f:
+#     dev_install_reqs = f.read().splitlines()
+
+setuptools.setup(
+    name="stocklake",
+    version="0.0.6",
+    py_modules=["stocklake"],
+    install_requires=[
+        "polygon-api-client",
+        "python-dotenv",
+        "pandas",
+        "click",
+        "requests",
+    ],
+    packages=setuptools.find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    extras_require={"dev": ["ruff-lsp", "black"]},
+    entry_points={
+        "console_scripts": [
+            "stocklake = stocklake.cli:cli",
+        ],
+    },
+)
