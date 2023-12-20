9fda96bf253f262442814c0682e00e7c6cc44fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:06 +0000

    Add package quool/0.3.1/quool-0.3.1.tar.gz

---
+++ packages/quool/quool-0.3.1.tar.gz/quool-0.3.1/quool/__init__.py
@@ -0,0 +1,33 @@
+from .tools import (
+    Logger,
+    parse_commastr,
+    parse_date,
+    panelize,
+    reduce_mem_usage,
+    format_code,
+    strip_stock_code,
+)
+
+from .database import (
+    Table,
+    PanelTable,
+    FrameTable,
+)
+
+from .collector import (
+    Request,
+)
+
+from .backtest import (
+    BackTrader,
+    EventStudy,
+    Relocator,
+    Strategy,
+    Indicator,
+    Analyzer,
+    Observer,
+    OrderTable,
+    CashValueRecorder,
+)
+
+__version__ = "0.3.1"
