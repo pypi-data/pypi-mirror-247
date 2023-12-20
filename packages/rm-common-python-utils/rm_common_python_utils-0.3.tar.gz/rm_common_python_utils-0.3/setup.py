dfa1a09ca710e7043a7c167ed3dc5190c41a46c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:58 +0000

    Add package rm-common-python-utils/0.3/rm_common_python_utils-0.3.tar.gz

---
+++ packages/rm-common-python-utils/rm_common_python_utils-0.3.tar.gz/rm_common_python_utils-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rm_common_python_utils",
+    version="0.3",
+    author="qwe",
+    author_email="",
+    description="asdasdasd",
+    packages=find_packages(),
+)
