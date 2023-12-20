7cfd42b08ff1306f160d16360dc16fcfe388ecaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:30 +0000

    Add package spotiflow/0.0.0.1/spotiflow-0.0.0.1.tar.gz

---
+++ packages/spotiflow/spotiflow-0.0.0.1.tar.gz/spotiflow-0.0.0.1/setup.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, print_function
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from numpy.distutils.misc_util import get_numpy_include_dirs
+from os import path
+
+# ------------------------------------------------------------------------------------
+_dir = path.dirname(__file__)
+
+setup(
+)
