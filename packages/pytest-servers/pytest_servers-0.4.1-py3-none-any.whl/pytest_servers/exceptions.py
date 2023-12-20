80ed102a9830a3d0fd263fdc592fdb8c467b5c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:32 +0000

    Add package pytest-servers/0.4.1/pytest_servers-0.4.1-py3-none-any.whl

---
+++ packages/pytest-servers/pytest_servers-0.4.1-py3-none-any.whl/pytest_servers/exceptions.py
@@ -0,0 +1,11 @@
+class PytestServersException(Exception):  # noqa: N818
+    """Base class for all pytest-servers exceptions."""
+
+    def __init__(self, msg: str, *args):
+        assert msg
+        self.msg = msg
+        super().__init__(msg, *args)
+
+
+class RemoteUnavailable(PytestServersException):
+    """Raise when the given remote is not available."""
