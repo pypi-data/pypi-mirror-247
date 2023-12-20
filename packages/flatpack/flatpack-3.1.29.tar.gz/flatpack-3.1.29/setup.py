7578074c8d21eda9f386e8a19b43255d7d57afa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:54 +0000

    Add package flatpack/3.1.29/flatpack-3.1.29.tar.gz

---
+++ packages/flatpack/flatpack-3.1.29.tar.gz/flatpack-3.1.29/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="3.1.29",
+    license="Apache Software License (Apache-2.0)",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography==41.0.7",
+        "httpx==0.25.2",
+        "toml==0.10.2",
+        "transformers==4.36.2",
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Ready-to-assemble AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
