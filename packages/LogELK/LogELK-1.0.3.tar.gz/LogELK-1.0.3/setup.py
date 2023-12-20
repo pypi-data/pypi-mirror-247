8f7cc76890cfe1263a4ea6270b7fe614b63ba615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:04:43 +0000

    Add package LogELK/1.0.3/LogELK-1.0.3.tar.gz

---
+++ packages/LogELK/LogELK-1.0.3.tar.gz/LogELK-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.3' 
+DESCRIPTION = 'Envia os logs para o ELK'
+LONG_DESCRIPTION = 'Envia os logs para o ELK através de requisições HTTP'
+
+setup(
+        name="LogELK",
+        version=VERSION,
+        author="Jones Vieira",
+        author_email="<jones.vieira@tivit.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            'requests'
+            'urllib3'
+        ]
+)
