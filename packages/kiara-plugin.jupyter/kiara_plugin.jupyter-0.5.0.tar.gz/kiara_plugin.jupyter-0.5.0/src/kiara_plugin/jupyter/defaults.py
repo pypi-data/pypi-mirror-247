b1155e3a38b32e31b69f3047863b103115b0a06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:18 +0000

    Add package kiara-plugin.jupyter/0.5.0/kiara_plugin.jupyter-0.5.0.tar.gz

---
+++ packages/kiara-plugin.jupyter/kiara_plugin.jupyter-0.5.0.tar.gz/kiara_plugin.jupyter-0.5.0/src/kiara_plugin/jupyter/defaults.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+OFFICIAL_KIARA_PLUGINS = [
+    "core_types",
+    "tabular",
+    "onboarding",
+    "network_analysis",
+    "language_processing",
+    "html",
+    "streamlit",
+]
