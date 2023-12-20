42e786e80e2d6133a929d8e2af306f1aa9b1cc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:10 +0000

    Add package klinoff-math/0.1.1/klinoff-math-0.1.1.tar.gz

---
+++ packages/klinoff-math/klinoff-math-0.1.1.tar.gz/klinoff-math-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='klinoff-math',
+    version='0.1.1',
+    description='A simple klinoff math library',
+    author='Klinoff TMS',
+    py_modules=['klinoffmath'],
+    install_requires=['simpleaudio'],
+)
