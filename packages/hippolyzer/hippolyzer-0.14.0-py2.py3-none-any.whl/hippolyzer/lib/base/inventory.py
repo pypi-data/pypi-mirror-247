7a22d75a9b167735451fc2d3d7ca912e740e7b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:50 +0000

    Add package hippolyzer/0.14.0/hippolyzer-0.14.0-py2.py3-none-any.whl

---
+++ packages/hippolyzer/hippolyzer-0.14.0-py2.py3-none-any.whl/hippolyzer/lib/base/inventory.py
@@ -0,0 +1,436 @@
+"""
+Parse the horrible legacy inventory-related format.
+
+It's typically only used for object contents now.
+"""
+from __future__ import annotations
+
+import dataclasses
+import datetime as dt
+import logging
+import struct
+import typing
+import weakref
+from io import StringIO
+from typing import *
+
+from hippolyzer.lib.base.datatypes import UUID
+from hippolyzer.lib.base.legacy_schema import (
+    parse_schema_line,
+    SchemaBase,
+    SchemaDate,
+    SchemaFieldSerializer,
+    SchemaHexInt,
+    SchemaInt,
+    SchemaLLSD,
+    SchemaMultilineStr,
+    SchemaParsingError,
+    SchemaStr,
+    SchemaUUID,
+    schema_field,
+)
+
+MAGIC_ID = UUID("3c115e51-04f4-523c-9fa6-98aff1034730")
+LOG = logging.getLogger(__name__)
+_T = TypeVar("_T")
+
+
+class SchemaFlagField(SchemaHexInt):
+    """Like a hex int, but must be serialized as bytes in LLSD due to being a U32"""
+    @classmethod
+    def from_llsd(cls, val: Any) -> int:
+        return struct.unpack("!I", val)[0]
+
+    @classmethod
+    def to_llsd(cls, val: int) -> Any:
+        return struct.pack("!I", val)
+
+
+def _yield_schema_tokens(reader: StringIO):
+    in_bracket = False
+    # empty str == EOF in Python
+    while line := reader.readline():
+        line = line.strip()
+        # Whitespace-only lines are automatically skipped
+        if not line:
+            continue
+        try:
+            key, val = parse_schema_line(line)
+        except SchemaParsingError:
+            # Can happen if there's a malformed multi-line string, just
+            # skip by it.
+            LOG.warning(f"Found invalid inventory line {line!r}")
+            continue
+        if key == "{":
+            if in_bracket:
+                LOG.warning("Found multiple opening brackets inside structure, "
+                            "was a nested structure not handled?")
+            in_bracket = True
+            continue
+        if key == "}":
+            if not in_bracket:
+                LOG.warning("Unexpected closing bracket")
+            in_bracket = False
+            break
+        yield key, val
+    if in_bracket:
+        LOG.warning("Reached EOF while inside a bracket")
+
+
+class InventoryBase(SchemaBase):
+    SCHEMA_NAME: ClassVar[str]
+
+    @classmethod
+    def from_reader(cls, reader: StringIO, read_header=False) -> InventoryBase:
+        tok_iter = _yield_schema_tokens(reader)
+        # Someone else hasn't already read the header for us
+        if read_header:
+            schema_name, _ = next(tok_iter)
+            if schema_name != cls.SCHEMA_NAME:
+                raise ValueError(f"Expected schema name {schema_name!r} to be {cls.SCHEMA_NAME!r}")
+
+        fields = cls._get_fields_dict()
+        obj_dict = {}
+        for key, val in tok_iter:
+            if key in fields:
+                field: dataclasses.Field = fields[key]
+                spec = field.metadata.get("spec")
+                # Not a real key, an internal var on our dataclass
+                if not spec:
+                    LOG.warning(f"Internal key {key!r}")
+                    continue
+                # some kind of nested structure like sale_info
+                if issubclass(spec, SchemaBase):
+                    obj_dict[key] = spec.from_reader(reader)
+                elif issubclass(spec, SchemaFieldSerializer):
+                    obj_dict[key] = spec.deserialize(val)
+                else:
+                    raise ValueError(f"Unsupported spec for {key!r}, {spec!r}")
+            else:
+                LOG.warning(f"Unknown key {key!r}")
+        return cls._obj_from_dict(obj_dict)
+
+    def to_writer(self, writer: StringIO):
+        writer.write(f"\t{self.SCHEMA_NAME}\t0\n")
+        writer.write("\t{\n")
+        for field_name, field in self._get_fields_dict().items():
+            spec = field.metadata.get("spec")
+            # Not meant to be serialized
+            if not spec:
+                continue
+            if field.metadata.get("llsd_only"):
+                continue
+
+            val = getattr(self, field_name)
+            if val is None:
+                continue
+
+            # Some kind of nested structure like sale_info
+            if isinstance(val, SchemaBase):
+                val.to_writer(writer)
+            elif issubclass(spec, SchemaFieldSerializer):
+                writer.write(f"\t\t{field_name}\t{spec.serialize(val)}\n")
+            else:
+                raise ValueError(f"Bad inventory spec {spec!r}")
+        writer.write("\t}\n")
+
+
+class InventoryDifferences(typing.NamedTuple):
+    changed: List[InventoryNodeBase]
+    removed: List[InventoryNodeBase]
+
+
+class InventoryModel(InventoryBase):
+    def __init__(self):
+        self.nodes: Dict[UUID, InventoryNodeBase] = {}
+        self.root: Optional[InventoryContainerBase] = None
+
+    @classmethod
+    def from_reader(cls, reader: StringIO, read_header=False) -> InventoryModel:
+        model = cls()
+        for key, value in _yield_schema_tokens(reader):
+            if key == "inv_object":
+                obj = InventoryObject.from_reader(reader)
+                if obj is not None:
+                    model.add(obj)
+            elif key == "inv_category":
+                cat = InventoryCategory.from_reader(reader)
+                if cat is not None:
+                    model.add(cat)
+            elif key == "inv_item":
+                item = InventoryItem.from_reader(reader)
+                if item is not None:
+                    model.add(item)
+            else:
+                LOG.warning("Unknown key {0}".format(key))
+        return model
+
+    @classmethod
+    def from_llsd(cls, llsd_val: List[Dict]) -> InventoryModel:
+        model = cls()
+        for obj_dict in llsd_val:
+            for inv_type in INVENTORY_TYPES:
+                if inv_type.ID_ATTR in obj_dict:
+                    if (obj := inv_type.from_llsd(obj_dict)) is not None:
+                        model.add(obj)
+                    break
+                LOG.warning(f"Unknown object type {obj_dict!r}")
+        return model
+
+    @property
+    def ordered_nodes(self) -> Iterable[InventoryNodeBase]:
+        yield from self.all_containers
+        yield from self.all_items
+
+    @property
+    def all_containers(self) -> Iterable[InventoryContainerBase]:
+        for node in self.nodes.values():
+            if isinstance(node, InventoryContainerBase):
+                yield node
+
+    @property
+    def all_items(self) -> Iterable[InventoryItem]:
+        for node in self.nodes.values():
+            if not isinstance(node, InventoryContainerBase):
+                yield node
+
+    def __eq__(self, other):
+        if not isinstance(other, InventoryModel):
+            return False
+        return set(self.nodes.values()) == set(other.nodes.values())
+
+    def to_writer(self, writer: StringIO):
+        for node in self.ordered_nodes:
+            node.to_writer(writer)
+
+    def to_llsd(self):
+        return list(node.to_llsd() for node in self.ordered_nodes)
+
+    def add(self, node: InventoryNodeBase):
+        if node.node_id in self.nodes:
+            raise KeyError(f"{node.node_id} already exists in the inventory model")
+
+        self.nodes[node.node_id] = node
+        if isinstance(node, InventoryContainerBase):
+            if node.parent_id == UUID.ZERO:
+                self.root = node
+        node.model = weakref.proxy(self)
+
+    def unlink(self, node: InventoryNodeBase, single_only: bool = False) -> Sequence[InventoryNodeBase]:
+        """Unlink a node and its descendants from the tree, returning the removed nodes"""
+        assert node.model == self
+        if node == self.root:
+            self.root = None
+        unlinked = [node]
+        if isinstance(node, InventoryContainerBase) and not single_only:
+            for child in node.children:
+                unlinked.extend(self.unlink(child))
+        self.nodes.pop(node.node_id, None)
+        node.model = None
+        return unlinked
+
+    def get_differences(self, other: InventoryModel) -> InventoryDifferences:
+        # Includes modified things with the same ID
+        changed_in_other = []
+        removed_in_other = []
+
+        other_keys = set(other.nodes.keys())
+        our_keys = set(self.nodes.keys())
+
+        # Removed
+        for key in our_keys - other_keys:
+            removed_in_other.append(self.nodes[key])
+
+        # Updated
+        for key in other_keys.intersection(our_keys):
+            other_node = other.nodes[key]
+            if other_node != self.nodes[key]:
+                changed_in_other.append(other_node)
+
+        # Added
+        for key in other_keys - our_keys:
+            changed_in_other.append(other.nodes[key])
+        return InventoryDifferences(
+            changed=changed_in_other,
+            removed=removed_in_other,
+        )
+
+    def __getitem__(self, item: UUID) -> InventoryNodeBase:
+        return self.nodes[item]
+
+    def __contains__(self, item: UUID):
+        return item in self.nodes
+
+    def get(self, item: UUID) -> Optional[InventoryNodeBase]:
+        return self.nodes.get(item)
+
+
+@dataclasses.dataclass
+class InventoryPermissions(InventoryBase):
+    SCHEMA_NAME: ClassVar[str] = "permissions"
+
+    base_mask: int = schema_field(SchemaHexInt)
+    owner_mask: int = schema_field(SchemaHexInt)
+    group_mask: int = schema_field(SchemaHexInt)
+    everyone_mask: int = schema_field(SchemaHexInt)
+    next_owner_mask: int = schema_field(SchemaHexInt)
+    creator_id: UUID = schema_field(SchemaUUID)
+    owner_id: UUID = schema_field(SchemaUUID)
+    last_owner_id: UUID = schema_field(SchemaUUID)
+    group_id: UUID = schema_field(SchemaUUID)
+    # Nothing actually cares about this, but it could be there.
+    # It's kind of redundant since it just means owner_id == NULL_KEY && group_id != NULL_KEY.
+    is_owner_group: int = schema_field(SchemaInt, default=0, llsd_only=True)
+
+
+@dataclasses.dataclass
+class InventorySaleInfo(InventoryBase):
+    SCHEMA_NAME: ClassVar[str] = "sale_info"
+
+    sale_type: str = schema_field(SchemaStr)
+    sale_price: int = schema_field(SchemaInt)
+
+
+@dataclasses.dataclass
+class InventoryNodeBase(InventoryBase):
+    ID_ATTR: ClassVar[str]
+
+    name: str
+
+    parent_id: Optional[UUID] = schema_field(SchemaUUID)
+    model: Optional[InventoryModel] = dataclasses.field(
+        default=None, init=False, hash=False, compare=False, repr=False
+    )
+
+    @property
+    def node_id(self) -> UUID:
+        return getattr(self, self.ID_ATTR)
+
+    @node_id.setter
+    def node_id(self, val: UUID):
+        setattr(self, self.ID_ATTR, val)
+
+    @property
+    def parent(self) -> Optional[InventoryContainerBase]:
+        return self.model.nodes.get(self.parent_id)
+
+    def unlink(self) -> Sequence[InventoryNodeBase]:
+        return self.model.unlink(self)
+
+    @classmethod
+    def _obj_from_dict(cls, obj_dict):
+        # Bad entry, ignore
+        # TODO: Check on these. might be symlinks or something.
+        if obj_dict.get("type") == "-1":
+            LOG.warning(f"Skipping bad object with type == -1: {obj_dict!r}")
+            return None
+        return super()._obj_from_dict(obj_dict)
+
+    def __hash__(self):
+        return hash(self.node_id)
+
+    def __iter__(self) -> Iterator[InventoryNodeBase]:
+        return iter(())
+
+    def __contains__(self, item) -> bool:
+        return item in tuple(self)
+
+
+@dataclasses.dataclass
+class InventoryContainerBase(InventoryNodeBase):
+    type: str = schema_field(SchemaStr)
+    name: str = schema_field(SchemaMultilineStr)
+
+    @property
+    def children(self) -> Sequence[InventoryNodeBase]:
+        return tuple(
+            x for x in self.model.nodes.values()
+            if x.parent_id == self.node_id
+        )
+
+    def __getitem__(self, item: Union[int, str]) -> InventoryNodeBase:
+        if isinstance(item, int):
+            return self.children[item]
+
+        for child in self.children:
+            if child.name == item:
+                return child
+        raise KeyError(f"{item!r} not found in children")
+
+    def __iter__(self) -> Iterator[InventoryNodeBase]:
+        return iter(self.children)
+
+    def get_or_create_subcategory(self, name: str) -> InventoryCategory:
+        for child in self:
+            if child.name == name and isinstance(child, InventoryCategory):
+                return child
+        child = InventoryCategory(
+            name=name,
+            cat_id=UUID.random(),
+            parent_id=self.node_id,
+            type="category",
+            pref_type="-1",
+            owner_id=getattr(self, 'owner_id', UUID.ZERO),
+            version=1,
+        )
+        self.model.add(child)
+        return child
+
+    # So autogenerated __hash__ doesn't kill our inherited one
+    __hash__ = InventoryNodeBase.__hash__
+
+
+@dataclasses.dataclass
+class InventoryObject(InventoryContainerBase):
+    SCHEMA_NAME: ClassVar[str] = "inv_object"
+    ID_ATTR: ClassVar[str] = "obj_id"
+
+    obj_id: UUID = schema_field(SchemaUUID)
+    metadata: Optional[Dict[str, Any]] = schema_field(SchemaLLSD, default=None)
+
+    __hash__ = InventoryNodeBase.__hash__
+
+
+@dataclasses.dataclass
+class InventoryCategory(InventoryContainerBase):
+    ID_ATTR: ClassVar[str] = "cat_id"
+    SCHEMA_NAME: ClassVar[str] = "inv_category"
+    VERSION_NONE: ClassVar[int] = -1
+
+    cat_id: UUID = schema_field(SchemaUUID)
+    pref_type: str = schema_field(SchemaStr, llsd_name="preferred_type")
+    owner_id: UUID = schema_field(SchemaUUID)
+    version: int = schema_field(SchemaInt)
+    metadata: Optional[Dict[str, Any]] = schema_field(SchemaLLSD, default=None)
+
+    __hash__ = InventoryNodeBase.__hash__
+
+
+@dataclasses.dataclass
+class InventoryItem(InventoryNodeBase):
+    SCHEMA_NAME: ClassVar[str] = "inv_item"
+    ID_ATTR: ClassVar[str] = "item_id"
+
+    item_id: UUID = schema_field(SchemaUUID)
+    type: str = schema_field(SchemaStr)
+    inv_type: str = schema_field(SchemaStr)
+    flags: int = schema_field(SchemaFlagField)
+    name: str = schema_field(SchemaMultilineStr)
+    desc: str = schema_field(SchemaMultilineStr)
+    creation_date: dt.datetime = schema_field(SchemaDate, llsd_name="created_at")
+    permissions: InventoryPermissions = schema_field(InventoryPermissions)
+    sale_info: InventorySaleInfo = schema_field(InventorySaleInfo)
+    asset_id: Optional[UUID] = schema_field(SchemaUUID, default=None)
+    shadow_id: Optional[UUID] = schema_field(SchemaUUID, default=None)
+    metadata: Optional[Dict[str, Any]] = schema_field(SchemaLLSD, default=None)
+
+    __hash__ = InventoryNodeBase.__hash__
+
+    @property
+    def true_asset_id(self) -> UUID:
+        if self.asset_id is not None:
+            return self.asset_id
+        return self.shadow_id ^ MAGIC_ID
+
+
+INVENTORY_TYPES: Tuple[Type[InventoryNodeBase], ...] = (InventoryCategory, InventoryObject, InventoryItem)
---
+++ packages/hippolyzer/hippolyzer-0.14.0-py2.py3-none-any.whl/hippolyzer/lib/client/inventory_manager.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+
+import gzip
+import logging
+import secrets
+from pathlib import Path
+from typing import Union, List, Tuple, Set
+
+from hippolyzer.lib.base import llsd
+from hippolyzer.lib.base.datatypes import UUID
+from hippolyzer.lib.base.inventory import InventoryModel, InventoryCategory, InventoryItem
+from hippolyzer.lib.base.message.message import Block
+from hippolyzer.lib.client.state import BaseClientSession
+
+
+LOG = logging.getLogger(__name__)
+
+
+class InventoryManager:
+    def __init__(self, session: BaseClientSession):
+        self._session = session
+        self.model: InventoryModel = InventoryModel()
+        self._load_skeleton()
+
+    def _load_skeleton(self):
+        assert not self.model.nodes
+        skel_cats: List[dict] = self._session.login_data.get('inventory-skeleton', [])
+        for skel_cat in skel_cats:
+            self.model.add(InventoryCategory(
+                name=skel_cat["name"],
+                cat_id=UUID(skel_cat["folder_id"]),
+                parent_id=UUID(skel_cat["parent_id"]),
+                # Don't use the version from the skeleton, this flags the inventory as needing
+                # completion from the inventory cache. This matches indra's behavior.
+                version=InventoryCategory.VERSION_NONE,
+                type="category",
+                pref_type=skel_cat.get("type_default", "-1"),
+                owner_id=self._session.agent_id,
+            ))
+
+    def load_cache(self, path: Union[str, Path]):
+        # Per indra, rough flow for loading inv on login is:
+        # 1. Look at inventory skeleton from login response
+        # 2. Pre-populate model with categories from the skeleton, including their versions
+        # 3. Read the inventory cache, tracking categories and items separately
+        # 4. Walk the list of categories in our cache. If the cat exists in the skeleton and the versions
+        #    match, then we may load the category and its descendants from cache.
+        # 5. Any categories in the skeleton but not in the cache, or those with mismatched versions must be fetched.
+        #    The viewer does this by setting the local version of the cats to -1 and forcing a descendent fetch
+        #    over AIS.
+        #
+        # By the time you call this function call, you should have already loaded the inventory skeleton
+        # into the model set its inventory category versions to VERSION_NONE.
+
+        skel_cats: List[dict] = self._session.login_data['inventory-skeleton']
+        # UUID -> version map for inventory skeleton
+        skel_versions = {UUID(cat["folder_id"]): cat["version"] for cat in skel_cats}
+        LOG.info(f"Parsing inv cache at {path}")
+        cached_categories, cached_items = self._parse_cache(path)
+        LOG.info(f"Done parsing inv cache at {path}")
+        loaded_cat_ids: Set[UUID] = set()
+
+        for cached_cat in cached_categories:
+            existing_cat: InventoryCategory = self.model.get(cached_cat.cat_id)  # noqa
+            # Don't clobber an existing cat unless it just has a placeholder version,
+            # maybe from loading the skeleton?
+            if existing_cat and existing_cat.version != InventoryCategory.VERSION_NONE:
+                continue
+            # Cached cat isn't the same as what the inv server says it should be, can't use it.
+            if cached_cat.version != skel_versions.get(cached_cat.cat_id):
+                continue
+            if existing_cat:
+                # Remove the category so that we can replace it, but leave any children in place
+                self.model.unlink(existing_cat, single_only=True)
+            self.model.add(cached_cat)
+            # Any items in this category in our cache file are usable and should be added
+            loaded_cat_ids.add(cached_cat.cat_id)
+
+        for cached_item in cached_items:
+            # The skeleton doesn't have any items, so if we run into any items they should be exactly the
+            # same as what we're trying to add. No point clobbering.
+            if cached_item.item_id in self.model:
+                continue
+            # The parent category didn't have a cache hit against the inventory skeleton, can't add!
+            if cached_item.parent_id not in loaded_cat_ids:
+                continue
+            self.model.add(cached_item)
+
+    def _parse_cache(self, path: Union[str, Path]) -> Tuple[List[InventoryCategory], List[InventoryItem]]:
+        categories: List[InventoryCategory] = []
+        items: List[InventoryItem] = []
+        # Parse our cached items and categories out of the compressed inventory cache
+        first_line = True
+        with gzip.open(path, "rb") as f:
+            # Line-delimited LLSD notation!
+            for line in f.readlines():
+                # TODO: Parsing of invcache is dominated by `parse_notation()`. It's stupidly inefficient.
+                node_llsd = llsd.parse_notation(line)
+                if first_line:
+                    # First line is the file header
+                    first_line = False
+                    if node_llsd['inv_cache_version'] not in (2, 3):
+                        raise ValueError(f"Unknown cache version: {node_llsd!r}")
+                    continue
+
+                if InventoryCategory.ID_ATTR in node_llsd:
+                    if (cat_node := InventoryCategory.from_llsd(node_llsd)) is not None:
+                        categories.append(cat_node)
+                elif InventoryItem.ID_ATTR in node_llsd:
+                    if (item_node := InventoryItem.from_llsd(node_llsd)) is not None:
+                        items.append(item_node)
+                else:
+                    LOG.warning(f"Unknown node type in inv cache: {node_llsd!r}")
+        return categories, items
+
+
+# Thankfully we have 9 billion different ways to represent inventory data.
+def ais_item_to_inventory_data(ais_item: dict) -> Block:
+    return Block(
+        "InventoryData",
+        ItemID=ais_item["item_id"],
+        FolderID=ais_item["parent_id"],
+        CallbackID=0,
+        CreatorID=ais_item["permissions"]["creator_id"],
+        OwnerID=ais_item["permissions"]["owner_id"],
+        GroupID=ais_item["permissions"]["group_id"],
+        BaseMask=ais_item["permissions"]["base_mask"],
+        OwnerMask=ais_item["permissions"]["owner_mask"],
+        GroupMask=ais_item["permissions"]["group_mask"],
+        EveryoneMask=ais_item["permissions"]["everyone_mask"],
+        NextOwnerMask=ais_item["permissions"]["next_owner_mask"],
+        GroupOwned=0,
+        AssetID=ais_item["asset_id"],
+        Type=ais_item["type"],
+        InvType=ais_item["inv_type"],
+        Flags=ais_item["flags"],
+        SaleType=ais_item["sale_info"]["sale_type"],
+        SalePrice=ais_item["sale_info"]["sale_price"],
+        Name=ais_item["name"],
+        Description=ais_item["desc"],
+        CreationDate=ais_item["created_at"],
+        # Meaningless here
+        CRC=secrets.randbits(32),
+    )
+
+
+def inventory_data_to_ais_item(inventory_data: Block) -> dict:
+    return dict(
+        item_id=inventory_data["ItemID"],
+        parent_id=inventory_data["ParentID"],
+        permissions=dict(
+            creator_id=inventory_data["CreatorID"],
+            owner_id=inventory_data["OwnerID"],
+            group_id=inventory_data["GroupID"],
+            base_mask=inventory_data["BaseMask"],
+            owner_mask=inventory_data["OwnerMask"],
+            group_mask=inventory_data["GroupMask"],
+            everyone_mask=inventory_data["EveryoneMask"],
+            next_owner_mask=inventory_data["NextOwnerMask"],
+        ),
+        asset_id=inventory_data["AssetID"],
+        type=inventory_data["Type"],
+        inv_type=inventory_data["InvType"],
+        flags=inventory_data["Flags"],
+        sale_info=dict(
+            sale_type=inventory_data["SaleType"],
+            sale_price=inventory_data["SalePrice"],
+        ),
+        name=inventory_data["Name"],
+        description=inventory_data["Description"],
+        creation_at=inventory_data["CreationDate"],
+    )
+
+
+def ais_folder_to_inventory_data(ais_folder: dict) -> Block:
+    return Block(
+        "FolderData",
+        FolderID=ais_folder["cat_id"],
+        ParentID=ais_folder["parent_id"],
+        CallbackID=0,
+        Type=ais_folder["preferred_type"],
+        Name=ais_folder["name"],
+    )
+
+
+def inventory_data_to_ais_folder(inventory_data: Block) -> dict:
+    return dict(
+        cat_id=inventory_data["FolderID"],
+        parent_id=inventory_data["ParentID"],
+        preferred_type=inventory_data["Type"],
+        name=inventory_data["Name"],
+    )
