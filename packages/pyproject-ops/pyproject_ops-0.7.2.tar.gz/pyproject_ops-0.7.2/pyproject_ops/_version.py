045e917135ab4ab5c66bb8b9ea1ba3ff8618d591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:11 +0000

    Add package pyproject-ops/0.7.2/pyproject_ops-0.7.2.tar.gz

---
+++ packages/pyproject-ops/pyproject_ops-0.7.2.tar.gz/pyproject_ops-0.7.2/pyproject_ops/_version.py
@@ -0,0 +1,6 @@
+__version__ = "0.7.2"
+
+# keep this ``if __name__ == "__main__"``, don't delete!
+# this is used by automation script to detect the project version
+if __name__ == "__main__":  # pragma: no cover
+    print(__version__)
