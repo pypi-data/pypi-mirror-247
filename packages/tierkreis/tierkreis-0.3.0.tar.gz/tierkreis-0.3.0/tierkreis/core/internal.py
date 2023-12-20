9b8e46911b886d728091d678ea0cad9088a5df63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:42 +0000

    Add package tierkreis/0.3.0/tierkreis-0.3.0.tar.gz

---
+++ packages/tierkreis/tierkreis-0.3.0.tar.gz/tierkreis-0.3.0/tierkreis/core/internal.py
@@ -0,0 +1,37 @@
+import typing
+from typing import Any, Dict, cast
+
+
+def python_struct_fields(type_: typing.Type) -> Dict[str, typing.Type]:
+    def substitute(
+        type_: typing.Type, subst: Dict[typing.TypeVar, typing.Type]
+    ) -> typing.Type:
+        "Substitute type variables in a type."
+        if isinstance(type_, typing.TypeVar):
+            if type_ in subst:
+                return subst[type_]
+            else:
+                return cast(typing.Type, type_)
+
+        type_origin = typing.get_origin(type_)
+        if type_origin is None:
+            return type_
+        else:
+            return type_origin[  # type: ignore
+                tuple(
+                    [substitute(arg_type, subst) for arg_type in typing.get_args(type_)]
+                )
+            ]
+
+    if typing.get_origin(type_) is None:
+        # Non generic type
+        return typing.get_type_hints(type_)
+    else:
+        # Generic type
+        type_origin = typing.get_origin(type_)
+        type_subst = dict(zip(cast(Any, type_).__parameters__, typing.get_args(type_)))
+
+        return {
+            field_name: substitute(field_type, type_subst)
+            for field_name, field_type in typing.get_type_hints(type_origin).items()
+        }
---
+++ packages/tierkreis/tierkreis-0.3.0.tar.gz/tierkreis-0.3.0/tierkreis/core/python.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import typing
+from dataclasses import dataclass
+from typing import Generic
+
+import tierkreis.core.protos.tierkreis.v1alpha.graph as pg
+from tierkreis.core.tierkreis_struct import TierkreisStruct
+
+if typing.TYPE_CHECKING:
+    from tierkreis.core.tierkreis_graph import TierkreisGraph
+
+
+In = typing.TypeVar("In", bound=TierkreisStruct)
+Out = typing.TypeVar("Out", bound=TierkreisStruct)
+
+
+@dataclass
+class RuntimeGraph(Generic[In, Out]):
+    "Graph with a `RuntimeStruct` annotation for inputs and outputs."
+
+    graph: TierkreisGraph
+
+    def to_proto(self) -> pg.Value:
+        return pg.Value(graph=self.graph.to_proto())
