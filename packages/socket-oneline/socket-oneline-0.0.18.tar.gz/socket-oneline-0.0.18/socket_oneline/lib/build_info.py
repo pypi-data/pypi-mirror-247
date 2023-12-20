521ca3945539e44de3ff4ea7e7a2d8e1541f1afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:49 +0000

    Add package socket-oneline/0.0.18/socket-oneline-0.0.18.tar.gz

---
+++ packages/socket-oneline/socket-oneline-0.0.18.tar.gz/socket-oneline-0.0.18/socket_oneline/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.13'
+    os_name = 'posix:ubuntu'
+    version = '0.0.18'
+    git_sha = 'd2fa04d1d6b36a827e05aae48706c97bf863ce0a'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
