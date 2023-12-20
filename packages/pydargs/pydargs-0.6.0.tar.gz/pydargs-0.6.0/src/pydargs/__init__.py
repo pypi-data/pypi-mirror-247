fc5ae2e89b21e0e74f921aad2f938d6d20f36093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:40 +0000

    Add package pydargs/0.6.0/pydargs-0.6.0.tar.gz

---
+++ packages/pydargs/pydargs-0.6.0.tar.gz/pydargs-0.6.0/src/pydargs/__init__.py
@@ -0,0 +1,194 @@
+import argparse
+import sys
+from argparse import ArgumentParser
+from collections.abc import Sequence
+from dataclasses import MISSING, dataclass, fields
+from datetime import date, datetime
+from enum import Enum
+from functools import partial
+from typing import (
+    Any,
+    Type,
+    TypeVar,
+    Optional,
+    Protocol,
+    get_origin,
+    get_args,
+    Union,
+    Literal,
+)
+
+UNION_TYPES: set[Any] = {Union}
+if sys.version_info >= (3, 10):
+    from types import UnionType
+
+    UNION_TYPES.add(UnionType)
+
+
+@dataclass
+class ADataclass(Protocol):
+    ...
+
+
+Dataclass = TypeVar("Dataclass", bound=ADataclass)
+
+
+def parse(tp: Type[Dataclass], args: Optional[list[str]] = None, **kwargs: Any) -> Dataclass:
+    namespace = _create_parser(tp, **kwargs).parse_args(args)
+    return tp(**namespace.__dict__)
+
+
+def _create_parser(tp: Type[Dataclass], **kwargs: Any) -> ArgumentParser:
+    parser = ArgumentParser(**kwargs)
+    for field in fields(tp):
+        if field.metadata.get("ignore_arg", False):
+            continue
+
+        argument_kwargs: dict[str, Any] = dict()
+        field_has_default = field.default is not MISSING or field.default_factory is not MISSING
+        positional = field.metadata.get("positional", False)
+        if positional:
+            argument_name = field.name
+            if field_has_default:
+                argument_kwargs["nargs"] = "?"
+        else:
+            argument_name = f"--{field.name.replace('_', '-')}"
+            argument_kwargs = dict(dest=field.name, required=not field_has_default)
+
+        if parser_fct := field.metadata.get("parser", None):
+            parser.add_argument(
+                argument_name,
+                default=argparse.SUPPRESS,
+                help=f"Override field {field.name}.",
+                type=parser_fct,
+                **argument_kwargs,
+            )
+        elif origin := get_origin(field.type):
+            if origin is Sequence or origin is list:
+                argument_kwargs["nargs"] = "*" if field_has_default else "+"
+                parser.add_argument(
+                    argument_name,
+                    default=argparse.SUPPRESS,
+                    help=f"Override field {field.name}.",
+                    type=get_args(field.type)[0],
+                    **argument_kwargs,
+                )
+            elif origin is Literal:
+                if len({type(arg) for arg in get_args(field.type)}) > 1:
+                    raise NotImplementedError("Parsing Literals with mixed types is not supported.")
+                parser.add_argument(
+                    argument_name,
+                    choices=get_args(field.type),
+                    default=field.default if positional and field_has_default else argparse.SUPPRESS,
+                    help=f"Override field {field.name}.",
+                    type=type(get_args(field.type)[0]),
+                    **argument_kwargs,
+                )
+            elif origin in UNION_TYPES:
+                union_parser = partial(_parse_union, union_type=field.type)
+                setattr(union_parser, "__name__", repr(field.type))
+                parser.add_argument(
+                    argument_name,
+                    default=argparse.SUPPRESS,
+                    help=f"Override field {field.name}.",
+                    type=union_parser,
+                    **argument_kwargs,
+                )
+            else:
+                raise NotImplementedError(f"Parsing into type {origin} is not implemented.")
+        elif field.type in (date, datetime):
+            parser.add_argument(
+                argument_name,
+                default=argparse.SUPPRESS,
+                help=f"Override field {field.name}.",
+                type=partial(
+                    _parse_datetime, is_date=field.type is date, date_format=field.metadata.get("date_format")
+                ),
+                **argument_kwargs,
+            )
+        elif field.type is bool:
+            if field.metadata.get("as_flags", False):
+                if positional:
+                    raise ValueError("A field cannot be positional as well as be represented by flags.")
+                parser.add_argument(
+                    f"--{field.name.replace('_', '-')}",
+                    action=argparse.BooleanOptionalAction,
+                    default=argparse.SUPPRESS,
+                    **argument_kwargs,
+                )
+            else:
+                parser.add_argument(
+                    argument_name,
+                    default=argparse.SUPPRESS,
+                    help=f"Override field {field.name}.",
+                    type=_parse_bool,
+                    **argument_kwargs,
+                )
+        elif issubclass(field.type, Enum):
+            parser.add_argument(
+                argument_name,
+                choices=list(field.type),
+                default=field.default if positional and field_has_default else argparse.SUPPRESS,
+                help=f"Override field {field.name}.",
+                type=lambda x: field.type[x],
+                **argument_kwargs,
+            )
+        elif field.type is bytes:
+            encoding = field.metadata.get("encoding", "utf-8")
+            bytes_parser = partial(field.type, encoding=encoding)
+            setattr(bytes_parser, "__name__", encoding)
+            parser.add_argument(
+                argument_name,
+                default=argparse.SUPPRESS,
+                help=f"Override field {field.name}.",
+                type=bytes_parser,
+                **argument_kwargs,
+            )
+        else:
+            parser.add_argument(
+                argument_name,
+                default=argparse.SUPPRESS,
+                help=f"Override field {field.name}.",
+                type=field.type,
+                **argument_kwargs,
+            )
+    return parser
+
+
+Fct = TypeVar("Fct")
+
+
+def rename(name: str):
+    def _rename(f: Fct) -> Fct:
+        setattr(f, "__name__", name)
+        return f
+
+    return _rename
+
+
+@rename("bool")
+def _parse_bool(arg: str) -> bool:
+    if arg.lower() == "true" or arg == "1":
+        return True
+    elif arg.lower() == "false" or arg == "0":
+        return False
+    raise TypeError(f"Unable to convert {arg} to boolean.")
+
+
+def _parse_datetime(date_string: str, is_date: bool, date_format: Optional[str] = None) -> Union[date, datetime]:
+    result = datetime.strptime(date_string, date_format) if date_format else datetime.fromisoformat(date_string)
+    return result.date() if is_date else result
+
+
+def _parse_union(value: str, union_type: Type) -> Any:
+    for arg in get_args(union_type):
+        if isinstance(None, arg):
+            continue
+        try:
+            return arg(value)
+        except ValueError:
+            continue
+    raise ValueError(f"Unable to parse '{value}' as one of {union_type}")
+
+
+__all__ = ["parse"]
---
+++ packages/pydargs/pydargs-0.6.0.tar.gz/pydargs-0.6.0/tests/test_parse.py
@@ -0,0 +1,258 @@
+import sys
+from dataclasses import dataclass, field
+from enum import Enum
+from json import loads
+from typing import Literal, Optional
+
+from pytest import raises
+
+from pydargs import parse
+
+
+class TestParseCustomParser:
+    def test_parser_optional(self):
+        @dataclass
+        class TConfig:
+            arg: Optional[list[int]] = field(default=None, metadata=dict(parser=loads))
+
+        t = parse(TConfig, [])
+        assert t.arg is None
+
+        t = parse(TConfig, ["--arg", "[1, 2]"])
+        assert t.arg == [1, 2]
+
+        t = parse(TConfig, ["--arg", '{"1": 2}'])
+        assert t.arg == {"1": 2}
+
+    def test_parser_required(self, capsys):
+        @dataclass
+        class TConfig:
+            arg: list[int] = field(metadata=dict(parser=loads))
+
+        with raises(SystemExit):
+            parse(TConfig, [])
+        captured = capsys.readouterr()
+        assert "the following arguments are required: --arg" in captured.err
+
+        t = parse(TConfig, ["--arg", "[1, 2]"])
+        assert t.arg == [1, 2]
+
+    def test_parser_invalid(self, capsys):
+        @dataclass
+        class TConfig:
+            arg: list[int] = field(metadata=dict(parser=loads))
+
+        with raises(SystemExit):
+            parse(TConfig, ["--arg", "[1, 2"])
+        captured = capsys.readouterr()
+        assert "argument --arg: invalid loads value: '[1, 2" in captured.err
+
+
+class TestParseChoices:
+    def test_enum(self, capsys):
+        class AnEnum(Enum):
+            one = 1
+            two = 2
+            three = 3
+
+        @dataclass
+        class TConfig:
+            an_enum: AnEnum
+
+        with raises(SystemExit):
+            parse(TConfig, [])
+        captured = capsys.readouterr()
+        assert "error: the following arguments are required: --an-enum" in captured.err
+
+        t = parse(TConfig, ["--an-enum", "one"])
+        assert t.an_enum == AnEnum.one
+
+    def test_str_enum(self, capsys):
+        class AnEnum(str, Enum):
+            one = "one"
+            two = "two"
+            three = "three"
+
+        @dataclass
+        class TConfig:
+            an_enum: AnEnum
+            another_enum: AnEnum = AnEnum.three
+
+        with raises(SystemExit):
+            parse(TConfig, [])
+        captured = capsys.readouterr()
+        assert "error: the following arguments are required: --an-enum" in captured.err
+
+        t = parse(TConfig, ["--an-enum", "one"])
+        assert t.an_enum == AnEnum.one
+        assert t.another_enum == AnEnum.three
+
+        t = parse(TConfig, ["--an-enum", "one", "--another-enum", "two"])
+        assert t.an_enum == AnEnum.one
+        assert t.another_enum == AnEnum.two
+
+    def test_str_literal(self, capsys):
+        @dataclass
+        class TConfig:
+            a_literal: Literal["x", "y"] = "x"
+
+        t = parse(TConfig, [])
+        assert t.a_literal == "x"
+
+        t = parse(TConfig, ["--a-literal", "y"])
+        assert t.a_literal == "y"
+
+        with raises(SystemExit):
+            parse(TConfig, ["--a-literal", "z"])
+        captured = capsys.readouterr()
+        assert "error: argument --a-literal: invalid choice: 'z'" in captured.err
+
+    def test_int_literal(self, capsys):
+        @dataclass
+        class TConfig:
+            a_literal: Literal[1, 2] = 1
+
+        t = parse(TConfig, [])
+        assert t.a_literal == 1
+
+        t = parse(TConfig, ["--a-literal", "2"])
+        assert t.a_literal == 2
+
+        with raises(SystemExit):
+            parse(TConfig, ["--a-literal", "3"])
+        captured = capsys.readouterr()
+        assert "error: argument --a-literal: invalid choice: 3" in captured.err
+
+    def test_fail_mixed_types(self):
+        @dataclass
+        class TConfig:
+            a_literal: Literal[1, "2"] = 1
+
+        with raises(NotImplementedError):
+            parse(TConfig, [])
+
+
+class TestNotImplemented:
+    def test_set(self):
+        @dataclass
+        class TConfig:
+            a: set[int]
+
+        with raises(NotImplementedError):
+            parse(TConfig, ["--a", "1", "1", "2"])
+
+    def test_tuple(self):
+        @dataclass
+        class TConfig:
+            a: tuple[str]
+
+        with raises(NotImplementedError):
+            parse(TConfig, ["--a", "1", "1", "2"])
+
+
+class TestIgnoreArg:
+    @dataclass
+    class Config:
+        a: int = 5
+        b: str = field(default="something", metadata={"ignore_arg": True})
+        c: bool = field(default=False, metadata=dict(ignore_arg=False, as_flags=True))
+        z: str = "dummy"
+
+    def test_ignore_default(self):
+        config = parse(self.Config, [])
+        assert config.a == 5
+        assert config.b == "something"
+        assert config.c is False
+        assert config.z == "dummy"
+
+    def test_ignore_valid(self):
+        config = parse(self.Config, ["--a", "1", "--c"])
+        assert config.a == 1
+        assert config.b == "something"
+        assert config.c is True
+
+    def test_ignore_invalid(self, capsys):
+        with raises(SystemExit):
+            parse(self.Config, ["--b", "2"])
+        captured = capsys.readouterr()
+        assert "error: unrecognized arguments: --b 2" in captured.err
+
+    def test_ignore_invalid_no_default(self, capsys):
+        @dataclass
+        class TConfig:
+            a: str = field(metadata={"ignore_arg": True})
+            b: int = 5
+
+        with raises(TypeError):
+            parse(TConfig, [])
+
+
+class TestPositional:
+    @dataclass
+    class Config:
+        a: Literal["a", "b"] = field(default="a", metadata={"positional": True})
+        z: str = "dummy"
+
+    def test_ignore_default(self):
+        config = parse(self.Config, [])
+        assert config.a == "a"
+        assert config.z == "dummy"
+
+
+class TestSysArgv:
+    @dataclass
+    class Config:
+        positional: str = field(default="positional", metadata=dict(positional=True))
+        a: int = 5
+        z: str = "dummy"
+
+    def test_empty_argv(self, monkeypatch):
+        monkeypatch.setattr(sys, "argv", ["name_of_program"])
+        config = parse(self.Config)
+        assert config.positional == "positional"
+        assert config.a == 5
+        assert config.z == "dummy"
+
+    def test_non_empty_argv(self, monkeypatch):
+        monkeypatch.setattr(sys, "argv", ["name_of_program", "--a", "3"])
+        config = parse(self.Config)
+        assert config.positional == "positional"
+        assert config.a == 3
+        assert config.z == "dummy"
+
+    def test_with_positional_argv(self, monkeypatch):
+        monkeypatch.setattr(sys, "argv", ["name_of_program", "--a", "3", "positional_value"])
+        config = parse(self.Config)
+        assert config.positional == "positional_value"
+        assert config.a == 3
+        assert config.z == "dummy"
+
+
+class TestKwargs:
+    @dataclass
+    class Config:
+        a: int = 5
+        some_long_argument: str = "something"
+        z: str = "dummy"
+
+    def test_with_args_and_kwargs(self):
+        config = parse(self.Config, ["--a", "1", "--z", "2"], prog="pydargs")
+        assert config.a == 1
+        assert config.z == "2"
+
+    def test_with_kwargs(self):
+        config = parse(self.Config, allow_abbrev=False)
+        assert config.a == 5
+        assert config.z == "dummy"
+
+    def test_allow_abbrev(self):
+        config = parse(self.Config, ["--so", "something_else"])
+        assert config.some_long_argument == "something_else"
+        assert config.a == 5
+        assert config.z == "dummy"
+
+    def test_disallow_abbrev(self, capsys):
+        with raises(SystemExit):
+            parse(self.Config, ["--so", "something_else"], allow_abbrev=False)
+        captured = capsys.readouterr()
+        assert "error: unrecognized arguments: --so something_else" in captured.err
