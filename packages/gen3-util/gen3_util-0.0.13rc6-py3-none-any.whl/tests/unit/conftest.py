0165b4c79cc807937f81a7f3c9e39d0b0c9ff93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:04:43 +0000

    Add package gen3-util/0.0.13rc6/gen3_util-0.0.13rc6-py3-none-any.whl

---
+++ packages/gen3-util/gen3_util-0.0.13rc6-py3-none-any.whl/tests/unit/conftest.py
@@ -0,0 +1,25 @@
+from typing import List
+
+import pytest
+from gen3.submission import Gen3Submission
+
+from gen3_util.config import ensure_auth
+
+
+@pytest.fixture
+def python_source_directories() -> List[str]:
+    """Directories to scan with flake8."""
+    return ["gen3_util", "tests"]
+
+
+@pytest.fixture
+def custom_config_path() -> str:
+    """User specified config path"""
+    return 'tests/fixtures/custom_config/config.yaml'
+
+
+@pytest.fixture
+def submission_client() -> str:
+    """Gen3Submission client"""
+    auth = ensure_auth(profile='development')
+    return Gen3Submission(auth_provider=auth)
