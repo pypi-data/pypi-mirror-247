23c58d5b40084a3cb5a492564fb913ec39ca4d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:46 +0000

    Add package dascore/0.0.15/dascore-0.0.15.tar.gz

---
+++ packages/dascore/dascore-0.0.15.tar.gz/dascore-0.0.15/dascore/utils/progress.py
@@ -0,0 +1,31 @@
+"""Simple interface for progress markers."""
+from __future__ import annotations
+
+import rich.progress as prog
+
+import dascore as dc
+
+
+def track(sequence, description):
+    """A simple iterator for tracking updates."""
+    # This is a dirty hack to allow debugging while running tests.
+    # Otherwise, pdb doesn't work in any tracking scope.
+    # See: https://github.com/Textualize/rich/issues/1053
+    if dc._debug or not len(sequence):
+        yield from sequence
+        return
+    # Normal progress bar behavior
+    progress = prog.Progress(
+        prog.SpinnerColumn(),
+        prog.TextColumn("[progress.description]{task.description}"),
+        prog.BarColumn(bar_width=30),
+        prog.TaskProgressColumn(),
+        prog.TimeRemainingColumn(),
+        prog.TimeElapsedColumn(),
+        prog.MofNCompleteColumn(),
+    )
+    total = len(sequence)
+    with progress:
+        yield from progress.track(
+            sequence, total=total, description=description, update_period=1.0
+        )
---
+++ packages/dascore/dascore-0.0.15.tar.gz/dascore-0.0.15/readme.md
@@ -0,0 +1,19 @@
+# DASCore
+
+A python library for distributed fiber optic sensing.
+
+[![coverage](https://codecov.io/gh/dasdae/dascore/branch/master/graph/badge.svg)](https://codecov.io/gh/dasdae/dascore)
+[![supported versions](https://img.shields.io/pypi/pyversions/dascore.svg?label=python_versions)](https://pypi.python.org/pypi/dascore)
+[![PyPI Downloads](https://img.shields.io/pypi/dm/dascore.svg?label=pypi)](https://pypi.org/project/dascore/)
+[![Conda Downloads](https://img.shields.io/conda/dn/conda-forge/dascore.svg?label=conda)](https://github.com/conda-forge/dascore-feedstock)
+[![DOI](https://zenodo.org/badge/422627477.svg)](https://zenodo.org/badge/latestdoi/422627477)
+[![Licence](https://www.gnu.org/graphics/lgplv3-88x31.png)](https://www.gnu.org/licenses/lgpl.html)
+
+[Code](https://github.com/DASDAE/dascore)
+
+Documentation [[stable](https://dascore.org), [development](https://dascore.quarto.pub/dascore/)]
+
+
+
+
+**WARNING**: dascore is still very new. There will be bugs and API changes.
