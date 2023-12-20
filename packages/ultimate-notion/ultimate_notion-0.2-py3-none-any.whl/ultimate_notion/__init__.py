6dee59d5fe93d999ec6149f63f022a4991adc535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 07:03:52 +0000

    Add package ultimate-notion/0.2/ultimate_notion-0.2-py3-none-any.whl

---
+++ packages/ultimate-notion/ultimate_notion-0.2-py3-none-any.whl/ultimate_notion/__init__.py
@@ -0,0 +1,42 @@
+"""Ultimate Notion provides a pythonic, high-level API for Notion
+
+Notion-API: https://developers.notion.com/reference/intro
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version('ultimate-notion')
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = 'unknown'
+finally:
+    del version, PackageNotFoundError
+
+from ultimate_notion.database import Database
+from ultimate_notion.obj_api.enums import AggFunc, BGColor, CodeLang, Color, NumberFormat, VState
+from ultimate_notion.objects import File, Option, RichText
+from ultimate_notion.page import Page
+from ultimate_notion.schema import ColType, Column, PageSchema, SelfRef
+from ultimate_notion.session import Session
+from ultimate_notion.utils import OptionNS
+
+__all__ = [
+    'AggFunc',
+    'BGColor',
+    'CodeLang',
+    'Color',
+    'ColType',
+    'Column',
+    'Database',
+    'File',
+    'NumberFormat',
+    'Option',
+    'OptionNS',
+    'Page',
+    'PageSchema',
+    'RichText',
+    'SelfRef',
+    'Session',
+    'VState',
+    '__version__',
+]
