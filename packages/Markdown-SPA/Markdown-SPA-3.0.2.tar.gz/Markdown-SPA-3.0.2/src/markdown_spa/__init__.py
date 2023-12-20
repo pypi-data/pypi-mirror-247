82eb9ca059cc7f67ae056dca4d8f53de27cf443a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:05:01 +0000

    Add package Markdown-SPA/3.0.2/Markdown-SPA-3.0.2.tar.gz

---
+++ packages/Markdown-SPA/Markdown-SPA-3.0.2.tar.gz/Markdown-SPA-3.0.2/src/markdown_spa/__init__.py
@@ -0,0 +1,7 @@
+from .generator import Generator, main
+from .packages import enable, silent_call
+from .extensions import Extension, SASS, Tailwind, Sitemap
+
+
+if __name__ == "__main__":
+    main()
