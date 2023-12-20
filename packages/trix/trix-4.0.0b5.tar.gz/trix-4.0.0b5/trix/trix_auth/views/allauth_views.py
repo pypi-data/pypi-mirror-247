f9eea9b4eb3464e8e2a7f431cbc8b87118a0111c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:04:12 +0000

    Add package trix/4.0.0b5/trix-4.0.0b5.tar.gz

---
+++ packages/trix/trix-4.0.0b5.tar.gz/trix-4.0.0b5/trix/trix_auth/views/allauth_views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+from allauth.account.views import LoginView, LogoutView
+from allauth.socialaccount.adapter import get_adapter
+
+
+class AllauthLoginView(LoginView):
+    def get(self, *args, **kwargs):
+
+        redirect_url = self.request.GET.get('next')
+        adapter = get_adapter(self.request)
+        all_providers = adapter.list_providers(self.request)
+        provider = all_providers[0]
+        return HttpResponseRedirect(
+            provider.get_login_url(
+                request=self.request,
+                process='login', next=redirect_url))
+
+
+class AllauthLogoutView(LogoutView):
+    template_name = 'trix_auth/auth_logout.django.html'
+
+    def get_redirect_url(self):
+        # If using Dataporten, redirect to log out from Dataporten as well.
+        logout_url = getattr(settings, 'DATAPORTEN_LOGOUT_URL', None)
+        if getattr(settings, 'DATAPORTEN_LOGIN', False):
+            return logout_url
+        else:
+            return super(AllauthLogoutView, self).get_redirect_url()
