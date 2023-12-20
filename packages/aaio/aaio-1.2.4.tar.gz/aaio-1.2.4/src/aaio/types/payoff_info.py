b5d83a97282eb8ff3972b7d05c6ce1f1812a93e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:12 +0000

    Add package aaio/1.2.4/aaio-1.2.4.tar.gz

---
+++ packages/aaio/aaio-1.2.4.tar.gz/aaio-1.2.4/src/aaio/types/payoff_info.py
@@ -0,0 +1,27 @@
+from typing import Literal, Optional
+
+from pydantic import BaseModel
+
+
+class PayoffInfo(BaseModel):
+    id: str
+    my_id: str
+    method: str
+    wallet: str
+    amount: float
+    amount_down: float
+    commission: float
+    commission_type: Literal[0, 1]
+    status: Literal['in_process', 'cancel', 'success']
+    cancel_message: Optional[str] = None
+    date: str
+    complete_date: Optional[str] = None
+
+    def is_success(self) -> bool:
+        return self.status == 'success'
+
+    def is_in_process(self) -> bool:
+        return self.status == 'in_process'
+
+    def is_canceled(self) -> bool:
+        return self.status == 'cancel'
