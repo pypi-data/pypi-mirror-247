6bce6617539fcc1022924ecc6648e0f06e0150a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:49 +0000

    Add package Face-Mash/0.0.0.1/Face_Mash-0.0.0.1.tar.gz

---
+++ packages/Face-Mash/Face_Mash-0.0.0.1.tar.gz/Face_Mash-0.0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='Face_Mash',
+    version='0.0.0.1',
+    description='drowsiness detection model',
+    author= 'Murali && Monica',
+    packages=setuptools.find_packages(),
+    keywords=['deep learning', 'drowsiness detection', 'fatigue analysis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['Face_Mash'],
+    package_dir={'':'src'},
+    install_requires = [
+        'keras>=2.6.0',
+        'opencv-python>=4.5.3',
+        'numpy>=1.19.5',
+    ]
+)
