b5110acd928eb6bd2ddcd6d2df08e261c03b880d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:11 +0000

    Add package llm-magnet/0.0.4/llm_magnet-0.0.4.tar.gz

---
+++ packages/llm-magnet/llm_magnet-0.0.4.tar.gz/llm_magnet-0.0.4/README.md
@@ -0,0 +1,53 @@
+<p align="center">
+   <img height="250" width="250" src="./magnet.png">
+   <br>
+   <h3 align="center">magnet</h3>
+   <p align="center">small, efficient embedding model toolkit</p>
+   <p align="center"><i>~ fine-tune SOTA LLMs on knowledge bases rapidly ~</i></p>
+</p>
+
+</small>
+
+## 💾 Installation
+
+``` bash
+pip install llm-magnet
+```
+or 
+``` bash
+python3 setup.py install
+```
+
+
+## 🎉 usage
+
+[check out this notebook, it's really useful](./example.ipynb) `(./example.ipynb)`
+
+<small>a snippet to get you started</small>
+
+``` python
+from magnet.filings import Processor
+source_data_file = "./raw/kb_export_clean.parquet" # your text documents data
+filings = Processor()
+filings.load(source_data_file)
+await filings.process('./data/filings.parquet','clean','file', nlp=False)
+```
+
+## 👏 features
+
+ - so long as your initial data has columns for article text and ids, `magnet` can do the rest
+ - sequential distributed processing with NATS
+ - finetune highly performant expert models from 0-1 in very little time
+ - upload to S3
+ - ideal cyberpunk vision of LLM power users in vectorspace
+
+## goals
+
+- [ ] add [mlx](https://github.com/ml-explore/mlx) support
+- [x] finish `README.md`
+- [x] add [NATS](https://nats.io) for distributed processing
+- [ ] `deepspeed` integration for model parallelism on multiple GPU
+
+## bad code
+
+- [x] `spacy.nlp` is used poorly throughout, need to make it possible to make sentence splitter hooks of our own and `spacy` can be a default fallback
---
+++ packages/llm-magnet/llm_magnet-0.0.4.tar.gz/llm_magnet-0.0.4/magnet/ic/field.py
@@ -0,0 +1,68 @@
+import nats, json
+from magnet.utils import _f
+from dataclasses import asdict, dataclass
+
+@dataclass
+class Payload:
+    text: str
+    document: str
+
+class Charge:
+    def __init__(self, server):
+        self.server = server
+
+    async def on(self, frequency: str = 'default', stream: str = 'default'):
+        self.frequency = frequency
+        self.stream = stream
+        try:
+            nc = await nats.connect(f'nats://{self.server}:4222')
+            self.nc = nc
+            self.js = self.nc.jetstream()
+            await self.js.add_stream(name=self.stream, subjects=[self.frequency])
+            self.sub = await self.js.pull_subscribe(self.frequency, 'magnet-charge')
+            _f("success", f'connected to {self.server}')
+        except TimeoutError:
+            _f('fatal', f'could not connect to {self.server}')
+    async def off(self):
+        await self.sub.unsubscribe()
+        _f('warn', f'unsubscribed from {self.stream}')
+        await self.nc.drain()
+        _f('warn', f'disconnected from {self.server}')
+    async def pulse(self, packet, document_id):
+        try:
+            payload = Payload(text=packet,document=document_id)
+            bytes_ = json.dumps(asdict(payload)).encode()
+        except:
+            _f('fatal', 'invalid JSON')
+        try:
+            await self.js.publish(self.frequency, bytes_)
+        except Exception as e:
+            print(e)
+            _f('fatal', f'could not send data to {self.server}')
+    async def emp(self):
+        await self.js.delete_stream(name=self.stream)
+        _f('success', f'{self.frequency} stream deleted')
+
+class Resonator:
+
+    def __init__(self, server):
+        self.server = server
+    async def on(self, frequency: str = 'default', stream: str = 'default', cb=print):
+        self.frequency = frequency
+        self.stream = stream
+        self.nc = await nats.connect(f'nats://{self.server}:4222')
+        self.js = self.nc.jetstream()
+        self.sub =  await self.js.pull_subscribe(self.frequency, stream=self.stream)
+        while True:
+            msgs = await self.sub.fetch(batch=10, timeout=60)
+            for msg in msgs:
+                try:
+                    payload = Payload(**json.loads(msg.data))
+                    cb(payload)
+                except json.decoder.JSONDecodeError:
+                    _f('fatal','invalid JSON')
+    async def off(self):
+        await self.sub.unsubscribe()
+        _f('warn', f'unsubscribed from {self.stream}')
+        await self.nc.drain()
+        _f('warn', f'disconnected from {self.server}')
