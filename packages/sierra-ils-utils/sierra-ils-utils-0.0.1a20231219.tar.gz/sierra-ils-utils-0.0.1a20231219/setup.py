cd12de6bd905bf3df5249d0cabc1f1e6f9a1d869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:50 +0000

    Add package sierra-ils-utils/0.0.1a20231219/sierra-ils-utils-0.0.1a20231219.tar.gz

---
+++ packages/sierra-ils-utils/sierra-ils-utils-0.0.1a20231219.tar.gz/sierra-ils-utils-0.0.1a20231219/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sierra-ils-utils",
+    version="0.0.1a20231219",
+    packages=find_packages(),
+    install_requires=[
+        "httpx>=0.25.2,<0.26.0",
+        "pydantic>=1.10.13,<2.0.0",
+        "pymarc>=5.1.0,<6.0.0",
+    ],
+    author="Ray Voelker",
+    author_email="ray.voelker@gmail.com",
+    description="Python wrappers for working with the Sierra ILS",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chimpy-me/sierra-ils-utils/",
+    license="MIT",
+)
