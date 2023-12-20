ecce73c83f84c37e15194dcbdd95cddd4779a1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:11 +0000

    Add package embedchain/0.1.35/embedchain-0.1.35-py3-none-any.whl

---
+++ packages/embedchain/embedchain-0.1.35-py3-none-any.whl/embedchain/models/providers.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Providers(Enum):
+    OPENAI = "OPENAI"
+    ANTHROPHIC = "ANTHPROPIC"
+    VERTEX_AI = "VERTEX_AI"
+    GPT4ALL = "GPT4ALL"
+    AZURE_OPENAI = "AZURE_OPENAI"
