5c249727d9b6b6848aaea557e9546ea1b7c1cafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:04:22 +0000

    Add package bestErrors/0.6/bestErrors-0.6-py3-none-any.whl

---
+++ packages/bestErrors/bestErrors-0.6-py3-none-any.whl/bestErrors/main.py
@@ -0,0 +1,14 @@
+from erodecor import Error
+
+
+@Error
+class MathError: ...
+
+@Error
+class AuthenticationError: ...
+
+@Error
+class PermissionDeniedError: ...
+
+@Error
+class OutOfRangeError: ...
