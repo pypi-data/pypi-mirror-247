529abb9d4eefc2874e01566cff37942510473244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:04:42 +0000

    Add package mesmer-emulator/0.9.0/mesmer-emulator-0.9.0.tar.gz

---
+++ packages/mesmer-emulator/mesmer-emulator-0.9.0.tar.gz/mesmer-emulator-0.9.0/mesmer/__init__.py
@@ -0,0 +1,48 @@
+# MESMER, land-climate dynamics group, S.I. Seneviratne
+# Copyright (c) 2021 ETH Zurich, MESMER contributors listed in AUTHORS.
+# Licensed under the GNU General Public License v3.0 or later see LICENSE or
+# https://www.gnu.org/licenses/
+"""
+The mesmer package provides tools to train the MESMER emulator, create emulations, and
+analyze the results.
+"""
+
+from . import calibrate_mesmer, core, create_emulations, io, stats, testing, utils
+from .core import _data as data
+from .core import geospatial, grid, mask, volc, weighted
+
+# "legacy" modules
+__all__ = [
+    "calibrate_mesmer",
+    "create_emulations",
+    "io",
+    "utils",
+]
+
+# "new" "modules"
+__all__ += [
+    "core",
+    "data",
+    "geospatial",
+    "grid",
+    "mask",
+    "stats",
+    "testing",
+    "volc",
+    "weighted",
+]
+
+try:
+    from importlib.metadata import version as _get_version
+except ImportError:
+    # importlib.metadata not available in python 3.7
+    import pkg_resources
+
+    _get_version = lambda pkg: pkg_resources.get_distribution(pkg).version
+
+try:
+    __version__ = _get_version("mesmer-emulator")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
