6490d24c6859974c070a40fd6c0b441f0e1e03ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:43 +0000

    Add package dean-utils/0.0.2/dean_utils-0.0.2-py3-none-any.whl

---
+++ packages/dean-utils/dean_utils-0.0.2-py3-none-any.whl/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["async_abfs", "cos_query_all", "send_to_queue", "send_email"]
+from az_utils import async_abfs, cos_query_all, send_to_queue
+from email_utility import send_email
+import polars_extras
