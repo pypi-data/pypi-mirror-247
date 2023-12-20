88d5b9f9e060f66549f94d48da6835d9c1f823b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:58 +0000

    Add package UCTool-1.0/1.0.1/UCTool-1.0-1.0.1.tar.gz

---
+++ packages/UCTool-1.0/UCTool-1.0-1.0.1.tar.gz/UCTool-1.0-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+str_version = '1.0.1'
+
+setup(name='UCTool-1.0',
+      version=str_version,
+      description='UCTool-1.0',
+      author='xiejunwei',
+      author_email='xiejunwei@qq.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires= ['pypinyin', 'opencv-python'],
+      python_requires='>=3')
+
