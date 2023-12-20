f2f82eb27ee34a45088a20afdc83e5730888a087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:03:44 +0000

    Add package j-pandas-datalib/0.1.1/j_pandas_datalib-0.1.1-py3-none-any.whl

---
+++ packages/j-pandas-datalib/j_pandas_datalib-0.1.1-py3-none-any.whl/j_pandas_datalib/__init__.py
@@ -0,0 +1,5 @@
+"""Joshs Pandas Datalib."""
+__version__ = "0.1.1"
+from .csv_tools import pandas_read_csv  # noqa
+from .dataset import Dataset  # noqa
+from .env_vars import ensure_env_var  # noqa
