6d312062eb999e7c17465f3ad75372d6eb2ed806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:00 +0000

    Add package cobastem/1.0.2/cobastem-1.0.2.tar.gz

---
+++ packages/cobastem/cobastem-1.0.2.tar.gz/cobastem-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name="cobastem",
+    version="1.0.2",
+    packages=find_packages(),
+    setup_requires=["setuptools", "wheel"],
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Library for stemming Madurese text",
+)
