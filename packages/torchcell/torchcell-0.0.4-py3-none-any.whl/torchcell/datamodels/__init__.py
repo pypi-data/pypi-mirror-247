8088863c3cfe58330e03c4ede3658f3a584d6a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:45 +0000

    Add package torchcell/0.0.4/torchcell-0.0.4-py3-none-any.whl

---
+++ packages/torchcell/torchcell-0.0.4-py3-none-any.whl/torchcell/datamodels/__init__.py
@@ -0,0 +1,45 @@
+from .pydant import ModelStrict, ModelStrictArbitrary
+
+from .ontology_pydantic import (
+    BaseEnvironment,
+    BaseGenotype,
+    BasePhenotype,
+    BaseExperiment,
+    GenePerturbation,
+    Media,
+    ModelStrict,
+    ReferenceGenome,
+    Temperature,
+    DeletionGenotype,
+    DeletionPerturbation,
+    SysGeneName,
+    FitnessPhenotype,
+    FitnessExperiment,
+    DampPerturbation,
+    TsAllelePerturbation,
+    InterferenceGenotype,
+    FitnessExperimentReference,
+    ExperimentReference,
+)
+
+core_models = ["ModelStrict", "ModelStrictArbitrary"]
+ontology_models = ["BaseEnvironment",
+    "BaseGenotype",
+    "BasePhenotype",
+    "BaseExperiment",
+    "GenePerturbation",
+    "Media",
+    "ReferenceGenome",
+    "Temperature",
+    "DeletionGenotype",
+    "DeletionPerturbation",
+    "SysGeneName",
+    "FitnessPhenotype",
+    "FitnessExperiment",
+    "DampPerturbation",
+    "TsAllelePerturbation",
+    "InterferenceGenotype",
+    "FitnessExperimentReference",
+    "ExperimentReference"]
+
+__all__ = core_models  + ontology_models
