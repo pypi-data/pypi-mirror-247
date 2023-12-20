6f3dae417549445652a71b611eca9d8344d6d3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:03:50 +0000

    Add package pydantic-resolve/1.8.2/pydantic_resolve-1.8.2-py3-none-any.whl

---
+++ packages/pydantic-resolve/pydantic_resolve-1.8.2-py3-none-any.whl/pydantic_resolve/exceptions.py
@@ -0,0 +1,11 @@
+class ResolverTargetAttrNotFound(Exception):
+    pass
+
+class DataloaderDependCantBeResolved(Exception):
+    pass
+
+class LoaderFieldNotProvidedError(Exception):
+    pass
+
+class MissingAnnotationError(Exception):
+    pass
