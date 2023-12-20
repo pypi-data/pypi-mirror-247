d8edc75476a9a93fa5ed3afaa3c65cec04643fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:45 +0000

    Add package logly/0.0.5/logly-0.0.5.tar.gz

---
+++ packages/logly/logly-0.0.5.tar.gz/logly-0.0.5/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.5"
+
+DESCRIPTION = 'Logly: Python logging utility with color-coded messages and file support. Easily log and trace messages with customizable colors. Simple integration for effective debugging and monitoring.'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="logly",
+    version=VERSION,
+    author="Muhammad Fiaz",
+    author_email="contact@muhammmadfiaz.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url='https://github.com/muhammad-fiaz/logly.git',
+    packages=find_packages(),
+    keywords=[
+        'log', 'logging', 'logly', 'python'
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'pytest==7.4.3',
+        'packaging==23.2',
+        'colorama>=0.4.4',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    license='MIT License',
+    project_urls={
+        'Source Code': 'https://github.com/muhammad-fiaz/logly.git',
+        'Bug Tracker': 'https://github.com/muhammad-fiaz/logly/issues',
+        'Documentation': 'https://github.com/muhammad-fiaz/logly#readme',
+    },
+)
+
+
+print("Happy Coding!")
