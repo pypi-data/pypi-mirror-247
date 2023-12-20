90062205a0536aa88ec5f61ac47fc9d5c77e07c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 14:04:53 +0000

    Add package pi-conf/0.6.7/pi_conf-0.6.7-py3-none-any.whl

---
+++ packages/pi-conf/pi_conf-0.6.7-py3-none-any.whl/pi_conf/__init__.py
@@ -0,0 +1,4 @@
+from pi_conf.config import Config as Config
+from pi_conf.config import cfg as cfg
+from pi_conf.config import load_config as load_config
+from pi_conf.config import set_config as set_config
