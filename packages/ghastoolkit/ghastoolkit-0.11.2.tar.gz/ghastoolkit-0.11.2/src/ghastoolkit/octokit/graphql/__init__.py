8badbd87458d2c3431e12b578cb4f38df9e032bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:29 +0000

    Add package ghastoolkit/0.11.2/ghastoolkit-0.11.2.tar.gz

---
+++ packages/ghastoolkit/ghastoolkit-0.11.2.tar.gz/ghastoolkit-0.11.2/src/ghastoolkit/octokit/graphql/__init__.py
@@ -0,0 +1,99 @@
+DEPENDENCY_GRAPH_STATUS = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        vulnerabilityAlerts(first: 100, states: [OPEN], $cursor) {
+            totalCount
+        }
+    }
+}
+"""
+
+DEPENDENCY_GRAPH_ALERTS = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        vulnerabilityAlerts(first: 100, states: [OPEN], $cursor) {
+            totalCount
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            edges {
+                node {
+                    number
+                    state
+                    createdAt
+                    dismissReason
+                    securityVulnerability {
+                        package {
+                            ecosystem
+                            name
+                        }
+                    }
+                    securityAdvisory {
+                        ghsaId
+                        severity
+                        cwes(first: 100) {
+                            edges {
+                                node {
+                                    cweId
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+DEPENDENCY_GRAPH_INFO = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        name
+        licenseInfo {
+            name
+        }
+        dependencyGraphManifests {
+            totalCount
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            edges {
+                node {
+                    filename
+                    dependencies {
+                        edges {
+                            node {
+                                packageName
+                                packageManager
+                                requirements
+                                repository {
+                                    nameWithOwner
+                                    isArchived
+                                    isDisabled
+                                    isEmpty
+                                    isFork
+                                    isSecurityPolicyEnabled
+                                    isInOrganization
+                                    licenseInfo {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+
+QUERIES = {
+    "GetDependencyStatus": DEPENDENCY_GRAPH_STATUS,
+    "GetDependencyAlerts": DEPENDENCY_GRAPH_ALERTS,
+    "GetDependencyInfo": DEPENDENCY_GRAPH_INFO,
+}
