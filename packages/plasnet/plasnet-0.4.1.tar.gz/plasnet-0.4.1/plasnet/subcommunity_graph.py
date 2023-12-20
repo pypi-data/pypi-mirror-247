623bf987e9b67cdefc171894fd0733bd9dc2c8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 05:03:29 +0000

    Add package plasnet/0.4.1/plasnet-0.4.1.tar.gz

---
+++ packages/plasnet/plasnet-0.4.1.tar.gz/plasnet-0.4.1/plasnet/subcommunity_graph.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+import networkx as nx
+
+from plasnet.blackhole_graph import BlackholeGraph
+from plasnet.ColorPicker import ColorPicker
+
+
+class SubcommunityGraph(BlackholeGraph):
+    def __init__(
+        self,
+        graph: Optional[nx.Graph] = None,
+        blackhole_connectivity_threshold: int = 0,
+        edge_density: float = 0.0,
+        label: str = "",
+        colour: str = ColorPicker.get_default_color(),
+    ):
+        super().__init__(graph, blackhole_connectivity_threshold, edge_density, label)
+        self._colour = colour
+
+    def _get_libs_relative_path(self) -> str:
+        return ".."
+
+    def _get_samples_selectors_HTML(self) -> str:
+        return ""
+
+    def _get_node_color(self, node: str) -> str:
+        return self._colour
