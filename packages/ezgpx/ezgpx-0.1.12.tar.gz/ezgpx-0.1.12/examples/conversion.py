926ef5099716e349c8693e3a398bab8c5cb1d184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:04:53 +0000

    Add package ezgpx/0.1.12/ezgpx-0.1.12.tar.gz

---
+++ packages/ezgpx/ezgpx-0.1.12.tar.gz/ezgpx-0.1.12/examples/conversion.py
@@ -0,0 +1,16 @@
+import ezgpx
+
+# Parse GPX file
+gpx = ezgpx.GPX("file.gpx")
+
+# Save as GPX file
+gpx.to_gpx("new_file.gpx")
+
+# Save as KML file
+gpx.to_kml("new_file.kml")
+
+# Save as CSV file
+gpx.to_csv("new_file.csv", columns=["lat", "lon", "ele"])
+
+# Convert to Pandas Dataframe
+df = gpx.to_dataframe()
---
+++ packages/ezgpx/ezgpx-0.1.12.tar.gz/ezgpx-0.1.12/examples/plot.py
@@ -0,0 +1,52 @@
+import ezgpx
+
+# Parse GPX file
+gpx = ezgpx.GPX("file.gpx")
+
+# Plot with Matplotlib
+gpx.matplotlib_plot(color="elevation",
+                    start_stop_colors=("green", "red"),
+                    way_points_color="blue",
+                    title=gpx.name(),
+                    duration=(0, 0),
+                    distance=(0.5, 0),
+                    ascent=None,
+                    pace=(1, 0),
+                    speed=None,
+                    file_path="img_1")
+
+# Plot with Matplotlib Basemap Toolkit
+gpx.matplotlib_basemap_plot(color="darkorange",
+                            start_stop_colors=("darkgreen", "darkred"),
+                            way_points_color="darkblue",
+                            title=gpx.name(),
+                            duration=(0,0),
+                            distance=(0.5,0),
+                            ascent=None,
+                            pace=None,
+                            speed=(1,0),
+                            file_path="img_2")
+
+# Plot with gmplot (Google Maps)
+gpx.gmplot_plot(color="yellow",
+                start_stop_colors=("green", "red"),
+                way_points_color="blue",
+                zoom=14,
+                title=gpx.name(),
+                file_path="map_1.html",
+                open=False)
+
+# Plot with Folium
+gpx.folium_plot(tiles="OpenStreetMap",
+                color="orange",
+                start_stop_colors=("green", "red"),
+                way_points_color="blue",
+                minimap=True,
+                coord_popup=False,
+                title="Very nice track!",
+                zoom=8,
+                file_path="map_2.html",
+                open=True)
+
+# Write new GPX file
+gpx.to_gpx("new_file.gpx")
