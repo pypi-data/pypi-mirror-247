241342ae22c58b82da9314fbfc3f3e538fc3c0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 04:04:22 +0000

    Add package ptrack/2.0.2/ptrack-2.0.2.tar.gz

---
+++ packages/ptrack/ptrack-2.0.2.tar.gz/ptrack-2.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ptrack',
+    version="2.0.2",
+    description='A simple CLI utility for asthetically tracking progress when copying, moving or downloading files.',
+    author='Connor Etherington',
+    author_email='connor@concise.cc',
+    packages=find_packages(),
+    install_requires=[
+        "ascii_magic",
+        "beautifulsoup4",
+        "Pillow",
+        "Requests",
+        "rich",
+        "yt_dlp",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ptc=ptrack.main:copy',
+            'ptm=ptrack.main:move',
+            'ptd=ptrack.main:download',
+            'ptrack=ptrack.main:main',
+        ]
+    }
+)
