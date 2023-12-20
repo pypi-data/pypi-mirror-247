6f68e0afd0eff8f5004dcad93218914e93cbd95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:04:23 +0000

    Add package standalone003gntmcallmln/0.0.13/standalone003gntmcallmln-0.0.13.tar.gz

---
+++ packages/standalone003gntmcallmln/standalone003gntmcallmln-0.0.13.tar.gz/standalone003gntmcallmln-0.0.13/tests/test_kangroll_basic_fn001_mln.py
@@ -0,0 +1,27 @@
+# from sa003package.kankrool_basiac_fn001_mln import fn001_mln
+
+# import sys
+# import os
+
+# # 计算 my_package 的路径
+# my_package_path = os.path.dirname(os.path.abspath(__file__))
+
+# # 将 my_package 添加到 sys.path
+# if my_package_path not in sys.path:
+#     sys.path.append(my_package_path)
+
+# import sys
+# sys.path.append('/Users/kang/1.live_wit_GPT4/code_pypi/standalone003gntmcallmlnWrokspace/sa003_env/lib/python3.8/site-packages/sa003package/')
+
+
+from sa003package.kangroll_basic_fn001_mln_release import fn001_mln
+
+# sa003package;sa003package.
+
+def test_add():
+    result = fn001_mln(3, 50)
+    print("Obfuscated:", result)
+
+if __name__ == "__main__":
+    print('test from usr,import add sa002package.exfn001add_release:')
+    test_add()
