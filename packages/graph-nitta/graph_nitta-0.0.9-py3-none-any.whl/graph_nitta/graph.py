487ff5136313dbc5f33b3117932abe9766c053be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:03:49 +0000

    Add package graph-nitta/0.0.9/graph_nitta-0.0.9-py3-none-any.whl

---
+++ packages/graph-nitta/graph_nitta-0.0.9-py3-none-any.whl/graph_nitta/graph.py
@@ -0,0 +1,22 @@
+import math
+
+from matplotlib import pyplot as plt
+
+from .style.apply import apply_basic_style
+
+
+def calc_grid(number_of_subplots: int):
+    return math.ceil(math.sqrt(number_of_subplots))
+
+
+def make_graph(
+    number_of_subplots: int = 1, row: int | None = None, column: int | None = None
+):
+    fig = plt.figure()
+    grid = calc_grid(number_of_subplots)
+    row = row or grid
+    column = column or grid
+    fig.set_size_inches(8 * column, 8 * row)
+    axes = [fig.add_subplot(row, column, i + 1) for i in range(number_of_subplots)]
+    apply_basic_style()
+    return fig, axes
