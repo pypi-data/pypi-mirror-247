9f845de490057836fcf40897870e9cb63591c23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 03:04:02 +0000

    Add package zuken/0.0.3/zuken-0.0.3-py3-none-any.whl

---
+++ packages/zuken/zuken-0.0.3-py3-none-any.whl/zuken/e3.py
@@ -0,0 +1,83 @@
+# More reliable way of making a COM connection to Zuken
+# Taken from "https://stackoverflow.com/a/69288053" by Pelelter
+def dispatch(app_name: str):
+    try:
+        from win32com import client
+        app = client.gencache.EnsureDispatch(app_name)
+    except AttributeError:
+        # Corner case dependencies.
+        import os
+        import re
+        import sys
+        import shutil
+        # Remove cache and try again.
+        MODULE_LIST = [m.__name__ for m in sys.modules.values()]
+        for module in MODULE_LIST:
+            if re.match(r'win32com\.gen_py\..+', module):
+                del sys.modules[module]
+        shutil.rmtree(os.path.join(os.environ.get('LOCALAPPDATA'), 'Temp', 'gen_py'))
+        from win32com import client
+        app = client.gencache.EnsureDispatch(app_name)
+    return app
+
+def print(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutInfo" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
+    e3.PutInfo(popup, msg, item)
+
+def warn(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutWarning" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
+    e3.PutWarning(popup, msg, item)
+
+def error(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutError" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
+    e3.PutError(popup, msg, item)
+
+def message(msg, item=0):
+    """
+    Shorthand for the "PutMessage" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        item: ID of an item in the project (allows you to jump to it)
+    """
+    e3.PutMessage(msg, item)
+
+def verify(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutVerify" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
+    e3.PutVerify(popup, msg, item)
+
+e3 = dispatch("CT.Application")
+"""E3 Object - Links to all instances of the application"""
+
+job = e3.CreateJobObject()
+"""E3 Job Object - Links to a single instance of the application"""
+
+
---
+++ packages/zuken/zuken-0.0.3-py3-none-any.whl/zuken/symbols.py
@@ -0,0 +1,72 @@
+from .e3 import job as _job
+
+_symbol = _job.CreateSymbolObject()
+_ = None
+
+def placeSymbol(name: str, version, sheet: int, x: float, y: float) -> int:
+    """
+    Places specified symbol on the sheet
+
+    Args:
+        name: Symbol Name
+        version: Symbol Version
+        sheet: Sheet to place on
+        x: x coordinate
+        y: y coordinate
+
+    Returns:
+        ID of the new symbol (0 if not created)
+    """
+
+    _symbol.Load(name, version)
+    # I personally don't have any need for the rotation/scale optional parameters
+    return _symbol.Place(sheet, x, y)
+
+def extractSymbolText(symbol: int, category=0, text="") -> list:
+    """
+    Returns the text IDs from a symbol. Able to filter by category (text type) and text content
+
+    Args:
+        symbol: Symbol ID to get text from
+        category: Text type to filter the text IDs to. Can be found in Database Editor -> Format -> Text Types
+        text: Text string to search for (must be an exact match)
+
+    Returns:
+        List of text IDs, will be empty if none are found
+    """
+
+    _symbol.SetId(symbol)
+    return list(_symbol.GetTextIds(_, category, text)[1][1:])
+
+def filterBySymbolName(symbols: list[int], name: str) -> list:
+    """
+    Filters by the symbol name, uses startswith to cast a broader net of matching symbols
+
+    Args:
+        symbols: List of symbol IDs
+        name: Symbol name to search for
+
+    Returns:
+        List of symbols whose name matches the (start of the) string
+    """
+
+    def GetName(symbol):
+        _symbol.SetId(symbol)
+        return _symbol.GetSymbolTypeName()
+
+    return [s for s in symbols if GetName(s).startswith(name)]
+
+def deleteSymbol(symbol: int) -> int:
+    """
+    Wrapper to delete symbols in one line
+
+    Args:
+        symbol: Symbol ID
+
+    Returns:
+        0 for successful deletion, symbol ID otherwise
+    """
+    _symbol.SetId(symbol)
+    return _symbol.Delete()
+
+__all__ = [f for f in dir() if not f.startswith("_")]
