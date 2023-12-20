0907341edda026ecd2dfe91d2a8484722ad83f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 15:04:07 +0000

    Add package minidevice/2.3.5/minidevice-2.3.5.tar.gz

---
+++ packages/minidevice/minidevice-2.3.5.tar.gz/minidevice-2.3.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(name='minidevice',
+      version='2.3.5',
+      description='Android Auto Pypi',
+      author='KateTseng',
+      author_email='Kate.TsengK@outlook.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/NakanoSanku/minidevice',
+      license='MIT',
+      keywords='Android',
+      project_urls={},
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['pyminitouch>=0.3.3',
+                        "adbutils==1.2.9"
+                        ],
+      python_requires='>=3'
+      )
