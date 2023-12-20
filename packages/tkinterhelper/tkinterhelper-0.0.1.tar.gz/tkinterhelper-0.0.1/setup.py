136fb037eba32a700b1ee88b923303cf3202b432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:04:20 +0000

    Add package tkinterhelper/0.0.1/tkinterhelper-0.0.1.tar.gz

---
+++ packages/tkinterhelper/tkinterhelper-0.0.1.tar.gz/tkinterhelper-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='tkinterhelper',
+  version='0.0.1',
+  author='Jonnxnet',
+  author_email='example@gmail.com',
+  python_requires='>=3.6'
+)
