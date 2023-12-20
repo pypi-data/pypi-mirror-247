035ae7ac1d7c4bb3640bae273fcead4d1d258d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:11 +0000

    Add package dynamixel-control/0.2.4a2/dynamixel_control-0.2.4a2.tar.gz

---
+++ packages/dynamixel-control/dynamixel_control-0.2.4a2.tar.gz/dynamixel_control-0.2.4a2/src/dynamixel_control/__init__.py
@@ -0,0 +1,13 @@
+
+# """
+# dynamixel_control
+
+# Package for controlling Dynamixel motors.
+# """
+
+# __version__ = "0.2.4a2"
+# __author__ = 'Kyle DuFrene'
+# __credits__ = 'Oregon State University'
+
+from .dynamixel import Dynamixel
+from .dxl import Dxl
