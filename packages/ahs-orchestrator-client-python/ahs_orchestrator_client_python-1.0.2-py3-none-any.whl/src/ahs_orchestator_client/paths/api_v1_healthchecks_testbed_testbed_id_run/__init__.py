d25de3240ad2f5fc59b6be18a1c8a08a3123befd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 16:04:45 +0000

    Add package ahs-orchestrator-client-python/1.0.2/ahs_orchestrator_client_python-1.0.2-py3-none-any.whl

---
+++ packages/ahs-orchestrator-client-python/ahs_orchestrator_client_python-1.0.2-py3-none-any.whl/src/ahs_orchestator_client/paths/api_v1_healthchecks_testbed_testbed_id_run/__init__.py
@@ -0,0 +1,5 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from ahs_orchestator_client.apis.paths.api_v1_healthchecks_testbed_testbed_id_run import ApiV1HealthchecksTestbedTestbedIdRun
+
+path = "/api/v1/healthchecks/testbed/{testbed_id}/run"
