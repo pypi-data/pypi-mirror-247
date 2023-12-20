3c802c92f5a20226cfb8e0f97bd60d903eefe7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-20 06:05:01 +0000

    Add package bucketed-scene-flow-eval/0.0.8/bucketed_scene_flow_eval-0.0.8-py3-none-any.whl

---
+++ packages/bucketed-scene-flow-eval/bucketed_scene_flow_eval-0.0.8-py3-none-any.whl/bucketed_scene_flow_eval/datasets/waymoopen/__init__.py
@@ -0,0 +1,6 @@
+from .waymo_supervised_flow import WaymoSupervisedSceneFlowSequenceLoader, WaymoSupervisedSceneFlowSequence, CATEGORY_MAP
+from .dataset import WaymoOpenSceneFlow
+
+__all__ = [
+    'WaymoOpenSceneFlow',
+]
