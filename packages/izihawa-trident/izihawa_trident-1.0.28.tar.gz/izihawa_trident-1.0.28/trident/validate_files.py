f5615970825f1446953a2e27d6339a3864991ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:08 +0000

    Add package izihawa-trident/1.0.28/izihawa_trident-1.0.28.tar.gz

---
+++ packages/izihawa-trident/izihawa_trident-1.0.28.tar.gz/izihawa_trident-1.0.28/trident/validate_files.py
@@ -0,0 +1,39 @@
+import glob
+import os
+import zipfile
+
+from pypdf import PdfReader
+from pypdf.errors import PdfReadError
+
+
+def test_pdf(filepath):
+    try:
+        PdfReader(filepath)
+    except PdfReadError:
+        return False
+    return True
+
+
+def test_epub(filepath):
+    the_zip_file = zipfile.ZipFile(filepath)
+    ret = the_zip_file.testzip()
+    if ret is not None:
+        return False
+    return True
+
+
+def validate_files(path):
+    for infile in glob.iglob(os.path.join(path, '*.*')):
+        _, extension = infile.rsplit('.', 1)
+        match extension:
+            case 'pdf':
+                if not test_pdf(infile):
+                    print('broken', infile)
+            case 'epub':
+                if not test_epub(infile):
+                    print('broken', infile)
+            case _:
+                print('unknown file')
+
+
+
