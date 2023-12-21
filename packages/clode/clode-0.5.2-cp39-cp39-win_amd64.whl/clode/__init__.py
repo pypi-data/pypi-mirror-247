2682dcae3557e9cf66f73c7e8a72967fb6e71063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:05:42 +0000

    Add package clode/0.5.2/clode-0.5.2-cp39-cp39-win_amd64.whl

---
+++ packages/clode/clode-0.5.2-cp39-cp39-win_amd64.whl/clode/__init__.py
@@ -0,0 +1,19 @@
+from . import clode_cpp_wrapper as _clode  # type: ignore
+from .features import CLODEFeatures
+from .observer import Observer, ObserverOutput
+from .problem_info import ProblemInfo
+from .runtime import (
+    cl_device_type,
+    cl_vendor,
+    get_cpp,
+    get_log_level,
+    initialise_runtime,
+    log_level,
+    set_log_level,
+    set_log_pattern,
+)
+from .stepper import Stepper
+from .trajectory import CLODETrajectory
+from .xpp_parser import convert_xpp_file, format_opencl_rhs, read_ode_parameters
+
+__version__ = "0.5.2"
