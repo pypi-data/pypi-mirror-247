29a6b4e1e756d09e69d788382ae130c8ddb1bec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:15 +0000

    Add package mediaToolsUtils/1.0.0/mediaToolsUtils-1.0.0-py3-none-any.whl

---
+++ packages/mediaToolsUtils/mediaToolsUtils-1.0.0-py3-none-any.whl/mediaToolsUtils/VideoCapture.py
@@ -0,0 +1,10 @@
+class VideoCapture():
+    # 构造函数
+    def __init__(self) -> None:
+        pass
+    # 测试函数
+    def test(self):
+        print('视频采集测试函数')
+    # 析构函数
+    def __del__(self):
+        pass
---
+++ packages/mediaToolsUtils/mediaToolsUtils-1.0.0-py3-none-any.whl/mediaToolsUtils/__init__.py
@@ -0,0 +1,8 @@
+from sys import exit
+from . import SoundCollection, VideoCapture
+
+__all__ = ['SoundCollection', 'exit', 'VideoCapture']
+__title__ = 'mediaToolsUtils'
+__version__ = '1.0.0'
+__author__ = 'nicknice'
+__copyright__ = 'Copyright 2018 nicknice'
