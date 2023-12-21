059b3661537f16c66405f3e89571ba9ff89c8dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:04:51 +0000

    Add package reflexive/0.1.6/reflexive-0.1.6.tar.gz

---
+++ packages/reflexive/reflexive-0.1.6.tar.gz/reflexive-0.1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "reflexive",
+    version = "0.1.6",
+    author = "Andrew Gibson",
+    author_email = "andrew@nlytx.io",
+    description = "Supports AWS Reflexive Expressions Analysis",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/nlytx/reflexive",
+    project_urls = {
+        "Bug Tracker": "https://github.com/nlytx/reflexive/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.10"
+)
