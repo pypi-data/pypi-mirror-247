316ca1484c2a7efa83f5d4d31ca531815b374715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 06:04:23 +0000

    Add package funcchain/0.2.0a2/funcchain-0.2.0a2.tar.gz

---
+++ packages/funcchain/funcchain-0.2.0a2.tar.gz/funcchain-0.2.0a2/dev-setup.sh
@@ -0,0 +1,20 @@
+# AUTO DEV SETUP
+
+# check if rye is installed
+if ! command -v rye &> /dev/null
+then
+    echo "rye could not be found: installing now ..."
+    curl -sSf https://rye-up.com/get | bash
+    echo "Check the rye docs for more info: https://rye-up.com/"
+fi
+
+echo "SYNC: setup .venv"
+rye sync
+
+echo "ACTIVATE: activate .venv"
+rye shell
+
+echo "SETUP: install pre-commit hooks"
+pre-commit install
+
+echo "Try running 'python example/chatgpt.py' to test your setup"
