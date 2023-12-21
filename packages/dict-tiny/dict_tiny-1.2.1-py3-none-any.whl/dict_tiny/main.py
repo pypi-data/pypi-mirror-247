d819675a2f15a94b5886281da1d374c7fa6509b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:22 +0000

    Add package dict-tiny/1.2.1/dict_tiny-1.2.1-py3-none-any.whl

---
+++ packages/dict-tiny/dict_tiny-1.2.1-py3-none-any.whl/dict_tiny/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from plumbum import cli
+from plumbum import colors
+
+from dict_tiny.util import is_alphabet
+from dict_tiny import version
+
+from dict_tiny.translators import _ALL_TRANSLATORS, DEFAULT_TRANSLATOR
+
+APP_DESC = """
+tiny command-line translator
+"""
+APP_NAME = version.name
+APP_VERSION = version.__version__
+
+
+class Dict_tiny(cli.Application):
+    PROGNAME = colors.green | APP_NAME
+    VERSION = colors.yellow | APP_VERSION
+    DESCRIPTION = version.DESCRIPTION
+    COLOR_GROUPS = {
+        "Switches": colors.yellow,
+        "Google translate": colors.green,
+        "Youdao dict": colors.green
+    }
+
+    stop = False  # whether return directly in main
+    clipBoardContent = None  # Record the word in clipboard
+
+    def main(self, *words):
+        if self.stop: return
+        text = words or self.clipBoardContent  # word has high priority
+        if not text:
+            self.help()
+            return
+        text = " ".join(text)
+
+        if_english = True if is_alphabet(text) == 'en' else False
+        if not self.target_language and self.use_googletrans:
+            self.target_language = "zh" if if_english else "en"
+
+        trans_objs = [translator.trans_obj_getter(text, self) for translator in _ALL_TRANSLATORS]
+        if not any(trans_objs):
+            trans_objs.append(DEFAULT_TRANSLATOR(text, self))
+        for trans_obj in trans_objs:
+            if trans_obj is None: continue
+            trans_obj.translate()
+
+
+def run():
+    for translator in _ALL_TRANSLATORS:
+        translator.attr_setter(Dict_tiny)
+    Dict_tiny()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/dict-tiny/dict_tiny-1.2.1-py3-none-any.whl/dict_tiny/util.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+import requests
+from lxml import html
+from plumbum import colors
+
+from dict_tiny.config import TIMEOUT
+
+
+def is_alphabet(word):
+    """
+    return the word is English or Chinese
+    :param word:
+    :return:
+    """
+    is_alphabet = defaultdict(int)
+    word = word.replace(' ', '')
+    for each_letter in word:
+        if each_letter >= '\u4e00' and each_letter <= '\u9fff':
+            is_alphabet['cn'] += 1
+        # elif word >= '\u0030' and word <= '\u0039':
+        #     return 'num'
+        elif (each_letter >= '\u0041' and each_letter <= '\u005a') or (
+                each_letter >= '\u0061' and each_letter <= '\u007a'):
+            is_alphabet['en'] += 1
+        else:
+            is_alphabet['other'] += 1
+
+    is_alphabet['en'] /= 4
+
+    for len_type, num in is_alphabet.items():
+        if num >= sum(is_alphabet.values()) * 0.7:
+            return len_type
+    return 'other'
+
+
+def downloader(url, header):
+    """
+    :param url: url need to be downloaded
+    :param header: fake header
+    :return:
+    """
+    try:
+        result = requests.get(url, headers=header, timeout=TIMEOUT)
+        result_selector = html.etree.HTML(result.text)
+        resp_code = result.status_code
+    except requests.exceptions.ConnectionError as e:
+        print(colors.red | "[Error!] Time out.")
+        print("<%s>" % e)
+        result_selector = None
+        resp_code = None
+    return result_selector, resp_code
+
+
+def downloader_plain(url, header):
+    """
+    plain download. Do not make the resp to selector
+    :param url:
+    :param header:
+    :return:
+    """
+    try:
+        return requests.get(url, headers=header).text
+    except:
+        return None
