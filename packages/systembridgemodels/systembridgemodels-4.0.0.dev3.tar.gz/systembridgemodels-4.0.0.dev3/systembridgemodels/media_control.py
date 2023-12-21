65cdd96d9dd2c5da7e5dc77146f95b1b9bfef2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:05:49 +0000

    Add package systembridgemodels/4.0.0.dev3/systembridgemodels-4.0.0.dev3.tar.gz

---
+++ packages/systembridgemodels/systembridgemodels-4.0.0.dev3.tar.gz/systembridgemodels-4.0.0.dev3/systembridgemodels/media_control.py
@@ -0,0 +1,32 @@
+"""Media Control."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+
+
+class MediaAction(Enum):
+    """Media Action."""
+
+    play = "PLAY"
+    pause = "PAUSE"
+    stop = "STOP"
+    previous = "PREVIOUS"
+    next = "NEXT"
+    seek = "SEEK"
+    rewind = "REWIND"
+    fastforward = "FASTFORWARD"
+    shuffle = "SHUFFLE"
+    repeat = "REPEAT"
+    mute = "MUTE"
+    volumedown = "VOLUMEDOWN"
+    volumeup = "VOLUMEUP"
+
+
+@dataclass
+class MediaControl:
+    """Media Control."""
+
+    action: MediaAction
+    value: Any | None = None
