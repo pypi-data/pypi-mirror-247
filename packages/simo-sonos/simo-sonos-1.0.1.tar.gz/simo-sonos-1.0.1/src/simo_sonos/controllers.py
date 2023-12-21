35ce8ce00544c08609a815298fbba17559eac939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:05:21 +0000

    Add package simo-sonos/1.0.1/simo-sonos-1.0.1.tar.gz

---
+++ packages/simo-sonos/simo-sonos-1.0.1.tar.gz/simo-sonos-1.0.1/src/simo_sonos/controllers.py
@@ -0,0 +1,35 @@
+from simo.multimedia.controllers import BaseAudioPlayer
+from .gateways import SONOSGatewayHandler
+from .forms import SONOSPlayerConfigForm
+
+
+class SONOSPlayer(BaseAudioPlayer):
+    gateway_class = SONOSGatewayHandler
+    config_form = SONOSPlayerConfigForm
+
+    def play_uri(self, uri, volume=None):
+        if volume:
+            assert 0 <= volume <= 100
+        self.send({"play_uri": uri, 'volume': volume})
+
+    def play_alert(self, val, volume=None):
+        '''Val can be sound id or uri'''
+        assert type(val) in (int, str)
+        if volume:
+            assert 0 <= volume <= 100
+        self.send({"alert": val, 'volume': volume})
+
+    def play_playlist(self, item_id):
+        from simo_sonos.models import SonosPlayer
+        sonos_player = SonosPlayer.objects.filter(
+            id=self.component.config['sonos_device']
+        ).first()
+        if not sonos_player:
+            return
+        for plst in sonos_player.soco.get_sonos_playlists():
+            if plst.item_id == item_id:
+                sonos_player.soco.clear_queue()
+                sonos_player.soco.add_to_queue(plst)
+                sonos_player.soco.play_from_queue(0, False)
+                sonos_player.soco.play()
+                return
