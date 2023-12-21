dab73b9c4e24d2f1921cff03c1d294f6f8ceaa98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:04:38 +0000

    Add package distcrab/0.0.42/distcrab-0.0.42.tar.gz

---
+++ packages/distcrab/distcrab-0.0.42.tar.gz/distcrab-0.0.42/distcrab/future.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .logger import logger
+from .parser import PARSER
+
+async def future(Procedure):
+    async for item in Procedure(**vars(PARSER.parse_args())):
+        logger.warning(item.decode().strip())
