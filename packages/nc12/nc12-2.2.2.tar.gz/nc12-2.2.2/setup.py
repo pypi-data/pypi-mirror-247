810076637010747d3f08143fb2a34409b242e8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:03:57 +0000

    Add package nc12/2.2.2/nc12-2.2.2.tar.gz

---
+++ packages/nc12/nc12-2.2.2.tar.gz/nc12-2.2.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nc12',
+    version='2.2.2',
+    description='NC12_01',
+    author='luyendd',
+    author_email='tudu8603@gmail.com',
+    packages=['a', 'b'],
+)
