41cb5136c0f92d5d938dc7bb96171ee28de52510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:23 +0000

    Add package mitesh-palindrome-lib/0.1.0/mitesh_palindrome_lib-0.1.0.tar.gz

---
+++ packages/mitesh-palindrome-lib/mitesh_palindrome_lib-0.1.0.tar.gz/mitesh_palindrome_lib-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mitesh_palindrome_lib",
+    version = "0.1.0",
+    author="mitesh gupta",
+    author_email="mitesh51294@gmail.com",
+    description="This lib will help you validate a palindrome ",
+    packages=(),
+)
+
