492c88ec1326861adf29a7ad431d2172bba490bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:38 +0000

    Add package garak/0.9.0.10/garak-0.9.0.10-py3-none-any.whl

---
+++ packages/garak/garak-0.9.0.10-py3-none-any.whl/garak/generators/replicate.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Replicate generator interface
+
+Generator for https://replicate.com/
+
+Put your replicate key in an environment variable called
+REPLICATE_API_TOKEN. It's found on your Replicate account
+page, https://replicate.com/account.
+
+Text-output models are supported.
+"""
+
+import importlib
+import os
+
+import backoff
+import replicate.exceptions
+
+from garak import _config
+from garak.generators.base import Generator
+
+
+class ReplicateGenerator(Generator):
+    """Wrapper for the Replicate hosted models (replicate.com). Expects API key in REPLICATE_API_TOKEN environment variable."""
+
+    generator_family_name = "Replicate"
+    temperature = 1
+    top_p = 1.0
+    repetition_penalty = 1
+    supports_multiple_generations = False
+
+    def __init__(self, name, generations=10):
+        self.name = name
+        self.fullname = f"Replicate {self.name}"
+        self.seed = 9
+        if hasattr(_config.run, "seed"):
+            self.seed = _config.run.seed
+
+        super().__init__(name, generations=generations)
+
+        if os.getenv("REPLICATE_API_TOKEN", default=None) is None:
+            raise ValueError(
+                'Put the Replicate API token in the REPLICATE_API_TOKEN environment variable (this was empty)\n \
+                e.g.: export REPLICATE_API_TOKEN="r8-123XXXXXXXXXXXX"'
+            )
+        self.replicate = importlib.import_module("replicate")
+
+    @backoff.on_exception(
+        backoff.fibo, replicate.exceptions.ReplicateError, max_value=70
+    )
+    def _call_model(self, prompt):
+        response_iterator = self.replicate.run(
+            self.name,
+            input={
+                "prompt": prompt,
+                "max_length": self.max_tokens,
+                "temperature": self.temperature,
+                "top_p": self.top_p,
+                "repetition_penalty": self.repetition_penalty,
+                "seed": self.seed,
+            },
+        )
+        return "".join(response_iterator)
+
+
+default_class = "ReplicateGenerator"
