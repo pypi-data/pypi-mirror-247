01fd58b207c13ec85ac94eae88e63e01aed7ed03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:17 +0000

    Add package openrl/0.2.0/openrl-0.2.0.tar.gz

---
+++ packages/openrl/openrl-0.2.0.tar.gz/openrl-0.2.0/openrl/envs/__init__.py
@@ -0,0 +1,29 @@
+mpe_all_envs = [
+    "simple_spread",
+]
+nlp_all_envs = [
+    "daily_dialog",
+    "fake_dialog_data",
+]
+super_mario_all_envs = [
+    "SuperMarioBros",
+]
+
+connect_all_envs = [
+    "connect3",
+    "connect4",
+]
+
+toy_all_envs = [
+    "BitFlippingEnv",
+    "IdentityEnv",
+    "IdentityEnvcontinuous",
+    "IdentityEnvBox",
+    "SimpleMultiObsEnv",
+    "SimpleMultiObsEnv",
+]
+gridworld_all_envs = ["GridWorldEnv", "GridWorldEnvRandomGoal"]
+
+offline_all_envs = ["OfflineEnv"]
+
+pettingzoo_all_envs = ["tictactoe_v3", "snakes_1v1", "snakes_3v3"]
