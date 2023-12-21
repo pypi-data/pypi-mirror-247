bd44ae14e0e47eedda7fa538266a324d827efb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:03:59 +0000

    Add package aphyt/0.1.18/aphyt-0.1.18-py3-none-any.whl

---
+++ packages/aphyt/aphyt-0.1.18-py3-none-any.whl/aphyt/omron/omron_cip_objects.py
@@ -0,0 +1,13 @@
+__author__ = 'Joseph Ryan'
+__license__ = "GPLv2"
+__maintainer__ = "Joseph Ryan"
+__email__ = "jr@aphyt.com"
+
+from aphyt.cip import *
+
+
+class TCPIPInterfaceObject:
+    class_id = b'\xf5'
+
+    def __init__(self):
+        pass
