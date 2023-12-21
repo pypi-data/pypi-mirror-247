f81a6bc8a617becb726eb0f0eea8883a89bdb23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 06:04:32 +0000

    Add package jupyter-pieces/1.9.0/jupyter_pieces-1.9.0.tar.gz

---
+++ packages/jupyter-pieces/jupyter_pieces-1.9.0.tar.gz/jupyter_pieces-1.9.0/src/ui/LabIcons.ts
@@ -0,0 +1,90 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+import Constants from '../const';
+
+export const PiecesLogo = new LabIcon({
+  name: 'jupyter-pieces:logo',
+  svgstr: Constants.PIECES_LOGO,
+});
+export const refreshIcon = new LabIcon({
+  name: 'jupyter-pieces:refresh',
+  svgstr: Constants.REFRESH_SVG,
+});
+
+export const cancelIcon = new LabIcon({
+  name: 'jupyter-pieces:cancel',
+  svgstr: Constants.CANCEL_SVG,
+});
+
+export const codeSVG = new LabIcon({
+  name: 'jupyter-pieces:codeSVG',
+  svgstr: Constants.CODE_SVG,
+});
+
+export const openFile = new LabIcon({
+  name: 'jupyter-pieces:openFile',
+  svgstr: Constants.OPEN_ICON,
+});
+
+export const userIcon = new LabIcon({
+  name: 'jupyter-pieces:userSVG',
+  svgstr: Constants.USER_SVG,
+});
+
+export const aiSVG = new LabIcon({
+  name: 'jupyter-pieces:aiSVG',
+  svgstr: Constants.AI_SVG,
+});
+
+export const copyIcon = new LabIcon({
+  name: 'jupyter-pieces:copy',
+  svgstr: Constants.COPY_SVG,
+});
+export const shareIcon = new LabIcon({
+  name: 'jupyter-pieces:share',
+  svgstr: Constants.SHARE_SVG,
+});
+export const deleteIcon = new LabIcon({
+  name: 'jupyter-pieces:delete',
+  svgstr: Constants.DELETE_SVG,
+});
+export const expandIcon = new LabIcon({
+  name: 'jupyter-pieces:expand',
+  svgstr: Constants.EXPAND_SVG,
+});
+export const editIcon = new LabIcon({
+  name: 'jupyter-pieces:edit',
+  svgstr: Constants.EDIT_SVG,
+});
+export const saveIcon = new LabIcon({
+  name: 'jupyter-pieces:save',
+  svgstr: Constants.SAVE_SVG,
+});
+export const copilotWhite = new LabIcon({
+  name: 'jupyter-pieces:copilotWhite',
+  svgstr: Constants.COPILOT_WHITE,
+});
+
+export const copilotBlack = new LabIcon({
+  name: 'jupyter-pieces:copilotBlack',
+  svgstr: Constants.COPILOT_BLACK,
+});
+export const sendSVG = new LabIcon({
+  name: 'jupyter-pieces:sendSVG',
+  svgstr: Constants.SEND_SVG,
+});
+export const saveAllIcon = new LabIcon({
+  name: 'jupyter-pieces:save-all',
+  svgstr: Constants.SAVE_ALL_SVG,
+});
+export const starIcon = new LabIcon({
+  name: 'jupyter-pieces:star',
+  svgstr: Constants.STAR_ICON,
+});
+export const annotationIcon = new LabIcon({
+  name: 'jupyter-pieces:annotation',
+  svgstr: Constants.STICKY_NOTE_ICON,
+});
+export const plusIcon = new LabIcon({
+  name: 'jupyter-pieces:plus',
+  svgstr: Constants.PLUS_ICON,
+});
