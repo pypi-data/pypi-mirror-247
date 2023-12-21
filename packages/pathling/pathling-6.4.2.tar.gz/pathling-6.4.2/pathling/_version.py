9cf99f39d4d1477dfebb0ad5d6b17b056ed28dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:04:11 +0000

    Add package pathling/6.4.2/pathling-6.4.2.tar.gz

---
+++ packages/pathling/pathling-6.4.2.tar.gz/pathling-6.4.2/pathling/_version.py
@@ -0,0 +1,9 @@
+#
+# Auto generated from POM project version.
+# Please do not modify.
+#
+__version__="6.4.2"
+__java_version__="6.4.2"
+__scala_version__="2.12"
+__delta_version__="2.4.0"
+__hadoop_version__="3.3.4"
