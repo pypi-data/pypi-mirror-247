4e0ba7304e0746328ee91c266438f119b668779e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:07 +0000

    Add package toolformer-pytorch/0.0.30/toolformer-pytorch-0.0.30.tar.gz

---
+++ packages/toolformer-pytorch/toolformer-pytorch-0.0.30.tar.gz/toolformer-pytorch-0.0.30/toolformer_pytorch/__init__.py
@@ -0,0 +1,11 @@
+from toolformer_pytorch.palm import PaLM
+
+from toolformer_pytorch.toolformer_pytorch import (
+    Toolformer,
+    filter_tokens_with_api_response,
+    sample,
+    sample_with_api_call,
+    has_api_calls,
+    invoke_tools,
+    replace_all_but_first
+)
