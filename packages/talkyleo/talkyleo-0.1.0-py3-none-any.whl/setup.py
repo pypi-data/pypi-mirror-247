23811380296c867e16a556b41d55a6877bf560a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:02 +0000

    Add package talkyleo/0.1.0/talkyleo-0.1.0-py3-none-any.whl

---
+++ packages/talkyleo/talkyleo-0.1.0-py3-none-any.whl/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='talkyleo',
+    version='0.0.1',
+    description='',
+    author='naelsondouglas',
+    author_email='naelson17@gmail.com',
+    packages=[],
+    install_requires=[
+        'boto3',
+        'pysub-parser',
+        'pydantic',
+        'pydub'
+    ],
+    scripts=[],
+)
+
+#'git+https://github.com/NaelsonDouglas/PyWikiHow.git'
