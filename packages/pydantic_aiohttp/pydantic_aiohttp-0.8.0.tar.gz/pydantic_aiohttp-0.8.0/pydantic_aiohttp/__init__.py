16a8532e5c1aeb7b5821931ebd10e43e3f6514ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:19 +0000

    Add package pydantic_aiohttp/0.8.0/pydantic_aiohttp-0.8.0.tar.gz

---
+++ packages/pydantic_aiohttp/pydantic_aiohttp-0.8.0.tar.gz/pydantic_aiohttp-0.8.0/pydantic_aiohttp/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.8.0'
+__author__ = "pylakey <pylakey@protonmail.com>"
+
+from . import encoders
+from . import utils
+from .client import Client
+from .errors import *
+from .types import *
