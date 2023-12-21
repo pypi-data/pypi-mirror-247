67ef7aad4db8e4e99ef4cfe84092cdb88fb7b3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:07 +0000

    Add package monarch-py/0.19.2/monarch_py-0.19.2-py3-none-any.whl

---
+++ packages/monarch-py/monarch_py-0.19.2-py3-none-any.whl/monarch_py/api/main.py
@@ -0,0 +1,56 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from monarch_py.api import association, entity, histopheno, search, semsim, text_annotation
+from monarch_py.api.config import semsimian
+from monarch_py.api.middleware.logging_middleware import LoggingMiddleware
+
+PREFIX = "/v3/api"
+
+app = FastAPI(
+    docs_url="/v3/docs",
+    redoc_url="/v3/redoc",
+)
+
+
+@app.on_event("startup")
+async def initialize_app():
+    semsimian()
+    # oak()
+
+
+app.include_router(association.router, prefix=f"{PREFIX}/association")
+app.include_router(entity.router, prefix=f"{PREFIX}/entity")
+app.include_router(histopheno.router, prefix=f"{PREFIX}/histopheno")
+app.include_router(search.router, prefix=PREFIX)
+app.include_router(semsim.router, prefix=f"{PREFIX}/semsim")
+app.include_router(text_annotation.router, prefix=PREFIX)
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(LoggingMiddleware)
+
+
+@app.get("/")
+async def _root():
+    return RedirectResponse(url="/v3/docs")
+
+
+@app.get("/api")
+async def _api():
+    return RedirectResponse(url="/v3/docs")
+
+
+def run():
+    uvicorn.run("monarch_py.api.main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    run()
