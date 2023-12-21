224ea1bc8fd57ca3df473294632e8dd6fb8c297c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:03:44 +0000

    Add package streamlit-component-video/0.1.0/streamlit-component-video-0.1.0.tar.gz

---
+++ packages/streamlit-component-video/streamlit-component-video-0.1.0.tar.gz/streamlit-component-video-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-component-video",
+    version="0.1.0",
+    author="Bob Lin",
+    author_email="bob401710@gmail.com",
+    description="A new video with track",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "jinja2"],
+)
