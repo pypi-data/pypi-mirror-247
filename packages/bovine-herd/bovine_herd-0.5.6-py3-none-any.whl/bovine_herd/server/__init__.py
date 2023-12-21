21ba2a02d65892cdc44fe1cad5e8a1f3fa239b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:16 +0000

    Add package bovine-herd/0.5.6/bovine_herd-0.5.6-py3-none-any.whl

---
+++ packages/bovine-herd/bovine_herd-0.5.6-py3-none-any.whl/bovine_herd/server/__init__.py
@@ -0,0 +1,8 @@
+from quart import Blueprint
+
+from .activitypub import activitypub
+from .wellknown import wellknown
+
+default_configuration = Blueprint("default_configuration", __name__)
+default_configuration.register_blueprint(wellknown)
+default_configuration.register_blueprint(activitypub)
