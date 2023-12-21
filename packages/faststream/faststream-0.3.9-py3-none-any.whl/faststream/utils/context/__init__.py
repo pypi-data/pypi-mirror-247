4abf0ef2ab2c73d592b373b2d65b784209175eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 06:04:22 +0000

    Add package faststream/0.3.9/faststream-0.3.9-py3-none-any.whl

---
+++ packages/faststream/faststream-0.3.9-py3-none-any.whl/faststream/utils/context/__init__.py
@@ -0,0 +1,10 @@
+from faststream.utils.context.builders import Context, Header, Path
+from faststream.utils.context.main import ContextRepo, context
+
+__all__ = (
+    "Context",
+    "context",
+    "ContextRepo",
+    "Header",
+    "Path",
+)
