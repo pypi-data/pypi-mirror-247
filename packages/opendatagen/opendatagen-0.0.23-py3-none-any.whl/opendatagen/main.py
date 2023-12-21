8589fcfffeecc2aa1cc7b78af94e5edf4d27f59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:20 +0000

    Add package opendatagen/0.0.23/opendatagen-0.0.23-py3-none-any.whl

---
+++ packages/opendatagen/opendatagen-0.0.23-py3-none-any.whl/opendatagen/main.py
@@ -0,0 +1,126 @@
+from opendatagen.template import Template, TemplateManager, TemplateName, Variable
+from opendatagen.data_generator import DataGenerator
+from opendatagen.model import OpenAIChatModel, OpenAIInstructModel, OpenAIEmbeddingModel, ModelName, MistralChatModel
+from mistralai.models.chat_completion import ChatMessage
+from opendatagen.anonymizer import Anonymizer
+from opendatagen.utils import function_to_call
+from opendatagen.agent import DataAgent
+import warnings
+import json 
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+import pandas as pd
+
+def anonymize_text(): 
+
+    text_to_anonymize = """
+            My name is Thomas, Call me at 0601010129 or email me at john.doe@example.com. 
+            My SSN is 123-45-6789 and 4242 4242 8605 2607 is my credit card number. 
+            Living in the best city in the world: Melbourne.
+            New York & Co is a restaurant.
+            It is 10 am.
+            I have 10€ in my pocket. Oh my god.
+            I have park my Tesla next to your house.
+            My id is 0//1//2//2//2//2
+    """
+    
+    completion_model = OpenAIChatModel(model_name="gpt-3.5-turbo-1106")
+
+    anonymizer = Anonymizer(completion_model=completion_model)
+
+    anonymized_text = anonymizer.anonymize(text=text_to_anonymize)
+    
+    print(anonymized_text)
+
+def generate_data_from_predefined_template(template_file_path:str, template_name:str, output_path:str): 
+    
+    manager = TemplateManager(template_file_path=template_file_path)
+    template = manager.get_template(template_name=template_name)
+
+    if template:
+        
+        generator = DataGenerator(template=template)
+        
+        data = generator.generate_data(output_path=output_path)
+        
+        print(data)
+
+def check_text(result: dict):
+
+    relevant_sentence = result["relevant_sentence"].value.lower().replace("\n", " ").replace("'''", "")
+    wikipedia_content = result["wikipedia_content"].value.lower().replace("\n", " ").replace("'''", "")
+
+    if relevant_sentence in wikipedia_content:
+        return True, "All ok."
+    else:
+        return False, "The sentence must be contained in the given text, please correct."
+
+
+if __name__ == "__main__":
+
+    #agent = DataAgent(model_name="gpt-4")
+    #agent.run()
+    #anonymize_text()
+
+    data = generate_data_from_predefined_template(template_file_path="opendatagen/template.json", 
+                                           template_name="factuality", 
+                                           output_path="factuality.csv"
+                                           )
+    
+    
+
+    '''    # Assuming 'factuality.csv' is in the same folder and has columns 'question' and 'answer'
+    df = pd.read_csv('factuality.csv')
+
+    # Initialize counters for each model
+    true_counts = {'mistral7b': 0, 'mistra8x7b': 0, 'mistraX': 0, 'GPT35': 0}
+    total_counts = {'mistral7b': 0, 'mistra8x7b': 0, 'mistraX': 0, 'GPT35': 0}
+
+    for index, row in df.iterrows():
+    
+        print(f"Processing {index + 1}/{len(df)}")
+
+        question = row['question']
+        reference_answer = row['answer']
+
+        mistral7b = MistralChatModel(name="mistral-tiny", max_tokens=64, temperature=[0])
+        mistra8x7b = MistralChatModel(name="mistral-small", max_tokens=64, temperature=[0])
+        mistraX = MistralChatModel(name="mistral-medium", max_tokens=64, temperature=[0])
+        gpt35 = OpenAIChatModel(name="gpt-3.5-turbo-1106", max_tokens=64, temperature=[0])
+
+        system_prompt_evaluator = "You are EvaluatorGPT, given the question and the reference answer please determine if the new answer is True or False. Only answer with 'True' or 'False'. No verbose."
+
+        messages = [ChatMessage(role="user", content=f"Answer accurately and consicely to the following question:\n{question}")]
+
+        gpt_messages = [
+                {"role":"system", "content": "Answer to the question consicely."},
+                {"role":"system", "content": f"Question:\n{question}"}
+            ]
+
+        answer7b = mistral7b.ask(messages=messages)
+        answer8x7b = mistra8x7b.ask(messages=messages)
+        answerX = mistraX.ask(messages=messages)
+        answergpt35 = gpt35.ask(messages=gpt_messages)
+
+        # For each model, evaluate the answer
+        for model_name, model_answer in [('mistral7b', answer7b), ('mistra8x7b', answer8x7b), ('mistraX', answerX), ('GPT35', answergpt35)]:
+
+            evaluator = OpenAIChatModel(name="gpt-4-1106-preview", max_tokens=5, system_prompt=system_prompt_evaluator)
+
+            evaluator_messages = [
+                {"role":"system", "content": system_prompt_evaluator},
+                {"role":"system", "content": f"Question:\n{question}\nReference answer:\n{reference_answer}\nNew answer:\n{model_answer}"}
+            ]
+    
+            evaluator_answer = evaluator.ask(messages=evaluator_messages)
+            
+            if evaluator_answer.lower() == "true":
+                true_counts[model_name] += 1
+            total_counts[model_name] += 1
+
+    # Calculate and display the results
+    for model_name in true_counts:
+        accuracy = (true_counts[model_name] / total_counts[model_name]) * 100
+        print(f"Model {model_name}: {accuracy:.2f}% True answers")
+
+ '''
---
+++ packages/opendatagen/opendatagen-0.0.23-py3-none-any.whl/opendatagen/template.py
@@ -0,0 +1,369 @@
+from pydantic import BaseModel, validator, ValidationError, ConfigDict
+from typing import Optional, List, Dict, Union
+from enum import Enum
+import os
+import json
+from opendatagen.utils import load_file
+from opendatagen.model import OpenAIChatModel, OpenAIInstructModel, OpenAIEmbeddingModel, HuggingFaceModel, Model
+from urllib.parse import quote_plus
+import requests
+import trafilatura
+from PyPDF2 import PdfReader
+import pandas as pd
+from datasets import load_dataset, Dataset
+from opendatagen.utils import get_first_n_tokens, num_tokens_from_string
+import random
+import uuid
+
+
+class RAGHuggingFace(BaseModel):
+
+    dataset_path:str
+    dataset_name:Optional[str] = None
+    data_dir:Optional[str] = None
+    column_name:str
+    streaming:bool = True
+    min_tokens:Optional[int] = 0
+    max_tokens:Optional[int] = None
+    subset_size:Optional[int] = 10000
+
+    class Config:
+        extra = "forbid"
+
+    def get_random_value_from_dataset(self):
+
+        param = {}
+
+        if self.dataset_path:
+            param["path"] = self.dataset_path
+
+        if self.data_dir:
+            param["data_dir"] = self.data_dir
+
+        if self.dataset_name:
+            param["name"] = self.dataset_name
+
+        param["streaming"] = self.streaming
+
+        dst = load_dataset(**param)
+
+        subset = [sample[self.column_name] for _, sample in zip(range(self.subset_size), dst["train"])]
+
+        max_attempts = 100
+        count = 0
+
+        while count < max_attempts:
+
+            index = random.randint(0, len(subset) - 1)
+
+            text = subset[index]
+
+            num_tokens = num_tokens_from_string(text, encoding_name="cl100k_base")
+
+            if num_tokens >= self.min_tokens:
+
+                if self.max_tokens:
+
+                    text = subset[index]
+
+                    result = get_first_n_tokens(n=self.max_tokens, text=text, encoding_name="cl100k_base")
+                    
+                    return result
+
+                else:
+
+                    result = subset[index]
+
+                    return result
+
+            count = count + 1
+
+
+
+    """
+    def get_n_nearest_value_from_dataset(self, n:int=1, prompt:str)
+    """
+
+
+class RAGLocalPath(BaseModel):
+
+    localPath:Optional[str] = None
+    directoryPath:Optional[str] = None
+    content:Optional[str] = None
+
+    class Config:
+        extra = "forbid"
+
+    def get_content_from_file(self):
+        """
+        Reads the content from a file based on its extension.
+        Handles CSV, TXT, and PDF files.
+        """
+        file_content = ''
+        if self.localPath.endswith('.csv'):
+            df = pd.read_csv(self.localPath)
+            df = df.astype(str)
+            file_content = df.to_string(header=True, index=False, max_rows=None)
+            print(file_content)
+        elif self.localPath.endswith('.txt'):
+            with open(self.localPath, 'r') as file:
+                file_content = file.read()
+        elif self.localPath.endswith('.pdf'):
+            reader = PdfReader(self.localPath)
+            for page in reader.pages:
+                file_content += page.extract_text() + '\n'
+        else:
+            raise ValueError("Unsupported file format")
+
+        self.content = file_content
+        return file_content
+
+    def get_content_from_directory(self):
+        """
+        Iterates over files in the directory, reads their content,
+        and concatenates it into a single string.
+        """
+        concatenated_content = ''
+        for filename in os.listdir(self.directoryPath):
+            filepath = os.path.join(self.directoryPath, filename)
+            if filepath.endswith(('.csv', '.txt', '.pdf')):
+                self.localPath = filepath  # Temporarily update the localPath
+                file_content = self.get_content_from_file()
+                concatenated_content += file_content + '\n'
+
+        self.content = concatenated_content  # Store concatenated content
+        return concatenated_content
+
+
+class RAGInternet(BaseModel):
+
+    keywords:List[str]
+    return_chunks: Optional[bool] = False
+    minimum_number_of_words_by_article: Optional[int] = 500
+    maximum_number_of_words_by_article: Optional[int] = 50000
+    content: Optional[str] = None
+
+    def word_counter(self, input_string):
+        # Split the string into words based on whitespace
+        words = input_string.split()
+
+        # Count the number of words
+        number_of_words = len(words)
+
+        return number_of_words
+
+    def get_google_search_result(self, keyword:dict, maximum_number_of_link:int = None):
+
+        encoded_keyword = quote_plus(keyword)
+
+        url = f"https://api.serply.io/v1/search/q={encoded_keyword}"
+
+        headers = {
+            "Content-Type": "application/json",
+            "X-User-Agent": "",
+            "X-Proxy-Location": "",
+            "X-Api-Key": os.environ.get("SERPLY_API_KEY"),
+            "X-Proxy-Location": "US"
+        }
+
+        response = requests.request("GET", url, headers=headers)
+
+        response_json = json.loads(response.text)["results"]
+
+        result = []
+
+        for element in response_json:
+
+            link = element['link']
+            result.append(link)
+
+        if maximum_number_of_link:
+            return result[:maximum_number_of_link]
+
+        return result
+
+    def get_content_from_url(self, link:str):
+
+        downloaded = trafilatura.fetch_url(link)
+        content = trafilatura.extract(downloaded)
+
+        return content
+
+    def extract_content_from_internet(self):
+
+        print(f"Browsing...")
+
+        for keyword in self.keywords:
+
+            result = ""
+
+            urls = self.get_google_search_result(keyword)
+
+            for url in urls:
+
+                content = self.get_content_from_url(url)
+
+                if content and self.word_counter(content) > self.minimum_number_of_words_by_article and self.word_counter(content) < self.maximum_number_of_words_by_article:
+
+                    print(url)
+
+                    result = result + "\n" + content
+
+        print("Finish browsing...")
+        self.content = result
+        return result
+
+class Validator(BaseModel):
+
+    function_name:str
+    additional_parameters:Optional[List[str]] = None
+    from_notebook:bool = False
+    retry_number:Optional[int] = 3
+
+
+class Variations(BaseModel):
+
+    id:str
+    parent_id:Optional[str] = None
+    value:str
+    confidence_score:Optional[Dict] = None 
+    error_message:str = None
+
+    class Config:
+        extra = "forbid"  # This will raise an error for extra fields
+ 
+
+
+class Variable(BaseModel):
+
+    name: str
+    models:Optional[List[Model]] = None 
+    generation_number: int = 1
+    source_internet: Optional[RAGInternet] = None
+    source_localfile: Optional[RAGLocalPath] = None
+    source_localdirectory: Optional[RAGLocalPath] = None
+    source_huggingface:Optional[RAGHuggingFace] = None
+    get_value_from_huggingface:Optional[RAGHuggingFace] = None
+    get_value_from_localfile:Optional[RAGLocalPath] = None
+    note: Optional[List[str]] = None
+    rag_content: Optional[str] = None
+    validator:Optional[Validator] = None
+    values:Optional[Dict[str, Variations]] = {}
+
+    model_config = ConfigDict(
+            protected_namespaces=('protect_me_', 'also_protect_'),
+            extra = "forbid"
+        )
+
+    def load_internet_source(self):
+
+        if self.source_internet is not None:
+            self.rag_content = self.source_internet.extract_content_from_internet()
+
+    def load_local_file(self):
+
+        if self.source_localfile is not None and self.source_localfile.localPath is not None:
+            self.rag_content = self.source_localfile.get_content_from_file()
+
+    def load_local_directory(self):
+
+        if self.source_localfile is not None and self.source_localfile.directoryPath is not None:
+            self.rag_content = self.source_localfile.get_content_from_directory()
+
+    def load_huggingface_dataset(self):
+
+        if self.source_huggingface is not None:
+            self.rag_content = self.source_huggingface.get_random_value_from_dataset()
+
+    def load_value(self):
+
+        if self.get_value_from_huggingface:
+            self.value = self.get_value_from_huggingface.get_random_value_from_dataset(max_token=self.max_tokens)
+
+
+
+class Template(BaseModel):
+
+    description: str
+    prompt: str
+    completion: str
+    prompt_variation_number: Optional[int] = 1
+    variables: Optional[Dict[str, Variable]] = None
+    source_internet: Optional[RAGInternet] = None
+    source_localfile: Optional[RAGLocalPath] = None
+    rag_content: Optional[str] = None
+    value:Optional[List[str]] = None
+
+    class Config:
+        extra = "forbid"  # This will raise an error for extra fields
+
+    def load_internet_source(self):
+
+        if self.source_internet is not None:
+            self.rag_content = self.source_internet.extract_content_from_internet()
+
+    def load_local_file(self):
+
+        if self.source_localfile is not None and self.source_localfile.localPath is not None:
+            self.rag_content = self.source_localfile.get_content_from_file()
+
+    def load_local_directory(self):
+
+        if self.source_localfile is not None and self.source_localfile.directoryPath is not None:
+            self.rag_content = self.source_localfile.get_content_from_directory()
+
+class TemplateName(Enum):
+    PRODUCT_REVIEW = "product-review"
+    CHUNK = "chunk"
+    CHUNK2 = "chunk2"
+    HALLUCINATION = "hallucination"
+
+
+class TemplateManager:
+
+    def __init__(self, template_file_path:str):
+        self.template_file_path = self.get_template_file_path(template_file_path)
+        self.templates = self.load_templates()
+
+    def get_template_file_path(self, filename: str) -> str:
+        base_path = os.getcwd()  
+        
+        if os.path.isabs(filename):
+            return filename
+        else:
+            return os.path.join(base_path, filename) 
+
+    def load_templates(self) -> Dict[str, Template]:
+        with open(self.template_file_path, 'r') as file:
+            raw_data = json.load(file)
+
+        templates = {}
+        for key, data in raw_data.items():
+            
+            template_name = key
+            template = Template(**data)
+            templates[template_name] = template
+            
+        return templates
+
+    def get_template(self, template_name: str) -> Template:
+
+        template = self.templates.get(template_name)
+
+        if template:
+
+            template.load_internet_source()
+            template.load_local_file()
+            template.load_local_directory()
+
+        return template
+
+def create_variable_from_name(model:OpenAIChatModel, variable_name:str) -> Variable:
+
+    prompt = load_file(path="files/variable_generation.txt")
+
+    prompt = prompt.format(variable_name=variable_name)
+
+    completion = model.ask_instruct_gpt(prompt=prompt, temperature=0, max_tokens=30)
+
+    return Variable(**completion)
