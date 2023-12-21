ac881b0dae73f11d905d0615e9c5564357e6690d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:25 +0000

    Add package izihawa-trident/1.0.29/izihawa_trident-1.0.29-py3-none-any.whl

---
+++ packages/izihawa-trident/izihawa_trident-1.0.29-py3-none-any.whl/trident/key_cache.py
@@ -0,0 +1,14 @@
+class KeyCache:
+    def __init__(self):
+        self._cache = set()
+
+    def __contains__(self, key):
+        return key in self._cache
+
+    def exists(self, key: str) -> bool:
+        if key in self._cache:
+            return True
+        return False
+
+    def add(self, key: str):
+        return self._cache.add(key)
