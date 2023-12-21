4cae5ed1b4be2e7778eb3afc5fce4cf01971dec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:04:11 +0000

    Add package bosma-ble/0.1.0/bosma_ble-0.1.0.tar.gz

---
+++ packages/bosma-ble/bosma_ble-0.1.0.tar.gz/bosma_ble-0.1.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['bleak>=0.21.1', 'bleak-retry-connector>=3.3.0']
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Brett Jurgens",
+    author_email='brett@brettjurgens.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Library for interacting with Bosma Smart Home (https://www.bosmasmarthome.com)",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='bosma',
+    name='bosma_ble',
+    packages=find_packages(include=['bosma_ble', 'bosma_ble.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/brettjurgens/bosma',
+    version='0.1.0',
+    zip_safe=False,
+)
