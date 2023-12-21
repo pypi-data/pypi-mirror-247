0798320a4e5389207381c7e0a91c222da13eef07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:06:19 +0000

    Add package deepnight/0.1/deepnight-0.1.tar.gz

---
+++ packages/deepnight/deepnight-0.1.tar.gz/deepnight-0.1/deepnight/vani.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+def vani(file_path: str, model="vani-v1"):
+    resp = requests.post("https://api.deepnight.tech/v1", data={
+        "file": open(file_path, "rb"),
+        "model": model
+    })
+
+    return resp
