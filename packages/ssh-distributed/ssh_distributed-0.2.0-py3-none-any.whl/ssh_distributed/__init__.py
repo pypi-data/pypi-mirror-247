3cd254f63c27571873064ead9cd48a641aa3b4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:29 +0000

    Add package ssh-distributed/0.2.0/ssh_distributed-0.2.0-py3-none-any.whl

---
+++ packages/ssh-distributed/ssh_distributed-0.2.0-py3-none-any.whl/ssh_distributed/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.0"
+
+from .base import (
+    create_tunnel,
+    delete_directory_from_hosts,
+    download_from_hosts,
+    kill_tmux_session_on_host,
+    run_on_host,
+    upload_to_hosts
+)
