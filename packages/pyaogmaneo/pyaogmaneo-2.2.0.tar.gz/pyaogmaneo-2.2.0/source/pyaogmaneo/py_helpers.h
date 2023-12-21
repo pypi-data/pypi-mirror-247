9dfc53fda79cd4d7086aad15c8e23286352c4975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:04 +0000

    Add package pyaogmaneo/2.2.0/pyaogmaneo-2.2.0.tar.gz

---
+++ packages/pyaogmaneo/pyaogmaneo-2.2.0.tar.gz/pyaogmaneo-2.2.0/source/pyaogmaneo/py_helpers.h
@@ -0,0 +1,107 @@
+// ----------------------------------------------------------------------------
+//  PyAOgmaNeo
+//  Copyright(c) 2020-2023 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of PyAOgmaNeo is licensed to you under the terms described
+//  in the PYAOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include <aogmaneo/helpers.h>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <exception>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+
+namespace pyaon {
+inline void set_num_threads(
+    int num_threads
+) {
+    aon::set_num_threads(num_threads);
+}
+
+inline int get_num_threads() {
+    return aon::get_num_threads();
+}
+
+inline void set_global_state(
+    unsigned long state
+) {
+    aon::global_state = state;
+}
+
+inline unsigned long get_global_state() {
+    return aon::global_state;
+}
+
+class File_Reader : public aon::Stream_Reader {
+public:
+    std::ifstream ins;
+
+    void read(
+        void* data,
+        int len
+    ) override;
+};
+
+class File_Writer : public aon::Stream_Writer {
+public:
+    std::ofstream outs;
+
+    void write(
+        const void* data,
+        int len
+    ) override;
+};
+
+class Buffer_Reader : public aon::Stream_Reader {
+public:
+    int start;
+    const py::array_t<unsigned char>* buffer;
+
+    Buffer_Reader()
+    :
+    start(0),
+    buffer(nullptr)
+    {}
+
+    void read(
+        void* data,
+        int len
+    ) override;
+};
+
+class Buffer_Writer : public aon::Stream_Writer {
+public:
+    int start;
+    py::array_t<unsigned char> buffer;
+
+    Buffer_Writer(
+        int buffer_size
+    )
+    :
+    start(0)
+    {
+        buffer = py::array_t<unsigned char>(buffer_size);
+    }
+
+    void write(
+        const void* data,
+        int len
+    ) override;
+};
+
+enum Merge_Mode {
+    merge_random = 0,
+    merge_average = 1
+};
+}
