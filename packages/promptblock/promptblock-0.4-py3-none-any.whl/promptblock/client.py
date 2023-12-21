59bb8ca392436bc0b46c810e77360d8f1d39b832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:04:17 +0000

    Add package promptblock/0.4/promptblock-0.4-py3-none-any.whl

---
+++ packages/promptblock/promptblock-0.4-py3-none-any.whl/promptblock/client.py
@@ -0,0 +1,28 @@
+# promptblock/client.py
+import requests
+
+class Client:
+    def __init__(self, key):
+        self.key = key
+        self.base_url = "https://prompt-block.onrender.com/api"
+
+    def get_prompt(self, name):
+        endpoint = "/prompt-by-name"
+        url = f"{self.base_url}{endpoint}?name={name}&key={self.key}"
+        headers = {"Authorization": f"Bearer {self.key}"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get content: {response.status_code}")
+
+    def get_function(self, name):
+        endpoint = "/function-by-name"
+        url = f"{self.base_url}{endpoint}?name={name}&key={self.key}"
+        headers = {"Authorization": f"Bearer {self.key}"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get content: {response.status_code}")
+
