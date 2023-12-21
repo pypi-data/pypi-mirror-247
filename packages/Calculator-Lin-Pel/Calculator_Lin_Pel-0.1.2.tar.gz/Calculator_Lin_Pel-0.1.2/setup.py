038bad433d68b8eeaf4150344352802c5e464324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:39 +0000

    Add package Calculator-Lin-Pel/0.1.2/Calculator_Lin_Pel-0.1.2.tar.gz

---
+++ packages/Calculator-Lin-Pel/Calculator_Lin_Pel-0.1.2.tar.gz/Calculator_Lin_Pel-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Basic Calculator'
+LONG_DESCRIPTION = 'Basic Python Calculator for simple arithmetic calculations'
+
+# Setting up
+setup(
+    name="Calculator_Lin_Pel",
+    version=VERSION,
+    author="Linas Peleckas",
+    author_email="<peleckas.l@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'calculations'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
