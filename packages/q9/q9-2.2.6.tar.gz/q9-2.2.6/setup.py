afb67a466532ea3cb21643605cefa7d8d232e083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:02 +0000

    Add package q9/2.2.6/q9-2.2.6.tar.gz

---
+++ packages/q9/q9-2.2.6.tar.gz/q9-2.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='q9',
+    version='2.2.6',
+    description='nguoiyeutaroicungxata',
+    author='luyendd',
+    author_email='tudu8603@gmail.com',
+    packages=['a', 'b', 'c'],
+    install_requires=['opencv-python']
+)
