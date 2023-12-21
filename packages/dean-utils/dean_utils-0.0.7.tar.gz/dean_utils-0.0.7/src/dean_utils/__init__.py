461dfff49bc2a7823716c319a14b5f522e01a4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:06:34 +0000

    Add package dean-utils/0.0.7/dean_utils-0.0.7.tar.gz

---
+++ packages/dean-utils/dean_utils-0.0.7.tar.gz/dean_utils-0.0.7/src/dean_utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["async_abfs", "cos_query_all", "send_to_queue", "send_email", "def_cos"]
+from dean_utils.utils.az_utils import async_abfs, cos_query_all, send_to_queue, def_cos
+from dean_utils.utils.email_utility import send_email
