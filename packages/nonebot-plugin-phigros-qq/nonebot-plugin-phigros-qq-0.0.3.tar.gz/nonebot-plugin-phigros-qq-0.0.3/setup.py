7c61b0d6885bd5cb9594abddb5953e4e4c334dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:04:53 +0000

    Add package nonebot-plugin-phigros-qq/0.0.3/nonebot-plugin-phigros-qq-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-phigros-qq/nonebot-plugin-phigros-qq-0.0.3.tar.gz/nonebot-plugin-phigros-qq-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nonebot-plugin-phigros-qq',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow'
+    ],
+    package_data={
+        '': ['simyou.ttf'],
+    },
+    author='NightWind',
+    author_email='2125714976@qq.com',
+    description='一个简单的基于PhigrosLibrary的Nonebot Phigros查分插件，适用于Adapter-qq',
+    url='https://github.com/XTxiaoting14332/nonebot-plugin-phigros-qq',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+)
