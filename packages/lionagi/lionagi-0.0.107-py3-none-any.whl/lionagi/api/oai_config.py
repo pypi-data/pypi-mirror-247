94864bd006ec1e33606d9ed9a088afcdc660c3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:03:49 +0000

    Add package lionagi/0.0.107/lionagi-0.0.107-py3-none-any.whl

---
+++ packages/lionagi/lionagi-0.0.107-py3-none-any.whl/lionagi/api/oai_config.py
@@ -0,0 +1,16 @@
+oai_llmconfig = {
+    "model": "gpt-4-1106-preview",
+    "frequency_penalty": 0,
+    "max_tokens": None,
+    "n": 1,
+    "presence_penalty": 0, 
+    "response_format": {"type": "text"}, 
+    "seed": None, 
+    "stop": None,
+    "stream": False,
+    "temperature": 0.7, 
+    "top_p": 1, 
+    "tools": None,
+    "tool_choice": "none", 
+    "user": None
+    }
