29e57a433279a184d5d0f0336ec73229e87f869c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:05:21 +0000

    Add package baunomian/0.3/baunomian-0.3.tar.gz

---
+++ packages/baunomian/baunomian-0.3.tar.gz/baunomian-0.3/README.md
@@ -0,0 +1,3 @@
+# Baunomian  
+
+A python library for calculations of gaussian and binomial distributions. For the purposes of statistics and data science.
---
+++ packages/baunomian/baunomian-0.3.tar.gz/baunomian-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='baunomian',
+      version='0.3',
+      description='Gaussian and Binomial distribution',
+      packages=['distributions'],
+      zip_safe=False)
