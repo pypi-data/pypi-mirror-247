91b1c44f0fbec6c4d1cf3151c7a760deef90031c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:05:17 +0000

    Add package plate-model-manager/1.2.0/plate_model_manager-1.2.0-py3-none-any.whl

---
+++ packages/plate-model-manager/plate_model_manager-1.2.0-py3-none-any.whl/plate_model_manager/__init__.py
@@ -0,0 +1,7 @@
+# import functions into __init__.py
+
+__version__ = "1.2.0"
+
+from .plate_model import PlateModel
+from .plate_model_manager import PlateModelManager
+from .present_day_rasters import PresentDayRasterManager
