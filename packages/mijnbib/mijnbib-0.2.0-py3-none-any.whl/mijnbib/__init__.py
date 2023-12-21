11c1b36eb897a9063c271cee9367af1327a585a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:34 +0000

    Add package mijnbib/0.2.0/mijnbib-0.2.0-py3-none-any.whl

---
+++ packages/mijnbib/mijnbib-0.2.0-py3-none-any.whl/mijnbib/__init__.py
@@ -0,0 +1,14 @@
+# ruff: noqa: F401
+# So user can do e.g.
+#   from mijnbib import MijnBibliotheek, Loan
+
+from .mijnbibliotheek import MijnBibliotheek
+from .models import Account, Loan, Reservation
+from .plugin_errors import (
+    AuthenticationError,
+    CanNotConnectError,
+    ExtendLoanError,
+    IncompatibleSourceError,
+    ItemAccessError,
+    PluginError,
+)
