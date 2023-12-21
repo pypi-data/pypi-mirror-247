935bef016f3ed834b3a9ddee8252c082c40c5c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:43 +0000

    Add package eradiate/0.25.0/eradiate-0.25.0-py3-none-any.whl

---
+++ packages/eradiate/eradiate-0.25.0-py3-none-any.whl/eradiate/scenes/biosphere/__init__.pyi
@@ -0,0 +1,10 @@
+from ._canopies import wellington_citrus_orchard as wellington_citrus_orchard
+from ._core import Canopy as Canopy
+from ._core import CanopyElement as CanopyElement
+from ._core import InstancedCanopyElement as InstancedCanopyElement
+from ._core import biosphere_factory as biosphere_factory
+from ._discrete import DiscreteCanopy as DiscreteCanopy
+from ._leaf_cloud import LeafCloud as LeafCloud
+from ._tree import AbstractTree as AbstractTree
+from ._tree import MeshTree as MeshTree
+from ._tree import MeshTreeElement as MeshTreeElement
