56edeb0a11376e75bf8dac49b64f3b13b175d2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:24 +0000

    Add package lcwc/0.12.2/lcwc-0.12.2.tar.gz

---
+++ packages/lcwc/lcwc-0.12.2.tar.gz/lcwc-0.12.2/src/lcwc/web/incident.py
@@ -0,0 +1,10 @@
+import datetime
+from lcwc.category import IncidentCategory
+from lcwc.incident import Incident
+from lcwc.unit import Unit
+
+
+class WebIncident(Incident):
+    """Represents an incident from the web incident feed"""
+
+    pass
