01a240c05f2fdff6a4434c4abfb4f64be2cd1d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:03:57 +0000

    Add package metabeaver/0.3.0/metabeaver-0.3.0.tar.gz

---
+++ packages/metabeaver/metabeaver-0.3.0.tar.gz/metabeaver-0.3.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metabeaver',
+    version='0.3.0', # Major, minor, patch
+    packages=find_packages(exclude=['Testing', '*.xlsx', '*.xls']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'google-cloud-core',
+        'google-cloud-bigquery',
+    ],
+    description='Beaverish about data.',
+    author='Luke Anthony Pollen',
+    author_email='luke@pollenanalytics.com',
+    url='https://github.com/rainbowpusheenthe3rd/dataBeaver',
+)
+
+
+##### ERRATA #####
+
+"""
+   ______
+  //    \\__
+(||      `--`)
+  \\_/||\_/`
+
+"""
+
+### Create the wheel and the tar in dist folder
+#python setup.py sdist bdist_wheel
+
+### Upload to PyPi, using twine
+#twine upload dist/*
+#twine upload *
+
+### Where does Twine live?
+#pip show twine
+#dir /s twine.exe
+
+##### ERRATA #####
