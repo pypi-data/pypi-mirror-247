acd04583bed97e90b433fe8de7a1e8bef5745df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:23 +0000

    Add package Hello33423312/0.1/Hello33423312-0.1-py3-none-any.whl

---
+++ packages/Hello33423312/Hello33423312-0.1-py3-none-any.whl/Hello33423312/hello.py
@@ -0,0 +1,8 @@
+from tkinter import messagebox
+def hello_world():
+    """
+    Fungsi untuk mencetak pesan "Hello, World!" ke konsol.
+    """
+    print("Kelompok 4:\n Syahrul\n Ariel\n Nihlah\n Iqbal\n Tsabitah")
+
+    messagebox.showinfo("Kelompok 4:","\n Syahrul\n Ariel\n Nihlah\n Iqbal\n Tsabitah")
