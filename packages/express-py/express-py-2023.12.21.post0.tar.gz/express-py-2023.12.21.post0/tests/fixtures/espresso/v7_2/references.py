67d9c6d1883a1fc5f1802cdd9640d2147e607c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:04:38 +0000

    Add package express-py/2023.12.21.post0/express-py-2023.12.21.post0.tar.gz

---
+++ packages/express-py/express-py-2023.12.21.post0.tar.gz/express-py-2023.12.21.post0/tests/fixtures/espresso/v7_2/references.py
@@ -0,0 +1,16 @@
+HUBBARD_U_PARAMS = {
+    "values": [
+        {
+            "id": 1,
+            "atomicSpecies": "Co1",
+            "orbitalName": "3d",
+            "value": 6.7553
+        },
+        {
+            "id": 2,
+            "atomicSpecies": "Co2",
+            "orbitalName": "3d",
+            "value": 6.7553
+        },
+    ],
+}
