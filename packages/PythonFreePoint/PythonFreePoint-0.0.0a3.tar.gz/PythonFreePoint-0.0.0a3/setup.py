52aaf61a5cef67666bdf56f2f659f1954a2db53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:36 +0000

    Add package PythonFreePoint/0.0.0a3/PythonFreePoint-0.0.0a3.tar.gz

---
+++ packages/PythonFreePoint/PythonFreePoint-0.0.0a3.tar.gz/PythonFreePoint-0.0.0a3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='PythonFreePoint',
+    version='0.0.0a3',
+    author='itrufat',
+    description='Free Point Style for Python',
+    long_description='A library that seamlessly integrates `Free Point Style` into Python3.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/itruffat/python_point_free',
+    packages=['PythonFreePoint'],
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
