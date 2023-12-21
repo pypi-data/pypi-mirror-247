743b0eae31b45f081120e27e159f492136777b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:12 +0000

    Add package hqm/0.0.13/hqm-0.0.13.tar.gz

---
+++ packages/hqm/hqm-0.0.13.tar.gz/hqm-0.0.13/hqm/__init__.py
@@ -0,0 +1,10 @@
+"""
+   .. include:: ../README.md
+"""
+
+from .classification import *
+from .regression import *
+from .encoding import *
+from .circuits import *
+from .layers import *
+from .utils import *
