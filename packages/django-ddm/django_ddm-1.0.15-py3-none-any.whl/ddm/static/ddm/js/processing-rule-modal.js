a9df95ac7ec99f87e881279560c3acb5572a4abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:28 +0000

    Add package django-ddm/1.0.15/django_ddm-1.0.15-py3-none-any.whl

---
+++ packages/django-ddm/django_ddm-1.0.15-py3-none-any.whl/ddm/static/ddm/js/processing-rule-modal.js
@@ -0,0 +1,35 @@
+hideOrShowReplacementAndComparisonValue = function( id ) {
+  let replacementInput = $("#id_processingrule_set-" + id + "-replacement_value");
+  let comparisonValue = $("#id_processingrule_set-" + id + "-comparison_value");
+
+  let val = $("[id$=" + id + "-comparison_operator]").val();
+
+  if (val.indexOf("regex-replace-match") >= 0 ) {
+    replacementInput.parent().show();
+    comparisonValue.parent().show();
+  } else if (val === "") {
+    replacementInput.parent().hide();
+    comparisonValue.parent().hide();
+  } else {
+    replacementInput.parent().hide();
+    comparisonValue.parent().show();
+  }
+}
+
+$( "body" ).on("change", "select[id$='-comparison_operator']", function() {
+  const current_id = $(this).attr("id").match(/\d/)[0];
+  hideOrShowReplacementAndComparisonValue(current_id);
+
+});
+
+$(document).ready(function() {
+  let IDs = new Set();
+  $("[id$=-comparison_operator]").each(function() {
+    if( /\d/.test($( this ).attr("id")) ) {
+      IDs.add($( this ).attr("id").match(/\d/)[0]);
+    }
+  });
+  for ( const id of IDs ) {
+    hideOrShowReplacementAndComparisonValue(id);
+  }
+});
