86d3b2fe7947d6a1e268f757778dc2b608f733b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:25 +0000

    Add package autograder-py/0.3.1/autograder-py-0.3.1.tar.gz

---
+++ packages/autograder-py/autograder-py-0.3.1.tar.gz/autograder-py-0.3.1/autograder/cli/submission/submit.py
@@ -0,0 +1,35 @@
+import sys
+
+import autograder.api.submission.submit
+import autograder.assignment
+
+def run(arguments):
+    result = autograder.api.submission.submit.send(arguments, arguments.files, exit_on_error = True)
+
+    if (result['rejected']):
+        print("Submission was rejected by the autograder.")
+        print("Reason: ", result['reject-reason'])
+        return 1
+
+    if (not result['grading-success']):
+        print("Grading failed.")
+        return 2
+
+    submission = autograder.assignment.GradedAssignment.from_dict(result['result'])
+    print(submission.report())
+    return 0
+
+def main():
+    return run(_get_parser().parse_args())
+
+def _get_parser():
+    parser = autograder.api.submission.submit._get_parser()
+
+    parser.add_argument('files', metavar = 'FILE',
+        action = 'store', type = str, nargs = '+',
+        help = 'The path to your submission file(s).')
+
+    return parser
+
+if (__name__ == '__main__'):
+    sys.exit(main())
