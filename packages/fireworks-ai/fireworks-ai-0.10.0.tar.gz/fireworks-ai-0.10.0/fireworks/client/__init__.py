f2983c3db31664c7cf48f6e98f9b9d0c2df81402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 06:04:19 +0000

    Add package fireworks-ai/0.10.0/fireworks-ai-0.10.0.tar.gz

---
+++ packages/fireworks-ai/fireworks-ai-0.10.0.tar.gz/fireworks-ai-0.10.0/fireworks/client/__init__.py
@@ -0,0 +1,19 @@
+import os
+from .completion import Completion
+from .chat_completion import ChatCompletion
+from .chat import Chat
+from .model import Model
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+api_key = os.environ.get("FIREWORKS_API_KEY")
+base_url = os.environ.get("FIREWORKS_API_BASE", "https://api.fireworks.ai/inference/v1")
+
+__all__ = [
+    "__version__",
+    "Chat",
+    "ChatCompletion",
+    "Completion",
+    "Model",
+]
