5149d18b88fb0eea9a04ff4906cc36047b798db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:26 +0000

    Add package outlines/0.0.19/outlines-0.0.19-py3-none-any.whl

---
+++ packages/outlines/outlines-0.0.19-py3-none-any.whl/outlines/models/__init__.py
@@ -0,0 +1,12 @@
+"""Module that contains all the models integrated in outlines.
+
+We group the models in submodules by provider instead of theme (completion, chat
+completion, diffusers, etc.) and use routing functions everywhere else in the
+codebase.
+
+"""
+from .awq import awq
+from .gptq import gptq
+from .mamba import Mamba, mamba
+from .openai import OpenAI, openai
+from .transformers import Transformer, transformers
