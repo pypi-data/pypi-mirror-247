d5610e4a55b86582097168daf0a758c2a06c21f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:34 +0000

    Add package mijnbib/0.2.0/mijnbib-0.2.0.tar.gz

---
+++ packages/mijnbib/mijnbib-0.2.0.tar.gz/mijnbib-0.2.0/src/mijnbib/plugin_errors.py
@@ -0,0 +1,54 @@
+class PluginError(Exception):
+    """Base exception."""
+
+
+# *** client-side errors ***
+
+
+class AuthenticationError(PluginError):
+    """Raised when authentication has failed."""
+
+
+class ItemAccessError(PluginError):
+    """Raised when an item (loan, reservation) could not be accessed.
+
+    This is likely a client-side error, but in rare cases might have a
+    server-side cause.
+    """
+
+
+class InvalidExtendLoanURL(PluginError):
+    """Raised when the extending loan(s) url is not considered valid."""
+
+
+# *** server-side errors ***
+
+
+class CanNotConnectError(PluginError):
+    """Raised when a url can not be reached.
+
+    Args:
+        msg     Descriptive message of the error
+        url     Url that could not be reached
+    """
+
+    def __init__(self, msg: str, url: str):
+        super().__init__(msg)
+        self.url = url
+
+
+class IncompatibleSourceError(PluginError):
+    """Raised for any general errors in parsing the source.
+
+    Args:
+        msg         Descriptive message of the error
+        html_body   Html source that was used in parsing and caused error
+    """
+
+    def __init__(self, msg, html_body: str):
+        super().__init__(msg)
+        self.html_body = html_body
+
+
+class ExtendLoanError(PluginError):
+    """Raised when extending loan(s) failed for unclear reasons."""
---
+++ packages/mijnbib/mijnbib-0.2.0.tar.gz/mijnbib-0.2.0/tests/test_mijnbib.py
@@ -0,0 +1,23 @@
+def test_mijnbib_available_imports():
+    import mijnbib
+
+    # make sure we don't expose too few, or too much
+    imps = [i for i in dir(mijnbib) if not i.startswith("__")]
+    assert set(imps) == set(
+        [
+            "MijnBibliotheek",
+            "Loan",
+            "Reservation",
+            "Account",
+            "ItemAccessError",
+            "AuthenticationError",
+            "CanNotConnectError",
+            "ExtendLoanError",
+            "IncompatibleSourceError",
+            "PluginError",
+            "plugin_errors",
+            "mijnbibliotheek",
+            "parsers",
+            "models",
+        ]
+    )
