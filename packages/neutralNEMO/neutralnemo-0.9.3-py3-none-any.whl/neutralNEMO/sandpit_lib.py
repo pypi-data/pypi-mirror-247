2002e7aa77d07f8db487a1812e8b0941fce6ee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:03:56 +0000

    Add package neutralNEMO/0.9.3/neutralnemo-0.9.3-py3-none-any.whl

---
+++ packages/neutralNEMO/neutralnemo-0.9.3-py3-none-any.whl/neutralNEMO/sandpit_lib.py
@@ -0,0 +1,23 @@
+#Sandpit to test out neutralNEMO functions
+
+from neutralNEMO import grid, surf
+import os
+
+tests_dir = os.path.abspath("../../tests/")
+mm_path = tests_dir + "/mesh_mask.nc"
+orc_path = tests_dir + "/ORCA_R2_zps_domcfg.nc"
+t_path = tests_dir + "/GYRE_1m_04110101_04201230_grid_T.nc"
+
+hgd = grid.load_hgriddata( mm_path )
+
+neutral_grid = grid.build_nemo_hgrid( hgd, iperio=False, jperio = False)
+
+zgd = grid.load_zgriddata( mm_path)
+
+tsd = surf.load_tsdata( t_path, zgd, to_varname="votemper", so_varname="vosaline")
+
+surf_dataset = surf.find_omega_surfs( tsd, neutral_grid , zgd, [200., 3000.], [14,10], [6,9], [-1,-1], eos="gsw",
+                                      ITER_MAX=10, calc_veronis=True, calc_potsurf=True)
+
+
+
