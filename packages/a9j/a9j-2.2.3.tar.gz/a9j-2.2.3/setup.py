c7652280edcde045b6da344a1f4771fc0f884cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:03:58 +0000

    Add package a9j/2.2.3/a9j-2.2.3.tar.gz

---
+++ packages/a9j/a9j-2.2.3.tar.gz/a9j-2.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='a9j',
+    version='2.2.3',
+    description='noodle contest',
+    author='luyendd',
+    author_email='tudu8603@gmail.com',
+    packages=['a', 'b'],
+)
