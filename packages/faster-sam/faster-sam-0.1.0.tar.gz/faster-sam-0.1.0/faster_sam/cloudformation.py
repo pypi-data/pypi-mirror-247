a7b57b288e6bfe71540bcf1a67c736a4485f09f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 06:04:33 +0000

    Add package faster-sam/0.1.0/faster-sam-0.1.0.tar.gz

---
+++ packages/faster-sam/faster-sam-0.1.0.tar.gz/faster-sam-0.1.0/faster_sam/cloudformation.py
@@ -0,0 +1,122 @@
+from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import yaml
+
+PREFIX = "Fn::"
+WITHOUT_PREFIX = ("Ref", "Condition")
+
+
+class CFTemplateNotFound(FileNotFoundError):
+    pass
+
+
+class CFBadTag(TypeError):
+    pass
+
+
+class CFBadNode(ValueError):
+    pass
+
+
+class CFLoader(yaml.SafeLoader):
+    pass
+
+
+class NodeType(Enum):
+    API_GATEWAY = "AWS::Serverless::Api"
+    LAMBDA = "AWS::Serverless::Function"
+    API_EVENT = "Api"
+
+
+def multi_constructor(loader: CFLoader, tag_suffix: str, node: yaml.nodes.Node) -> Dict[str, Any]:
+    tag = tag_suffix
+
+    if tag not in WITHOUT_PREFIX:
+        tag = f"{PREFIX}{tag}"
+
+    if tag == "Fn::GetAtt":
+        return {tag: construct_getatt(node)}
+    elif isinstance(node, yaml.ScalarNode):
+        return {tag: loader.construct_scalar(node)}
+    elif isinstance(node, yaml.SequenceNode):
+        return {tag: loader.construct_sequence(node)}
+    elif isinstance(node, yaml.MappingNode):
+        return {tag: loader.construct_mapping(node)}
+
+    raise CFBadTag(f"!{tag} <{type(node)}>")
+
+
+def construct_getatt(node: yaml.nodes.Node) -> List[Any]:
+    if isinstance(node.value, str):
+        return node.value.split(".", 1)
+    elif isinstance(node.value, list):
+        return [s.value for s in node.value]
+
+    raise CFBadNode(f"Type <{type(node.value)}>")
+
+
+CFLoader.add_multi_constructor("!", multi_constructor)
+
+
+class CloudformationTemplate:
+    def __init__(self, template_path: Optional[str] = None) -> None:
+        self.template = self.load(template_path)
+        self.include_files()
+
+    @property
+    def functions(self) -> Dict[str, Any]:
+        if not hasattr(self, "_functions"):
+            self._functions = self.find_nodes(self.template["Resources"], NodeType.LAMBDA)
+        return self._functions
+
+    @property
+    def gateways(self) -> Dict[str, Any]:
+        if not hasattr(self, "_gateways"):
+            self._gateways = self.find_nodes(self.template["Resources"], NodeType.API_GATEWAY)
+        return self._gateways
+
+    def include_files(self):
+        for gateway in self.gateways.values():
+            if "DefinitionBody" not in gateway["Properties"]:
+                continue
+
+            lc = gateway["Properties"]["DefinitionBody"]["Fn::Transform"]["Parameters"]["Location"]
+
+            with open(lc) as fp:
+                swagger = yaml.safe_load(fp)
+
+            gateway["Properties"]["DefinitionBody"] = swagger
+
+    def load(self, template: Optional[str] = None) -> Dict[str, Any]:
+        path: Optional[Path] = None
+
+        if isinstance(template, str):
+            path = Path(template)
+        else:
+            paths = (Path("template.yml"), Path("template.yaml"))
+            path_generator = (p for p in paths if p.is_file())
+            path = next(path_generator, None)
+
+        if path is None or not path.is_file():
+            filename = template or "[template.yml, template.yaml]"
+            raise CFTemplateNotFound(filename)
+
+        with path.open() as fp:
+            return yaml.load(fp, CFLoader)
+
+    def find_nodes(self, tree: Dict[str, Any], node_type: NodeType) -> Dict[str, Any]:
+        nodes = {}
+
+        for key, node in tree.items():
+            if node["Type"] == node_type.value:
+                nodes[key] = node
+
+        return nodes
+
+    def lambda_handler(self, resource_id: str) -> str:
+        code_uri = self.functions[resource_id]["Properties"]["CodeUri"]
+        handler = self.functions[resource_id]["Properties"]["Handler"]
+        handler_path = f"{code_uri}.{handler}".replace("/", "")
+        return handler_path
---
+++ packages/faster-sam/faster-sam-0.1.0.tar.gz/faster-sam-0.1.0/faster_sam/routing.py
@@ -0,0 +1,75 @@
+import logging
+from datetime import datetime, timezone
+from typing import Any, Awaitable, Callable, Dict
+from uuid import uuid4
+
+from fastapi import Request, Response, routing
+
+logger = logging.getLogger(__name__)
+
+Handler = Callable[[Dict[str, Any], Any], Dict[str, Any]]
+Endpoint = Callable[[Request], Awaitable[Response]]
+
+
+class ApiGatewayResponse(Response):
+    def __init__(self, data: Dict[str, Any]):
+        super().__init__(
+            content=data["body"],
+            status_code=data["statusCode"],
+            headers=data.get("headers"),
+            media_type=data.get("headers", {}).get("Content-Type"),
+        )
+
+
+async def event_builder(request: Request) -> Dict[str, Any]:
+    now = datetime.now(timezone.utc)
+    body = await request.body()
+    event = {
+        "body": body.decode(),
+        "path": request.url.path,
+        "httpMethod": request.method,
+        "isBase64Encoded": False,
+        "queryStringParameters": dict(request.query_params),
+        "pathParameters": dict(request.path_params),
+        "headers": dict(request.headers),
+        "requestContext": {
+            "stage": request.app.version,
+            "requestId": str(uuid4()),
+            "requestTime": now.strftime(r"%d/%b/%Y:%H:%M:%S %z"),
+            "requestTimeEpoch": int(now.timestamp()),
+            "identity": {
+                "sourceIp": getattr(request.client, "host", None),
+                "userAgent": request.headers.get("user-agent"),
+            },
+            "path": request.url.path,
+            "httpMethod": request.method,
+            "protocol": f"HTTP/{request.scope['http_version']}",
+        },
+    }
+
+    return event
+
+
+def handler(func: Handler) -> Endpoint:
+    async def wrapper(request: Request) -> Response:
+        event = await event_builder(request)
+        result = func(event, None)
+        response = ApiGatewayResponse(result)
+
+        return response
+
+    return wrapper
+
+
+def import_handler(path: str) -> Handler:
+    module_name, handler_name = path.rsplit(".", maxsplit=1)
+    module = __import__(module_name, fromlist=(handler_name,))
+
+    return getattr(module, handler_name)
+
+
+class APIRoute(routing.APIRoute):
+    def __init__(self, path: str, endpoint: str, *args, **kwargs):
+        handler_path = endpoint
+        handler_func = import_handler(handler_path)
+        super().__init__(path=path, endpoint=handler(handler_func), *args, **kwargs)
