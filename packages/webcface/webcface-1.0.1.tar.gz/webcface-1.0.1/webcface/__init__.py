c15f767d1266928c2025258899bb12c8905baa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:27 +0000

    Add package webcface/1.0.1/webcface-1.0.1.tar.gz

---
+++ packages/webcface/webcface-1.0.1.tar.gz/webcface-1.0.1/webcface/__init__.py
@@ -0,0 +1,24 @@
+from .member import Member
+from .value import Value
+from .text import Text
+from .func import Func
+from .view import View
+from .log import Log
+from .client import Client
+from .func_info import ValType, Arg
+from .view_components import ViewComponentType, ViewColor
+
+__all__ = [
+    "Member",
+    "Value",
+    "Text",
+    "Func",
+    "View",
+    "Log",
+    "Client",
+    "ValType",
+    "Arg",
+    "ViewComponentType",
+    "ViewColor",
+    "view_components",
+]
