aa0082bd3ff73c1cd73fe2cca49b7dfc383fd7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:05:14 +0000

    Add package ebtorch/0.18.3/ebtorch-0.18.3.tar.gz

---
+++ packages/ebtorch/ebtorch-0.18.3.tar.gz/ebtorch-0.18.3/ebtorch/nn/utils/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#  Copyright (c) 2020-2023 Emanuele Ballarin <emanuele@ballarin.cc>
+#  Released under the terms of the MIT License
+#  (see: https://url.ballarin.cc/mitlicense)
+#
+# ------------------------------------------------------------------------------
+#
+# SPDX-License-Identifier: MIT
+#
+# ------------------------------------------------------------------------------
+# Imports
+from .adverutils import AdverApply
+from .adverutils import TA2ATAdapter
+from .autoclip import AutoClipper
+from .evalutils import eval_model_on_test
+from .onlyutils import argser_f
+from .onlyutils import argsink
+from .onlyutils import download_gdrive
+from .onlyutils import emplace_kv
+from .onlyutils import no_op
+from .onlyutils import subset_state_dict
+from .patches import patchify_2d
+from .patches import patchify_batch
+from .patches import patchify_dataset
+from .reprutils import gather_model_repr
+from .reprutils import model_reqgrad
+from .reprutils import model_reqgrad_
+from .reprutils import store_repr_autohook
+from .reprutils import store_repr_fx
+from .reprutils import store_repr_hook
+
+# Deletions (from .)
+del adverutils
+del autoclip
+del evalutils
+del onlyutils
+del patches
+del reprutils
