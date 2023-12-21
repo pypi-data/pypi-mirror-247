41225735869dbf0b961b3b376d600a2eec9a96f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:10 +0000

    Add package moki/0.0.1/moki-0.0.1.tar.gz

---
+++ packages/moki/moki-0.0.1.tar.gz/moki-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="moki",
+    version="0.0.1",
+    author="bk",
+    author_email="wowlupin@outlook.com",
+    description="remove boring print warning of mobi",
+    long_description="",
+    long_description_content_type="",
+    packages=setuptools.find_packages(),
+    license="GPL-3.0",
+    install_requires = ["loguru>=0.6"]
+)
