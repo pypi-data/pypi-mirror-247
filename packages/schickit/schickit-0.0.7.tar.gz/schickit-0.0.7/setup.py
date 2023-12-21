f649ee9b2fc1291db5f085a4934547b7b30d5b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:04:53 +0000

    Add package schickit/0.0.7/schickit-0.0.7.tar.gz

---
+++ packages/schickit/schickit-0.0.7.tar.gz/schickit-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="schickit",
+    version="0.0.7",
+    description="a toolkit for processing single cell Hi-C data",
+    author="ABC",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'schickit_run = schickit.run:main',
+            'schickit_index = schickit.index:main',
+        ]}
+)
