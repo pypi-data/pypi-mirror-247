c57252e1dbae8bbc16ea090fd7991b220e6f64f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:04:33 +0000

    Add package tdcord/0.1.3/tdcord-0.1.3-py3-none-any.whl

---
+++ packages/tdcord/tdcord-0.1.3-py3-none-any.whl/tdcord/__init__.py
@@ -0,0 +1,14 @@
+__title__ = "tdcord"
+__author__ = "tdius9"
+__license__ = "MIT"
+__version__ = "0.1.3"
+
+from . import blacklist, components, emb, logs, sql, times, utils
+from .bot import *
+from .components import *
+from .emb import *
+from .enums import *
+from .logs import *
+from .sql import *
+from .times import *
+from .utils import *
