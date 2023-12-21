da21781b8b2b2aa236abf2451e25a01fa7cb7e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:20 +0000

    Add package decart/0.0.2/decart-0.0.2.tar.gz

---
+++ packages/decart/decart-0.0.2.tar.gz/decart-0.0.2/decart/resources/completions.py
@@ -0,0 +1,11 @@
+from . import base_resource
+from ..clients import base_client
+
+
+class Completions(base_resource.BaseResource):
+    def __init__(self, client: base_client.BaseClient) -> None:
+        super().__init__(client)
+
+class AsyncCompletions(base_resource.AsyncBaseResource):
+    def __init__(self, client: base_client.AsyncBaseClient) -> None:
+        super().__init__(client)
