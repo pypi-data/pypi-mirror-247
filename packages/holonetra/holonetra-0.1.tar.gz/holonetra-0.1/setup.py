7f75e3fd07ab94e6bc413739fc163ea3c768ac65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:12 +0000

    Add package holonetra/0.1/holonetra-0.1.tar.gz

---
+++ packages/holonetra/holonetra-0.1.tar.gz/holonetra-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holonetra',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'geopandas',
+    ],
+    author='Akhil Chhibber',
+    author_email='akhil.chibber@gmail.com',
+    description='A Python library for spatial operations',
+    keywords='GIS geopandas buffer shapefile',
+    url='https://github.com/akhilchibber',
+)
