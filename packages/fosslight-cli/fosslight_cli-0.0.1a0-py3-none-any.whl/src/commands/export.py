de4530c09e2cf1f26dd467e12963c3754cf4ac2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:33 +0000

    Add package fosslight-cli/0.0.1a0/fosslight_cli-0.0.1a0-py3-none-any.whl

---
+++ packages/fosslight-cli/fosslight_cli-0.0.1a0-py3-none-any.whl/src/commands/export.py
@@ -0,0 +1,48 @@
+import datetime
+
+import click
+
+from src.commands.base import cli
+from src.services.project import ProjectService
+from src.services.self_check import SelfCheckService
+from src.utils.json import pretty_print_dict
+
+
+@cli.group()
+def export():
+    pass
+
+
+@export.group("project")
+def export_project():
+    pass
+
+
+@export_project.command("bom")
+@click.option("--prjId", "prjId", required=True, help="project id")
+@click.option("--mergeSaveFlag", "mergeSaveFlag", help="mergeSaveFlag")
+@click.option("--output", "-o", "output", help="output file path")
+def export_project_bom(prjId, mergeSaveFlag, output):
+    response = ProjectService().export_bom(prjId, mergeSaveFlag)
+    path = output if output else f"bom_{int(datetime.datetime.now().timestamp())}.xlsx"
+    if not path.endswith(".xlsx"):
+        path += ".xlsx"
+    with open(path, "wb") as f:
+        f.write(response.content)
+    print("Success: Export project bom")
+
+
+@export_project.command("bomJson")
+@click.option("--prjId", "prjId", required=True, help="project id")
+def export_project_bom_json(prjId):
+    data = ProjectService().export_bom_json(prjId)
+    pretty_print_dict(data)
+
+
+@export.command("selfCheck")
+@click.option("--selfCheckId", "selfCheckId", required=True, help="selfCheck id")
+def export_self_check(selfCheckId):
+    response = SelfCheckService().export(selfCheckId)
+    with open(f"bom_{int(datetime.datetime.now().timestamp())}.xlsx", "wb") as f:
+        f.write(response.content)
+    print("Success: Export self-check")
---
+++ packages/fosslight-cli/fosslight_cli-0.0.1a0-py3-none-any.whl/src/commands/get.py
@@ -0,0 +1,154 @@
+import datetime
+
+import click
+
+from src.client import get_api_client
+from src.commands.base import cli
+from src.services.project import ProjectService
+from src.utils.json import pretty_print_dict
+from src.utils.response import check_response
+
+
+@cli.group()
+def get():
+    pass
+
+
+@get.command("projects")
+@click.option("--createDate", "createDate")
+@click.option("--creator", "creator")
+@click.option("--division", "division")
+@click.option("--modelName", "modelName")
+@click.option("--prjIdList", "prjIdList")
+@click.option("--status", "status")
+@click.option("--updateDate", "updateDate")
+def get_projects(
+    createDate,
+    creator,
+    division,
+    modelName,
+    prjIdList,
+    status,
+    updateDate,
+):
+    data = ProjectService().get(
+        createDate=createDate,
+        creator=creator,
+        division=division,
+        modelName=modelName,
+        prjIdList=prjIdList,
+        status=status,
+        updateDate=updateDate,
+    )
+    pretty_print_dict(data)
+
+
+@get.command("projectModels")
+@click.option("--prjIdList", "prjIdList")
+def get_project_models(prjIdList):
+    data = ProjectService().get_models(prjIdList)
+    pretty_print_dict(data)
+
+
+@get.command("licenses")
+@click.option("--licenseName", "licenseName", required=True, help="license name")
+def get_licenses(licenseName):
+    client = get_api_client()
+    response = client.get_licenses(licenseName=licenseName)
+    if response.status_code == 404:
+        print("Not found")
+        return
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("oss")
+@click.option("--ossName", "ossName", required=True, help="oss name")
+@click.option("--ossVersion", "ossVersion", help="oss version")
+@click.option("--downloadLocation", "downloadLocation", help="download location")
+def get_oss(ossName, ossVersion, downloadLocation):
+    client = get_api_client()
+    response = client.get_oss(
+        ossName=ossName,
+        ossVersion=ossVersion,
+        downloadLocation=downloadLocation,
+    )
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("partners")
+@click.option("--createDate", "createDate")
+@click.option("--creator", "creator")
+@click.option("--division", "division")
+@click.option("--partnerIdList", "partnerIdList")
+@click.option("--status", "status")
+@click.option("--updateDate", "updateDate")
+def get_partners(
+    createDate,
+    creator,
+    division,
+    partnerIdList,
+    status,
+    updateDate,
+):
+    client = get_api_client()
+    response = client.get_partners(
+        createDate=createDate,
+        creator=creator,
+        division=division,
+        partnerIdList=partnerIdList,
+        status=status,
+        updateDate=updateDate,
+    )
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("maxVulnerability")
+@click.option("--ossName", "ossName", required=True, help="oss name")
+@click.option("--ossVersion", "ossVersion", help="oss version")
+def get_max_vulnerability(ossName, ossVersion):
+    client = get_api_client()
+    response = client.get_max_vulnerability(ossName=ossName, ossVersion=ossVersion)
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("vulnerability")
+@click.option("--cveId", "cveId", help="cve id")
+@click.option("--ossName", "ossName", help="oss name")
+@click.option("--ossVersion", "ossVersion", help="oss version")
+def get_vulnerability(cveId, ossName, ossVersion):
+    client = get_api_client()
+    response = client.get_vulnerability(
+        cveId=cveId,
+        ossName=ossName,
+        ossVersion=ossVersion,
+    )
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("codes")
+@click.option("--codeType", "codeType", required=True, help="code type")
+@click.option("--detailValue", "detailValue", help="detail value")
+def get_codes(codeType, detailValue):
+    client = get_api_client()
+    response = client.get_codes(codeType=codeType, detailValue=detailValue)
+    check_response(response)
+    pretty_print_dict(response.json())
+
+
+@get.command("notice")
+@click.option("--prjId", "prjId", required=True, help="project id")
+@click.option("--output", "-o", "output", help="output file path")
+def get_notice(prjId, output):
+    response = ProjectService().get_notice(prjId)
+    path = output if output else f"notice_{int(datetime.datetime.now().timestamp())}.html"
+
+    if not path.endswith(".html"):
+        path += ".html"
+    with open(path, "w") as f:
+        f.write(response.text)
+    print(f"Success: {path} created")
