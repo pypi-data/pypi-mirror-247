b096b0c44bb6d001e8b4ef83be4daf257caa0675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:04:23 +0000

    Add package kuantsol-transformation/0.0.3/kuantsol_transformation-0.0.3.tar.gz

---
+++ packages/kuantsol-transformation/kuantsol_transformation-0.0.3.tar.gz/kuantsol_transformation-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Kuantsol Transformation Package'
+LONG_DESCRIPTION = 'Kuantsol Transformation Package'
+
+# Setting up
+setup(
+    name="kuantsol_transformation",
+    version=VERSION,
+    author="Barış Coşkun",
+    author_email="<baris.coskun@kuantsol.ai>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+
+    keywords=['python', 'kuantsol_transformation'],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
