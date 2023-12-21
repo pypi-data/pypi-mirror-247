6dc976a7557a8af086dcc3861b50186a351b024b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:03:45 +0000

    Add package rio-tiler/6.2.9/rio_tiler-6.2.9-py3-none-any.whl

---
+++ packages/rio-tiler/rio_tiler-6.2.9-py3-none-any.whl/rio_tiler/__init__.py
@@ -0,0 +1,16 @@
+"""rio-tiler."""
+
+__version__ = "6.2.9"
+
+from . import (  # noqa
+    colormap,
+    constants,
+    errors,
+    expression,
+    io,
+    mosaic,
+    profiles,
+    reader,
+    tasks,
+    utils,
+)
