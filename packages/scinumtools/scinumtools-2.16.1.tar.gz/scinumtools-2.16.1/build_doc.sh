4afa563391ad71bec228dec4318ee6ce18b8606f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 16:04:21 +0000

    Add package scinumtools/2.16.1/scinumtools-2.16.1.tar.gz

---
+++ packages/scinumtools/scinumtools-2.16.1.tar.gz/scinumtools-2.16.1/build_doc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+if [[ "$1" == "-g" ]]; then
+    ./generate.sh build_docs
+    ./generate.sh build_exports
+    ./generate.sh build_figures
+    shift
+fi
+cd docs
+if [[ "$1" != "-u" ]]; then
+    make clean
+    rm -fr ./source/api
+fi
+sphinx-apidoc -o ./source/api ../src/*
+sphinx-build -b html source build/html -v
+cd build/html
+echo "Documentation HTML:"
+echo $(pwd)/index.html
