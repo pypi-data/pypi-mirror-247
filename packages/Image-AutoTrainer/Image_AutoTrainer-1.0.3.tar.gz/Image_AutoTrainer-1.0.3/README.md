26e6f30d893c8f6fce6265a95d4e6bbb09720d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:23 +0000

    Add package Image-AutoTrainer/1.0.3/Image_AutoTrainer-1.0.3.tar.gz

---
+++ packages/Image-AutoTrainer/Image_AutoTrainer-1.0.3.tar.gz/Image_AutoTrainer-1.0.3/README.md
@@ -0,0 +1,24 @@
+## Image_AutoTrainer: An Object Classification Library
+
+Authors:
+Yash Dhakade
+
+Email: yinsights8@gmail.com
+
+Github: https://github.com/yinsights8
+
+🚀 About Me
+I'm a Data Science learner. This library I've developed this for learning purposes only
+
+
+About Image_AutoTrainer 
+Image_AutoTrainer is an object classification library that is built on top of machine learning libraries such as TensorFlow and Keras. This library allows you to experiment faster with image classification problems on different art of state models in deep learning. you can classify any image with any model without writing n lines of code. 
+
+Some advantages Image_AutoTrainer :
+
+Simple -- it is simple in implementation, it saves a lot of time for a developer, 
+
+Flexible -- Keras adopts the principle of progressive disclosure of complexity: simple workflows should be quick and easy that’s why 
+Image_AutoTrainer is Flexible.
+
+Powerful -- Keras offers robust performance and scalability, enabling the utilization of Image_AutoTrainer in a production environment.
---
+++ packages/Image-AutoTrainer/Image_AutoTrainer-1.0.3.tar.gz/Image_AutoTrainer-1.0.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+name="Image_AutoTrainer",
+version='1.0.3',
+packages=find_packages(),
+include_package_data=True,
+license='MIT',
+  description = "It's an auto image classification library",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/yinsights8/Image_AutoTrainer",
+  author = 'Yash Dhakade',
+  author_email = 'yinsights8@gmail.com',
+  keywords = ['autotrainer'],
+  install_requires=[
+        'tensorflow==2.15.0',
+        'scipy==1.11.4',
+        'numpy==1.26.2',
+        'pandas',
+        'Pillow==10.1.0',
+        'Flask==3.0.0',
+        'Flask-Cors==3.0.10'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+
+  ],
+  entry_points={
+        "console_scripts": [
+            "autotrainer = Image_AutoTrainer.app:start_app",
+        ]},
+)
