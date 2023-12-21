28d14effc682255a142a0806a1ddbb32a6783992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:05:12 +0000

    Add package fishball/0.0.10/fishball-0.0.10.tar.gz

---
+++ packages/fishball/fishball-0.0.10.tar.gz/fishball-0.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '00.00.10'
+DESCRIPTION = 'fishball is a Data Analysis package'
+LONG_DESCRIPTION = 'This Python package will help you to analyse text'
+
+
+setup(
+        name="fishball", 
+        version=VERSION,
+        author="Dr Anna Sung and Prof Kelvin Leong",
+        author_email="<k.leong@chester.ac.uk>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pandas','transformers','tabulate'], # add any additional packages that 
+               
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Education",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
