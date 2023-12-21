34399d80ef95cc1ba778e9d3d847add382b32f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:04:12 +0000

    Add package qdrant-openapi/1.0.0/qdrant_openapi-1.0.0-py3-none-any.whl

---
+++ packages/qdrant-openapi/qdrant_openapi-1.0.0-py3-none-any.whl/qdrant_openapi/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from qdrant_openapi.api.cluster_api import ClusterApi
+from qdrant_openapi.api.collections_api import CollectionsApi
+from qdrant_openapi.api.points_api import PointsApi
+from qdrant_openapi.api.service_api import ServiceApi
+from qdrant_openapi.api.snapshots_api import SnapshotsApi
+
