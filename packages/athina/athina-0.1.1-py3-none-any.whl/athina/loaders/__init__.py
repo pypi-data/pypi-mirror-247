c7988475f92d1248086a4efd8967e8f50a05fee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:40 +0000

    Add package athina/0.1.1/athina-0.1.1-py3-none-any.whl

---
+++ packages/athina/athina-0.1.1-py3-none-any.whl/athina/loaders/__init__.py
@@ -0,0 +1,10 @@
+from .rag_loader import RagLoader
+from .response_loader import ResponseLoader
+from .loader import Loader, LoadFormat
+
+__all__ = [
+    "RagLoader",
+    "ResponseLoader",
+    "Loader",
+    "LoadFormat",
+]
---
+++ packages/athina/athina-0.1.1-py3-none-any.whl/athina/loaders/response_loader.py
@@ -0,0 +1,66 @@
+from typing import List, Optional
+from athina.interfaces.athina import AthinaFilters
+from athina.interfaces.data import DataPoint
+from athina.services.athina_api_service import AthinaApiService
+from .loader import Loader
+
+
+class ResponseLoader(Loader):
+    """
+    This class is a data loader for evals that only evaluate the response.
+
+    Attributes:
+        col_response (str): The column name corresponding to the response.
+        raw_dataset (dict): The raw dataset as loaded from the source.
+        processed_dataset (list): The processed dataset with responses.
+    """
+
+    def __init__(
+        self,
+        col_response="response",
+    ):
+        """
+        Initializes the loader with specified or default column names.
+        """
+        self.col_response = col_response
+        self._raw_dataset = {}
+        self._processed_dataset: List[DataPoint] = []
+
+    def process(self) -> None:
+        """
+        Transforms the raw data into a structured format. Processes each entry from the raw dataset, and extracts attributes.
+
+        Raises:
+            KeyError: If mandatory columns (response) are missing in the raw dataset.
+        """
+        for raw_instance in self._raw_dataset:
+            # Check for mandatory columns in raw_instance
+            if self.col_response not in raw_instance:
+                raise KeyError(f"'{self.col_response}' not found in provided data.")
+            # Create a processed instance with mandatory fields
+            processed_instance = {
+                "response": raw_instance[self.col_response],
+            }
+
+            # Store the results
+            self._processed_dataset.append(processed_instance)
+
+    def load_athina_inferences(
+        self,
+        filters: Optional[AthinaFilters] = None,
+        limit: Optional[int] = None,
+    ) -> List[DataPoint]:
+        """
+        Load data from Athina API.
+        """
+        athina_inferences = AthinaApiService.fetch_inferences(filters, limit=limit)
+        self._raw_dataset = list(
+            map(
+                lambda x: {
+                    "response": x.prompt_response,
+                },
+                athina_inferences,
+            )
+        )
+        self.process()
+        return self._processed_dataset
