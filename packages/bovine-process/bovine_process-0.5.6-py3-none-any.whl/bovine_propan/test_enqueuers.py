c9eada40408930815322758cd05367bda189db93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 15:05:15 +0000

    Add package bovine-process/0.5.6/bovine_process-0.5.6-py3-none-any.whl

---
+++ packages/bovine-process/bovine_process-0.5.6-py3-none-any.whl/bovine_propan/test_enqueuers.py
@@ -0,0 +1,18 @@
+import pytest
+from faststream.rabbit import TestRabbitBroker
+from unittest.mock import MagicMock
+
+from bovine_process.types import ProcessingItem
+
+from .enqueuers import enqueue_to_inbox, enqueue_broker
+
+
+@pytest.fixture
+async def test_broker():
+    async with TestRabbitBroker(enqueue_broker) as broker:
+        yield broker
+
+
+async def test_enqueue_inbox(test_broker):
+    item = ProcessingItem("submitter", {})
+    await enqueue_to_inbox(item, MagicMock(bovine_name="bovine_name"))
