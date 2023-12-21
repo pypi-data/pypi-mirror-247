75476d7f20498596dee6b523957acacec016e8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:06:09 +0000

    Add package GirderPatchLoad/1.1.0/GirderPatchLoad-1.1.0.tar.gz

---
+++ packages/GirderPatchLoad/GirderPatchLoad-1.1.0.tar.gz/GirderPatchLoad-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GirderPatchLoad',
+    version='1.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'joblib',
+        'xgboost',
+    ],
+)
