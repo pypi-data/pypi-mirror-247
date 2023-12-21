b6e4d9f99d6d1a09526e9fbf2ee02da27ab9a4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 05:04:30 +0000

    Add package salt-infisical/0.0.4/salt_infisical-0.0.4.tar.gz

---
+++ packages/salt-infisical/salt_infisical-0.0.4.tar.gz/salt_infisical-0.0.4/README.md
@@ -0,0 +1,21 @@
+# salt-infisical
+
+[![PyPI - Version](https://img.shields.io/pypi/v/salt-infisical.svg)](https://pypi.org/project/salt-infisical)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/salt-infisical.svg)](https://pypi.org/project/salt-infisical)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install salt-infisical
+```
+
+## License
+
+`salt-infisical` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/salt-infisical/salt_infisical-0.0.4.tar.gz/salt_infisical-0.0.4/src/salt_infisical/__init__.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2023-present Jonathan Treffler <mail@jonathan-treffler.de>
+#
+# SPDX-License-Identifier: MIT
+
+
+def get_token():
+    path = "/etc/salt/infisical.token"    
+    f = open(path, "r")
+    token = ""
+
+    try:
+        token = f.readline().strip('\n')
+    finally:
+        f.close()
+        return token
+
+def get_client():
+    from infisical import InfisicalClient
+    return InfisicalClient(token = get_token(), debug=True)
+
+def fetch_infisical_secrets(
+        environment: str = "dev",
+        path: str = "/",
+    ):
+    client = get_client()
+    salt = { 'infisical': {} }
+    salt["infisical"][environment] = client.get_all_secrets(environment=environment, path=path)
+    return salt
+
+def fetch_infisical_secret(
+        secret_name: str,
+        environment: str = "dev",
+        path: str = "/",
+    ):
+    client = get_client()
+    salt = { 'infisical': {} }
+    salt["infisical"][environment] = {}
+    salt["infisical"][environment][secret_name] = client.get_secret(secret_name=secret_name, environment=environment, path=path)
+    return salt
