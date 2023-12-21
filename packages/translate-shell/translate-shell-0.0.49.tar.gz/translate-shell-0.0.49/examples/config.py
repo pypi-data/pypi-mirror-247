e7bb757fce59c1410da50e39c028a2dab679bd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 03:05:17 +0000

    Add package translate-shell/0.0.49/translate-shell-0.0.49.tar.gz

---
+++ packages/translate-shell/translate-shell-0.0.49.tar.gz/translate-shell-0.0.49/examples/config.py
@@ -0,0 +1,13 @@
+"""Configure."""
+from translate_shell.config import Configuration
+
+
+def configure() -> Configuration:
+    """Configure.
+
+    :rtype: Configuration
+    """
+    config = Configuration()
+    config.target_lang = "zh_CN"
+    config.translators = "google,speaker"
+    return config
