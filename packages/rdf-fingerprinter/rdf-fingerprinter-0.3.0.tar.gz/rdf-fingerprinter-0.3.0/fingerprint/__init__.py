6e1b80e2e3ad056f0d353cc843ba0e1643490907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:43 +0000

    Add package rdf-fingerprinter/0.3.0/rdf-fingerprinter-0.3.0.tar.gz

---
+++ packages/rdf-fingerprinter/rdf-fingerprinter-0.3.0.tar.gz/rdf-fingerprinter-0.3.0/fingerprint/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# __init__.py
+# Date:  15/08/2019
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com
+
+import logging.config
+
+__docformat__ = "restructuredtext en"
+
+__version__ = "0.3.0"
+__date__ = "2023-10-20"
+
+logging.config.fileConfig('logging.conf')
