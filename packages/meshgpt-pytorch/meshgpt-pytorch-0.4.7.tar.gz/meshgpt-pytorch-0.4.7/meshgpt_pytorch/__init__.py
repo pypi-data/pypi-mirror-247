431b5dfed459b2bd2760713d6b1e0b8d4049995d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 04:04:29 +0000

    Add package meshgpt-pytorch/0.4.7/meshgpt-pytorch-0.4.7.tar.gz

---
+++ packages/meshgpt-pytorch/meshgpt-pytorch-0.4.7.tar.gz/meshgpt-pytorch-0.4.7/meshgpt_pytorch/__init__.py
@@ -0,0 +1,14 @@
+from meshgpt_pytorch.meshgpt_pytorch import (
+    MeshAutoencoder,
+    MeshDiscretizer,
+    MeshTransformer
+)
+
+from meshgpt_pytorch.trainer import (
+    MeshAutoencoderTrainer,
+    MeshTransformerTrainer
+)
+
+from meshgpt_pytorch.data import (
+    DatasetFromTransforms
+)
