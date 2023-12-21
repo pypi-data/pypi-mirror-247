a6a239bf8aac9cc048f667e9eb702515763233a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-21 14:03:51 +0000

    Add package figurl/0.2.20/figurl-0.2.20.tar.gz

---
+++ packages/figurl/figurl-0.2.20.tar.gz/figurl-0.2.20/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    scripts=[],
+    include_package_data = True,
+    package_data={
+        'figurl': ['preserve/templates/*']
+    },
+    install_requires=[
+        'click',
+        'altair',
+        'requests',
+        'kachery_cloud>=0.2.11'
+    ],
+    entry_points={
+        "console_scripts": [
+            "figurl=figurl.cli:main",
+        ],
+    }
+)
