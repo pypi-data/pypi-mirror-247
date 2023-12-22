0d314705571d0261de5cbc949ca49173a648b855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 14:04:16 +0000

    Add package dyna/1.0/dyna-1.0.tar.gz

---
+++ packages/dyna/dyna-1.0.tar.gz/dyna-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dyna',
+      version='1.0',
+      description='Dyna is a domain-specific langauge for dynamic programming.',
+      author='Tim Vieira',
+      packages = ['dyna'],
+)
