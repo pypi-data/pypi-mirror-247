7fc7a27c51a07e02db1e10decec6f559ff69d8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:05:59 +0000

    Add package django-ses-sns-tracker/4.0.0/django_ses_sns_tracker-4.0.0-py3-none-any.whl

---
+++ packages/django-ses-sns-tracker/django_ses_sns_tracker-4.0.0-py3-none-any.whl/ses_sns_tracker/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from django_ses.views import SESEventWebhookView
+
+
+"""
+URLConf left for compatibility. New setups should use the view directly in their own URLConf.
+"""
+
+app_name = "ses_sns_tracker"
+
+urlpatterns = [
+    path("bounce/", SESEventWebhookView.as_view(), name="handle-event-webhook"),
+]
