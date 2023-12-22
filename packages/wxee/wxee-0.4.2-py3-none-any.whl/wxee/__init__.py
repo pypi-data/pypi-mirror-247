a0c0fd61be63d614379924b8c166fc6f7d0ff0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 14:04:19 +0000

    Add package wxee/0.4.2/wxee-0.4.2-py3-none-any.whl

---
+++ packages/wxee/wxee-0.4.2-py3-none-any.whl/wxee/__init__.py
@@ -0,0 +1,9 @@
+from wxee import xarray
+from wxee.collection import ImageCollection
+from wxee.image import Image
+from wxee.time_series import TimeSeries
+from wxee.utils import Initialize
+
+__version__ = "0.4.2"
+
+__all__ = ["xarray", "Image", "ImageCollection", "TimeSeries", "Initialize"]
