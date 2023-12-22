a682bf0a710274845d15aab25c514c82ad6495c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:06:05 +0000

    Add package wortsalat/0.0.4/wortsalat-0.0.4.tar.gz

---
+++ packages/wortsalat/wortsalat-0.0.4.tar.gz/wortsalat-0.0.4/tests/test_analyze_wortsalat.py
@@ -0,0 +1,17 @@
+from wortsalat.analyze_wortsalat import analyze_wortsalat, print_wortsalat_small, print_wortsalat_big 
+
+def test_analyze_words():
+    text = "Hallo leute wir sind heute auf einem Bauernhof. Alle Tiere sind in Ordnung nur eins ist doof. Das Rapphuhn. Das rappt nun."
+    analyze_wortsalat(text)
+
+def test_analyze_wordsalat_division_by_zero():
+    text = "Es kann nicht durch null geteilt werden."
+    analyze_wortsalat(text)
+
+def test_print_wortsalat_small():
+    text = "Hallo leute wir sind heute auf einem Bauernhof. Alle Tiere sind in Ordnung nur eins ist doof. Das Rapphuhn. Das rappt nun."
+    print_wortsalat_small(text)
+
+def test_print_wortsalat_big():
+    text = "Hallo leute wir sind heute auf einem Bauernhof. Alle Tiere sind in Ordnung nur eins ist doof. Das Rapphuhn. Das rappt nun."
+    print_wortsalat_big(text)
