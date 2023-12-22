6395a7ec9897c72342f3cb8b09331cfd61b65152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:05:27 +0000

    Add package citros/0.1.3/citros-0.1.3.tar.gz

---
+++ packages/citros/citros-0.1.3.tar.gz/citros-0.1.3/data/sample_code/my_func.py
@@ -0,0 +1,9 @@
+def my_awesome_func(num):
+    """
+    A few things to notice:
+    1. Any imports must be done inside the function.
+    2. Return either native types or numpy scalars. Do not return non-scalar numpy values.
+    """
+    import numpy
+
+    return numpy.multiply(num, 2.0)
