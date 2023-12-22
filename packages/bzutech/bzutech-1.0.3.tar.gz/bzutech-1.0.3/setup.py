1d5279e1fe96fc998bb59fb4dc90e9f2933b2a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:44 +0000

    Add package bzutech/1.0.3/bzutech-1.0.3.tar.gz

---
+++ packages/bzutech/bzutech-1.0.3.tar.gz/bzutech-1.0.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="bzutech",
+    version="1.0.3",
+    author="enioo22",
+    description="library to bzutech services",
+    packages=["bzutech"],
+)
