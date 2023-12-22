0aad36646445367ef57c6ff859b272a50a315aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:04:40 +0000

    Add package spaceone-monitoring/2.0.dev11/spaceone-monitoring-2.0.dev11.tar.gz

---
+++ packages/spaceone-monitoring/spaceone-monitoring-2.0.dev11.tar.gz/spaceone-monitoring-2.0.dev11/spaceone/monitoring/connector/__init__.py
@@ -0,0 +1,6 @@
+from spaceone.monitoring.connector.datasource_plugin_connector import (
+    DataSourcePluginConnector,
+)
+from spaceone.monitoring.connector.webhook_plugin_connector import (
+    WebhookPluginConnector,
+)
