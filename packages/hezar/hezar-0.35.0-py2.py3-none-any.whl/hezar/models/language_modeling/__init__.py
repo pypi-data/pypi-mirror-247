ae3c0a33d0888434963065d6f3ce192f2f5577de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:06:13 +0000

    Add package hezar/0.35.0/hezar-0.35.0-py2.py3-none-any.whl

---
+++ packages/hezar/hezar-0.35.0-py2.py3-none-any.whl/hezar/models/language_modeling/__init__.py
@@ -0,0 +1,3 @@
+from .bert import BertLM, BertLMConfig
+from .distilbert import DistilBertLM, DistilBertLMConfig
+from .roberta import RobertaLM, RobertaLMConfig
---
+++ packages/hezar/hezar-0.35.0-py2.py3-none-any.whl/hezar/models/text_classification/__init__.py
@@ -0,0 +1,3 @@
+from .bert import BertTextClassification, BertTextClassificationConfig
+from .distilbert import DistilBertTextClassification, DistilBertTextClassificationConfig
+from .roberta import RobertaTextClassification, RobertaTextClassificationConfig
