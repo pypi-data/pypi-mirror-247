f828dfb4f00aa0e4cae8207929d92f5772660192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:05:02 +0000

    Add package MobileInventoryCLI/0.0.4/MobileInventoryCLI-0.0.4.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.0.4.tar.gz/MobileInventoryCLI-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+from datetime import datetime
+version='0.0.4'
+
+setup(name='MobileInventoryCLI',
+      version=version,
+      author="Carl Joseph Hirner III",
+      author_email="k.j.hirner.wisdom@gmail.com",
+      description="modify/update/use MobileInventoryPro *.bck files",
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+          ],
+      python_requires='>=3.6',
+      pacakage_data={
+        '':["*.config",],
+        }
+      )
