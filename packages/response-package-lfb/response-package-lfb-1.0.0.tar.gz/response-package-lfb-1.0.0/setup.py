bcbda9abd3dd01598b613c44df2c8e84bb5f909c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:04:58 +0000

    Add package response-package-lfb/1.0.0/response-package-lfb-1.0.0.tar.gz

---
+++ packages/response-package-lfb/response-package-lfb-1.0.0.tar.gz/response-package-lfb-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='response-package-lfb',  # Substitua pelo nome do pacote desejado
+    version='1.0.0',  # Substitua pela versão desejada
+    author='Lucas Frota Barroso',
+    author_email='lucasfrotabarroso14@gmail.com',
+    description='Pacote que contém modelos de response',
+    packages=find_packages(),
+    install_requires=[]  # Liste aqui quaisquer dependências
+)
