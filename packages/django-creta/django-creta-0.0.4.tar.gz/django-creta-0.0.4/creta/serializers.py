94138bb3cf36a5797d41eaedf02191f909cb9228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:06:02 +0000

    Add package django-creta/0.0.4/django-creta-0.0.4.tar.gz

---
+++ packages/django-creta/django-creta-0.0.4.tar.gz/django-creta-0.0.4/creta/serializers.py
@@ -0,0 +1,34 @@
+# Django Rest Framework
+from rest_framework import serializers
+
+# App
+from creta.models import ApiHistory, NFTHistory, NFT
+
+
+# Classes
+class NFTSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NFT
+        fields = ['id', 'name', 'address', 'url', 'nft_type', 'attributes', 'request_id', 'image_url', 'animation_url', 'external_url', 'extra_url', 'created_at', 'updated_at']
+
+
+class NftStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NFT
+        fields = ('request_id',)
+
+
+class NFTHistorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NFTHistory
+        fields = ['id', 'nft', 'type', 'title', 'created_at', 'updated_at']
+
+
+class ApiHistorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ApiHistory
+        fields = ['id', 'title', 'method', 'url', 'headers', 'request', 'response', 'error', 'created_at', 'updated_at']
