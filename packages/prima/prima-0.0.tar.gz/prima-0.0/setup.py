7a0f1faa01a1733ca6b8d6574ed536df7d2dcd71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:05:10 +0000

    Add package prima/0.0/prima-0.0.tar.gz

---
+++ packages/prima/prima-0.0.tar.gz/prima-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Tom M. Ragonneau and Zaikun Zhang',
+    author_email='pdfocode@gmail.com',
+    description="PDFO - Powell's Derivative-Free Optimization solvers",
+    install_requires=['pdfo'],
+    long_description='Use `pdfo <https://pypi.python.org/pypi/pdfo/>`_ instead.',
+    maintainer='Tom M. Ragonneau and Zaikun Zhang',
+    maintainer_email='pdfocode@gmail.com',
+    name='prima',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/pdfo/',
+    version='0.0',
+    zip_safe=False,
+)
