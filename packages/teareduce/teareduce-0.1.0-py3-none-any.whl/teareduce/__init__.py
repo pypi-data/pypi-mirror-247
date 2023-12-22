bbf835bb5b828e02b5a627b5091dc382c785dbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:06:42 +0000

    Add package teareduce/0.1.0/teareduce-0.1.0-py3-none-any.whl

---
+++ packages/teareduce/teareduce-0.1.0-py3-none-any.whl/teareduce/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2023-2024 Universidad Complutense de Madrid
+#
+# This file is part of teareduce
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+
+from .version import version
+
+__version__ = version
