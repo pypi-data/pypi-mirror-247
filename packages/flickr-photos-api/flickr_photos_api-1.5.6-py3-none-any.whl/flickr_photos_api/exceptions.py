db87a873d5e2c4d158ea7838ced201e1a801af79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:45 +0000

    Add package flickr-photos-api/1.5.6/flickr_photos_api-1.5.6-py3-none-any.whl

---
+++ packages/flickr-photos-api/flickr_photos_api-1.5.6-py3-none-any.whl/flickr_photos_api/exceptions.py
@@ -0,0 +1,31 @@
+class FlickrApiException(Exception):
+    """
+    Base class for all exceptions thrown from the Flickr API.
+    """
+
+    pass
+
+
+class InvalidApiKey(FlickrApiException):
+    """
+    Thrown when you try to use an API key with an invalid format.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(f"Flickr API rejected the API key as invalid ({message!r})")
+
+
+class ResourceNotFound(FlickrApiException):
+    """
+    Thrown when you try to look up a resource that doesn't exist.
+    """
+
+    def __init__(self, method: str, params: dict[str, str] | None):
+        super().__init__(
+            f"Unable to find resource at {method} with properties {params}"
+        )
+
+
+class LicenseNotFound(FlickrApiException):
+    def __init__(self, license_id: str):
+        super().__init__(f"Unable to find license with ID {license_id}")
