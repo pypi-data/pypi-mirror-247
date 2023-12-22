cf0416e59c1fea29b18b0acc6bb1946eda2160e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:15 +0000

    Add package standalone003gntmcallmln/0.0.14/standalone003gntmcallmln-0.0.14.tar.gz

---
+++ packages/standalone003gntmcallmln/standalone003gntmcallmln-0.0.14.tar.gz/standalone003gntmcallmln-0.0.14/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='standalone003gntmcallmln',
+    version='0.0.14',
+    packages=find_packages(),
+    package_data={
+        'sa003package': [
+        'kangroll_basic_fn001_mln',
+        'kangroll_basic_fn001_mln_release.py',
+        'pytransform/*',
+        'pytransform/_pytransform.dylib',
+        'pytransform/_pytransform.dylib'],
+    },
+    description='一个 Python 混淆工具包',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='xiaowen',
+    author_email='xiaowenseekjob@gmail.com',
+    url='https://github.com/yourusername/standalone001pyarmor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
