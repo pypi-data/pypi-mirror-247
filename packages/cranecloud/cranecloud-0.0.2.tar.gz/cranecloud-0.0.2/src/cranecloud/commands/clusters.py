b8688758e0432cdd3854a28167e6c938bec457fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 14:04:35 +0000

    Add package cranecloud/0.0.2/cranecloud-0.0.2.tar.gz

---
+++ packages/cranecloud/cranecloud-0.0.2.tar.gz/cranecloud-0.0.2/src/cranecloud/commands/clusters.py
@@ -0,0 +1,40 @@
+import click
+import requests
+from src.config import API_BASE_URL
+from tabulate import tabulate
+
+from src.cranecloud.helpers import get_token
+
+
+@click.group()
+def clusters_group():
+    pass
+
+
+@clusters_group.command('clusters', help='List all clusters.')
+def get_clusters_list():
+    """Get all clusters."""
+    click.echo("Getting clusters list...")
+    try:
+        token = get_token()
+        response = requests.get(
+            f"{API_BASE_URL}/clusters", headers={'Authorization': f"Bearer {token}"})
+        response.raise_for_status()
+        if response.status_code == 200:
+            clusters = response.json()['data']['clusters']
+            click.echo(clusters)
+            table_data = []
+            for cluster in clusters:
+                table_data.append(
+                    [cluster.get('id'), cluster.get('name'),  cluster.get('description')])
+            headers = ['ID', 'Name',  'Description']
+            click.echo(tabulate(table_data, headers, tablefmt='simple'))
+        else:
+            click.echo("Failed to get clusters list.")
+    except requests.RequestException as e:
+        if e.response or e.response.reason:
+            click.echo(f"Error: {e.response.reason}")
+        else:
+            click.echo(f"Failed to connect to the server: {e}")
+            click.echo(
+                "Please check your internet connection or try again later.")
