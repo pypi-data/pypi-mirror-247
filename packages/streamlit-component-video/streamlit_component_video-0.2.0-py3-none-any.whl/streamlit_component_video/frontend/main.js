2eda6a8010d043dd89a612458d8834ec3c6424ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 14:04:21 +0000

    Add package streamlit-component-video/0.2.0/streamlit_component_video-0.2.0-py3-none-any.whl

---
+++ packages/streamlit-component-video/streamlit_component_video-0.2.0-py3-none-any.whl/streamlit_component_video/frontend/main.js
@@ -0,0 +1,18 @@
+function sendValue(value) {
+  Streamlit.setComponentValue(value)
+}
+
+function onRender(event) {
+  if (!window.rendered) {
+    const {video, mimetype, track} = event.detail.args
+
+    if (video != "" && mimetype != "" && track != "") {
+      sendValue({video: video, mimetype: mimetype, track: track})
+      window.rendered = true
+    }
+  }
+}
+
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
+Streamlit.setComponentReady()
+Streamlit.setFrameHeight(399)
