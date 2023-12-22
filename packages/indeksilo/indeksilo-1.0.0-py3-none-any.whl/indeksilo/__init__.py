ab449b115ec9fd7b4ea999dd40535467d0db74b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:04:46 +0000

    Add package indeksilo/1.0.0/indeksilo-1.0.0-py3-none-any.whl

---
+++ packages/indeksilo/indeksilo-1.0.0-py3-none-any.whl/indeksilo/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 hugues de keyzer
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from indeksilo.indexer import build_index, read_build_write_index
+
+__all__ = ["build_index", "read_build_write_index"]
+
+__version__ = "1.0.0"
