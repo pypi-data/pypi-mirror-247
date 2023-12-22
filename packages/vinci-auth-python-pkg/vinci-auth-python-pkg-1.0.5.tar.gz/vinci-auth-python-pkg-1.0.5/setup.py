9e55fc7a6b88af02bd37c9cac6ba2830624d9210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:04:58 +0000

    Add package vinci-auth-python-pkg/1.0.5/vinci-auth-python-pkg-1.0.5.tar.gz

---
+++ packages/vinci-auth-python-pkg/vinci-auth-python-pkg-1.0.5.tar.gz/vinci-auth-python-pkg-1.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vinci-auth-python-pkg",
+    version="1.0.5",
+    author="Igor Lisbôa (TI-Sistemas)",
+    author_email="ti-sistemas@vincipartners.com",
+    description="A simple package to get vinci users auth token",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://tfs.util.vinci.net:8080/tfs/DefaultCollection/Vinci.Poc/Vinci.Poc%20Team/_git/vinci-auth-python-pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
