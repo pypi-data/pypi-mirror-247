4cbf1bd5d76a63dac0af033f10edd9de13c1cd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:54 +0000

    Add package fast-script-utils/0.0.1/fast_script_utils-0.0.1.tar.gz

---
+++ packages/fast-script-utils/fast_script_utils-0.0.1.tar.gz/fast_script_utils-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fast_script_utils",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],  # 依赖列表，格式是 ['package>=version']
+)
