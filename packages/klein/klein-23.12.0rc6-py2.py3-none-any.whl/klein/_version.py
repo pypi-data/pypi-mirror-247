1301aee2880e298b1cfdec121ce909f542638be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:05:18 +0000

    Add package klein/23.12.0rc6/klein-23.12.0rc6-py2.py3-none-any.whl

---
+++ packages/klein/klein-23.12.0rc6-py2.py3-none-any.whl/klein/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Klein version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Klein` to change this file.
+
+from incremental import Version
+
+__version__ = Version("Klein", 23, 12, 0, release_candidate=6)
+__all__ = ["__version__"]
