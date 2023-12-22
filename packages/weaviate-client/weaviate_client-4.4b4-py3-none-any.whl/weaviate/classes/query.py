ae39aa51443163f531d03fae245bf2aa70ede16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:03:59 +0000

    Add package weaviate-client/4.4b4/weaviate_client-4.4b4-py3-none-any.whl

---
+++ packages/weaviate-client/weaviate_client-4.4b4-py3-none-any.whl/weaviate/classes/query.py
@@ -0,0 +1,27 @@
+from weaviate.collections.classes.filters import Filter, FilterMetadata
+from weaviate.collections.classes.grpc import (
+    HybridFusion,
+    FromNested,
+    FromReference,
+    FromReferenceMultiTarget,
+    MetadataQuery,
+    Move,
+    QueryNested,
+    QueryReference,
+    QueryReferenceMultiTarget,
+)
+
+
+__all__ = [
+    "Filter",
+    "FilterMetadata",
+    "FromNested",
+    "FromReference",
+    "FromReferenceMultiTarget",
+    "HybridFusion",
+    "MetadataQuery",
+    "Move",
+    "QueryNested",
+    "QueryReference",
+    "QueryReferenceMultiTarget",
+]
