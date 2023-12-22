76bbe5d3b13a1018105543a0194e8b1b9723398c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:29 +0000

    Add package chaiverse/1.5.9/chaiverse-1.5.9.tar.gz

---
+++ packages/chaiverse/chaiverse-1.5.9.tar.gz/chaiverse-1.5.9/__init__.py
@@ -0,0 +1,13 @@
+from chaiverse.chat import SubmissionChatbot
+from chaiverse.feedback import get_feedback
+from chaiverse.login_cli import developer_login
+from chaiverse.metrics import (
+    get_leaderboard,
+    display_leaderboard
+)
+from chaiverse.submit import (
+    ModelSubmitter,
+    deactivate_model,
+    get_model_info,
+    get_my_submissions,
+)
