bac7d5719fb5087088e996220815aa7d83f3341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:05:37 +0000

    Add package tp8/0.1.0/tp8-0.1.0.tar.gz

---
+++ packages/tp8/tp8-0.1.0.tar.gz/tp8-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tp8',
+    version='0.1.0',
+    author='tp8',
+    author_email='tp8@example.com',
+    description='tp8 package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
