2d3d2c66ecc5de16eecd8f327a5a7d7a4c4596b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:45 +0000

    Add package isapilib/1.4.2/isapilib-1.4.2.tar.gz

---
+++ packages/isapilib/isapilib-1.4.2.tar.gz/isapilib-1.4.2/isapilib/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+class LoginViewUserCredentials(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh)
+        })
