7a706c16c9f8844546c567761e168be1563e238b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:26 +0000

    Add package apify/1.4.2a1/apify-1.4.2a1-py3-none-any.whl

---
+++ packages/apify/apify-1.4.2a1-py3-none-any.whl/apify/scrapy/__init__.py
@@ -0,0 +1,3 @@
+from .pipelines import ActorDatasetPushPipeline
+from .scheduler import ApifyScheduler
+from .utils import get_running_event_loop_id, open_queue_with_custom_client, to_apify_request, to_scrapy_request
