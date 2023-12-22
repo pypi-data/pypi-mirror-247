673528153f7ba86245b2ba1cf76eef931a26985e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:55 +0000

    Add package crispy-parser/2.0.2/crispy-parser-2.0.2.tar.gz

---
+++ packages/crispy-parser/crispy-parser-2.0.2.tar.gz/crispy-parser-2.0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+    f.close()
+
+setup(
+    name='crispy-parser',
+    version='2.0.2',
+    packages=['crispy'],
+    url='https://github.com/fybx/crispy',
+    license='GNU LGPL-v2.1',
+    author='F. Y. BALABAN, <fyb@fybx.dev>',
+    author_email='fyb@fybx.dev',
+    description='Crispy is a simple command line argument parser, ready to be integrated to any project of any size!',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.7"
+)
