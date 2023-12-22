7e4af1d619ca79d4b4b8282059e461b730499b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:06:33 +0000

    Add package django-daiquiri/0.4.5/django-daiquiri-0.4.5.tar.gz

---
+++ packages/django-daiquiri/django-daiquiri-0.4.5.tar.gz/django-daiquiri-0.4.5/daiquiri/stats/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from daiquiri.core.encoders import UUIDJSONEncoder
+
+
+class Record(models.Model):
+
+    time = models.DateTimeField()
+
+    resource_type = models.CharField(max_length=32)
+    resource = models.JSONField(encoder=UUIDJSONEncoder)
+
+    client_ip = models.GenericIPAddressField(blank=True, null=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        ordering = ('-time', )
+
+        verbose_name = _('Record')
+        verbose_name_plural = _('Records')
+
+    def __str__(self):
+        return '%s %s' % (self.time, self.resource_type)
