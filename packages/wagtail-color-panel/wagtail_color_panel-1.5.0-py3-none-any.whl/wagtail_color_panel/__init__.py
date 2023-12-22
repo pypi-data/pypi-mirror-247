874005d86c5b2d71dce6c893a50b24a3a0f2c302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:05:16 +0000

    Add package wagtail-color-panel/1.5.0/wagtail_color_panel-1.5.0-py3-none-any.whl

---
+++ packages/wagtail-color-panel/wagtail_color_panel-1.5.0-py3-none-any.whl/wagtail_color_panel/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "wagtail_color_panel"
+__version__ = "1.5.0"
+__build__ = 9
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-present Martin Sandström"
