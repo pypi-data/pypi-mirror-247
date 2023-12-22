53cd8eeb4f71595065591600fbc1217971482c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:03:57 +0000

    Add package kelompok2tubes3342331018/0.1/kelompok2tubes3342331018-0.1-py3-none-any.whl

---
+++ packages/kelompok2tubes3342331018/kelompok2tubes3342331018-0.1-py3-none-any.whl/kelompok2tubes3342331018/kelompok2.py
@@ -0,0 +1,9 @@
+from tkinter import messagebox
+
+def hello_world():
+    """
+    Fungsi untuk mencetak pesan "Hello, World!" ke konsol.
+    """
+    print("Kode Program ini merupakan kode program untuk absensi mahasiswa")
+
+    messagebox.showinfo("Kode Program ini merupakan kode program untuk absensi mahasiswa")
