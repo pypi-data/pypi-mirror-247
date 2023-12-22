dc8a73b8300f9e9e386d3c092e01dcf9a8bd6794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:05:57 +0000

    Add package pypostgresutil/1.0.0/pypostgresutil-1.0.0.tar.gz

---
+++ packages/pypostgresutil/pypostgresutil-1.0.0.tar.gz/pypostgresutil-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r', encoding='utf-8') as f:
+    return f.read()
+
+setup(
+  name='pypostgresutil',
+  version='1.0.0',
+  author='An0nX',
+  author_email='example@gmail.com',
+  description='Python library for simple PostgreSQL management',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/An0nX/PostgreSQLController',
+  packages=find_packages(),
+  install_requires=['psycopg2', 'loguru'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='Python Library PostgreSQL Management Pypi Tool Database Interaction Relational Databases Python Programming ',
+  python_requires='>=3.7'
+)
