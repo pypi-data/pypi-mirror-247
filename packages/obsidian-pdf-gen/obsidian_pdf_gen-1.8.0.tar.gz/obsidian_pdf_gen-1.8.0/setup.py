d4d7e9e6f16fb58092d0de7b40ebcb0aba8945d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:35 +0000

    Add package obsidian-pdf-gen/1.8.0/obsidian_pdf_gen-1.8.0.tar.gz

---
+++ packages/obsidian-pdf-gen/obsidian_pdf_gen-1.8.0.tar.gz/obsidian_pdf_gen-1.8.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name="obsidian_pdf_gen",
+    version="1.8.0",
+    author="Marc-Henri Bleu-Laine",
+    url="https://github.com/mhbl3/obsidian-pdf-gen",
+    description="A python package to convert Obsidian Markdown files into stylish PDFs",
+    packages=[
+        "obsidian_pdf_gen",
+        "obsidian_pdf_gen.generate_pdf",
+        "obsidian_pdf_gen.config",
+        "obsidian_pdf_gen.media_retriever",
+    ],
+    entry_points={
+        "console_scripts": [
+            "obsi_pdf = obsidian_pdf_gen.generate_pdf.md_notes_pdf:main"
+        ]
+    },
+    zip_safe=True,
+    include_package_data=True,
+    install_requires=["pytest", "pyyaml"],
+)
