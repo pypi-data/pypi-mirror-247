ffcfcbfacd100505b93b789e8a5d25a05784ecad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:52 +0000

    Add package tap-readthedocs/0.3.0/tap_readthedocs-0.3.0.tar.gz

---
+++ packages/tap-readthedocs/tap_readthedocs-0.3.0.tar.gz/tap_readthedocs-0.3.0/tap_readthedocs/client.py
@@ -0,0 +1,131 @@
+"""REST client handling, including ReadTheDocsStream base class."""
+
+from __future__ import annotations
+
+import typing as t
+from http import HTTPStatus
+
+import requests_cache
+from singer_sdk.authenticators import APIKeyAuthenticator
+from singer_sdk.exceptions import RetriableAPIError
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.pagination import BaseOffsetPaginator, first
+from singer_sdk.streams import RESTStream
+
+if t.TYPE_CHECKING:
+    import requests
+
+requests_cache.install_cache()
+TStream = t.TypeVar("TStream", bound=RESTStream[int])
+
+
+class ReadTheDocsPaginator(BaseOffsetPaginator):
+    """Paginator that stops when a page with 0 items is returned."""
+
+    def __init__(self, start_value: int, page_size: int, records_jsonpath: str) -> None:
+        """Create a new paginator.
+
+        Args:
+            start_value: Initial value.
+            page_size: Number of items per page.
+            records_jsonpath: A JSONPath expression.
+        """
+        super().__init__(start_value, page_size)
+        self._records_jsonpath = records_jsonpath
+
+    def has_more(self, response: requests.Response) -> bool:
+        """Check if response has any items.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            True if response contains at least one item.
+        """
+        try:
+            first(
+                extract_jsonpath(
+                    self._records_jsonpath,
+                    response.json(),
+                ),
+            )
+        except StopIteration:
+            return False
+
+        return True
+
+
+class ReadTheDocsStream(RESTStream[int]):
+    """ReadTheDocs stream class."""
+
+    url_base = "https://readthedocs.org"
+    records_jsonpath = "$.results[*]"
+    page_size = 50
+
+    @property
+    def authenticator(self) -> APIKeyAuthenticator:
+        """Return a new authenticator object.
+
+        Returns:
+            An API authenticator.
+        """
+        return APIKeyAuthenticator.create_for_stream(
+            self,
+            key="Authorization",
+            value="Token {token}".format(**self.config),
+            location="header",
+        )
+
+    @property
+    def http_headers(self) -> dict[str, str]:
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
+        return {"User-Agent": f"{self.tap_name}/{self._tap.plugin_version}"}
+
+    def validate_response(self, response: requests.Response) -> None:
+        """Validate HTTP response.
+
+        Args:
+            response: A `requests.Response`_ object.
+
+        Raises:
+            RetriableAPIError: If the rate limit is hit.
+        """
+        if response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
+            raise RetriableAPIError(response.reason)
+        super().validate_response(response)
+
+    def get_url_params(
+        self,
+        context: dict[str, t.Any] | None,  # noqa: ARG002
+        next_page_token: int | None,
+    ) -> dict[str, t.Any]:
+        """Get URL query parameters.
+
+        Args:
+            context: Stream sync context.
+            next_page_token: Next offset.
+
+        Returns:
+            Mapping of URL query parameters.
+        """
+        return {
+            "limit": self.page_size,
+            "offset": next_page_token,
+            "expand": "config",
+        }
+
+    def get_new_paginator(self) -> ReadTheDocsPaginator:
+        """Get a fresh paginator for this API endpoint.
+
+        Returns:
+            A paginator instance.
+        """
+        return ReadTheDocsPaginator(
+            start_value=0,
+            page_size=self.page_size,
+            records_jsonpath=self.records_jsonpath,
+        )
---
+++ packages/tap-readthedocs/tap_readthedocs-0.3.0.tar.gz/tap_readthedocs-0.3.0/tests/test_core.py
@@ -0,0 +1,59 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from requests import Response
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
+
+from tap_readthedocs.client import ReadTheDocsPaginator, ReadTheDocsStream
+from tap_readthedocs.tap import TapReadTheDocs
+
+SAMPLE_CONFIG: dict[str, Any] = {}
+
+
+TestTapReadTheDocs = get_tap_test_class(
+    TapReadTheDocs,
+    config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        max_records_limit=10,
+        ignore_no_records_for_streams=[
+            "projects",
+            "redirects",
+            "subprojects",
+            "translations",
+            "versions",
+        ],
+    ),
+)
+
+
+def test_paginator() -> None:
+    """Validate paginator that uses the page offset."""
+    response = Response()
+    paginator = ReadTheDocsPaginator(
+        start_value=0,
+        page_size=2,
+        records_jsonpath=ReadTheDocsStream.records_jsonpath,
+    )
+
+    assert not paginator.finished
+    assert paginator.current_value == 0
+
+    response._content = b'{"results": [{}, {}]}'
+    paginator.advance(response)
+    assert not paginator.finished
+    assert paginator.current_value == 2  # noqa: PLR2004
+    assert paginator.count == 1
+
+    response._content = b'{"results": [{}, {}]}'
+    paginator.advance(response)
+    assert not paginator.finished
+    assert paginator.current_value == 4  # noqa: PLR2004
+    assert paginator.count == 2  # noqa: PLR2004
+
+    response._content = b'{"results": []}'
+    paginator.advance(response)
+    assert paginator.finished
+    assert paginator.count == 3  # noqa: PLR2004
