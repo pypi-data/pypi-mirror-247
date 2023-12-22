7739442d1e973f86d2610474ea87715292cc9e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 14:04:31 +0000

    Add package wortsalat/0.0.3/wortsalat-0.0.3.tar.gz

---
+++ packages/wortsalat/wortsalat-0.0.3.tar.gz/wortsalat-0.0.3/src/wortsalat/identify_words.py
@@ -0,0 +1,30 @@
+def identify_words(key:str, text: str) -> list[str]:
+    """
+    Identify words in a given text that match a specific word list.
+
+    Parameters:
+    - key (str): The word list to match.
+    - text (str): The input text for which the words will be identified.
+
+    Returns:
+    - list: The words in the input text that match the specified word list.
+    """
+    word_lists = {
+        "ich":  ['ich', 'Ich'],
+        "wir":  ['wir', 'Wir'],
+        "emojis":   [':)', ':D', '-.-', ':(']
+    }
+    identified_words = [word for word in text.split() if word in word_lists[key]]
+    return identified_words
+
+def count_identified_words(identified_words: list[str]) -> int:
+    """
+    Count the number of words in a given list.
+
+    Parameters:
+    - words (list): The list of words.
+
+    Returns:
+    - int: The number of words in the list.
+    """
+    return len(identified_words)
