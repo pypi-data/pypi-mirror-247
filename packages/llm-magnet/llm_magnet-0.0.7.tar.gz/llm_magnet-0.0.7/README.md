e30ec5cf7bea399a6eb9f5589203b767d67da351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:05:05 +0000

    Add package llm-magnet/0.0.7/llm_magnet-0.0.7.tar.gz

---
+++ packages/llm-magnet/llm_magnet-0.0.7.tar.gz/llm_magnet-0.0.7/README.md
@@ -0,0 +1,81 @@
+<p align="center">
+   <img height="250" width="250" src="./magnet.png">
+   <br>
+   <h3 align="center">magnet</h3>
+   <p align="center">small, efficient embedding model toolkit</p>
+   <p align="center"><i>~ fine-tune SOTA LLMs on knowledge bases rapidly ~</i></p>
+</p>
+
+</small>
+
+## 🧬 Installation
+
+``` bash
+pip install llm-magnet
+```
+
+or
+
+``` bash
+python3 setup.py install
+```
+
+## 🎉 usage
+
+[check out the example notebooks](./examples/)
+
+<small>a snippet to get you started</small>
+
+``` python
+from magnet.filings import Processor
+# your text documents data
+source_data_file = "./raw/export.parquet"
+filings = Processor()
+filings.load(source_data_file)
+# output, text column, id column, and we disable sentence splitting for fastest processing
+await filings.process('./data/filings.parquet','clean','file', nlp=False)
+```
+
+## 🔮 features
+
+- ⚡️ **It's Fast**
+   - <small>fast on consumer hardware</small>
+   - <small>**extremely** fast on accelerated hardware</small>
+- 🫵 **Automatic or your way**
+   - <small>rely on established transformer patterns to let `magnet` do the work</small>
+   - <small>keep your existing data processing functions, bring them to `magnet`!</small>
+ - 🛰️ **100% Distributed**
+   - <small>processing, embedding, storage, retrieval, querying, or inference from anywhere</small>
+   - <small>as much or as little compute as you need</small>
+ - 🧮 **Choose Inference Method**
+   - <small>HuggingFace</small>
+   - <small>vLLM node</small>
+   - <small>localhost</small>
+ - 🌎 **Huge Volumes**
+   - <small>handle gigantic amounts of data inexpensively</small>
+   - <small>fault-tolerant by design</small>
+   - <small>decentralized workloads</small>
+ - 🔐 **Secure**
+   - <small>JWT</small>
+   - <small>Basic</small>
+ - 🧾 **Clean Logging**
+   - <small>easy to trace</small>
+   - <small>emojis are the future</small>
+
+
+## 🧲 ideas
+
+- so long as your initial data has columns for article text and some unique identifier per source document, `magnet` can do the rest
+- embed & index to vector db with [milvus](https://milvus.io)
+- distributed processing with [NATS](https://nats.io)
+- upload to S3
+- ideal cyberpunk vision of LLM power users in vectorspace
+
+## goals
+
+- [x] add [vllm](https://vllm.ai) implementation
+- [x] add [huggingface](https://huggingface.co/docs/api-inference/detailed_parameters?code=python) implementation
+- [x] add [milvus](https://milvus.io) implementation
+- [x] finish `README.md`
+- [x] add [NATS](https://nats.io) for distributed processing
+- [ ] add [mlx](https://github.com/ml-explore/mlx) support
---
+++ packages/llm-magnet/llm_magnet-0.0.7.tar.gz/llm_magnet-0.0.7/magnet/filings.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import os
+from .utils import _f, Utils
+from tqdm import tqdm
+from magnet.ic.utils.data_classes import *
+
+class Processor:
+    def __init__(self):
+        self.df = None
+        self.utils = Utils()
+        
+    def save(self, filename: str = None, raw: pd.DataFrame = None):
+        try:
+            file_extension = os.path.splitext(filename)[-1]
+            file_handlers = {
+                ".csv": raw.to_csv,
+                ".json": raw.to_json,
+                ".xlsx": raw.to_excel,
+                ".parquet": raw.to_parquet,
+            }
+            if file_extension in file_handlers:
+                file_handlers[file_extension](filename)
+                _f("success", f"saved - {filename}")
+            else:
+                _f("fatal", "unsupported data")
+        except Exception as e:
+            _f("fatal", e)
+    def load(self, raw: str | pd.DataFrame = None, text_column: str = "clean", id_column: str = 'id'):
+        self.id_column = id_column
+        self.text_column = text_column
+        try:
+            if isinstance(raw, str):
+                raw_data_dir = raw
+                file_extension = os.path.splitext(raw)[-1]
+                file_handlers = {
+                    ".csv": pd.read_csv,
+                    ".json": pd.read_json,
+                    ".xlsx": pd.read_excel,
+                    ".parquet": pd.read_parquet,
+                }
+                if file_extension in file_handlers:
+                    _f("wait", f"loading - {raw_data_dir}")
+                    self.df = file_handlers[file_extension](raw_data_dir)
+                    _f("success", f"loaded - {raw_data_dir}")
+                else:
+                    _f("fatal", "unsupported file type")
+            elif isinstance(raw, pd.DataFrame):
+                self.df = raw
+                _f("success", f"loaded - {raw}")
+            else:
+                _f("fatal", "data type not in [csv, json, xlsx, parquet, pd.DataFrame]")
+        except Exception as e:
+            _f("fatal", e)
+    async def process(self, path: str = None, splitter: any = None, nlp=True):
+        self.df = self.df.dropna()
+        if self.df is not None:
+            try:
+                _f("wait", f"get coffee or tea - {len(self.df)} processing...")
+                sentence_splitter = self.default_splitter if splitter is None else splitter
+                chunks = []
+                knowledge_base = pd.DataFrame()
+                tqdm.pandas()
+                self.df["chunks"] = self.df[self.text_column].progress_apply(
+                    lambda x: [
+                        str(s) for s in sentence_splitter(self.utils.normalize_text(x), nlp=nlp)
+                    ]
+                )
+                for i in tqdm(range(len(self.df))):
+                    for c in self.df['chunks'].iloc[i]:
+                        d = self.df[self.id_column].iloc[i]
+                        chunks.append((d, c))
+                knowledge_base['id'] = [c[0] for c in chunks]
+                knowledge_base['chunks'] = [c[1] for c in chunks]
+
+                self.df = knowledge_base
+                _f('wait', f'saving to {path}')
+                self.save(path, self.df)
+                return
+            except Exception as e:
+                _f("fatal", e)
+        else:
+            return _f("fatal", "no data loaded!")
+        
+    async def create_charge(self, field=None):
+        if self.df is not None and field is not None:
+            self.field = field
+            for i in tqdm(range(len(self.df))):
+                d = self.df[self.id_column].iloc[i]
+                c = self.df[self.text_column].iloc[i]
+                payload = Payload(
+                    document = d
+                    , text = c
+                )
+                if self.field:
+                    await self.field.pulse(payload)
+                else:
+                    _f('fatal', 'no field initialized')
+        else:
+            return _f("fatal", "no data loaded!") if field else _f('fatal', 'no field loaded!')
+    
+    def default_splitter(self, data, window_size=768, overlap=76, nlp=True):
+        if nlp:
+            self.utils.nlp.max_length = len(data) + 100
+            sentences = [str(x) for x in self.utils.nlp(data).sents]
+            return sentences
+        else:
+            # Perform chunked splitting by a fixed character length
+            chunks = []
+            start_char_idx = 0
+            while start_char_idx < len(data):
+                end_char_idx = start_char_idx + window_size
+                chunk = data[start_char_idx:end_char_idx]
+                chunks.append(chunk)
+                start_char_idx += (window_size - overlap)
+            return chunks
