ddcc940eacd400a6006bc3df76bf891dcfc8f20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:47 +0000

    Add package nonebot-plugin-phigros/0.0.4/nonebot-plugin-phigros-0.0.4.tar.gz

---
+++ packages/nonebot-plugin-phigros/nonebot-plugin-phigros-0.0.4.tar.gz/nonebot-plugin-phigros-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="nonebot-plugin-phigros",
+    version="0.0.4",
+    packages=["nonebot_plugin_phigros"],
+    install_requires=["nonebot2","Pillow","httpx","pathlib","uuid","nonebot_plugin_session"],
+    package_data={
+        '': ['simyou.ttf'],
+    },
+    description="一个简单的基于PhigrosLibrary的Phigros查分插件，适用于Nonebot2",
+    author="NightWind",
+    author_email="2125714976@qq.com",
+    url="https://github.com/XTxiaoting14332/nonebot-plugin-phigros",
+)
