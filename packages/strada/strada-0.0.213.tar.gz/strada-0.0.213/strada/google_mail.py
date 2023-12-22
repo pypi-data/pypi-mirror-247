018c16b2b29208a063e6d6555a910a99b6535d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:05:28 +0000

    Add package strada/0.0.213/strada-0.0.213.tar.gz

---
+++ packages/strada/strada-0.0.213.tar.gz/strada-0.0.213/strada/google_mail.py
@@ -0,0 +1,98 @@
+from .sdk import HttpRequestExecutor
+from .exception_handler import exception_handler
+from .common import (
+    build_input_schema_from_strada_param_definitions,
+)
+
+
+class GmailCustomHttpActionBuilder:
+    def __init__(self):
+        self._instance = None
+
+    def set_param_schema(self, param_schema):
+        self._get_instance().param_schema_definition = (
+            build_input_schema_from_strada_param_definitions(param_schema)
+        )
+        return self
+
+    def set_url(self, url):
+        self._get_instance().url = url
+        return self
+
+    def set_method(self, method):
+        self._get_instance().method = method
+        return self
+
+    def set_token(self, access_token):
+        self._get_instance().token = access_token
+        return self
+
+    def set_headers(self, headers):
+        self._instance.headers = headers
+        return self
+
+    def set_path_params(self, path_params):
+        self._instance.path = path_params
+        return self
+
+    def set_query_params(self, params):
+        self._instance.params = params
+        return self
+
+    def set_body(self, body):
+        self._instance.body = body
+        return self
+
+    def build(self):
+        return self._get_instance()
+
+    def _get_instance(self):
+        if self._instance is None:
+            self._instance = GmailCustomHttpAction()
+        return self._instance
+
+
+class GmailCustomHttpAction:
+    def __init__(self):
+        self.param_schema_definition = None
+        self.url = None
+        self.method = None
+        self.token = None
+        self.headers = "{}"
+        self.path = "{}"
+        self.params = "{}"
+        self.body = "{}"
+
+    @exception_handler
+    def execute(self, **kwargs):
+        return HttpRequestExecutor.execute(
+            dynamic_parameter_json_schema=self.param_schema_definition,
+            base_path_params=self.path,
+            base_headers=self.headers,
+            base_query_params=self.params,
+            base_body=self.body,
+            base_url=self.url,
+            method=self.method,
+            header_overrides={
+                "Authorization": f"Bearer {self.token}",
+                "Accept": "application/json",
+            },
+            instance_name="GmailCustomHttpAction",
+            app_name="gmail",
+            **kwargs,
+        )
+
+    @staticmethod
+    def prepare(data):
+        builder = GmailCustomHttpActionBuilder()
+        return (
+            builder.set_param_schema(data["param_schema_definition"])
+            .set_url(data["url"])
+            .set_method(data["method"])
+            .set_token(data["access_token"])
+            .set_path_params(data.get("path", "{}"))
+            .set_headers(data.get("headers", "{}"))
+            .set_query_params(data.get("query", "{}"))
+            .set_body(data.get("body", "{}"))
+            .build()
+        )
