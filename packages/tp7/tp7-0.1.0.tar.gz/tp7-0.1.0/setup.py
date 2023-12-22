4c1c824500eb5c9ccb69d86a86601ad3066c5e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:56 +0000

    Add package tp7/0.1.0/tp7-0.1.0.tar.gz

---
+++ packages/tp7/tp7-0.1.0.tar.gz/tp7-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tp7',
+    version='0.1.0',
+    author='tp7',
+    author_email='tp8@example.com',
+    description='tp8 package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
