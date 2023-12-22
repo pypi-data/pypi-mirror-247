35673fbf899f6c1b503c1c5f1869ae8f9b38ba40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:04 +0000

    Add package cha-hashing/0.4.8.7.9/cha_hashing-0.4.8.7.9.tar.gz

---
+++ packages/cha-hashing/cha_hashing-0.4.8.7.9.tar.gz/cha_hashing-0.4.8.7.9/CHA/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ["CowCowModes", "CowCow", 'FeistelN', "KRY", 'CHAObject', 'HashMaker', 'CHAF', 'Hashing_Algorithms', 'BlackFrog', 'BlackFrogKey', "OAEP", "Padding", "PEMFile", "PEM", "Piranha", "Krhash","PKCS7", "Modes", "CHAFHMAC", "CommonAlgs"]
+from .CHAF import FeistelN, CHAFHMAC
+from .Hashing_Algorithms import CHAObject, HashMaker, Krhash
+from .BlackFrog import BlackFrogKey, BlackFrog
+from .OAEP import OAEP
+from .PEMFile import PEM
+from .Piranha import Piranha
+from .Modes import Modes
+from .CommonAlgs import CommonAlgs
+from .Padding import PKCS7
+from .KRY import KRY
+from .CowCow import CowCow
+from .CowCowModes import CowCowModes
---
+++ packages/cha-hashing/cha_hashing-0.4.8.7.9.tar.gz/cha_hashing-0.4.8.7.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+DESC = 'A hashing and encryption with Customizable Hashing Algorithm CHA, WARNING: DO NOT USE IN REAL USE CASES! THIS WAS MADE JUST FOR FUN!'
+with open("README.md", 'r') as f:
+    LONG_DESC = f.read()
+setup(
+    name="cha_hashing",
+    version="0.4.8.7.9",
+    author="Roy Nisimov",
+    description=DESC,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    packages=find_packages(),
+    url='https://github.com/RoyNisimov1/CHA',
+    license='MIT',
+    install_requires=[],
+    keywords=['python', 'hashin', 'cha', 'encryption', 'decryption'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
