c1d4eb14d74d6e13a0cde059e7ac3ccfc8a331b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 06:04:45 +0000

    Add package TurboType/3.1/TurboType-3.1.tar.gz

---
+++ packages/TurboType/TurboType-3.1.tar.gz/TurboType-3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TurboType",
+    version="3.1",
+    author="1337",
+    description="A Game for Typing challenge",
+    packages=find_packages(),
+    package_data={'TurboType': ['data/*.txt']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pygame"],
+    entry_points={"console_scripts": ["TurboType = src.TurboType:main"]},
+)
