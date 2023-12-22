f8c5988d05ff3d22f224fadf2698db8407e226a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 06:05:01 +0000

    Add package pyvariant/2.2.4/pyvariant-2.2.4-py3-none-any.whl

---
+++ packages/pyvariant/pyvariant-2.2.4-py3-none-any.whl/pyvariant/__init__.py
@@ -0,0 +1,38 @@
+"""Python package for mapping biological sequence variants (mutations) to their equivalent
+chromosome, cDNA, gene, exon, protein, and RNA positions.
+"""
+from .ensembl_release import EnsemblRelease
+from .variants import (
+    CdnaDeletion,
+    CdnaDelins,
+    CdnaDuplication,
+    CdnaFusion,
+    CdnaInsertion,
+    CdnaPosition,
+    CdnaSubstitution,
+    DnaDeletion,
+    DnaDelins,
+    DnaDuplication,
+    DnaFusion,
+    DnaInsertion,
+    DnaPosition,
+    DnaSubstitution,
+    ExonFusion,
+    ExonPosition,
+    ExonSmallVariant,
+    ProteinDeletion,
+    ProteinDelins,
+    ProteinDuplication,
+    ProteinFrameshift,
+    ProteinFusion,
+    ProteinInsertion,
+    ProteinPosition,
+    ProteinSubstitution,
+    RnaDeletion,
+    RnaDelins,
+    RnaDuplication,
+    RnaFusion,
+    RnaInsertion,
+    RnaPosition,
+    RnaSubstitution,
+)
---
+++ packages/pyvariant/pyvariant-2.2.4-py3-none-any.whl/pyvariant/constants.py
@@ -0,0 +1,40 @@
+"""Collection of constants used throughout the package."""
+import os.path
+
+# Default Ensembl species and release
+DEFAULT_SPECIES = "homo_sapiens"
+DEFAULT_ENSEMBL_RELEASE = 100
+
+# Environmental variable used to set the cache directory
+CACHE_DIR_ENV = "PYVARIANT_CACHE"
+CACHE_DIR_NAME = "pyvariant"
+
+# Attribute name literals
+CONTIG_ID = "contig_id"
+EXON_ID = "exon_id"
+GENE_ID = "gene_id"
+GENE_NAME = "gene_name"
+PROTEIN_ID = "protein_id"
+TRANSCRIPT_ID = "transcript_id"
+TRANSCRIPT_NAME = "transcript_name"
+
+# Position type literals
+CDNA = "cdna"
+CDS = "CDS"
+DNA = "dna"
+EXON = "exon"
+PROTEIN = "protein"
+RNA = "rna"
+STOP_CODON = "stop_codon"
+
+# Variant type literals
+DELETION = "deletion"
+DELINS = "delins"
+DUPLICATION = "duplication"
+FRAMESHIFT = "frameshift"
+FUSION = "fusion"
+INSERTION = "insertion"
+SUBSTITUTION = "substitution"
+
+# Fallback FASTA file used if none are provided
+EMPTY_FASTA = os.path.join(os.path.dirname(__file__), "data", "empty.fa")
