b4c5a10cdc8c89df99daa50e5fdafcda5d2a4e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:14 +0000

    Add package autoviz/0.1.802/autoviz-0.1.802.tar.gz

---
+++ packages/autoviz/autoviz-0.1.802.tar.gz/autoviz-0.1.802/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autoviz",
+    version="0.1.802",
+    author="Ram Seshadri",
+    # author_email="author@example.com",
+    description="Automatically Visualize any dataset, any size with a single line of code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/AutoViz.git",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "xlrd",
+        "wordcloud",
+        "emoji",
+        "numpy",
+        "pandas",
+        "pyamg",
+        "matplotlib<=3.7.4",
+        "seaborn>=0.12.2",
+        "scikit-learn",
+        "statsmodels",
+        "nltk",
+        "textblob",
+        "xgboost>=0.82,<1.7",
+        "fsspec>=0.8.3",
+        "typing-extensions>=4.1.1",
+        "pandas-dq>=1.29"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
