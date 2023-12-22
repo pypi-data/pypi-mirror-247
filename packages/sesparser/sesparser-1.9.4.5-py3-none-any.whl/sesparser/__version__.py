5b0d7d762bafce52d7ab04b5e73a4f779fd39a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 06:04:46 +0000

    Add package sesparser/1.9.4.5/sesparser-1.9.4.5-py3-none-any.whl

---
+++ packages/sesparser/sesparser-1.9.4.5-py3-none-any.whl/sesparser/__version__.py
@@ -0,0 +1,12 @@
+# .::.  ::  :: ..::.. \  / ..::..
+# :...  ::  :: ::__::  ::  ::__::
+# :..:  :...:: ::  \\  ::  ::  ::
+
+__title__ = "sesparser"
+__description__ = "SES Incoming email Parser"
+__url__ = "https://sesparser.readthedocs.io"
+__version__ = "1.9.4.5"
+__author__ = "Vaddiraju Surya Teja"
+__author_email__ = "suryavaddiraju@proton.me"
+__license__ = "Apache-2.0"
+__copyright__ = "2023, Vaddiraju Surya Teja"
