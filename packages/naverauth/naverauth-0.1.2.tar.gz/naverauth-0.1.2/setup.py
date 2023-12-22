d6c00e62f333c684ea83d4b40a8be9128bf68bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:04:37 +0000

    Add package naverauth/0.1.2/naverauth-0.1.2.tar.gz

---
+++ packages/naverauth/naverauth-0.1.2.tar.gz/naverauth-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='naverauth',
+    version='0.1.2',
+    description='Naver Login Implementation',
+    author='stateofai',
+    author_email='tellme@duck.com',
+    url='https://github.com',
+    install_requires=['uuid', 'requests','rsa','lzstring','Retry','HTTPAdapter',],
+    packages=find_packages(exclude=[]),
+    keywords=['clien', 'naver'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
