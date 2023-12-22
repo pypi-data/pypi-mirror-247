4f761e4d6f41eadd015b1e189b64fd2612dcaac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:05 +0000

    Add package quantconnect-stubs/16107/quantconnect_stubs-16107-py3-none-any.whl

---
+++ packages/quantconnect-stubs/quantconnect_stubs-16107-py3-none-any.whl/QuantConnect/Queues/__init__.pyi
@@ -0,0 +1,34 @@
+from typing import overload
+import typing
+
+import QuantConnect.Interfaces
+import QuantConnect.Packets
+import QuantConnect.Queues
+import System
+
+
+class JobQueue(System.Object, QuantConnect.Interfaces.IJobQueueHandler):
+    """Implementation of local/desktop job request:"""
+
+    def AcknowledgeJob(self, job: QuantConnect.Packets.AlgorithmNodePacket) -> None:
+        """Desktop/Local acknowledge the task processed. Nothing to do."""
+        ...
+
+    @staticmethod
+    def GetFactoryFromDataQueueHandler(dataQueueHandler: str) -> QuantConnect.Interfaces.IBrokerageFactory:
+        """
+        Gets Brokerage Factory for provided IDQH
+        
+        :returns: An Instance of Brokerage Factory if possible, otherwise null.
+        """
+        ...
+
+    def Initialize(self, api: QuantConnect.Interfaces.IApi) -> None:
+        """Initialize the job queue:"""
+        ...
+
+    def NextJob(self, location: typing.Optional[str]) -> typing.Union[QuantConnect.Packets.AlgorithmNodePacket, str]:
+        """Desktop/Local Get Next Task - Get task from the Algorithm folder of VS Solution."""
+        ...
+
+
