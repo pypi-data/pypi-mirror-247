b1af2e72da7f9d1d5cb2c9bac3dc08ba1a567a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:11 +0000

    Add package quantbt/0.9.4/quantbt-0.9.4.tar.gz

---
+++ packages/quantbt/quantbt-0.9.4.tar.gz/quantbt-0.9.4/quantbt/core/calculate_commission.py
@@ -0,0 +1,14 @@
+from quantbt.core.enums import CommissionType
+from numba import njit
+
+
+@njit
+def calculate_commission(commission_type, commission, current_price, entry_price, volume) -> float:
+    if commission_type == CommissionType.FIXED.value:
+        return commission * 2
+    elif commission_type == CommissionType.PERCENTAGE.value:
+        entry_commission = entry_price * volume * commission
+        current_commission = current_price * volume * commission
+        return entry_commission + current_commission
+    else:
+        return commission
---
+++ packages/quantbt/quantbt-0.9.4.tar.gz/quantbt-0.9.4/quantbt/core/enums.py
@@ -0,0 +1,72 @@
+from enum import IntEnum
+
+
+class TradeMode(IntEnum):
+    HEDGE = 0
+    ONE_WAY = 1
+
+
+class OrderDirection(IntEnum):
+    SHORT = 0
+    LONG = 1
+
+
+class TradeSizeType(IntEnum):
+    FIXED = 0
+    PERCENTAGE = 1
+
+
+class CommissionType(IntEnum):
+    FIXED = 0
+    PERCENTAGE = 1
+
+
+class SlippageType(IntEnum):
+    FIXED = 0
+    PERCENTAGE = 1
+
+
+class DataType(IntEnum):
+    OHLC = 0
+    BID_ASK = 1
+
+
+class OrderType(IntEnum):
+    MARKET = 0
+    LIMIT = 1
+    STOP_LIMIT = 2
+
+
+class PositionCloseReason(IntEnum):
+    SIGNAL = 0
+    SL = 1
+    TP = 2
+    TIME_SL = 3
+    TSL = 4
+    BE_FAIL = 5
+
+
+# ["Index", "Direction", "EntryTime", "EntryPrice", "ExitTime", "ExitPrice", "Volume", "TP", "SL", "PNL", "Commission", "Active"]
+class Trade(IntEnum):
+    IDX = 0
+    Index = 1
+    Direction = 2
+    EntryTime = 3
+    EntryPrice = 4
+    ExitTime = 5
+    ExitPrice = 6
+    Volume = 7
+    SL = 8
+    TSL = 9
+    TP = 10
+    TIME_SL = 11
+    PNL = 12
+    Commission = 13
+    Active = 14
+    CloseReason = 15
+    Extra = 16
+
+
+class StrategyType:
+    FROM_SIGNALS = 0
+    FROM_TRADES = 0
