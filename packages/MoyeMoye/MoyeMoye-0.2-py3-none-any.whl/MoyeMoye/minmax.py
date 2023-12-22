d86819029b27a39dda49d72aa8adb7e5df354f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:05:29 +0000

    Add package MoyeMoye/0.2/MoyeMoye-0.2-py3-none-any.whl

---
+++ packages/MoyeMoye/MoyeMoye-0.2-py3-none-any.whl/MoyeMoye/minmax.py
@@ -0,0 +1,24 @@
+import math
+ 
+def minimax (curDepth, nodeIndex,maxTurn, scores, targetDepth):
+ 
+    
+    if (curDepth == targetDepth): 
+        return scores[nodeIndex]
+     
+    if (maxTurn):
+        return max(minimax(curDepth + 1, nodeIndex * 2, 
+                    False, scores, targetDepth), 
+                   minimax(curDepth + 1, nodeIndex * 2 + 1, 
+                    False, scores, targetDepth))
+     
+    else:
+        return min(minimax(curDepth + 1, nodeIndex * 2, 
+                     True, scores, targetDepth), 
+                   minimax(curDepth + 1, nodeIndex * 2 + 1, 
+                     True, scores, targetDepth))
+     
+
+
+ 
+ 
