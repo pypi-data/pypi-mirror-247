3afebc912e8f540283cc9117699c92560361f8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 16:06:05 +0000

    Add package soliz/0.1.0/soliz-0.1.0.tar.gz

---
+++ packages/soliz/soliz-0.1.0.tar.gz/soliz-0.1.0/src/soliz/__init__.py
@@ -0,0 +1,12 @@
+"""
+Soliz is a lexer creation tool.
+
+Get started by implementing the Rule type, and instantiating a lexer with the rule.
+Or, utilize Soliz's builtin rules in the `impls` module:
+
+- SymbolRule
+- StringRule
+- NumberRule
+- OperatorRule
+- IdentifierRule
+"""
