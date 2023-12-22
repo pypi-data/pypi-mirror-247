276436a0757f6651ffbca37cca1eab6eefab44de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:17 +0000

    Add package SeaPlayer/0.8.7/seaplayer-0.8.7.tar.gz

---
+++ packages/SeaPlayer/seaplayer-0.8.7.tar.gz/seaplayer-0.8.7/seaplayer/objects/__init__.py
@@ -0,0 +1,13 @@
+from .Log import LogMenu
+from .Labels import FillLabel
+from .Input import InputField
+from .Buttons import ClikableButton, WaitButton
+from .Notification import Nofy, CallNofy
+from .ProgressBar import IndeterminateProgress
+from .DataOptions import DataOption, DataOptionList
+from .Image import AsyncImageLabel, StandartImageLabel
+from .MusicList import MusicListView, MusicListViewItem
+from .Configurate import ConfigurateListItem, ConfigurateList
+from .Radio import DataRadioSet, DataRadioButton
+from .PopUp import PopUp, PopUpWindow
+from .Rheostat import Rheostat
