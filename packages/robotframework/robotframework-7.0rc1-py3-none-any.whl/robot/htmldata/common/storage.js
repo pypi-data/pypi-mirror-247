a99f83e912537c6e5a6b3880b84b3e50f852eb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:04:59 +0000

    Add package robotframework/7.0rc1/robotframework-7.0rc1-py3-none-any.whl

---
+++ packages/robotframework/robotframework-7.0rc1-py3-none-any.whl/robot/htmldata/common/storage.js
@@ -0,0 +1,36 @@
+storage = function () {
+
+    const prefix = 'robot-framework-';
+    var storage;
+
+    function init(user) {
+        if (user)
+            prefix += user + '-';
+        storage = getStorage();
+    }
+
+    function getStorage() {
+        // Use localStorage if it's accessible, normal object otherwise.
+        // Inspired by https://stackoverflow.com/questions/11214404
+        try {
+            localStorage.setItem(prefix, prefix);
+            localStorage.removeItem(prefix);
+            return localStorage;
+        } catch (exception) {
+            return {};
+        }
+    }
+
+    function get(name, defaultValue) {
+        var value = storage[prefix + name];
+        if (typeof value === 'undefined')
+            return defaultValue;
+        return value;
+    }
+
+    function set(name, value) {
+        storage[prefix + name] = value;
+    }
+
+    return {init: init, get: get, set: set, prefix: prefix};
+}();
