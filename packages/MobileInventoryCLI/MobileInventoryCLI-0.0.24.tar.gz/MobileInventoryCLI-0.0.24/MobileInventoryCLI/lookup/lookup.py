89bca1fe8c8e78a58783452b65198e584ae005b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:06:00 +0000

    Add package MobileInventoryCLI/0.0.24/MobileInventoryCLI-0.0.24.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.0.24.tar.gz/MobileInventoryCLI-0.0.24/MobileInventoryCLI/lookup/lookup.py
@@ -0,0 +1,200 @@
+from MobileInventoryCLI.error.error import writeError,obj2dict
+
+	
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base as dbase
+from sqlalchemy.ext.automap import automap_base
+from pathlib import Path
+import os,sys,json,base64
+from colored import attr,fg,bg
+
+class Search:
+	def __str__(self):
+		return "LookupCode"
+	def seeCustomFields(self,result,config,engine,error_log,tbl):
+		if not result:
+			raise Exception('result is "{}":{}'.format(result,type(result)))
+		else:
+			with Session(engine) as session:
+				print("___CustomFields___")
+				query=session.query(tbl.ItemCustomField)
+				result=query.filter(tbl.ItemCustomField.ItemId==result.ItemId).all()
+				for num,r in enumerate(result):
+					print(fg('green')+'++++ICF {}:{}++++'.format(num,r.CustomFieldId)+attr(0))
+					name,typeNo,typeStr='',0,'Text'
+					q=session.query(tbl.CustomField)
+					q=q.filter(tbl.CustomField.CustomFieldId==r.CustomFieldId)
+					qr=q.first()
+					name,typeNo=qr.Name,qr.Type
+					if typeNo == 0:
+						typeStr='Text'
+					elif typeNo == 1:
+						typeStr='Numbers'
+					elif typeNo == 2:
+						typeStr='Date'
+					print(fg('RED')+'Name:'+attr(0),name)
+					print(fg('DARK_ORANGE')+'\tType: '+attr(0),typeStr)
+					for column in r.__table__.columns:
+						if column.name in ['Value']:
+							print(fg('RED')+column.name+attr(0),getattr(r,column.name),sep=" -> : ")
+						else:
+							print(fg('DARK_ORANGE')+column.name+attr(0),getattr(r,column.name),sep=" -> : ")
+					
+					print(fg('green')+'++++ICF {}:{}++++'.format(num,r.CustomFieldId)+attr(0))
+				print("___CustomFields___")
+		#lookup item result custom fields
+	def __init__(self,config,engine,error_log,tbl):
+		self.tbl=tbl
+		self.engine=engine
+		self.error_log=error_log
+		self.config=config
+		with config.open('r') as d:
+			self.cfg=json.load(d)
+		self.cmds={
+			'1':'lookup by itemCode',
+			'2':'lookup by Barcode',
+			'3':'lookup by ItemId',
+			'4':'lookup by barcode|itemcode|itemid',
+			'5':'edit item by itemCode',
+			'6':'edit item by Barcode',
+			'7':'edit item byItemId',
+			'8':'quit',
+			's|S|9':'Search',
+			'quit':'quit',
+			'q':'quit',
+			'back':'go back a menu',
+		}
+		msg='\n'.join(['{}->{}'.format(i,self.cmds[i]) for i in self.cmds.keys()])
+		while True:
+			cmd=input('{}\n----------\nwhat do you want to do? '.format(msg))
+			if cmd.lower() in ('q','quit','8'):
+				exit('user quit!')
+			elif cmd.lower() == "back":
+				break
+			else:
+				if cmd  == '1':
+					while True:
+						itemcode=input('itemcode[or #back,#quit]: ')
+						if itemcode == '#back':
+							break
+						if itemcode == '#quit':
+							exit("user quit!")
+						t=input('==/like: ')
+						while t not in ['==','like','#back']:
+							t=input('==/like: ')
+							
+						if itemcode.lower() == '#back':
+							break
+						else:
+							with Session(engine) as session:
+								result=session.query(tbl.Item)
+								result=result.filter(tbl.Item.StorageId==self.cfg.get('storageId'))
+								if self.cfg.get('storageid'):
+									result=result.filter(tbl.Item.StorageId==self.cfg.get('storageid'))
+								result=result.filter(tbl.Item.Code==itemcode)
+								results=result.all()
+								self.processResults(results)
+				elif cmd  == '2':
+					while True:
+						itemcode=input('barcode[or #back,#quit]: ')
+						if itemcode == '#back':
+							break
+						if itemcode == '#quit':
+							exit("user quit!")
+
+						t=input('==/like: ')
+						while t not in ['==','like','#back']:
+							t=input('==/like: ')
+							
+						if itemcode.lower() == '#back':
+							break
+						else:
+							with Session(engine) as session:
+								result=session.query(tbl.Item)
+								result=result.filter(tbl.Item.StorageId==self.cfg.get('storageId'))
+								if self.cfg.get('storageid'):
+									result=result.filter(tbl.Item.StorageId==self.cfg.get('storageid'))
+								result=result.filter(tbl.Item.Barcode==itemcode)
+								results=result.all()
+								self.processResults(results)
+				elif cmd  == '3':
+					while True:
+						itemcode=input('ItemId[or #back,#quit]: ')
+						if itemcode == '#back':
+							break
+						if itemcode == '#quit':
+							exit("user quit!")
+						t=input('==/like: ')
+						while t not in ['==','like','#back']:
+							t=input('==/like: ')
+							
+						if itemcode.lower() == '#back':
+							break
+						else:
+							with Session(engine) as session:
+								result=session.query(tbl.Item)
+								result=result.filter(tbl.Item.StorageId==self.cfg.get('storageId'))
+								if self.cfg.get('storageid'):
+									result=result.filter(tbl.Item.StorageId==self.cfg.get('storageid'))
+								result=result.filter(tbl.Item.ItemId==itemcode)
+								results=result.all()
+								self.processResults(results)
+				elif cmd  == '4':
+					while True:
+						itemcode=input('ItemId|Barcode|ItemCode[or #back,#quit]: ')
+						if itemcode == '#back':
+							break
+						if itemcode == '#quit':
+							exit("user quit!")
+
+						t=input('==/like: ')
+						while t not in ['==','like','#back']:
+							t=input('==/like: ')
+							
+						if itemcode.lower() == '#back':
+							break
+						else:
+							with Session(engine) as session:
+								result=session.query(tbl.Item)
+								#if self.cfg.get('storageid'):
+								#print(self.cfg.get('storageId'))
+								result=result.filter(tbl.Item.StorageId==self.cfg.get('storageId'))
+								if t == "like":
+									result=result.filter(or_(tbl.Item.ItemId.icontains(itemcode),tbl.Item.Code.icontains(itemcode),tbl.Item.Barcode.icontains(itemcode)))
+								else:
+									result=result.filter(or_(tbl.Item.ItemId==itemcode,tbl.Item.Code==itemcode,tbl.Item.Barcode==itemcode))
+								results=result.all()
+								self.processResults(results)
+				elif cmd.lower()  == 's' or cmd == '9':
+					while True:
+						itemcode=input('Name Search[or #back,#quit]: ')
+						if itemcode == '#back':
+							break
+						if itemcode == '#quit':
+							exit("user quit!")
+						t=input('==/like: ')
+						while t not in ['==','like','#back']:
+							t=input('==/like: ')
+							
+						if itemcode.lower() == '#back':
+							break
+						else:
+							with Session(engine) as session:
+								result=session.query(tbl.Item)
+								if self.cfg.get('storageid'):
+									result=result.filter(tbl.Item.StorageId==self.cfg.get('storageid'))
+								if t == "like":
+									result=result.filter(tbl.Item.Name.icontains(itemcode))
+								else:
+									result=result.filter(tbl.Item.Name==itemcode)
+								results=result.all()
+								self.processResults(results)
+	def processResults(self,results):
+		for num,r in enumerate(results):
+			print(fg('blue')+'---Item ({})---'.format(num)+attr(0))
+			o=obj2dict(r)
+			for k in o.keys():
+				print(fg('RED')+k+attr(0),':',fg("dark_orange")+str(o[k])+attr(0))
+			self.seeCustomFields(r,self.config,self.engine,self.error_log,self.tbl)
+			print('===Item ({})==='.format(num))
