955e3177adaf713195fab9c197d91fe9858c0660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 04:05:06 +0000

    Add package netorca-sdk/0.1.19/netorca-sdk-0.1.19.tar.gz

---
+++ packages/netorca-sdk/netorca-sdk-0.1.19.tar.gz/netorca-sdk-0.1.19/netorca_sdk/exceptions.py
@@ -0,0 +1,44 @@
+class NetorcaException(Exception):
+    pass
+
+
+class NetorcaBaseException(Exception):
+    def __init__(self, message):
+        self.message = message
+        print(f"Netorca Exception: {message}")
+
+
+class NetorcaValueError(NetorcaBaseException):
+    pass
+
+
+class NetorcaNotFoundError(NetorcaBaseException):
+    pass
+
+
+class NetorcaPermissionError(NetorcaBaseException):
+    pass
+
+
+class NetorcaTimeoutError(NetorcaBaseException):
+    pass
+
+
+class NetorcaAPIError(NetorcaBaseException):
+    pass
+
+
+class NetorcaAuthenticationError(NetorcaAPIError):
+    pass
+
+
+class NetorcaServerUnavailableError(NetorcaAPIError):
+    pass
+
+
+class NetorcaGatewayError(NetorcaAPIError):
+    pass
+
+
+class NetorcaInvalidContextError(NetorcaAPIError):
+    pass
