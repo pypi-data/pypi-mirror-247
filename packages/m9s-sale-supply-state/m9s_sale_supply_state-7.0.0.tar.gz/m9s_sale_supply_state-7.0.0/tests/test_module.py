e5f6210e868c663765caf25a7af66afd305fd4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:25 +0000

    Add package m9s-sale-supply-state/7.0.0/m9s_sale_supply_state-7.0.0.tar.gz

---
+++ packages/m9s-sale-supply-state/m9s_sale_supply_state-7.0.0.tar.gz/m9s_sale_supply_state-7.0.0/tests/test_module.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class SaleSupplyStateTestCase(ModuleTestCase):
+    "Test Sale Supply State module"
+    module = 'sale_supply_state'
+
+
+del ModuleTestCase
