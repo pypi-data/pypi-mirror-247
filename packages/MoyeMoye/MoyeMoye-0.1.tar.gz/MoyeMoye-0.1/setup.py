243e34f03123f8438ec63f785b4a5596472143cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 15:05:13 +0000

    Add package MoyeMoye/0.1/MoyeMoye-0.1.tar.gz

---
+++ packages/MoyeMoye/MoyeMoye-0.1.tar.gz/MoyeMoye-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MoyeMoye',  # The name of your package
+    version='0.1',  # The version of your package
+    packages=find_packages(),  # Automatically find the packages in your directory
+    description='Your package description',  # A brief description of your package
+    author='DZANUM',  # Your name
+    author_email='random.email@example.com',  # Your email
+      
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
