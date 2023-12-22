463312726200c086313c368c5b6018be86481952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:05:14 +0000

    Add package bigtree/0.14.7/bigtree-0.14.7.tar.gz

---
+++ packages/bigtree/bigtree-0.14.7.tar.gz/bigtree-0.14.7/docs/source/conf.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
+import bigtree
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "bigtree"
+copyright = "2022, Kay Jan WONG"
+author = "Kay Jan WONG"
+release = bigtree.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "autodocsumm",
+    "myst_parser",
+    "sphinxemoji.sphinxemoji",
+    "sphinx.ext.mathjax",
+]
+autodoc_default_options = {"autosummary": True}
+sphinxemoji_style = "twemoji"
+myst_enable_extensions = [
+    "attrs_block",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "Python"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "karma_sphinx_theme"
+html_static_path = ["_static"]
+html_favicon = "_static/favicon.ico"
+html_logo = "_static/favicon.ico"
+
+
+def setup(app):
+    app.add_css_file("custom.css")
