eba32c9334ae78de163c67e18f197f940b4e5743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:04:10 +0000

    Add package kafka-lib-nurkhat/0.1.0/kafka-lib-nurkhat-0.1.0.tar.gz

---
+++ packages/kafka-lib-nurkhat/kafka-lib-nurkhat-0.1.0.tar.gz/kafka-lib-nurkhat-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kafka-lib-nurkhat',
+    version='0.1.0',
+    author='Pizdos',
+    author_email='pizdos@example.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
