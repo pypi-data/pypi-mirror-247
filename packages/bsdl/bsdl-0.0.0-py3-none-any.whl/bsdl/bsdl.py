49efdde2b849f2d4e5536a933f0713939e21f506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 05:04:58 +0000

    Add package bsdl/0.0.0/bsdl-0.0.0-py3-none-any.whl

---
+++ packages/bsdl/bsdl-0.0.0-py3-none-any.whl/bsdl/bsdl.py
@@ -0,0 +1,152 @@
+from bsdl.scrape import *
+
+import click
+import os
+from halo import Halo
+import mutagen
+
+red = '\033[91m'
+green = '\033[92m'
+white = '\033[97m'
+
+
+def folderCheck(artist):  # from ttdl
+    if (not os.path.exists(os.path.join(artist))):
+        os.mkdir(os.path.join(artist))
+
+
+def tagSong(beatPath, title, artist, cover, description, permalink, genre):
+    song = mutagen.File(beatPath)
+
+    song["TIT2"] = mutagen.id3.TIT2(encoding=3, text=title)
+    song["TPE1"] = mutagen.id3.TPE1(encoding=3, text=artist)
+
+    response = requests.get(cover, headers=headers)
+    coverData = response.content
+    song["APIC"] = mutagen.id3.APIC(
+        encoding=3,
+        mime="image/jpeg",
+        type=3,
+        data=coverData
+    )
+
+    song["COMM"] = mutagen.id3.COMM(
+        encoding=3, lang="ENG", text=description)
+
+    song["WOAS"] = mutagen.id3.WOAS(url=permalink)
+    song["TCON"] = mutagen.id3.TCON(encoding=3, text=genre)
+
+    song.save()
+
+
+def downloadArtist(artist):
+    spinner = Halo(text='Retrieving Data', spinner='dots')
+    spinner.start()
+    tracksData = getTracksData(artist=artist)
+    artistName = getArtist(tracksData)
+    streams = getStreams(tracksData)
+    titles = getTitles(tracksData)
+    covers = getCoverArts(tracksData)
+    timestamps = getTimestamps(tracksData)
+    descriptions = getDescriptions(tracksData)
+    permalinks = getPermalinks(tracksData)
+    genres = getGenres(tracksData)
+    spinner.stop()
+
+    if streams != None:
+        loadFormat = "Downloading tracks..."
+        with Halo(text=loadFormat, spinner='dots') as h:
+            for (title, link, cover, timestamp, description, permalink, genre) in zip(titles, streams, covers, timestamps, descriptions, permalinks, genres):
+                song = requests.get(link, headers=headers)
+                if (song.status_code == 200):
+                    folderCheck(artistName)
+                    songFile = title.replace("/", "_")
+
+                    if song.headers.get('content-type') == 'audio/mpeg':
+                        songFile += ".mp3"
+                    elif song.headers.get('content-type') == 'audio/wav':
+                        songFile += ".wav"
+                    else:
+                        print()
+                        h.stop_and_persist(
+                            symbol=f'{red}✖' + f"{red} Unsupported file type for '{title}'!")
+                        continue
+
+                    filePath = os.path.join(os.path.join(artistName), songFile)
+                    open(filePath, 'wb').write(song.content)
+
+                    tagSong(filePath, title, artistName,
+                            cover, description, permalink, genre)
+
+                    current_atime = os.path.getatime(filePath)
+                    os.utime(filePath, (current_atime, timestamp))
+
+                    h.stop_and_persist(
+                        symbol=f'{green}✔', text=f"{white}Downloaded '{title}' successfully!")
+                else:
+                    h.stop_and_persist(
+                        symbol=f'{red}✖' + f"{red} Error Occured")
+    else:
+        print(f"{artistName} does not exist or doesn't have any tracks!")
+
+
+def downloadTrack(link):
+    spinner = Halo(text='Retrieving Data', spinner='dots')
+    ID = link.split("-")[-1]
+
+    trackData = getTrackData(ID)
+    artistName = getArtist(trackData)
+    stream = getStreams(trackData)[0]
+    title = getTitles(trackData)[0]
+    cover = getCoverArts(trackData)[0]
+    timestamp = getTimestamps(trackData)[0]
+    description = getDescriptions(trackData)[0]
+    permalink = getPermalinks(trackData)[0]
+    genre = getGenres(trackData)[0]
+    spinner.stop()
+
+    if stream != None:
+        loadFormat = "Downloading track..."
+        with Halo(text=loadFormat, spinner='dots') as h:
+            song = requests.get(stream, headers=headers)
+            if (song.status_code == 200):
+                songFile = title.replace("/", "_")
+
+                if song.headers.get('content-type') == 'audio/mpeg':
+                    songFile += ".mp3"
+                elif song.headers.get('content-type') == 'audio/wav':
+                    songFile += ".wav"
+                else:
+                    h.stop_and_persist(
+                        symbol=f'{red}✖' + f"{red} Unsupported file type for '{title}'!")
+
+                filePath = songFile
+                open(filePath, 'wb').write(song.content)
+
+                tagSong(filePath, title, artistName,
+                        cover, description, permalink, genre)
+
+                current_atime = os.path.getatime(filePath)
+                os.utime(filePath, (current_atime, timestamp))
+
+                h.stop_and_persist(
+                    symbol=f'{green}✔', text=f"{white}Downloaded '{title}' successfully!")
+            else:
+                h.stop_and_persist(
+                    symbol=f'{red}✖' + f"{red} Error Occured")
+    else:
+        print(f"Invalid link!")
+
+
+@ click.command(help="bsdl - a CLI tool for downloading BeatStars music.")
+@ click.option("-artist", "-a", help='Download all tracks from a artist', metavar="[ARTIST]")
+@ click.option("-track", "-t", help='Download a track', metavar="[LINK]")
+def main(artist: str, track: str):
+    if artist:
+        downloadArtist(artist)
+    if track:
+        downloadTrack(track)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/bsdl/bsdl-0.0.0-py3-none-any.whl/bsdl/test.py
@@ -0,0 +1,137 @@
+import json
+import requests
+from datetime import datetime
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Origin': 'https://www.beatstars.com',
+    'Referer': 'https://www.beatstars.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'content-type': 'application/x-www-form-urlencoded',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'x-algolia-api-key': 'b3513eb709fe8f444b4d5c191b63ea47',
+    'x-algolia-application-id': 'NMMGZJQ6QI',
+}
+
+
+def getTrackData(ID):
+    trackData = [requests.get(
+        f'https://main.v2.beatstars.com/beat?id={ID}&fields=details,stats,licenses', headers=headers).json()]
+
+    return trackData
+
+
+def getTracksData(artist: str):
+
+    artistPageResponse = requests.get(
+        f'https://main.v2.beatstars.com/musician?permalink={artist}&fields=profile,user_contents,stats,bulk_deals,social_networks', headers=headers)
+
+    if artistPageResponse.status_code == 404:
+        return None
+
+    userID = artistPageResponse.json()[
+        'response']['data']['profile']['user_id']
+    memberId = f'MR{userID}'
+
+    page = 0
+    dataBundle = []
+
+    while True:
+
+        data = '{{"query":"","page":{0},"hitsPerPage":1000,"facets":["*"],"analytics":true,"clickAnalytics":true,"tagFilters":[],"facetFilters":[["profile.memberId:{1}"]],"maxValuesPerFacet":1000,"maxFacetHits":100,"enableABTest":false,"userToken":null,"filters":"","ruleContexts":[]}}'.format(
+            page, memberId)
+
+        profileDataResponse = requests.post(
+            'https://nmmgzjq6qi-dsn.algolia.net/1/indexes/public_prod_inventory_track_index_bycustom/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.12.0)%3B%20Browser',
+            headers=headers,
+            data=data,
+        )
+
+        # No tracks in artist/server error
+        if profileDataResponse.status_code == 404:
+            return
+
+        if profileDataResponse.status_code == 200:
+            profileData = profileDataResponse.json()
+
+            hits = profileData['hits']
+            dataBundle.extend(hits)
+
+            if profileData.get('nbPages', 0) <= page:
+                break
+            page += 1
+
+    with open('data.json', "w") as file:
+        json.dump(dataBundle, file)
+
+    trackIDs = [v2Id['v2Id'] for v2Id in dataBundle]
+    tracksData = [getTrackData(ID=ID)[0] for ID in trackIDs]
+
+    return tracksData
+
+
+print(getTracksData('shinjin'))
+print(len(getTracksData('shinjin')))
+
+
+def getTitles(tracksData):
+    titles = [data['response']['data']['details']['title']
+              for data in tracksData]
+
+    return titles
+
+
+def getArtist(tracksData):
+    artist = [data['response']['data']['details']
+              ['musician']['display_name'] for data in tracksData][0]
+
+    return artist
+
+
+def getStreams(tracksData):
+    streamLinks = [data['response']['data']['details']['stream_ssl_url']
+                   for data in tracksData]
+
+    return streamLinks
+
+
+def getCoverArts(tracksData):
+    coverLinks = [data['response']['data']['details']['artwork']['original']
+                  for data in tracksData]
+
+    return coverLinks
+
+
+def getTimestamps(tracksData):
+    timestamps = [data['response']['data']['details']['release_date_time']
+                  for data in tracksData]
+
+    return timestamps
+
+
+def getDescriptions(tracksData):
+    descriptions = [data['response']['data']['details']['description']
+                    for data in tracksData]
+
+    return descriptions
+
+
+def getPermalinks(tracksData):
+    permalinks = [data['response']['data']['details']['beatstars_uri']
+                  for data in tracksData]
+
+    return permalinks
+
+
+def getGenres(tracksData):
+    genres = [data['response']['data']['details']['genre'][0]['name']
+              for data in tracksData]
+
+    return genres
