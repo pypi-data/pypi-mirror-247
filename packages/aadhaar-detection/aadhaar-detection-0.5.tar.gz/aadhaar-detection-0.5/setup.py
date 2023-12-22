7f3a1549fcd265dd7751af6b3d5a496e6f0ee708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-22 03:05:03 +0000

    Add package aadhaar-detection/0.5/aadhaar-detection-0.5.tar.gz

---
+++ packages/aadhaar-detection/aadhaar-detection-0.5.tar.gz/aadhaar-detection-0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aadhaar-detection',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'tensorflow==2.7'
+    ],
+    author='Girinath',
+    author_email='girinathr@simplfin.tech',
+    description='Aadhaar card detection library',
+    long_description='A library for detecting Aadhaar cards in images.',
+    url='https://github.com/yourusername/aadhaar-detection',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
