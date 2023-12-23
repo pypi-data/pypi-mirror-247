daf3cca89af27b54f11e24d5de2a2ce8a2435feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:41 +0000

    Add package heatzypy/2.2.0/heatzypy-2.2.0.tar.gz

---
+++ packages/heatzypy/heatzypy-2.2.0.tar.gz/heatzypy-2.2.0/heatzypy/exception.py
@@ -0,0 +1,25 @@
+"""Exceptions for Heatzy."""
+
+
+class HeatzyException(Exception):
+    """Heatzy exception."""
+
+
+class HttpRequestFailed(HeatzyException):
+    """Request exception."""
+
+
+class AuthenticationFailed(HeatzyException):
+    """Authentication exception."""
+
+
+class RetrieveFailed(HeatzyException):
+    """Retrieve exception."""
+
+
+class CommandFailed(HeatzyException):
+    """Command exception."""
+
+
+class TimeoutExceededError(HeatzyException):
+    """Timeout exception."""
