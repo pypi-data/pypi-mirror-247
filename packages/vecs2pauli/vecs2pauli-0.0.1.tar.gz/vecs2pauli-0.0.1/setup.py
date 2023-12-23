763a42b350d6b0256951c7eedaef89fa707eaa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:05:09 +0000

    Add package vecs2pauli/0.0.1/vecs2pauli-0.0.1.tar.gz

---
+++ packages/vecs2pauli/vecs2pauli-0.0.1.tar.gz/vecs2pauli-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vecs2pauli',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://vecs2pauli.github.io/',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
