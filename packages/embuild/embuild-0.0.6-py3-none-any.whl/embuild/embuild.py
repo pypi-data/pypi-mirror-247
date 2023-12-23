e8c89faf50fa64a7e54b84adf4787ab95d45367d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:51 +0000

    Add package embuild/0.0.6/embuild-0.0.6-py3-none-any.whl

---
+++ packages/embuild/embuild-0.0.6-py3-none-any.whl/embuild/embuild.py
@@ -0,0 +1,17 @@
+from vt100logging import vt100logging_init, I, E
+from embuild.helpers.check_environment import check_environment
+
+
+def main():
+    vt100logging_init('embuild')
+    try:
+        I("START")
+        check_environment()
+        I("DONE")
+    except Exception as e:
+        E(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
