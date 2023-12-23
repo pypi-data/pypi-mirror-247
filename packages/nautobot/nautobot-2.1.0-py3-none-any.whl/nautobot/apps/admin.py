2d781f6e8c1c7b5de8b549ad667257012f1bf319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:32 +0000

    Add package nautobot/2.1.0/nautobot-2.1.0-py3-none-any.whl

---
+++ packages/nautobot/nautobot-2.1.0-py3-none-any.whl/nautobot/apps/admin.py
@@ -0,0 +1,6 @@
+"""Utilities for integrating with the Nautobot Admin UI."""
+
+from nautobot.core.admin import NautobotModelAdmin
+
+
+__all__ = ("NautobotModelAdmin",)
---
+++ packages/nautobot/nautobot-2.1.0-py3-none-any.whl/nautobot/apps/urls.py
@@ -0,0 +1,6 @@
+"""Utilities for apps to implement URL routing."""
+
+from nautobot.core.views.routers import NautobotUIViewSetRouter
+
+
+__all__ = ("NautobotUIViewSetRouter",)
