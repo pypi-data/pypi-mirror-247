4681a18efa04034eeb9ad6fb07451728eb6c7d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:08:45 +0000

    Add package amadeusgpt/0.0.2/amadeusgpt-0.0.2-py2.py3-none-any.whl

---
+++ packages/amadeusgpt/amadeusgpt-0.0.2-py2.py3-none-any.whl/amadeusgpt/__init__.py
@@ -0,0 +1,11 @@
+#
+"""AmadeusGPT: We turn natural language descriptions of behaviors into machine-executable code.
+Written by Shaokai Ye and contributors | see Ye et al. 2023 NeurIPS for details.
+SOURCE CODE: https://github.com/AdaptiveMotorControlLab/AmadeusGPT
+Apache-2.0 license
+"""
+
+from amadeusgpt.implementation import AnimalBehaviorAnalysis
+from amadeusgpt.main import AMADEUS
+
+from amadeusgpt.version import __version__, VERSION
