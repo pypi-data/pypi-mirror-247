f7ffeb52cfa96953fa04e26cf6d68e38bb532d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:18 +0000

    Add package lagrange-open/6.18.0/lagrange_open-6.18.0-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/lagrange-open/lagrange_open-6.18.0-cp39-cp39-macosx_10_15_x86_64.whl/lagrange/lagrange/image.pyi
@@ -0,0 +1,75 @@
+from typing import Any, Optional, overload, Typing, Sequence
+from enum import Enum
+import lagrange.image
+
+class ImageChannel:
+    """
+    <attribute '__doc__' of 'ImageChannel' objects>
+    """
+
+    @entries: dict
+    
+    def __init__(*args, **kwargs):
+        """
+        Initialize self.  See help(type(self)) for accurate signature.
+        """
+        ...
+    
+    four: ImageChannel
+    
+    one: ImageChannel
+    
+    three: ImageChannel
+    
+    unknown: ImageChannel
+    
+class ImagePrecision:
+    """
+    <attribute '__doc__' of 'ImagePrecision' objects>
+    """
+
+    @entries: dict
+    
+    def __init__(*args, **kwargs):
+        """
+        Initialize self.  See help(type(self)) for accurate signature.
+        """
+        ...
+    
+    float16: ImagePrecision
+    
+    float32: ImagePrecision
+    
+    float64: ImagePrecision
+    
+    int32: ImagePrecision
+    
+    int8: ImagePrecision
+    
+    uint32: ImagePrecision
+    
+    uint8: ImagePrecision
+    
+    unknown: ImagePrecision
+    
+class ImageStorage:
+
+    def __init__(self, arg0: int, arg1: int, arg2: int, /) -> None:
+        ...
+    
+    @property
+    def (self) -> numpy.typing.NDArray:
+        ...
+    
+    @property
+    def (self) -> int:
+        ...
+    
+    @property
+    def (self) -> int:
+        ...
+    
+    @property
+    def (self) -> int:
+        ...
+    
