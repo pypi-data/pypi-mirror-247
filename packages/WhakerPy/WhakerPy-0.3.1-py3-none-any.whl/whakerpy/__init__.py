d4cd6a4d9cb18922a974cdc0fe74d943d0b9aa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:36 +0000

    Add package WhakerPy/0.3.1/WhakerPy-0.3.1-py3-none-any.whl

---
+++ packages/WhakerPy/WhakerPy-0.3.1-py3-none-any.whl/whakerpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+This package name is `whakerpy` which stands for "Web HTML maker in Python".
+
+"""
+
+from .htmlmaker import *
+from .httpd import *
+from .messages import error
+from .webapp import *
