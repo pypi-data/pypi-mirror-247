b8c3527fa925df559e162ce790d32d052ef1b203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:07:55 +0000

    Add package promptquality/0.20.1/promptquality-0.20.1.tar.gz

---
+++ packages/promptquality/promptquality-0.20.1.tar.gz/promptquality-0.20.1/src/promptquality/constants/run.py
@@ -0,0 +1,8 @@
+from types import SimpleNamespace
+
+RunDefaults = SimpleNamespace(
+    project_type="prompt_evaluation",
+    job_name="prompt_run",
+    prompt_evaluation_task_type=7,
+    prompt_chain_task_type=12,
+)
