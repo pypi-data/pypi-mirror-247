c7d5d07853b6594f2b0fd196631f46ea47d25f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:07:57 +0000

    Add package verysimplemodule-annagodoy/0.0.5/verysimplemodule-annagodoy-0.0.5.tar.gz

---
+++ packages/verysimplemodule-annagodoy/verysimplemodule-annagodoy-0.0.5.tar.gz/verysimplemodule-annagodoy-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Pacote inicial'
+LONG_DESCRIPTION = 'Pacote inicial com contas simples de matematica'
+
+setup(
+    name="verysimplemodule-annagodoy",
+    version = VERSION,
+    author = "anna",
+    author_email = "annabeatriz_2019@outlook.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires = ['numpy'],
+    
+    keywords = ['python', 'primeiro pacote'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
