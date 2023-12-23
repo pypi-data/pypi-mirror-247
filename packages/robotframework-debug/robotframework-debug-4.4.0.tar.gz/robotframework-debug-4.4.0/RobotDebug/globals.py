b4efd27d7096a76f75ff1a74c7f32bb7527dae4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:23 +0000

    Add package robotframework-debug/4.4.0/robotframework-debug-4.4.0.tar.gz

---
+++ packages/robotframework-debug/robotframework-debug-4.4.0.tar.gz/robotframework-debug-4.4.0/RobotDebug/globals.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from robot.version import get_version
+
+
+class SingletonContext:
+    in_step_mode = False
+    last_command = ""
+
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+
+context = SingletonContext()
+
+
+class StepMode(str, Enum):
+    INTO = "_INTO"
+    OVER = "_OVER"
+    OUT = "_OUT"
+    CONTINUE = "_CONTINUE"
+    STOP = "_STOP"
+
+
+IS_RF_7 = int(get_version().split(".", 1)[0]) >= 7  # noqa: PLR2004
