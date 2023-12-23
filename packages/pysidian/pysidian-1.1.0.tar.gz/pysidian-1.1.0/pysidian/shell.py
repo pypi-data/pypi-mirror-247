f6a731a3d2b47dd7b4db45d4993fcb25555af37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:08:00 +0000

    Add package pysidian/1.1.0/pysidian-1.1.0.tar.gz

---
+++ packages/pysidian/pysidian-1.1.0.tar.gz/pysidian-1.1.0/pysidian/shell.py
@@ -0,0 +1,85 @@
+import click
+import os
+import sys
+import click_shell
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from pysidian.core.vault import ObsidianVault # noqa
+from pysidian.core.plugin import ObsidianPlugin, testManifest # noqa
+
+@click_shell.shell(
+    "pysidian", 
+    help="Pysidian dev toolkit", 
+    invoke_without_command=True,
+    chain=True
+)
+@click.pass_context
+def cli(ctx : click.Context):
+    print("Pysidian dev toolkit")
+    ctx.ensure_object(dict)
+    
+
+@cli.command("tmf", help="test plugin manifest")
+@click.option("-p", "--path", default=os.getcwd(), help="Path to plugin")
+@click.pass_context
+def create_manifiest(ctx, path):
+    p = ObsidianPlugin(path)
+    p.manifest = testManifest
+    ctx.obj["targetPlugin"] = p
+
+@cli.command("bkupPlugin", help="backup plugin") 
+@click.argument("path")
+@click.pass_context
+def bkup(ctx, path):
+    ctx.obj["targetPlugin"].bkup(path)
+
+@cli.command("loadPlugin")
+@click.pass_context
+def update(ctx):
+    if "targetPlugin" not in ctx.obj:
+        ctx.obj["targetPlugin"] = ObsidianPlugin(os.getcwd())
+    
+    p : ObsidianPlugin =ctx.obj["targetPlugin"]
+    v : ObsidianVault = ctx.obj["targetVault"]
+    p.update(v)
+
+
+
+@cli.command("newVault", help="create new vault")
+@click.argument("name")
+@click.option("-p", "--path", default=".", help="Path to vault")
+@click.pass_context
+def new(ctx : click.Context, name, path):
+    ctx.obj["targetVault"] = ObsidianVault.new(name, path)
+
+@cli.command("selectVault", help="select vault")
+@click.argument("name")
+@click.pass_context
+def select(ctx : click.Context, name):
+    if "/" in name or "\\" in name:
+        ctx.obj["targetVault"] = ObsidianVault.fromLocal(name)
+        return
+    
+    v = ObsidianVault.fromName(name)
+    if v is not None:
+        ctx.obj["targetVault"] = v
+        return
+
+    v = ObsidianVault.fromLocal(name)
+    if v is not None:
+        ctx.obj["targetVault"] = v
+        return
+    
+    raise Exception(f"Vault {name} not found")
+    
+@cli.command("openVault")
+@click.pass_context
+def open(ctx):
+    ctx.obj["targetVault"].register()
+    ctx.obj["targetVault"].open()
+    
+def main():
+    cli()
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/pysidian/pysidian-1.1.0.tar.gz/pysidian-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="pysidian",
+    version="1.1.0",
+    packages=[
+        "pysidian",
+        "pysidian.core",
+    ],
+    install_requires=[
+        "zrcl3",
+        "click",
+        "click_shell"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pysidian = pysidian.shell:main",
+        ]
+    },
+    python_requires=">=3.8",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="ZackaryW",   
+)
