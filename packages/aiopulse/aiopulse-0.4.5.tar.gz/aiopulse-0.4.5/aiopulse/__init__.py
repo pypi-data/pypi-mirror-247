c408231fc78b46c9e60524a5ae1132902f7bc9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:37 +0000

    Add package aiopulse/0.4.5/aiopulse-0.4.5.tar.gz

---
+++ packages/aiopulse/aiopulse-0.4.5.tar.gz/aiopulse-0.4.5/aiopulse/__init__.py
@@ -0,0 +1,30 @@
+"""Rollease Acmeda Automate Pulse asyncio protocol implementation."""
+import logging
+
+from aiopulse.hub import Hub
+from aiopulse.roller import Roller
+from aiopulse.room import Room
+from aiopulse.scene import Scene
+from aiopulse.errors import (
+    CannotConnectException,
+    NotConnectedException,
+    NotRunningException,
+    InvalidResponseException,
+)
+from aiopulse.const import UpdateType
+
+__all__ = [
+    "Hub",
+    "Roller",
+    "Room",
+    "Scene",
+    "CannotConnectException",
+    "NotConnectedException",
+    "NotRunningException",
+    "InvalidResponseException",
+    "UpdateType",
+]
+__version__ = "0.4.5"
+__author__ = "Alan Murray"
+
+_LOGGER = logging.getLogger(__name__)
