ecd7e8742491a47c953ca2abb6e02404c0f7634e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:58 +0000

    Add package pyD3TN/0.13.0/pyD3TN-0.13.0.tar.gz

---
+++ packages/pyD3TN/pyD3TN-0.13.0.tar.gz/pyD3TN-0.13.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: BSD-3-Clause OR Apache-2.0
+
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='pyD3TN',
+    version='0.13.0',
+    author='D3TN GmbH',
+    author_email='contact@d3tn.com',
+    description='Collection of DTN protocol implementations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/d3tn/ud3tn',
+    packages=['pyd3tn'],
+    install_requires=['cbor==1.0.0'],
+    python_requires='>=3.8',
+)
