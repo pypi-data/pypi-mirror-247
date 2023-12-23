6fd75893a9798e87993463aed5dafe03c733772f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:47 +0000

    Add package pullback-new-exp-forex-next3/1.1/pullback_new_exp_forex_next3-1.1.tar.gz

---
+++ packages/pullback-new-exp-forex-next3/pullback_new_exp_forex_next3-1.1.tar.gz/pullback_new_exp_forex_next3-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name='pullback_new_exp_forex_next3' ,
+       version = '1.1' ,
+       description = 'pullback_new_exp_forex_next3' ,
+       author = 'omid' ,
+       packages =['pullback_new_exp_forex_next3'] ,
+       zip_safe = False
+       
+       )
