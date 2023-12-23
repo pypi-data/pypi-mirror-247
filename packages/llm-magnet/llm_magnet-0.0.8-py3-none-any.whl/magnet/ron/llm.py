417370f25007641dff5a7d8b458bd0b9f26ef750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:45 +0000

    Add package llm-magnet/0.0.8/llm_magnet-0.0.8-py3-none-any.whl

---
+++ packages/llm-magnet/llm_magnet-0.0.8-py3-none-any.whl/magnet/ron/llm.py
@@ -0,0 +1,70 @@
+from magnet.utils.globals import _f
+from magnet.utils.huggingface import InferenceAPI
+from magnet.utils.local import LocalInference
+from magnet.utils.prompts import *
+from magnet.utils.data_classes import *
+import requests, json
+
+class Generate:
+    def __init__(self, server: str = None, field = None, hf_token: str = None):
+        self.server = server if not hf_token else None
+        self.field = field
+        self.token = hf_token
+
+    async def on(self):
+        if self.field:
+            pass
+
+    async def ask(self
+                  , m: str = "mistralai/Mistral-7B-Instruct-v0.1"
+                  , q: str = "What is your itinerary?"
+                  , t: float = 1.0
+                  , n: int = 8096
+                  , p: str = "qa_ref"
+                  , cb: object = None
+                  , docs: list = []
+                  , v: bool = False
+                ):
+        prompt = getattr(globals()['Prompts'](), p)(docs,q)
+        _f('warn', '(p + q + d) > n') if len(prompt) > n else None
+        payload = json.dumps({
+            "model": m,
+            "prompt": prompt,
+            "inputs": prompt,
+            "parameters": {
+                "max_new_tokens": n
+                , "temperature": t,
+            }
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        if self.token:
+            llm = InferenceAPI(self.token)
+            response = llm.invoke(payload)
+            if not isinstance(response, list) and 'error' in response.keys():
+                return _f('fatal', response['error'])
+            else:
+                response = response[0]['generated_text'].split(json.loads(payload)['prompt'])[1]
+        elif v:
+            response = requests.request(
+                "POST", f"{self.server}/v1/completions", headers=headers, data=payload).text
+        else:
+            llm = LocalInference(model=m)
+            response = llm.invoke(payload)
+        if self.field:
+            payload = GeneratedPayload(
+                        query=q
+                        , prompt=prompt
+                        , context=docs
+                        , result=response
+                        , model=m
+                    )
+            try:
+                await self.field.pulse(payload)
+            except Exception as e:
+                _f('fatal',e)
+        if cb:
+            cb(response)
+        else:
+            return response
---
+++ packages/llm-magnet/llm_magnet-0.0.8-py3-none-any.whl/magnet/utils/local.py
@@ -0,0 +1,21 @@
+import requests
+from magnet.utils.globals import _f
+from magnet.ron.utils.mlx import mistral
+import json
+
+class LocalInference:
+    def __init__(self, model):
+        self.model = model
+    
+    def invoke(self, payload):
+        payload = json.loads(payload)
+        payload = {
+            "seed": 2077
+            , "model_path": self.model
+            , "temp": payload["parameters"]["temperature"]
+            , "prompt": payload["prompt"]
+            , "max_tokens": payload["parameters"]["max_new_tokens"]
+            , "tokens_per_eval": 10
+        }
+        response = mistral.generate(payload)
+        return response
