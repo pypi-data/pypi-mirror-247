86eec9e00907aaf0b74c92097ea60fd404bc6021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:32 +0000

    Add package wam2layers/3.0.0b6/wam2layers-3.0.0b6.tar.gz

---
+++ packages/wam2layers/wam2layers-3.0.0b6.tar.gz/wam2layers-3.0.0b6/src/wam2layers/utils/grids.py
@@ -0,0 +1,4 @@
+# In case of a periodic domain, input data is "complete" in the x direction.
+# With a non-periodic domain, the computational domain is smaller than the input
+# data: the outermost rows and columns are used as "ghost cells" in which we can
+# insert boundary conditions
