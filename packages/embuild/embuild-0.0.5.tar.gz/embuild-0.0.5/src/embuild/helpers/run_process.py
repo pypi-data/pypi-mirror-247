97c4729df0b780f971f2567593bdeb64b6b8b5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:29 +0000

    Add package embuild/0.0.5/embuild-0.0.5.tar.gz

---
+++ packages/embuild/embuild-0.0.5.tar.gz/embuild-0.0.5/src/embuild/helpers/run_process.py
@@ -0,0 +1,16 @@
+from vt100logging import D
+from subprocess import Popen, PIPE, DEVNULL, STDOUT
+from os import getcwd
+from helpers.verbosity import is_verbose
+
+
+def run_process(cmd: str, cwd: str = getcwd()) -> str:
+    """Run a process and return its output."""
+    if is_verbose:
+        D(f"Running: '{cmd}'")
+    process = Popen(cmd, cwd=cwd, stdout=PIPE if is_verbose else DEVNULL,
+                    stderr=STDOUT if is_verbose else DEVNULL, shell=True)
+    output = process.communicate()[0]
+    if process.returncode != 0:
+        raise Exception(f"Failed to run: '{cmd}'")
+    return output.decode('utf-8')
