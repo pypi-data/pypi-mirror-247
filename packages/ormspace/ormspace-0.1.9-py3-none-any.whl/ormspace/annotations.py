8e2a05cb7f0f91d27fe9121514401ea3245bdc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:16 +0000

    Add package ormspace/0.1.9/ormspace-0.1.9-py3-none-any.whl

---
+++ packages/ormspace/ormspace-0.1.9-py3-none-any.whl/ormspace/annotations.py
@@ -0,0 +1,23 @@
+import datetime
+from typing import Annotated, Optional
+
+from annotated_types import Ge, Le
+from pydantic import AfterValidator, BeforeValidator, Field, PlainSerializer
+
+from ormspace import functions
+
+def bytes_to_str(value: bytes) -> str:
+    return value.decode("utf-8")
+
+
+PasswordField = Annotated[bytes, PlainSerializer(bytes_to_str, return_type=str)]
+TitleField = Annotated[str, AfterValidator(functions.title_caps)]
+BirthDateField = Annotated[datetime.date, Ge(functions.today() - datetime.timedelta(days=365*125)), Le(functions.today())]
+DateField = Annotated[datetime.date, Field(default_factory=functions.today)]
+DateTimeField = Annotated[datetime.datetime, Field(default_factory=functions.now)]
+MultiLineTextField = Annotated[str, Field(default_factory=str)]
+StringField = Annotated[str, Field(default_factory=str)]
+PositiveIntegerField = Annotated[int, Ge(0)]
+IntegerField = Annotated[int, Field(None)]
+FloatField = Annotated[float, Field(None)]
+PositiveFloatField = Annotated[float, Ge(0)]
