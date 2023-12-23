cd3520371b0390dc7dbe25849b9e91e11c03a30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:54 +0000

    Add package mwtokenizer/0.2.0/mwtokenizer-0.2.0-py3-none-any.whl

---
+++ packages/mwtokenizer/mwtokenizer-0.2.0-py3-none-any.whl/mwtokenizer/__init__.py
@@ -0,0 +1,12 @@
+from .tokenizer import Tokenizer
+
+__title__ = "mwtokenizer"
+__summary__ = """mwtokenizer is a language-agnostic NLP toolkit for processing Wikipedia
+articles with adequate performance out-of-the-box."""
+__url__ = "https://gitlab.wikimedia.org/repos/research/wiki-nlp-tools/"
+
+__version__ = "0.2.0"
+
+__license__ = "MIT License"
+
+__all__ = ["Tokenizer"]
