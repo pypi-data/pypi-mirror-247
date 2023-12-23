c0f1429b4760e58e1241980217f30ffa30f5d6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:51 +0000

    Add package qrraj/0.1.3/qrraj-0.1.3.tar.gz

---
+++ packages/qrraj/qrraj-0.1.3.tar.gz/qrraj-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qrraj',
+    version='0.1.3',
+    author='Aryan Raj',
+    description='QR CODE GENERATOR!!',
+    packages = find_packages(),
+    install_requires = ["wheel", "setuptools", "setuptools", "qrcode" ],
+)
