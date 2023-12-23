f83709d5eae4107e3fefd8903fc883e28d6fc9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 15:04:58 +0000

    Add package netunicorn-director-infrastructure/0.4.0/netunicorn-director-infrastructure-0.4.0.tar.gz

---
+++ packages/netunicorn-director-infrastructure/netunicorn-director-infrastructure-0.4.0.tar.gz/netunicorn-director-infrastructure-0.4.0/src/netunicorn/director/infrastructure/connectors/dummy.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+import logging
+from typing import Any, Optional, Tuple
+
+from netunicorn.base.deployment import Deployment
+from netunicorn.base.nodes import CountableNodePool, Node, Nodes
+from netunicorn.director.base.connectors.protocol import NetunicornConnectorProtocol
+from netunicorn.director.base.connectors.types import StopExecutorRequest
+from returns.result import Result, Success
+
+
+class DummyNetunicornConnector(NetunicornConnectorProtocol):
+    def __init__(
+        self,
+        connector_name: str,
+        config_file: str | None,
+        netunicorn_gateway: str,
+        logger: Optional[logging.Logger] = None,
+    ):
+        if not logger:
+            logging.basicConfig()
+            logger = logging.getLogger(__name__)
+        self.logger = logger
+        self.logger.info(
+            f"I'm a dummy connector {connector_name}! I don't do anything! :("
+        )
+        self.logger.info(
+            f"I've been provided with config file {config_file} and netunicorn gateway {netunicorn_gateway}!"
+        )
+
+    async def initialize(self) -> None:
+        self.logger.info("Initialize called")
+        pass
+
+    async def health(self) -> Tuple[bool, str]:
+        return True, "I'm always healthy!"
+
+    async def shutdown(self) -> None:
+        self.logger.info("Shutdown called")
+
+    async def get_nodes(
+        self,
+        username: str,
+        authentication_context: Optional[dict[str, str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Nodes:
+        self.logger.info(f"Get nodes called with {username=}")
+        self.logger.info(f"Authentication context: {authentication_context=}")
+        self.logger.info(f"Additional args: {args=}, {kwargs=}")
+        self.logger.info("Returning dummy node pool")
+        return CountableNodePool(nodes=[Node(name="dummy", properties={})])
+
+    async def deploy(
+        self,
+        username: str,
+        experiment_id: str,
+        deployments: list[Deployment],
+        deployment_context: Optional[dict[str, str]],
+        authentication_context: Optional[dict[str, str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> dict[str, Result[Optional[str], str]]:
+        self.logger.info(
+            f"Deploy called with {username=}, {experiment_id=}, {deployments=}, {deployment_context=},"
+            f" {authentication_context=}, {args=}, {kwargs=}"
+        )
+        self.logger.info("Returning dummy deployment results")
+        return {deployment.executor_id: Success(None) for deployment in deployments}
+
+    async def execute(
+        self,
+        username: str,
+        experiment_id: str,
+        deployments: list[Deployment],
+        execution_context: Optional[dict[str, str]],
+        authentication_context: Optional[dict[str, str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> dict[str, Result[Optional[str], str]]:
+        self.logger.info(
+            f"Execute called with {username=}, {experiment_id=}, {deployments=}, {execution_context=},"
+            f" {authentication_context=}, {args=}, {kwargs=}"
+        )
+        self.logger.info("Returning dummy execution results")
+        return {deployment.executor_id: Success(None) for deployment in deployments}
+
+    async def stop_executors(
+        self,
+        username: str,
+        requests_list: list[StopExecutorRequest],
+        cancellation_context: Optional[dict[str, str]],
+        authentication_context: Optional[dict[str, str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> dict[str, Result[Optional[str], str]]:
+        self.logger.info(
+            f"Stop executors called with {username=}, {requests_list=}, {cancellation_context=},"
+            f" {authentication_context=}, {args=}, {kwargs=}"
+        )
+        self.logger.info("Returning dummy stop executors results")
+        return {request["executor_id"]: Success(None) for request in requests_list}
+
+    async def cleanup(
+        self,
+        experiment_id: str,
+        deployments: list[Deployment],
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        self.logger.info(
+            f"Cleanup called with {experiment_id=}, {deployments=}, {args=}, {kwargs=}"
+        )
---
+++ packages/netunicorn-director-infrastructure/netunicorn-director-infrastructure-0.4.0.tar.gz/netunicorn-director-infrastructure-0.4.0/src/netunicorn/director/infrastructure/server.py
@@ -0,0 +1,176 @@
+import json
+from contextlib import asynccontextmanager
+from typing import Annotated, Any, Optional
+
+import uvicorn
+from fastapi import BackgroundTasks, FastAPI, Header, HTTPException
+from fastapi.responses import Response
+from netunicorn.base.utils import UnicornEncoder
+from netunicorn.director.base.types import (
+    ConnectorContext,
+    ExecutorsCancellationRequest,
+)
+
+from .kernel import (
+    deploy,
+    execute,
+    get_nodes,
+    health,
+    initialize,
+    parse_config,
+    shutdown,
+    stop_execution,
+    stop_executors,
+)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):  # type: ignore[no-untyped-def]
+    await initialize(config)
+    yield
+    await shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+config: dict[str, Any]
+
+
+async def parse_context(json_str: Optional[str]) -> ConnectorContext:
+    if not json_str or json_str == "null":
+        return None
+    try:
+        return json.loads(json_str)  # type: ignore
+    except json.JSONDecodeError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Couldn't parse the context: {e}",
+        )
+
+
+@app.get("/health")
+async def health_handler() -> Response:
+    code, result = await health()
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+@app.get("/nodes/{username}", status_code=200)
+async def get_nodes_handler(
+    username: str,
+    netunicorn_auth_context: Annotated[Optional[str], Header()] = None,
+) -> Response:
+    parsed_netunicorn_auth_context = await parse_context(netunicorn_auth_context)
+    code, result = await get_nodes(username, parsed_netunicorn_auth_context)
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+@app.post("/deployment/{username}/{experiment_id}", status_code=200)
+async def deployment_handler(
+    username: str,
+    experiment_id: str,
+    background_tasks: BackgroundTasks,
+    netunicorn_auth_context: Annotated[Optional[str], Header()] = None,
+) -> Response:
+    parsed_netunicorn_auth_context = await parse_context(netunicorn_auth_context)
+    code, result = await deploy(
+        username, experiment_id, background_tasks, parsed_netunicorn_auth_context
+    )
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+@app.post("/execution/{username}/{experiment_id}")
+async def execution_handler(
+    username: str,
+    experiment_id: str,
+    background_tasks: BackgroundTasks,
+    execution_context: Optional[dict[str, dict[str, str]]] = None,
+    netunicorn_auth_context: Annotated[Optional[str], Header()] = None,
+) -> Response:
+    parsed_netunicorn_auth_context = await parse_context(netunicorn_auth_context)
+    code, result = await execute(
+        username,
+        experiment_id,
+        background_tasks,
+        execution_context,
+        parsed_netunicorn_auth_context,
+    )
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+@app.delete("/execution/{username}/{experiment_id}")
+async def stop_execution_handler(
+    username: str,
+    experiment_id: str,
+    background_tasks: BackgroundTasks,
+    cancellation_context: Optional[dict[str, dict[str, str]]] = None,
+    netunicorn_auth_context: Annotated[Optional[str], Header()] = None,
+) -> Response:
+    parsed_netunicorn_auth_context = await parse_context(netunicorn_auth_context)
+    code, result = await stop_execution(
+        username,
+        experiment_id,
+        background_tasks,
+        cancellation_context,
+        parsed_netunicorn_auth_context,
+    )
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+@app.delete("/executors/{username}")
+async def stop_executors_handler(
+    username: str,
+    cancellation_request: ExecutorsCancellationRequest,
+    background_tasks: BackgroundTasks,
+    netunicorn_auth_context: Annotated[Optional[str], Header()] = None,
+) -> Response:
+    parsed_netunicorn_auth_context = await parse_context(netunicorn_auth_context)
+    code, result = await stop_executors(
+        username,
+        cancellation_request["executors"],
+        background_tasks,
+        cancellation_request.get("cancellation_context", None),
+        parsed_netunicorn_auth_context,
+    )
+    return Response(
+        status_code=code,
+        content=json.dumps(result, cls=UnicornEncoder),
+        media_type="application/json",
+    )
+
+
+def main(filepath: str) -> None:
+    global config
+    config = parse_config(filepath)
+
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"][
+        "fmt"
+    ] = "%(asctime)s - %(levelname)s - %(message)s"
+    log_config["formatters"]["default"][
+        "fmt"
+    ] = "%(asctime)s - %(levelname)s - %(message)s"
+    uvicorn.run(
+        app,
+        host=config["netunicorn.infrastructure.host"],
+        port=config["netunicorn.infrastructure.port"],
+        log_level=config["netunicorn.infrastructure.log.level"],
+    )
