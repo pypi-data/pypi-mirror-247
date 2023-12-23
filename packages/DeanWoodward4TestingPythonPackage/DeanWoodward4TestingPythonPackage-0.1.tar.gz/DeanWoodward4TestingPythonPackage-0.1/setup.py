d75b0c328f706835864dfe5ae3549c9ce2da4ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:59 +0000

    Add package DeanWoodward4TestingPythonPackage/0.1/DeanWoodward4TestingPythonPackage-0.1.tar.gz

---
+++ packages/DeanWoodward4TestingPythonPackage/DeanWoodward4TestingPythonPackage-0.1.tar.gz/DeanWoodward4TestingPythonPackage-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DeanWoodward4TestingPythonPackage",
+    version="0.1",
+    description="A simple Python package to say hello",
+    author="Dean Woodward",
+    author_email="dean.woodward@aprllp.com",
+    url="https://github.com/APRLLP/MultiplePackageRepo",
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    # Add more parameters as needed
+)
