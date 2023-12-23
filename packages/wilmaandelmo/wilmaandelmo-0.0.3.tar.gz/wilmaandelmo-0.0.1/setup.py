8df42d604d59f5ba0f38b8e7d29f6b03803c5e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:47 +0000

    Add package wilmaandelmo/0.0.1/wilmaandelmo-0.0.3.tar.gz

---
+++ packages/wilmaandelmo/wilmaandelmo-0.0.3.tar.gz/wilmaandelmo-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="wilmaandelmo",
+    version="0.0.1",
+    author="Julian Marx",
+    author_email="jhw.marx@gmail.com",
+    description='''Wilma and Elmo''',
+    url="https://github.com/julianmarx/wilmaandelmo",
+    packages=setuptools.find_packages(),
+    keywords = ['christmas']
+)
