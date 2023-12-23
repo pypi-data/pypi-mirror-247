d4556f4796b278de6d48538d2518b567366545a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:55 +0000

    Add package embuild/0.0.7/embuild-0.0.7-py3-none-any.whl

---
+++ packages/embuild/embuild-0.0.7-py3-none-any.whl/embuild/helpers/check_environment.py
@@ -0,0 +1,16 @@
+from helpers.run_process import run_process
+from vt100logging import I
+
+
+def check_for(cmd: str):
+    try:
+        run_process(f'{cmd} --version')
+    except Exception as e:
+        raise Exception(f"{cmd} is not installed: {e}")
+
+
+def check_environment():
+    I("Checking environment")
+    check_for('git')
+    check_for('cmake')
+    check_for('ninja')
