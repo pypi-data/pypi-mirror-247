562c554d1e37abd8b059e8d75a9cccb1a21b32cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:07:45 +0000

    Add package spheroidal/0.1.1/spheroidal-0.1.1.tar.gz

---
+++ packages/spheroidal/spheroidal-0.1.1.tar.gz/spheroidal-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="spheroidal",
+    version="0.1.1",
+    author="Seyong Park",
+    description="Library for computing spin-weighted spheroidal harmonics",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/syp2001/spheroidal",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+    ),
+    install_requires=["scipy","numpy","numba"]
+)
