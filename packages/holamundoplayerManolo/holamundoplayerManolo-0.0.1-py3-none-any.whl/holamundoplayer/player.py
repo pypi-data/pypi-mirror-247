8eb5269e3640fbddbd60d35930051466abc224f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:29 +0000

    Add package holamundoplayerManolo/0.0.1/holamundoplayerManolo-0.0.1-py3-none-any.whl

---
+++ packages/holamundoplayerManolo/holamundoplayerManolo-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,29 @@
+"""
+esta es el modulo que incluye la clase
+de reprodctor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crear un reproductor
+    de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio como parametro
+
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito , en caso de fracaso devuelve 0
+        """
+        print("Reproduciendo cancion")
+
+    def stop(self):
+        """
+        Reproduc
+        """
+        print("Stoping")
