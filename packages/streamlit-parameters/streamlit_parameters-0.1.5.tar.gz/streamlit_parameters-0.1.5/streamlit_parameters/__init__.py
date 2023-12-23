df44295c379408e65ed55be8c79b6b99a15ce5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:06:14 +0000

    Add package streamlit-parameters/0.1.5/streamlit_parameters-0.1.5.tar.gz

---
+++ packages/streamlit-parameters/streamlit_parameters-0.1.5.tar.gz/streamlit_parameters-0.1.5/streamlit_parameters/__init__.py
@@ -0,0 +1,29 @@
+#
+# License: BSD
+#   https://raw.githubusercontent.com/stonier/streamlit_parameters/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""Parameter management for intialising and exporting the page configuration."""
+
+##############################################################################
+# Imports
+##############################################################################
+
+from . import demos  # noqa
+from . import hello  # noqa
+from . import parameters  # noqa
+
+##############################################################################
+# Version
+##############################################################################
+
+# Update in setup.py as well
+__version__ = "0.1.5"
+
+# import pkg_resources
+
+# Streamlit Cloud doesn't handle this one.
+# __version__ = pkg_resources.require('streamlit_parameters')[0].version
