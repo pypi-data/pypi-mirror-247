ccefba1e9e815f4b2740f0049067f45bf1736d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:05:24 +0000

    Add package bunkoer/0.0.6.dev0/bunkoer-0.0.6.dev0.tar.gz

---
+++ packages/bunkoer/bunkoer-0.0.6.dev0.tar.gz/bunkoer-0.0.6.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bunkoer',
+    version='0.0.6-dev',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'bunkoer=bunkoer.main:run_streamlit',
+        ],
+    },
+    install_requires=[
+        # liste des dépendances
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # Assurez-vous que cette ligne correspond à votre licence
+        'Programming Language :: Python :: 3',  # Assurez-vous que cette ligne correspond à votre version de Python
+    ],
+)
+
