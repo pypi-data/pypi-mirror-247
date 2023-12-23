eeb9b694c3daa33d11da4b243a36c803764ac702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:07:50 +0000

    Add package jawad/1.0.0/jawad-1.0.0-py3-none-any.whl

---
+++ packages/jawad/jawad-1.0.0-py3-none-any.whl/jawad/calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def min(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if b == 0:
+        Exception("U can't te9sem 3la 0")
+
+    return a / b
