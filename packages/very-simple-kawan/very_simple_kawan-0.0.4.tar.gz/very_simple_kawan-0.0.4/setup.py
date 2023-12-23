56e7bf8b131302683fc75c9da86b1c94b178d844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:05:04 +0000

    Add package very-simple-kawan/0.0.4/very_simple_kawan-0.0.4.tar.gz

---
+++ packages/very-simple-kawan/very_simple_kawan-0.0.4.tar.gz/very_simple_kawan-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.4"  # Atualize para a nova versão
+DESCRIPTION = "Pacote de teste python"
+LONG_DESCRIPTION = ("Pacote de teste para o curso de Python"
+                    "para iniciantes"
+                    "Funcoes:"
+                    "soma"
+                    "subtracao"
+                    "multiplicacao"
+                    "divisao")
+
+# Configuração do setup
+setup(
+    # O nome deve corresponder ao nome da pasta
+    name="very_simple_kawan",
+    version=VERSION,
+    author="Kawan S. Dias",
+    author_email="kawanevilgenius321@gmai.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["numpy"],  # Adiciona bibliotecas adicionais
+    keywords=['python', 'primeiro pacote python'],
+    classifiers=["Development Status :: 3 - Alpha", ]
+)
