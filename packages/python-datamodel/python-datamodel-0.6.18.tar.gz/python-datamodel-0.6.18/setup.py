adfafa7fc80ad5aab4e066a0d17ff5ad37d7108c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 04:05:53 +0000

    Add package python-datamodel/0.6.18/python-datamodel-0.6.18.tar.gz

---
+++ packages/python-datamodel/python-datamodel-0.6.18.tar.gz/python-datamodel-0.6.18/setup.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+"""DataModels.
+
+    Dataclass Reimplementation with true inheritance (without decorators.)
+See:
+https://github.com/phenobarbital/DataModel
+"""
+import ast
+from os import path
+
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+
+def get_path(filename):
+    return path.join(path.dirname(path.abspath(__file__)), filename)
+
+
+def readme():
+    with open(get_path('README.md'), encoding='utf-8') as rd:
+        return rd.read()
+
+
+version = get_path('datamodel/version.py')
+with open(version, 'r', encoding='utf-8') as meta:
+    t = compile(meta.read(), version, 'exec', ast.PyCF_ONLY_AST)
+    for node in (n for n in t.body if isinstance(n, ast.Assign)):
+        if len(node.targets) == 1:
+            name = node.targets[0]
+            if isinstance(name, ast.Name) and \
+                    name.id in (
+                        '__version__',
+                        '__title__',
+                        '__description__',
+                        '__author__',
+                        '__license__', '__author_email__'):
+                v = node.value
+                if name.id == '__version__':
+                    __version__ = v.s
+                if name.id == '__title__':
+                    __title__ = v.s
+                if name.id == '__description__':
+                    __description__ = v.s
+                if name.id == '__license__':
+                    __license__ = v.s
+                if name.id == '__author__':
+                    __author__ = v.s
+                if name.id == '__author_email__':
+                    __author_email__ = v.s
+
+
+COMPILE_ARGS = ["-O2"]
+
+extensions = [
+    Extension(
+        name='datamodel.fields',
+        sources=['datamodel/fields.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c++"
+    ),
+    Extension(
+        name='datamodel.converters',
+        sources=['datamodel/converters.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c"
+    ),
+    Extension(
+        name='datamodel.validation',
+        sources=['datamodel/validation.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c++"
+    ),
+    Extension(
+        name='datamodel.exceptions',
+        sources=['datamodel/exceptions.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c"
+    ),
+    Extension(
+        name='datamodel.types',
+        sources=['datamodel/types.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c"
+    ),
+    Extension(
+        name='datamodel.parsers.json',
+        sources=['datamodel/parsers/json.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c++"
+    ),
+    Extension(
+        name='datamodel.libs.mapping',
+        sources=['datamodel/libs/mapping.pyx'],
+        extra_compile_args=COMPILE_ARGS,
+        language="c"
+    )
+]
+
+setup(
+    name="python-datamodel",
+    version=__version__,
+    python_requires=">=3.9.14",
+    url="https://github.com/phenobarbital/python-datamodel",
+    description=__description__,
+    keywords=['asyncio', 'dataclass', 'dataclasses', 'data models'],
+    platforms=['any'],
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: AsyncIO",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+    ],
+    author=__author__,
+    author_email=__author_email__,
+    packages=find_packages(exclude=["contrib", "docs", "tests", "examples"]),
+    include_package_data=True,
+    license=__license__,
+    setup_requires=[
+        "setuptools==67.6.1",
+        "Cython==3.0.6",
+        "wheel==0.42.0"
+    ],
+    install_requires=[
+        "numpy==1.24.2",
+        "uvloop==0.19.0",
+        "asyncio==3.4.3",
+        "faust-cchardet==2.1.19",
+        "ciso8601==2.3.1",
+        "objectpath==0.6.1",
+        "orjson==3.9.10",
+        'typing_extensions==4.8.0',
+        "asyncpg==0.29.0",
+        "python-dateutil==2.8.2",
+        "pendulum==2.1.2",
+        "python-slugify==8.0.1"
+    ],
+    tests_require=[
+        'pytest>=7.2.2',
+        'pytest-asyncio==0.23.2',
+        'pytest-xdist==3.5.0',
+        'pytest-assume==2.4.3'
+    ],
+    test_suite='tests',
+    ext_modules=cythonize(extensions),
+    project_urls={  # Optional
+        "Source": "https://github.com/phenobarbital/datamodels",
+        "Funding": "https://paypal.me/phenobarbital",
+        "Say Thanks!": "https://saythanks.io/to/phenobarbital",
+    },
+)
