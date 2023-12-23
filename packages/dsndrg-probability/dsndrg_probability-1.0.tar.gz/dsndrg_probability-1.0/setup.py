ffa32bcac9d329eddefb0d51c2774fd2d3836c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:07:41 +0000

    Add package dsndrg-probability/1.0/dsndrg_probability-1.0.tar.gz

---
+++ packages/dsndrg-probability/dsndrg_probability-1.0.tar.gz/dsndrg_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsndrg_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsndrg_probability'],
+      author='Rafael Gonzaga',
+      author_email = 'ralozaga@gmail.com',
+      zip_safe=False)
