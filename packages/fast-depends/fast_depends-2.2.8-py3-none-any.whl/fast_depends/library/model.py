381967866c4b0fba245e80ce68153d3854b638af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 05:08:33 +0000

    Add package fast-depends/2.2.8/fast_depends-2.2.8-py3-none-any.whl

---
+++ packages/fast-depends/fast_depends-2.2.8-py3-none-any.whl/fast_depends/library/model.py
@@ -0,0 +1,28 @@
+from abc import ABC
+from typing import Any, Dict, Optional, TypeVar
+
+Cls = TypeVar("Cls", bound="CustomField")
+
+
+class CustomField(ABC):
+    param_name: Optional[str]
+    cast: bool
+    required: bool
+
+    def __init__(
+        self,
+        *,
+        cast: bool = True,
+        required: bool = True,
+    ) -> None:
+        self.cast = cast
+        self.param_name = None
+        self.required = required
+
+    def set_param_name(self: Cls, name: str) -> Cls:
+        self.param_name = name
+        return self
+
+    def use(self, /, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
+        assert self.param_name, "You should specify `param_name` before using"
+        return kwargs
