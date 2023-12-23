ccee0b25f968d27e2a5c3cbce2796c15a4a0383a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 15:04:56 +0000

    Add package jaraco.develop/8.6.0/jaraco.develop-8.6.0.tar.gz

---
+++ packages/jaraco.develop/jaraco.develop-8.6.0.tar.gz/jaraco.develop-8.6.0/conftest.py
@@ -0,0 +1,40 @@
+import os
+import textwrap
+
+import pytest
+
+
+def pytest_configure():
+    os.environ['GITHUB_TOKEN'] = 'abc'
+
+
+@pytest.fixture
+def git_url_substitutions(fake_process):
+    cmd = ['git', 'config', '--get-regexp', r'url\..*\.insteadof']
+    stdout = textwrap.dedent(
+        """
+        url.https://github.com/.insteadof gh://
+        url.https://gist.github.com/.insteadof gist://
+        """.lstrip()
+    )
+
+    fake_process.register(cmd, stdout=stdout)
+
+
+@pytest.fixture(autouse=True)
+def published_projects(monkeypatch):
+    monkeypatch.setenv(
+        'PROJECTS_LIST_URL',
+        'https://raw.githubusercontent.com/jaraco/dotfiles/main/projects.txt',
+    )
+
+
+@pytest.fixture(autouse=True)
+def workaround_pypy_4021(monkeypatch):
+    import importlib
+    import platform
+
+    if platform.python_implementation() != 'PyPy':
+        return
+    ssl = importlib.import_module('_cffi_ssl._stdssl')
+    monkeypatch.setattr(ssl, 'print', lambda *args: None, raising=False)
