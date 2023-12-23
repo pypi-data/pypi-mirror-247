8d773e98c46770d675aed4d5a9c91c6187ef56cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 03:04:25 +0000

    Add package manisble/0.1.11/manisble-0.1.11-py3-none-any.whl

---
+++ packages/manisble/manisble-0.1.11-py3-none-any.whl/manisble/ssh/__init__.py
@@ -0,0 +1,20 @@
+from . import ssh
+import argparse
+from ..common import prettyllog
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Keep manisble and automate ssh", usage="manisble_ssh <action> \n\n \
+               \
+               version : 0.0.1 manisble_ssh  \n\
+               actions:                  \n\
+               signssh                   \n\
+               \
+               2023 Knowit Miracle\
+               ")
+    parser.add_argument('action', metavar='<action>', type=str, nargs='+', help='setup jenkis')
+    args = parser.parse_args()
+    ready = False
+
+    if args.action[0] == "signssh":
+        ssh.signssh()
